dce0847cdc2b022896036f7f7da9b19cb30b20a0
Fang2hou <houshuu@live.jp>
2018-04-25 20:07:24 +0900

    修复任务列表错误

--- ElvUI_WindTools/Modules/Quest/QuestListEnhanced.lua
+++ ElvUI_WindTools/Modules/Quest/QuestListEnhanced.lua
@@ -18,84 +18,12 @@ P["WindTools"]["Quest List Enhanced"] = {
 	["titlelevel"] = true,
 	["detaillevel"] = true,
 	["titlefontsize"] = 13,
-	["height"] = 450,
+	["ignorehightlevel"] = true,
 	["width"] = 240,
 }
-if not E.db.WindTools["Quest List Enhanced"]["enabled"] then return end
-
-local vmheight = E.db.WindTools["Quest List Enhanced"]["height"]
-local vmwidth = E.db.WindTools["Quest List Enhanced"]["width"]
-local vm = ObjectiveTrackerFrame
-
--- 任务追踪名称职业着色 -------------------------------------------------------
-local r, g, b = 103/255, 103/255, 103/255
-local class = select(2, UnitClass("player"))
-local colour = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
-hooksecurefunc(QUEST_TRACKER_MODULE, "SetBlockHeader", function(_, block)
-block.HeaderText:SetFont(STANDARD_TEXT_FONT, E.db.WindTools["Quest List Enhanced"]["titlefontsize"], "OUTLINE")
-block.HeaderText:SetShadowOffset(.7, -.7)
-block.HeaderText:SetShadowColor(0, 0, 0, 1)
-if E.db.WindTools["Quest List Enhanced"]["titlecolor"] then
-	block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
-end
-block.HeaderText:SetJustifyH("Left")
-block.HeaderText:SetWidth(vmwidth)
-local heightcheck = block.HeaderText:GetNumLines()      
-	if heightcheck == 2 then
-		local height = block:GetHeight()     
-		block:SetHeight(height + 2)
-	end
-end)
-
-local function hoverquest(_, block)
-	if not E.db.WindTools["Quest List Enhanced"]["titlecolor"] then return end
-    block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
-end
-
-hooksecurefunc(QUEST_TRACKER_MODULE, "OnBlockHeaderEnter", hoverquest)  
-hooksecurefunc(QUEST_TRACKER_MODULE, "OnBlockHeaderLeave", hoverquest)
-hooksecurefunc(ACHIEVEMENT_TRACKER_MODULE, "SetBlockHeader", function(_, block)
-	if not E.db.WindTools["Quest List Enhanced"]["enabled"] then return end
-    local trackedAchievements = {GetTrackedAchievements()}
-    
-    for i = 1, #trackedAchievements do
-	    local achieveID = trackedAchievements[i]
-	    local _, achievementName, _, completed, _, _, _, description, _, icon, _, _, wasEarnedByMe = GetAchievementInfo(achieveID)
-        local showAchievement = true
-        
-	    if wasEarnedByMe then
-		    showAchievement = false
-	    elseif displayOnlyArena then
-		    if GetAchievementCategory(achieveID)~=ARENA_CATEGORY then
-			    showAchievement = false
-		    end
-	    end
-	    
-        if showAchievement then
-            block.HeaderText:SetFont(STANDARD_TEXT_FONT, 13,"OUTLINE")
-            block.HeaderText:SetShadowOffset(.7, -.7)
-            block.HeaderText:SetShadowColor(0, 0, 0, 1)
-            if E.db.WindTools["Quest List Enhanced"]["titlecolor"] then
-				block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
-			end
-            block.HeaderText:SetJustifyH("Left")
-            block.HeaderText:SetWidth(vmwidth)
-        end
-    end
-end)
-  
-local function hoverachieve(_, block)
-	if not (E.db.WindTools["Quest List Enhanced"]["enabled"] or E.db.WindTools["Quest List Enhanced"]["titlecolor"]) then return end
-	block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
-end
-  
-hooksecurefunc(ACHIEVEMENT_TRACKER_MODULE, "OnBlockHeaderEnter", hoverachieve)
-hooksecurefunc(ACHIEVEMENT_TRACKER_MODULE, "OnBlockHeaderLeave", hoverachieve)
-
 
 -- 追踪栏显示任务等级
-local QuestLevelPatch = {}
-function SetBlockHeader_hook()
+local function SetBlockHeader_hook()
 	if not E.db.WindTools["Quest List Enhanced"]["enabled"] then return end
 	if not E.db.WindTools["Quest List Enhanced"]["titlelevel"] then return end
 	for i = 1, GetNumQuestWatches() do
@@ -109,7 +37,7 @@ function SetBlockHeader_hook()
 			local questLevel = select(2, GetQuestLogTitle(questLogIndex))
 			local newTitle = title
 			-- 除去110等级的任务等级提示
-			if questLevel ~= 110 then
+			if questLevel ~= 110 or not E.db.WindTools["Quest List Enhanced"]["ignorehightlevel"] then
 				newTitle = "["..questLevel.."] "..title
 			end
 				-- 燃烧王座任务名缩短
@@ -119,10 +47,9 @@ function SetBlockHeader_hook()
 		end
 	end
 end
-hooksecurefunc(QUEST_TRACKER_MODULE, "Update", SetBlockHeader_hook)
 
 -- 任务详细信息显示任务等级
-function QuestInfo_hook(template, parentFrame, acceptButton, material, mapView)
+local function QuestInfo_hook(template, parentFrame, acceptButton, material, mapView)
 	if not E.db.WindTools["Quest List Enhanced"]["enabled"] then return end
 	if not E.db.WindTools["Quest List Enhanced"]["detaillevel"] then return end
 	local elementsTable = template.elements
@@ -137,7 +64,79 @@ function QuestInfo_hook(template, parentFrame, acceptButton, material, mapView)
 		end
 	end
 end
-hooksecurefunc("QuestInfo_Display", QuestInfo_hook)
+
+function QuestListEnhanced:Initialize()
+	if not E.db.WindTools["Quest List Enhanced"]["enabled"] then return end
+
+	local trackerWidth = E.db.WindTools["Quest List Enhanced"]["width"]
+	local vm = ObjectiveTrackerFrame
+
+	local r, g, b = 103/255, 103/255, 103/255
+	local class = select(2, UnitClass("player"))
+	local colour = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
+	hooksecurefunc(QUEST_TRACKER_MODULE, "SetBlockHeader", function(_, block)
+		block.HeaderText:SetFont(STANDARD_TEXT_FONT, E.db.WindTools["Quest List Enhanced"]["titlefontsize"], "OUTLINE")
+		block.HeaderText:SetShadowOffset(.7, -.7)
+		block.HeaderText:SetShadowColor(0, 0, 0, 1)
+		if E.db.WindTools["Quest List Enhanced"]["titlecolor"] then
+			block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
+		end
+		block.HeaderText:SetJustifyH("Left")
+		block.HeaderText:SetWidth(trackerWidth)
+		local heightcheck = block.HeaderText:GetNumLines()      
+		if heightcheck == 2 then
+			local height = block:GetHeight()   
+			block:SetHeight(height + 2)
+		end
+	end)
+
+	local function hoverquest(_, block)
+	    block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
+	end
+	  
+	local function hoverachieve( _, block)
+		block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
+	end
+
+	hooksecurefunc(ACHIEVEMENT_TRACKER_MODULE, "SetBlockHeader", function(_, block)
+		if not E.db.WindTools["Quest List Enhanced"]["enabled"] then return end
+	    local trackedAchievements = {GetTrackedAchievements()}
+	    
+	    for i = 1, #trackedAchievements do
+		    local achieveID = trackedAchievements[i]
+		    local _, achievementName, _, completed, _, _, _, description, _, icon, _, _, wasEarnedByMe = GetAchievementInfo(achieveID)
+	        local showAchievement = true
+	        
+		    if wasEarnedByMe then
+			    showAchievement = false
+		    elseif displayOnlyArena then
+			    if GetAchievementCategory(achieveID)~=ARENA_CATEGORY then
+				    showAchievement = false
+			    end
+		    end
+		    
+	        if showAchievement then
+	            block.HeaderText:SetFont(STANDARD_TEXT_FONT, 13,"OUTLINE")
+	            block.HeaderText:SetShadowOffset(.7, -.7)
+	            block.HeaderText:SetShadowColor(0, 0, 0, 1)
+	            if E.db.WindTools["Quest List Enhanced"]["titlecolor"] then
+					block.HeaderText:SetTextColor(colour.r, colour.g, colour.b)
+				end
+	            block.HeaderText:SetJustifyH("Left")
+	            block.HeaderText:SetWidth(trackerWidth)
+	        end
+	    end
+	end)
+
+	if E.db.WindTools["Quest List Enhanced"]["titlecolor"] then
+		hooksecurefunc(QUEST_TRACKER_MODULE, "OnBlockHeaderEnter", hoverquest)  
+		hooksecurefunc(QUEST_TRACKER_MODULE, "OnBlockHeaderLeave", hoverquest)
+		hooksecurefunc(ACHIEVEMENT_TRACKER_MODULE, "OnBlockHeaderEnter", hoverachieve)
+		hooksecurefunc(ACHIEVEMENT_TRACKER_MODULE, "OnBlockHeaderLeave", hoverachieve)
+	end
+	hooksecurefunc(QUEST_TRACKER_MODULE, "Update", SetBlockHeader_hook)
+	hooksecurefunc("QuestInfo_Display", QuestInfo_hook)
+end
 
 local function InsertOptions()
 	E.Options.args.WindTools.args["Quest"].args["Quest List Enhanced"].args["additionalconfig"] = {
@@ -166,7 +165,7 @@ local function InsertOptions()
 				name = L["Tracker Level"],
 				desc = L["Display level info in quest title (Tracker)"],
 				get = function(info) return E.db.WindTools["Quest List Enhanced"]["titlelevel"] end,
-				set = function(info, value) E.db.WindTools["Quest List Enhanced"]["titlelevel"] = value;end
+				set = function(info, value) E.db.WindTools["Quest List Enhanced"]["titlelevel"] = value; E:StaticPopup_Show("PRIVATE_RL")end
 			},
 			detaillevel = {
 				order = 4,
@@ -176,16 +175,15 @@ local function InsertOptions()
 				get = function(info) return E.db.WindTools["Quest List Enhanced"]["detaillevel"] end,
 				set = function(info, value) E.db.WindTools["Quest List Enhanced"]["detaillevel"] = value;end
 			},
-			frameheight = {
-				order = 5,
-				type = 'range',
-				name = L["Tracker height"],
-				min = 350, max = 550, step = 1,
-				get = function(info) return E.db.WindTools["Quest List Enhanced"]["height"] end,
-				set = function(info, value) E.db.WindTools["Quest List Enhanced"]["height"] = value;end
+			ignorehighlevel = {
+				order = 4,
+				type = "toggle",
+				name = L["Ignore high level"],
+				get = function(info) return E.db.WindTools["Quest List Enhanced"]["ignorehightlevel"] end,
+				set = function(info, value) E.db.WindTools["Quest List Enhanced"]["ignorehightlevel"] = value; E:StaticPopup_Show("PRIVATE_RL")end
 			},
 			framewidth = {
-				order = 6,
+				order = 5,
 				type = 'range',
 				name = L["Tracker width"],
 				min = 200, max = 300, step = 1,

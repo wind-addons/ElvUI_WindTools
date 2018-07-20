b3755a6aede3f7832774d2f0fdeac7c62e4fa3a2
SomeBlu <someblu@users.noreply.github.com>
2018-07-20 11:46:35 -0700

    fix autobutton (#7)
    squash and merge to master

--- ElvUI_WindTools/Modules/Interface/AutoButton.lua
+++ ElvUI_WindTools/Modules/Interface/AutoButton.lua
@@ -50,7 +50,7 @@ end
 
 --PrivateDB
 P["WindTools"]["Auto Buttons"] = {
-	["enable"] = true,
+	["enabled"] = true,
 	["questNum"] = 6,
 	["itemList"] = {},
 	["slotNum"] = 5,
@@ -612,7 +612,7 @@ function AB:UpdateBind()
 end
 
 function AB:ToggleAutoButton()
-	if E.db.WindTools["Auto Buttons"].enable then
+	if E.db.WindTools["Auto Buttons"].enabled then
 		self:RegisterEvent("BAG_UPDATE_DELAYED", "ScanItem")
 		self:RegisterEvent("UNIT_INVENTORY_CHANGED", "ScanItem")
 		self:RegisterEvent("ZONE_CHANGED", "ScanItem")
@@ -906,7 +906,7 @@ local function InsertOptions()
 end
 
 function AB:Initialize()
-	if not E.db.WindTools["Auto Buttons"].enable then return end
+	if not E.db.WindTools["Auto Buttons"].enabled then return end
 	
 	local db = E.db.WindTools["Auto Buttons"]
 	
@@ -921,14 +921,14 @@ function AB:Initialize()
 	AutoButtonAnchor:SetClampedToScreen(true)
 	AutoButtonAnchor:Point("BOTTOMLEFT", RightChatPanel or LeftChatPanel, "TOPLEFT", 0, 4)
 	AutoButtonAnchor:Size(db.questNum > 0 and db.questSize * db.questNum or 260, db.questNum > 0 and db.questSize or 40)
-	E:CreateMover(AutoButtonAnchor, "AutoButtonAnchorMover", L["Auto QuestItem Button"], nil, nil, nil, "ALL,EUI", function() return E.db.WindTools["Auto Buttons"].enable; end)
+	E:CreateMover(AutoButtonAnchor, "AutoButtonAnchorMover", L["Auto QuestItem Button"], nil, nil, nil, "ALL,EUI", function() return E.db.WindTools["Auto Buttons"].enabled; end)
 	
 	-- Create anchor2
 	local AutoButtonAnchor2 = CreateFrame("Frame", "AutoButtonAnchor2", UIParent)
 	AutoButtonAnchor2:SetClampedToScreen(true)
 	AutoButtonAnchor2:Point("BOTTOMLEFT", RightChatPanel or LeftChatPanel, "TOPLEFT", 0, 48)
 	AutoButtonAnchor2:Size(db.slotNum > 0 and db.slotSize * db.slotNum or 260, db.slotNum > 0 and db.slotSize or 40)
-	E:CreateMover(AutoButtonAnchor2, "AutoButtonAnchor2Mover", L["Auto InventoryItem Button"], nil, nil, nil, "ALL,EUI", function() return E.db.WindTools["Auto Buttons"].enable; end)
+	E:CreateMover(AutoButtonAnchor2, "AutoButtonAnchor2Mover", L["Auto InventoryItem Button"], nil, nil, nil, "ALL,EUI", function() return E.db.WindTools["Auto Buttons"].enabled; end)
 	
 	self:UpdateAutoButton()
 end

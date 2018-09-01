732f23d4748b7e4541069254624e695d40f8e407
Zhou Fang <houshuu@live.jp>
2018-09-01 18:51:30 +0900

    更新怪物进度库

--- Libs/LibObjectiveProgress-1.0/LibObjectiveProgress-1.0.lua
+++ Libs/LibObjectiveProgress-1.0/LibObjectiveProgress-1.0.lua
@@ -4,7 +4,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 ]]--
 
 -- Set major/minor version
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 29
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", 31
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library

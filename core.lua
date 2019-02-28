fdf9339b5c66a6adae6ab1fe46aa5081e55958c9
Zhou Fang <fang2hou@outlook.com>
2019-02-28 19:49:17 +0900

    更新库文件和说明

--- core.lua
+++ core.lua
@@ -138,7 +138,7 @@ function WT:InsertOptions()
 			author = {
 				order = 9,
 				type  = "description",
-				name  = "|cffC79C6Ehoushuu @ NGA|r (|cff00FF96五氣歸元|r @ TW-暗影之月)\nSomeBlu @ Github"
+				name  = "|cffC79C6Ehoushuu @ NGA|r (|cff00FF96Weakaura|r @ TW-暗影之月)\nSomeBlu @ Github"
 			},
 			credit = {
 				order = -1,

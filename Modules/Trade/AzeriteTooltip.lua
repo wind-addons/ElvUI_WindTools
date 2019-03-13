37119cdbca1653738ab185eff34aa05ef73ee1a6
Zhou Fang <fang2hou@outlook.com>
2019-03-14 04:26:56 +0900

    8.1.5 预更新 未测试

--- Modules/Trade/AzeriteTooltip.lua
+++ Modules/Trade/AzeriteTooltip.lua
@@ -494,7 +494,7 @@ function AT:Initialize()
     self:SecureHookScript(GameTooltip, 'OnTooltipSetItem', 'OnTooltipSetItem')
     self:SecureHookScript(ItemRefTooltip, 'OnTooltipSetItem', 'OnTooltipSetItem')
     self:SecureHookScript(ShoppingTooltip1, 'OnTooltipSetItem', 'OnTooltipSetItem')
-    self:SecureHookScript(WorldMapTooltip, 'OnTooltipSetItem', 'OnTooltipSetItem')
+    -- self:SecureHookScript(WorldMapTooltip, 'OnTooltipSetItem', 'OnTooltipSetItem')
     self:SecureHookScript(WorldMapCompareTooltip1, 'OnTooltipSetItem', 'OnTooltipSetItem')
     self:SecureHookScript(EmbeddedItemTooltip, 'OnTooltipSetItem', 'OnTooltipSetItem')
 end

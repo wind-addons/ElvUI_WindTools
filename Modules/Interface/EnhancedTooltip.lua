e5b9daedba6d26a017ce091e83a8bc11c7c86ee6
Zhou Fang <fang2hou@outlook.com>
2019-04-09 06:35:53 +0900

    移植 NDui 的鼠标提示图标功能

--- Modules/Interface/EnhancedTooltip.lua
+++ Modules/Interface/EnhancedTooltip.lua
@@ -118,6 +118,77 @@ local function getLevelColorString(level, short)
 	end
 end
 
+function ETT:ItemIcons()
+	-- 来自 Ndui
+	local newString = "0:0:64:64:5:59:5:59"
+
+	local function setTooltipIcon(self, icon)
+		local title = icon and _G[self:GetName().."TextLeft1"]
+		if title then
+			title:SetFormattedText("|T%s:20:20:"..newString..":%d|t %s", icon, 16, title:GetText())
+		end
+	
+		for i = 2, self:NumLines() do
+			local line = _G[self:GetName().."TextLeft"..i]
+			if not line then break end
+			local text = line:GetText() or ""
+			if strfind(text, "|T.+|t") then
+				line:SetText(gsub(text, ":(%d+)|t", ":20:20:"..newString.."|t"))
+			end
+		end
+	end
+	
+	local function newTooltipHooker(method, func)
+		return function(tooltip)
+			local modified = false
+			tooltip:HookScript("OnTooltipCleared", function()
+				modified = false
+			end)
+			tooltip:HookScript(method, function(self, ...)
+				if not modified then
+					modified = true
+					func(self, ...)
+				end
+			end)
+		end
+	end
+	
+	local hookItem = newTooltipHooker("OnTooltipSetItem", function(self)
+		local _, link = self:GetItem()
+		if link then
+			setTooltipIcon(self, GetItemIcon(link))
+		end
+	end)
+	
+	local hookSpell = newTooltipHooker("OnTooltipSetSpell", function(self)
+		local _, id = self:GetSpell()
+		if id then
+			setTooltipIcon(self, GetSpellTexture(id))
+		end
+	end)
+	
+	for _, tooltip in pairs{GameTooltip, ItemRefTooltip} do
+		hookItem(tooltip)
+		hookSpell(tooltip)
+	end
+	
+	-- Tooltip rewards icon
+	_G.BONUS_OBJECTIVE_REWARD_WITH_COUNT_FORMAT = "|T%1$s:16:16:"..newString.."|t |cffffffff%2$s|r %3$s"
+	_G.BONUS_OBJECTIVE_REWARD_FORMAT = "|T%1$s:16:16:"..newString.."|t %2$s"
+	
+	local function ReskinRewardIcon(self)
+		if self and self.Icon then
+			self.Icon:SetTexCoord(unpack(E.TexCoords))
+			self.IconBorder:Hide()
+		end
+	end
+
+	hooksecurefunc("EmbeddedItemTooltip_SetItemByQuestReward", ReskinRewardIcon)
+	hooksecurefunc("EmbeddedItemTooltip_SetItemByID", ReskinRewardIcon)
+	hooksecurefunc("EmbeddedItemTooltip_SetCurrencyByID", ReskinRewardIcon)
+	hooksecurefunc("QuestUtils_AddQuestCurrencyRewardsToTooltip", function(_, _, self) ReskinRewardIcon(self) end)
+end
+
 function ETT:UpdateProgression(guid, faction)
 	local statFunc = guid == playerGUID and GetStatistic or GetComparisonStatistic
 
@@ -290,6 +361,7 @@ function ETT:Initialize()
 	if not self.db.enabled then return end
 	-- 鼠标提示副本进度
 	hooksecurefunc(TT, 'AddInspectInfo', ETT.AddInspectInfo)
+	ETT:ItemIcons()
 end
 
 local function InitializeCallback()

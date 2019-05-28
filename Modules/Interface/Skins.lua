21f896d52632f0b715162b85d1be14219d1b8977
Zhou Fang <fang2hou@outlook.com>
2019-05-28 16:18:25 +0900

    添加開關音效音樂的文字美化

--- Modules/Interface/Skins.lua
+++ Modules/Interface/Skins.lua
@@ -652,6 +652,7 @@ function WS:CustomSkins()
 	if _G.UIErrorsFrame and self.db.ui_errors.enabled then
 		local db = self.db.ui_errors
 		_G.UIErrorsFrame:FontTemplate(LSM:Fetch('font', db.font), db.size, "OUTLINE")
+		_G.ActionStatusText:FontTemplate(LSM:Fetch('font', db.font), db.size, "OUTLINE")
 	end
 end
 

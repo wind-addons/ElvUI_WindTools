58a086fc9122892ea9cc785dd4985a414b5318e4
Zhou Fang <fang2hou@outlook.com>
2020-04-09 02:14:57 +0900

    修复有时候阴影错位的问题

--- Modules/Interface/Skins.lua
+++ Modules/Interface/Skins.lua
@@ -634,7 +634,11 @@ function WS:ShadowGeneralFrames()
 
 	for frame, noBackdrop in pairs(self.blizzard_frames) do
 		if noBackdrop then
-			if _G[frame] then _G[frame]:CreateShadow() end
+			if frame.Border then
+				if _G[frame] and _G[frame].Border then _G[frame].backdrop:CreateShadow() end
+			else
+				if _G[frame] then _G[frame]:CreateShadow() end
+			end
 		else
 			if _G[frame] and _G[frame].backdrop then _G[frame].backdrop:CreateShadow() end
 		end

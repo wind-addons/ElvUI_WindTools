254fe6e4eff55e1ae18c0b8a98a332170103b62d
Zhou Fang <fang2hou@outlook.com>
2020-05-03 03:14:55 +0900

    设定为大小写敏感

---
+++ Modules/Skins/ElvUI/DataBars.lua
@@ -0,0 +1,18 @@
+local W, F, E, L = unpack(select(2, ...))
+local S = W:GetModule('Skins')
+local B = E:GetModule('DataBars')
+
+local _G = _G
+local hooksecurefunc = hooksecurefunc
+
+function S:ElvUI_DataBars()
+    if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.databars) then return end
+
+    local bars = {_G.ElvUI_AzeriteBar, _G.ElvUI_ExperienceBar, _G.ElvUI_ReputationBar, _G.ElvUI_HonorBar}
+    for _, bar in pairs(bars) do if bar then S:CreateShadow(bar) end end
+
+    -- 后续进行配置更新时进行添加
+    hooksecurefunc(B, "CreateBar", function(_, name) if _G[name] then S:CreateShadow(_G[name]) end end)
+end
+
+S:AddCallback('ElvUI_DataBars')

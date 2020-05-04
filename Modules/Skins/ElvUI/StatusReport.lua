0125a6c36aa2b6adcba001ddf20b64c577798bbf
Zhou Fang <fang2hou@outlook.com>
2020-05-04 16:03:07 +0900

    添加ElvUI信息的插件信息框美化

--- Modules/Skins/ElvUI/StatusReport.lua
+++ Modules/Skins/ElvUI/StatusReport.lua
@@ -4,11 +4,17 @@ local S = W:GetModule('Skins')
 local _G = _G
 local hooksecurefunc = hooksecurefunc
 
+local function SkinStatusReport()
+    S:CreateShadow(_G.ElvUIStatusReport)
+    S:CreateShadow(_G.ElvUIStatusPlugins)
+end
+
 function S:ElvUI_StatusReport()
     if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.statusReport) then return end
 
-    if E.StatusFrame then S:CreateShadow(E.StatusFrame) end
-    hooksecurefunc(E, "CreateStatusFrame", function() S:CreateShadow(_G.ElvUIStatusReport) end)
+    if E.StatusFrame then SkinStatusReport() end
+
+    hooksecurefunc(E, "CreateStatusFrame", SkinStatusReport)
 end
 
 S:AddCallback('ElvUI_StatusReport')

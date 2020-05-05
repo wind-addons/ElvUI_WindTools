4d28131e522222ea2ac6f28e249aeb6656d524d7
Zhou Fang <fang2hou@outlook.com>
2020-05-05 16:10:46 +0900

    添加了通知系统的美化

--- Modules/Skins/Blizzard/Alerts.lua
+++ Modules/Skins/Blizzard/Alerts.lua
@@ -3,82 +3,108 @@ local S = W:GetModule('Skins')
 
 local _G = _G
 
-local function SkinOjectiveTrackerHeaders()
-    local frame = _G.ObjectiveTrackerFrame.MODULES
-    if frame then for i = 1, #frame do if frame[i] then F.SetFontOutline(frame[i].Header.Text) end end end
-end
+local function SkinAlert(alert)
+    if not alert or alert.windStyle then return end
 
-local function SkinItemButton(_, block)
-    local item = block.itemButton
-    if item and not item.windStyle then
-        S:CreateShadow(item)
-        item.windStyle = true
-	end
-end
+    S:CreateBackdropShadowAfterElvUISkins(alert)
 
-local function SkinFindGroupButton(block)
-	if block.hasGroupFinderButton and block.groupFinderButton then
-		if block.groupFinderButton and not block.groupFinderButton.windStyle then
-			S:CreateShadow(block.groupFinderButton)
-			block.groupFinderButton.windStyle = true
-		end
-	end
-end
-
-local function SkinProgressBars(_, _, line)
-	local progressBar = line and line.ProgressBar
-	local bar = progressBar and progressBar.Bar
-	if not bar or progressBar.windStyle then return end
-	local icon = bar.Icon
-	local label = bar.Label
-
-    -- 条阴影
-	S:CreateBackdropShadowAfterElvUISkins(bar)
-    
-    -- 稍微移动下图标位置，防止阴影重叠，更加美观！
-    if icon then
-        S:CreateBackdropShadowAfterElvUISkins(progressBar)
-        icon:Point("LEFT", bar, "RIGHT", E.PixelMode and 7 or 11, 0)
+    if alert.Label and alert.Icon then
+        alert.Label:ClearAllPoints()
+        alert.Label:SetPoint("TOPLEFT", alert.Icon, "TOPRIGHT", 5, 2)
     end
 
-    -- 修正字体位置
-    if label then
-        label:ClearAllPoints()
-        label:Point("CENTER", bar, 0, 0)
-        F.SetFontOutline(label)
+    if alert.Icon and alert.Icon.Texture and alert.Icon.Texture.b then
+        alert.Icon.Texture.b:Point("TOPLEFT", alert.Icon.Texture, "TOPLEFT", -1, 1)
+        alert.Icon.Texture.b:Point("BOTTOMRIGHT", alert.Icon.Texture, "BOTTOMRIGHT", 1, -1)
+        alert.Icon.Texture.b:CreateShadow(5)
     end
 
-    progressBar.windStyle = true
+    if alert.SpecIcon then alert.SpecIcon.b:CreateShadow() end
+
+    alert.windStyle = true
 end
 
-local function SkinTimerBars(_, _, line)
-	local timerBar = line and line.TimerBar
-	local bar = timerBar and timerBar.Bar
-    if bar.windStyle then return end
-    S:CreateBackdropShadowAfterElvUISkins(bar)
+local function SkinCriteriaAlert(frame)
+    if not frame or frame.windStyle then return end
+
+    S:CreateBackdropShadowAfterElvUISkins(frame)
+
+    F.SetFontOutline(frame.Unlocked)
+    F.SetFontOutline(frame.Name)
+
+    if frame.Icon.Texture.b then
+        frame.Icon.Texture.b:Point("TOPLEFT", frame.Icon.Texture, "TOPLEFT", -1, 1)
+        frame.Icon.Texture.b:Point("BOTTOMRIGHT", frame.Icon.Texture, "BOTTOMRIGHT", 1, -1)
+        S:CreateShadow(frame.Icon.Texture.b)
+
+        frame.Icon.Texture:ClearAllPoints()
+        frame.Icon.Texture:Point("RIGHT", frame.backdrop, "LEFT", 50, 0)
+    end
+
+    frame.windStyle = true
 end
 
-function S:ObjectiveTrackerFrame()
-    if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.objectiveTracker) then return end
-    if not (E.private.WT.skins.blizzard.enable and E.private.WT.skins.blizzard.objectiveTracker) then return end
+local function SkinMoneyWonAlert(frame)
+    if not frame or frame.windStyle then return end
 
-    local ObjectiveTrackerFrame = _G.ObjectiveTrackerFrame
-    local minimizeButton = ObjectiveTrackerFrame.HeaderMenu.MinimizeButton
+    S:CreateBackdropShadowAfterElvUISkins(frame)
+    F.SetFontOutline(frame.Label)
+    F.SetFontOutline(frame.Amount)
 
-    S:CreateShadow(minimizeButton)
-    minimizeButton.shadow:SetOutside()
+    frame.Label:ClearAllPoints()
+    frame.Label:Point("TOPLEFT", frame.Icon, "TOPRIGHT", 12, -2)
 
-	hooksecurefunc("ObjectiveTracker_Update",SkinOjectiveTrackerHeaders)					--[Skin]: Module Headers
-	hooksecurefunc("QuestObjectiveSetupBlockButton_FindGroup",SkinFindGroupButton)			--[Skin]: The eye
-	hooksecurefunc(_G.BONUS_OBJECTIVE_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)		--[Skin]: Bonus Objective Progress Bar
-	hooksecurefunc(_G.WORLD_QUEST_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)			--[Skin]: World Quest Progress Bar
-	hooksecurefunc(_G.DEFAULT_OBJECTIVE_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)	--[Skin]: Quest Progress Bar
-	hooksecurefunc(_G.SCENARIO_TRACKER_MODULE,"AddProgressBar",SkinProgressBars)			--[Skin]: Scenario Progress Bar
-	hooksecurefunc(_G.QUEST_TRACKER_MODULE,"AddTimerBar",SkinTimerBars)						--[Skin]: Quest Timer Bar
-	hooksecurefunc(_G.SCENARIO_TRACKER_MODULE,"AddTimerBar",SkinTimerBars)					--[Skin]: Scenario Timer Bar
-	hooksecurefunc(_G.ACHIEVEMENT_TRACKER_MODULE,"AddTimerBar",SkinTimerBars)				--[Skin]: Achievement Timer Bar
-	hooksecurefunc(_G.QUEST_TRACKER_MODULE,"SetBlockHeader",SkinItemButton)					--[Skin]: Quest Item Buttons
-	hooksecurefunc(_G.WORLD_QUEST_TRACKER_MODULE,"AddObjective",SkinItemButton)		
+    frame.Amount:ClearAllPoints()
+    frame.Amount:Point("BOTTOMLEFT", frame.Icon, "BOTTOMRIGHT", 12, 2)
+
+    frame.windStyle = true
 end
 
-S:AddCallback('ObjectiveTrackerFrame')
+function S:AlertFrames()
+    if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.alertframes) then return end
+    if not (E.private.WT.skins.blizzard.enable and E.private.WT.skins.blizzard.alerts) then return end
+
+    -- 成就
+    hooksecurefunc(_G.AchievementAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.CriteriaAlertSystem, "setUpFunction", SkinCriteriaAlert)
+
+    -- 遭遇
+    hooksecurefunc(_G.DungeonCompletionAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GuildChallengeAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.InvasionAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.ScenarioAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.WorldQuestCompleteAlertSystem, "setUpFunction", SkinAlert)
+
+    -- 要塞
+    hooksecurefunc(_G.GarrisonFollowerAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GarrisonShipFollowerAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GarrisonTalentAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GarrisonBuildingAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GarrisonMissionAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GarrisonShipMissionAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.GarrisonRandomMissionAlertSystem, "setUpFunction", SkinAlert)
+
+    -- 荣誉
+    hooksecurefunc(_G.HonorAwardedAlertSystem, "setUpFunction", SkinAlert)
+
+    -- 拾取
+    hooksecurefunc(_G.LegendaryItemAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.LootAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.LootUpgradeAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.MoneyWonAlertSystem, "setUpFunction", SkinMoneyWonAlert)
+
+    -- 专业技能
+    hooksecurefunc(_G.DigsiteCompleteAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.NewRecipeLearnedAlertSystem, "setUpFunction", SkinAlert)
+
+    -- 宠物 / 坐骑
+    hooksecurefunc(_G.NewPetAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.NewMountAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.NewToyAlertSystem, "setUpFunction", SkinAlert)
+
+    -- 其它
+    hooksecurefunc(_G.EntitlementDeliveredAlertSystem, "setUpFunction", SkinAlert)
+    hooksecurefunc(_G.RafRewardDeliveredAlertSystem, "setUpFunction", SkinAlert)
+end
+
+S:AddCallback('AlertFrames')

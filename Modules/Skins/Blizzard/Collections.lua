6efe9cd8db75ec5fbed95cefdc3fe29359f695f0
Zhou Fang <fang2hou@outlook.com>
2020-05-06 18:16:19 +0900

    添加收藏，幻化，导览手册美化

---
+++ Modules/Skins/Blizzard/Collections.lua
@@ -0,0 +1,16 @@
+local W, F, E, L = unpack(select(2, ...))
+local S = W:GetModule('Skins')
+
+local _G = _G
+
+function S:Blizzard_Collections()
+    if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.collections) then return end
+    if not (E.private.WT.skins.blizzard.enable and E.private.WT.skins.blizzard.collections) then return end
+
+    S:CreateShadow(_G.CollectionsJournal)
+    S:CreateShadow(_G.WardrobeFrame)
+
+    for i = 1, 5 do S:CreateBackdropShadowAfterElvUISkins(_G["CollectionsJournalTab" .. i]) end
+end
+
+S:AddCallbackForAddon('Blizzard_Collections')

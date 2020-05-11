f7a97f0513148bca7624f8804ef84a1cfe07969f
Zhou Fang <fang2hou@outlook.com>
2020-05-11 18:06:21 +0900

    缓存

--- Modules/Skins/ElvUI/Option.lua
+++ Modules/Skins/ElvUI/Option.lua
@@ -2,6 +2,7 @@ local W, F, E, L = unpack(select(2, ...))
 local S = W:GetModule("Skins")
 
 local _G = _G
+local InCombatLockdown = InCombatLockdown
 
 function S:ElvUI_OptionsUI()
     if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.option) then

8f41caf57d8a5bdcb24233ff3e2bfd0d9cc33eca
Zhou Fang <fang2hou@outlook.com>
2020-05-31 00:36:20 +0900

    调整一下设定分类的排序

--- Options/Core.lua
+++ Options/Core.lua
@@ -2,18 +2,18 @@ local W, F, E, L, V, P, G = unpack(select(2, ...))
 
 -- 设定类别
 W.options = {
-    combat = {
-        order = 101,
-        name = L["Combat"],
-        desc = L["Make combat more interesting."],
-        icon = W.Media.Icons.combat,
-        args = {}
-    },
     item = {
-        order = 102,
+        order = 101,
         name = L["Item"],
         desc = L["Make adventure life easier."],
         icon = W.Media.Icons.item,
+        args = {}
+    },
+    combat = {
+        order = 102,
+        name = L["Combat"],
+        desc = L["Make combat more interesting."],
+        icon = W.Media.Icons.combat,
         args = {}
     },
     maps = {
@@ -23,32 +23,32 @@ W.options = {
         icon = W.Media.Icons.map,
         args = {}
     },
-    misc = {
+    social = {
         order = 104,
-        name = L["Misc"],
-        desc = L["Miscellaneous modules."],
-        icon = W.Media.Icons.misc,
-        args = {}
-    },
-    skins = {
-        order = 105,
-        name = L["Skins"],
-        desc = L["Apply new shadow style for ElvUI."],
-        icon = W.Media.Icons.skins,
+        name = L["Social"],
+        desc = L["Make some enhancements on chat and friend frames."],
+        icon = W.Media.Icons.chat,
         args = {}
     },
     tooltips = {
-        order = 106,
+        order = 105,
         name = L["Tooltips"],
         desc = L["Add some additional information to your tooltips."],
         icon = W.Media.Icons.tooltips,
         args = {}
     },
-    social = {
+    skins = {
+        order = 106,
+        name = L["Skins"],
+        desc = L["Apply new shadow style for ElvUI."],
+        icon = W.Media.Icons.skins,
+        args = {}
+    },
+    misc = {
         order = 107,
-        name = L["Social"],
-        desc = L["Make some enhancements on chat and friend frames."],
-        icon = W.Media.Icons.chat,
+        name = L["Misc"],
+        desc = L["Miscellaneous modules."],
+        icon = W.Media.Icons.misc,
         args = {}
     }
 }

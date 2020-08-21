18d2c1516360a7d92a894ac436e3cce04b94145f
Zhou Fang <fang2hou@outlook.com>
2020-08-21 19:24:13 +0900

    适配9.0的新版理发店UI

--- Modules/Skins/Blizzard/Barbershop.lua
+++ Modules/Skins/Blizzard/Barbershop.lua
@@ -11,7 +11,29 @@ function S:Blizzard_BarbershopUI()
         return
     end
 
-    S:CreateShadow(_G.BarberShopFrame)
+    local frame = _G.BarberShopFrame
+
+    S:CreateShadow(frame.ResetButton)
+    S:CreateShadow(frame.CancelButton)
+    S:CreateShadow(frame.AcceptButton)
 end
 
 S:AddCallbackForAddon("Blizzard_BarbershopUI")
+
+function S:Blizzard_CharacterCustomize()
+    if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.barber) then
+        return
+    end -- yes, it belongs also to tbe BarberUI
+    if not (E.private.WT.skins.blizzard.enable and E.private.WT.skins.blizzard.barberShop) then
+        return
+    end
+    local frame = _G.CharCustomizeFrame
+
+    S:CreateShadow(frame.SmallButtons.ResetCameraButton)
+    S:CreateShadow(frame.SmallButtons.ZoomOutButton)
+    S:CreateShadow(frame.SmallButtons.ZoomInButton)
+    S:CreateShadow(frame.SmallButtons.RotateLeftButton)
+    S:CreateShadow(frame.SmallButtons.RotateRightButton)
+end
+
+S:AddCallbackForAddon("Blizzard_CharacterCustomize")

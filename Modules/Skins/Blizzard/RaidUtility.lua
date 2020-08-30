cfbf56b72ba4899de78b25b8ad09a90f65e05630
Zhou Fang <fang2hou@outlook.com>
2020-08-31 00:27:05 +0900

    微调团队工具的关闭按钮位置

--- Modules/Skins/Blizzard/RaidUtility.lua
+++ Modules/Skins/Blizzard/RaidUtility.lua
@@ -1,8 +1,10 @@
 local W, F, E, L = unpack(select(2, ...))
 local S = W:GetModule("Skins")
+local RU = E:GetModule("RaidUtility")
 
 local _G = _G
 local pairs = pairs
+local InCombatLockdown = InCombatLockdown
 
 function S:RaidUtility()
     if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.nonraid) then
@@ -17,6 +19,17 @@ function S:RaidUtility()
     for _, frame in pairs(frames) do
         S:CreateShadow(frame)
     end
+
+    S:SecureHookScript(
+        RaidUtility_ShowButton,
+        "OnClick",
+        function()
+            if not InCombatLockdown() then
+                _G.RaidUtility_CloseButton:ClearAllPoints()
+                _G.RaidUtility_CloseButton:Point("TOP", _G.RaidUtilityPanel, "BOTTOM", 0, -4)
+            end
+        end
+    )
 end
 
 S:AddCallback("RaidUtility")

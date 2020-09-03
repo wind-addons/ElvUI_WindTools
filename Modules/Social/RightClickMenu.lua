bf23a6eb5d90a133db3e5183d29fcbb507830274
Zhou Fang <fang2hou@outlook.com>
2020-09-03 10:52:32 +0900

    从头设计右键菜单

--- Modules/Social/RightClickMenu.lua
+++ Modules/Social/RightClickMenu.lua
@@ -1,5 +1,5 @@
 local W, F, E, L = unpack(select(2, ...))
-local RCM = W:NewModule("RightClickMenu", "AceHook-3.0", "AceEvent-3.0")
+local RCM = W:NewModule("RightClickMenu", "AceHook-3.0")
 local ES = E:GetModule("Skins")
 local WS = W:GetModule("Skins")
 
@@ -22,12 +22,82 @@ local supportedTypes = {
     COMMUNITIES_GUILD_MEMBER = true
 }
 
-local function CustomButtonOnEnter(self) -- UIDropDownMenuTemplates.xml#155
-    _G[self:GetName() .. "Highlight"]:Show()
+local function RightClickMenu_OnShow(menu)
+    local parent = menu:GetParent() or menu
+    local width = parent:GetWidth()
+    local height = 32
+    for i = 1, #menu.buttons do
+        local button = menu.buttons[i]
+        if button:IsShown() then
+            button:SetWidth(width - 32)
+            height = height + 16
+        end
+    end
+    menu:SetHeight(height)
+    return height
 end
 
-local function CustomButtonOnLeave(self) -- UIDropDownMenuTemplates.xml#178
-    _G[self:GetName() .. "Highlight"]:Hide()
+local function RightClickMenuButton_OnEnter(button)
+    _G[button:GetName() .. "Highlight"]:Show()
+end
+
+local function RightClickMenuButton_OnLeave(button)
+    _G[button:GetName() .. "Highlight"]:Hide()
+end
+
+function RCM:UpdateButton(index, name, onClick, closeAfterFunction)
+    local button = self.menu.buttons[index]
+    if not button then
+        return
+    end
+
+    button.Text:SetText(name)
+    button.Text:Show()
+
+    button:SetScript(
+        "OnClick",
+        function()
+            onClick()
+            if closeAfterFunction then
+                CloseDropDownMenus()
+            end
+        end
+    )
+
+    button:Show()
+end
+
+function RCM:SkinDropDownList(frame)
+    local Backdrop = _G[frame:GetName() .. "Backdrop"]
+    local menuBackdrop = _G[frame:GetName() .. "MenuBackdrop"]
+
+    if Backdrop then
+        Backdrop:Kill()
+    end
+
+    if menuBackdrop then
+        menuBackdrop:Kill()
+    end
+end
+
+function RCM:SkinButton(button)
+    local r, g, b = unpack(E.media.rgbvaluecolor)
+
+    local highlight = _G[button:GetName() .. "Highlight"]
+    highlight:SetTexture(E.Media.Textures.Highlight)
+    highlight:SetBlendMode("BLEND")
+    highlight:SetDrawLayer("BACKGROUND")
+    highlight:SetVertexColor(r, g, b)
+
+    button:SetScript("OnEnter", RightClickMenuButton_OnEnter)
+    button:SetScript("OnLeave", RightClickMenuButton_OnLeave)
+
+    _G[button:GetName() .. "Check"]:SetAlpha(0)
+    _G[button:GetName() .. "UnCheck"]:SetAlpha(0)
+    _G[button:GetName() .. "Icon"]:SetAlpha(0)
+    _G[button:GetName() .. "ColorSwatch"]:SetAlpha(0)
+    _G[button:GetName() .. "ExpandArrow"]:SetAlpha(0)
+    _G[button:GetName() .. "InvisibleButton"]:SetAlpha(0)
 end
 
 function RCM:CreateMenu()
@@ -35,41 +105,86 @@ function RCM:CreateMenu()
         return
     end
 
-    
-    local menu = CreateFrame("Button", "WTRightClickMenuFrame", E.UIParent, "UIDropDownListTemplate")
-    -- _G[menu:GetName() .. "Backdrop"]:Hide()
-    menu:Point("CENTER", 0, 0)
-    menu.buttons = {}
+    local frame = CreateFrame("Button", "WTRightClickDropDownList", E.UIParent, "UIDropDownListTemplate")
+    self:SkinDropDownList(frame)
+    frame:Hide()
+
+    frame:SetScript("OnShow", RightClickMenu_OnShow)
+    frame:SetScript("OnHide", nil)
+    frame:SetScript("OnClick", nil)
+    frame:SetScript("OnUpdate", nil)
+
+    frame.buttons = {}
+
     for i = 1, UIDROPDOWNMENU_MAXBUTTONS do
-        local b = CreateFrame("Button", "WTRightClickMenuFrameButton" .. i, menu, "UIDropDownMenuButtonTemplate")
-        local text = _G[b:GetName().."NormalText"]
-        text:Point("TOPLEFT", b, "TOPLEFT", 0, 0)
-        text:Point("BOTTOMRIGHT", b, "BOTTOMRIGHT", 0, 0)
-        text:Show()
-        menu.buttons[i] = b
+        local button = _G["WTRightClickDropDownListButton" .. i]
+        if not button then
+            break
+        end
+
+        local text = _G[button:GetName() .. "NormalText"]
+        text:ClearAllPoints()
+        text:SetPoint("TOPLEFT", button, "TOPLEFT", 0, 0)
+        text:SetPoint("BOTTOMRIGHT", button, "BOTTOMRIGHT", 0, 0)
+        button.Text = text
+
+        button:SetScript("OnEnable", nil)
+        button:SetScript("OnDisable", nil)
+        button:SetScript("OnClick", nil)
+
+        button:Point("TOPLEFT", frame, "TOPLEFT", 16, -16 * i)
+        self:SkinButton(button)
+        button:Hide()
+        frame.buttons[i] = button
     end
 
-    self.menu = menu
+    self.menu = frame
+
+    self:UpdateButton(
+        1,
+        "Test",
+        function()
+            print(2)
+        end,
+        false
+    )
 end
 
 function RCM:ShowMenu(frame)
-    -- local dropdown = frame.dropdown
-    -- if not dropdown or not dropdown.which or not supportedTypes[dropdown.which] then
-    --     return
-    -- end
+    local dropdown = frame.dropdown
+    if not dropdown then
+        return
+    end
 
-    local button1 = self.menu.buttons[1]
-    local text = _G[button1:GetName() .. "NormalText"]
-    text:SetText("Button #1")
-    text:Show()
-    button1:SetScript("OnClick", function() print("button1") end)
-    button1:Size(60,20)
-    button1:Show()
+    -- 预组队伍右键
+    -- dropdown.Button == _G.LFGListFrameDropDownButton
 
-    self.menu:Size(button1:GetSize())
-    -- self.menu:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 0, 0)
-    -- self.menu:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 0, 0)
-    self.menu:Show()
+    if dropdown.which and supportedTypes[dropdown.which] then
+        local dropdownFullName
+        if dropdown.name then
+            if dropdown.server and not dropdown.name:find("-") then
+                dropdownFullName = dropdown.name .. "-" .. dropdown.server
+            else
+                dropdownFullName = dropdown.name
+            end
+        end
+        self.menu:SetParent(frame)
+        self.menu:SetFrameStrata(frame:GetFrameStrata())
+        self.menu:SetFrameLevel(frame:GetFrameLevel() + 2)
+
+        local dropDownListHeight = frame:GetHeight()
+        local menuHeight = self.menu:GetHeight()
+        if menuHeight < 1 then
+            menuHeight = RightClickMenu_OnShow(self.menu)
+        end
+
+        frame:SetHeight(dropDownListHeight + menuHeight - 5)
+
+        self.menu:ClearAllPoints()
+        self.menu:Point("BOTTOMLEFT", 0, 0)
+        self.menu:Point("BOTTOMRIGHT", 0, 0)
+        self.menu:Show()
+    end
 end
 
 function RCM:CloseMenu()
@@ -79,6 +194,9 @@ function RCM:CloseMenu()
 end
 
 function RCM:Initialize()
+    self:CreateMenu()
+    self:SecureHookScript(_G.DropDownList1, "OnShow", "ShowMenu")
+    self:SecureHookScript(_G.DropDownList1, "OnHide", "CloseMenu")
 end
 
 function RCM:ProfileUpdate()

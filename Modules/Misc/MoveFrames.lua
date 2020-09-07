8be58911e70ba1cd6246eca1cbb082df0c490b87
Zhou Fang <fang2hou@outlook.com>
2020-09-07 16:40:52 +0900

    添加子框架移动

--- Modules/Misc/MoveFrames.lua
+++ Modules/Misc/MoveFrames.lua
@@ -1,5 +1,6 @@
 local W, F, E, L = unpack(select(2, ...))
 local MF = W:NewModule("MoveFrames", "AceEvent-3.0", "AceHook-3.0")
+local _G = _G
 
 local BlizzardFrames = {
     "AddonList",
@@ -7,7 +8,6 @@ local BlizzardFrames = {
     "BankFrame",
     "ChatConfigFrame",
     "CinematicFrame",
-    "CharacterFrame",
     "DestinyFrame",
     "DressUpFrame",
     "FriendsFrame",
@@ -22,12 +22,10 @@ local BlizzardFrames = {
     "LFGDungeonReadyDialog",
     "LFGDungeonReadyStatus",
     "LootFrame",
-    "MailFrame",
     "MerchantFrame",
     "OpenMailFrame",
     "PetitionFrame",
     "PetStableFrame",
-    "PVEFrame",
     "PVPReadyDialog",
     "QuestFrame",
     "QuestLogPopupDetailFrame",
@@ -47,17 +45,9 @@ local BlizzardFrames = {
     "TradeFrame",
     "TutorialFrame",
     "VideoOptionsFrame",
-    "WorldMapFrame"
-}
-
-local BlizzardSubFrames = {
     ["CharacterFrame"] = {
         "PaperDollFrame",
-        "PetPaperDollFrame",
-        "CompanionFrame",
         "ReputationFrame",
-        "SkillFrame",
-        "HonorFrame",
         "TokenFrame"
     },
     ["MailFrame"] = {
@@ -76,6 +66,172 @@ local BlizzardSubFrames = {
     }
 }
 
+local BlizzardFramesLoadOnDemand = {
+    ["Blizzard_AchievementUI"] = {
+        "AchievementFrame",
+        "AchievementFrameHeader"
+    },
+    ["Blizzard_AlliedRacesUI"] = {
+        "AlliedRacesFrame"
+    },
+    ["Blizzard_ArchaeologyUI"] = {
+        "ArchaeologyFrame"
+    },
+    ["Blizzard_ArtifactUI"] = {
+        "ArtifactFrame",
+        "ArtifactRelicForgeFrame"
+    },
+    ["Blizzard_AuctionHouseUI"] = {
+        "AuctionHouseFrame"
+    },
+    ["Blizzard_AuctionUI"] = {
+        "AuctionFrame"
+    },
+    ["Blizzard_AzeriteEssenceUI"] = {
+        "AzeriteEssenceUI"
+    },
+    ["Blizzard_AzeriteRespecUI"] = {
+        "AzeriteRespecFrame"
+    },
+    ["Blizzard_AzeriteUI"] = {
+        "AzeriteEmpoweredItemUI"
+    },
+    ["Blizzard_BarberShopUI"] = {
+        "BarberShopFrame"
+    },
+    ["Blizzard_BindingUI"] = {
+        "KeyBindingFrame"
+    },
+    ["Blizzard_BlackMarketUI"] = {
+        "BlackMarketFrame"
+    },
+    ["Blizzard_Calendar"] = {
+        "CalendarCreateEventFrame",
+        "CalendarFrame"
+    },
+    ["Blizzard_ChallengesUI"] = {
+        "ChallengesKeystoneFrame"
+    },
+    ["Blizzard_Collections"] = {
+        "CollectionsJournal",
+        "WardrobeFrame"
+    },
+    ["Blizzard_Communities"] = {
+        "CommunitiesFrame",
+        "CommunitiesGuildLogFrame",
+        "CommunitiesGuildNewsFiltersFrame",
+        "CommunitiesGuildTextEditFrame"
+    },
+    ["Blizzard_Contribution"] = {
+        "ContributionCollectionFrame"
+    },
+    ["Blizzard_DeathRecap"] = {
+        "DeathRecapFrame"
+    },
+    ["Blizzard_EncounterJournal"] = {
+        "EncounterJournal"
+    },
+    ["Blizzard_FlightMap"] = {
+        "FlightMapFrame"
+    },
+    ["Blizzard_GarrisonUI"] = {
+        "GarrisonLandingPage",
+        "GarrisonMissionFrame",
+        "GarrisonCapacitiveDisplayFrame",
+        "GarrisonBuildingFrame",
+        "GarrisonRecruiterFrame",
+        "GarrisonRecruitSelectFrame",
+        "GarrisonShipyardFrame",
+        "OrderHallMissionFrame",
+        "BFAMissionFrame",
+        "GarrisonMonumentFrame"
+    },
+    ["Blizzard_GlyphUI"] = {
+        "GlyphFrame"
+    },
+    ["Blizzard_GMChatUI"] = {
+        "GMChatStatusFrame"
+    },
+    ["Blizzard_GMSurveyUI"] = {
+        "GMSurveyFrame"
+    },
+    ["Blizzard_GuildBankUI"] = {
+        "GuildBankFrame"
+    },
+    ["Blizzard_GuildControlUI"] = {
+        "GuildControlUI"
+    },
+    ["Blizzard_GuildUI"] = {
+        "GuildFrame"
+    },
+    ["Blizzard_InspectUI"] = {
+        "InspectFrame"
+    },
+    ["Blizzard_IslandsPartyPoseUI"] = {
+        "IslandsPartyPoseFrame"
+    },
+    ["Blizzard_IslandsQueueUI"] = {
+        "IslandsQueueFrame"
+    },
+    ["Blizzard_ItemAlterationUI"] = {
+        "TransmogrifyFrame"
+    },
+    ["Blizzard_ItemInteractionUI"] = {
+        "ItemInteractionFrame"
+    },
+    ["Blizzard_ItemSocketingUI"] = {
+        "ItemSocketingFrame"
+    },
+    ["Blizzard_ItemUpgradeUI"] = {
+        "ItemUpgradeFrame"
+    },
+    ["Blizzard_LookingForGuildUI"] = {
+        "LookingForGuildFrame"
+    },
+    ["Blizzard_MacroUI"] = {
+        "MacroFrame"
+    },
+    ["Blizzard_ObliterumUI"] = {
+        "ObliterumForgeFrame"
+    },
+    ["Blizzard_OrderHallUI"] = {
+        "OrderHallTalentFrame"
+    },
+    ["Blizzard_PVPMatch"] = {
+        "PVPMatchResults"
+    },
+    ["Blizzard_PVPUI"] = {
+        "PVPMatchScoreboard"
+    },
+    ["Blizzard_ReforgingUI"] = {
+        "ReforgingFrame"
+    },
+    ["Blizzard_QuestChoice"] = {
+        "QuestChoiceFrame"
+    },
+    ["Blizzard_ScrappingMachineUI"] = {
+        "ScrappingMachineFrame"
+    },
+    ["Blizzard_TalentUI"] = {
+        "PlayerTalentFrame"
+    },
+    ["Blizzard_TradeSkillUI"] = {
+        "TradeSkillFrame"
+    },
+    ["Blizzard_TrainerUI"] = {
+        "ClassTrainerFrame"
+    },
+    ["Blizzard_VoidStorageUI"] = {
+        "VoidStorageFrame"
+    },
+    ["Blizzard_WarboardUI"] = {
+        "WarboardQuestChoiceFrame"
+    },
+    ["Blizzard_WarfrontsPartyPoseUI"] = {
+        "WarfrontsPartyPoseFrame"
+    }
+}
+
 local function OnMouseDown(frame, button)
     if button == "LeftButton" then
         if frame.MoveFrame:IsMovable() then
@@ -90,7 +246,22 @@ local function OnMouseUp(frame, button)
     end
 end
 
-function MF:HandleFrame(frame, hookFrame)
+function MF:HandleFrames(frameTable, mainFrame)
+    for key, value in pairs(frameTable) do
+        if type(key) == "number" and type(value) == "string" then
+            if _G[value] then
+                self:HandleFrame(_G[value], mainFrame)
+            else
+                F.DebugMessage(self, format("Cannot find the frame: %s", value))
+            end
+        elseif type(key) == "string" and type(value) == "table" then
+            self:HandleFrames(value, mainFrame or _G[key])
+            self:HandleFrame(_G[key], mainFrame)
+        end
+    end
+end
+
+function MF:HandleFrame(frame, mainFrame)
     if not frame or (InCombatLockdown() and frame:IsProtected()) then
         return
     end
@@ -98,19 +269,17 @@ function MF:HandleFrame(frame, hookFrame)
     frame:SetMovable(true)
     frame:SetClampedToScreen(true)
 
-    hookFrame = hookFrame or frame
-    hookFrame.MoveFrame = frame
+    mainFrame = mainFrame or frame
+    mainFrame.MoveFrame = frame
 
-    hookFrame:EnableMouse(true)
-    hookFrame:HookScript("OnMouseDown", OnMouseDown)
-    hookFrame:HookScript("OnMouseUp", OnMouseUp)
+    mainFrame:EnableMouse(true)
+    mainFrame:HookScript("OnMouseDown", OnMouseDown)
+    mainFrame:HookScript("OnMouseUp", OnMouseUp)
 end
 
 function MF:Test()
-    for _, frameName in pairs(BlizzardFrames) do
-        if not _G[frameName] then
-            print(frameName)
-        end
+    if self.db.moveBlizzardFrames then
+        self:HandleFrames(BlizzardFrames)
     end
 end
 
@@ -118,12 +287,6 @@ function MF:Initialize()
     self.db = E.private.WT.misc
     if not self.db then
         return
-    end
-
-    if self.db.moveBlizzardFrames then
-        for _, frameName in pairs(BlizzardFrames) do
-            self:HandleFrame(_G[frameName])
-        end
     end
 end
 

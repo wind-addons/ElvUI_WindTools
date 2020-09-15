2018db1e075ab354e33353e0ac02710febcf24db
Zhou Fang <fang2hou@outlook.com>
2020-09-15 17:42:18 +0900

    初步添加额外按钮条设定

--- Options/Misc.lua
+++ Options/Misc.lua
@@ -3,6 +3,7 @@ local options = W.options.misc.args
 local LSM = E.Libs.LSM
 local M = W:GetModule("Misc")
 local MF = W:GetModule("MoveFrames")
+local EB = W:GetModule("ExtraItemsBar")
 
 local _G = _G
 local format = format
@@ -13,8 +14,204 @@ local GetNumClasses = GetNumClasses
 local C_CVar_GetCVarBool = C_CVar.GetCVarBool
 local C_CVar_SetCVar = C_CVar.SetCVar
 
-options.cvars = {
+local customListSelected1
+local customListSelected2
+
+local points = {
+    TOPLEFT = L["TOPLEFT"],
+    TOPRIGHT = L["TOPRIGHT"],
+    BOTTOMLEFT = L["BOTTOMLEFT"],
+    BOTTOMRIGHT = L["BOTTOMRIGHT"]
+}
+
+options.extraItemsBar = {
     order = 1,
+    type = "group",
+    name = L["Extra Items Bar"],
+    get = function(info)
+        return E.db.WT.misc.extraItemsBar[info[#info]]
+    end,
+    set = function(info, value)
+        E.db.WT.misc.extraItemsBar[info[#info]] = value
+        EB:ProfileUpdate()
+    end,
+    args = {
+        desc = {
+            order = 1,
+            type = "group",
+            inline = true,
+            name = L["Description"],
+            args = {
+                feature = {
+                    order = 1,
+                    type = "description",
+                    name = L["Add a bar to contain quest items and usable equipment."],
+                    fontSize = "medium"
+                }
+            }
+        },
+        enable = {
+            order = 2,
+            type = "toggle",
+            name = L["Enable"]
+        },
+        custom = {
+            order = 6,
+            type = "group",
+            inline = true,
+            name = L["Custom Items"],
+            args = {
+                list = {
+                    order = 1,
+                    type = "select",
+                    name = L["List"],
+                    get = function()
+                        return customListSelected1
+                    end,
+                    set = function(_, value)
+                        customListSelected1 = value
+                    end,
+                    values = function()
+                        local list = E.db.WT.misc.extraItemsBar.customList
+                        local result = {}
+                        for key, value in pairs(list) do
+                            result[key] = select(1, GetItemInfo(value))
+                        end
+                        return result
+                    end
+                },
+                addToList = {
+                    order = 2,
+                    type = "input",
+                    name = L["New Item ID"],
+                    get = function()
+                        return ""
+                    end,
+                    set = function(_, value)
+                        local itemID = tonumber(value)
+                        local itemName = select(1, GetItemInfo(itemID))
+                        if itemName then
+                            tinsert(E.db.WT.misc.extraItemsBar.customList, itemID)
+                            EB:UpdateBars()
+                        else
+                            print(L["The item ID is invalid."])
+                        end
+                    end
+                },
+                deleteButton = {
+                    order = 3,
+                    type = "execute",
+                    name = L["Delete"],
+                    desc = L["Delete the selected item."],
+                    func = function()
+                        if customListSelected1 then
+                            local list = E.db.WT.misc.extraItemsBar.customList
+                            tremove(list, customListSelected1)
+                            EB:UpdateBars()
+                        end
+                    end
+                }
+            }
+        }
+    }
+}
+
+do -- 添加按钮设定组
+    for i = 1, 3 do
+        options.extraItemsBar.args["bar" .. i] = {
+            order = i + 2,
+            type = "group",
+            inline = true,
+            name = L["Bar"] .. " " .. i,
+            get = function(info)
+                return E.db.WT.misc.extraItemsBar[info[#info - 1]][info[#info]]
+            end,
+            set = function(info, value)
+                E.db.WT.misc.extraItemsBar[info[#info - 1]][info[#info]] = value
+                EB:UpdateBar(i)
+            end,
+            args = {
+                enable = {
+                    order = 1,
+                    type = "toggle",
+                    name = L["Enable"]
+                },
+                anchor = {
+                    order = 2,
+                    type = "select",
+                    name = L["Anchor Point"],
+                    desc = L["The first button anchors itself to this point on the bar."],
+                    values = points
+                },
+                betterOption1 = {
+                    order = 3,
+                    type = "description",
+                    name = " ",
+                    width = "full"
+                },
+                backdrop = {
+                    order = 4,
+                    type = "toggle",
+                    name = L["Bar Backdrop"],
+                    desc = L["Show a backdrop of the bar."]
+                },
+                backdropSpacing = {
+                    order = 5,
+                    type = "range",
+                    name = L["Backdrop Spacing"],
+                    desc = L["The spacing between the backdrop and the buttons."],
+                    min = 1,
+                    max = 30,
+                    step = 1
+                },
+                spacing = {
+                    order = 6,
+                    type = "range",
+                    name = L["Button Spacing"],
+                    desc = L["The spacing between buttons."],
+                    min = 1,
+                    max = 30,
+                    step = 1
+                },
+                betterOption2 = {
+                    order = 7,
+                    type = "description",
+                    name = " ",
+                    width = "full"
+                },
+                buttonWidth = {
+                    order = 8,
+                    type = "range",
+                    name = L["Button Width"],
+                    desc = L["The width of the buttons."],
+                    min = 2,
+                    max = 80,
+                    step = 1
+                },
+                buttonHeight = {
+                    order = 9,
+                    type = "range",
+                    name = L["Button Height"],
+                    desc = L["The height of the buttons."],
+                    min = 2,
+                    max = 60,
+                    step = 1
+                },
+                buttonsPerRow = {
+                    order = 10,
+                    type = "range",
+                    name = L["Buttons Per Row"],
+                    min = 1,
+                    max = 12,
+                    step = 1
+                }
+            }
+        }
+    end
+end
+
+options.cvars = {
+    order = 2,
     type = "group",
     name = L["CVars Editor"],
     get = function(info)
@@ -106,7 +303,7 @@ options.cvars = {
 }
 
 options.moveFrames = {
-    order = 2,
+    order = 3,
     type = "group",
     name = L["Move Frames"],
     get = function(info)
@@ -128,7 +325,10 @@ options.moveFrames = {
                     type = "description",
                     name = function()
                         if MF.StopRunning then
-                            return format("|cffff0000"..L["Because of %s, this module will not be loaded."].."|r", MF.StopRunning)
+                            return format(
+                                "|cffff0000" .. L["Because of %s, this module will not be loaded."] .. "|r",
+                                MF.StopRunning
+                            )
                         else
                             return L["This module provides the feature that repositions the frames with drag and drop."]
                         end
@@ -175,7 +375,7 @@ options.moveFrames = {
 }
 
 options.transmog = {
-    order = 3,
+    order = 4,
     type = "group",
     name = L["Transmog"],
     args = {
@@ -210,7 +410,7 @@ options.transmog = {
 }
 
 options.pauseToSlash = {
-    order = 4,
+    order = 5,
     type = "group",
     name = L["Pause to slash"],
     args = {
@@ -247,7 +447,7 @@ options.pauseToSlash = {
 }
 
 options.disableTalkingHead = {
-    order = 5,
+    order = 6,
     type = "group",
     name = L["Disable Talking Head"],
     args = {
@@ -281,7 +481,7 @@ options.disableTalkingHead = {
 }
 
 options.tags = {
-    order = 6,
+    order = 7,
     type = "group",
     name = L["Tags"],
     args = {

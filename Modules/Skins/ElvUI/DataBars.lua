e8cb63768374261088e00648b54fd6ed6144f6aa
Zhou Fang <fang2hou@outlook.com>
2020-10-12 13:38:30 +0900

    适配新版本的数据条美化

--- Modules/Skins/ElvUI/DataBars.lua
+++ Modules/Skins/ElvUI/DataBars.lua
@@ -1,6 +1,6 @@
 local W, F, E, L = unpack(select(2, ...))
 local S = W:GetModule("Skins")
-local B = E:GetModule("DataBars")
+local DB = E:GetModule("DataBars")
 
 local _G = _G
 local pairs = pairs
@@ -16,7 +16,13 @@ function S:ElvUI_DataBars()
         return
     end
 
-    local bars = {_G.ElvUI_AzeriteBar, _G.ElvUI_ExperienceBar, _G.ElvUI_ReputationBar, _G.ElvUI_HonorBar}
+    local bars = {
+        _G.ElvUI_AzeriteBarHolder,
+        _G.ElvUI_ExperienceBarHolder,
+        _G.ElvUI_ReputationBarHolder,
+        _G.ElvUI_HonorBarHolder,
+        _G.ElvUI_ThreatBarHolder
+    }
     for _, bar in pairs(bars) do
         if bar then
             self:CreateShadow(bar)
@@ -24,7 +30,7 @@ function S:ElvUI_DataBars()
     end
 
     -- 后续进行配置更新时进行添加
-    self:SecureHook(B, "CreateBar", "ElvUI_SkinDataBar")
+    self:SecureHook(DB, "CreateBar", "ElvUI_SkinDataBar")
 end
 
 S:AddCallback("ElvUI_DataBars")

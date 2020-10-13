efd0f37f2d1afd8c98a734d33f081037ce3e880f
Zhou Fang <fang2hou@outlook.com>
2020-10-13 17:24:50 +0900

    更新文档

--- Core/Changelog/2.01.lua
+++ Core/Changelog/2.01.lua
@@ -4,27 +4,27 @@ W.Changelog[201] = {
     RELEASE_DATE = "TBC",
     NEW = {
         ["zhCN"] = {
-            "占位符.",
+            "添加了 Wind 风格的配置文件和导入按钮(在信息-重置中)."
         },
         ["zhTW"] = {
-            "占位符.",
+            "添加了 Wind 風格的配置文件和導入按鍵(在信息-重置中)."
         },
         ["enUS"] = {
-            "Placeholder"
+            "Add Wind style profiles and buttons for importing. (Information - Reset)"
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "修正更新说明中的一些错误.",
-            "添加聊天条的间隔设定.",
+            "添加聊天条的间隔设定."
         },
         ["zhTW"] = {
             "修正更新說明的一些錯誤.",
-            "添加聊天條的間隔設定.",
+            "添加聊天條的間隔設定."
         },
         ["enUS"] = {
             "Ammend the changelog.",
-            "Add spacing option of chat bar.",
+            "Add spacing option of chat bar."
         }
     }
 }

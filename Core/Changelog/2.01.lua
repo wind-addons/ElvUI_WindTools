0bdc3027c5522905fad17000c9fb2cf5e43eb40c
Zhou Fang <fang2hou@outlook.com>
2020-10-13 17:52:41 +0900

    更新文档

--- Core/Changelog/2.01.lua
+++ Core/Changelog/2.01.lua
@@ -16,15 +16,21 @@ W.Changelog[201] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "修正更新说明中的一些错误.",
-            "添加聊天条的间隔设定."
+            "添加聊天条的间隔设定.",
+            "[施法条] 修复了切换配置时可能导致的报错.",
+            "[游戏条] 修复了切换配置时可能导致的报错."
         },
         ["zhTW"] = {
             "修正更新說明的一些錯誤.",
-            "添加聊天條的間隔設定."
+            "添加聊天條的間隔設定.",
+            "[施法條] 修復了切換設定時可能導致的報錯.",
+            "[遊戲條] 修復了切換設定時可能導致的報錯.",
         },
         ["enUS"] = {
             "Ammend the changelog.",
-            "Add spacing option of chat bar."
+            "Add spacing option of chat bar.",
+            "[Cast Bar] Fix the errors may occur after profile changed.",
+            "[Game Bar] Fix the errors may occur after profile changed."
         }
     }
 }

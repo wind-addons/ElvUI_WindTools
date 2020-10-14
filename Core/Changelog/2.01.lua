c7885a0c1c215b852fa13ff9becc2bd9691b10d4
Zhou Fang <fang2hou@outlook.com>
2020-10-14 13:33:12 +0900

    更新文档

--- Core/Changelog/2.01.lua
+++ Core/Changelog/2.01.lua
@@ -4,33 +4,42 @@ W.Changelog[201] = {
     RELEASE_DATE = "TBC",
     NEW = {
         ["zhCN"] = {
-            "添加了 Wind 风格的配置文件和导入按钮(在信息-重置中)."
+            "添加 Wind 风格的配置文件和导入按钮(信息-重置).",
+            "添加包月弹出广告的窗体美化."
         },
         ["zhTW"] = {
-            "添加了 Wind 風格的配置文件和導入按鍵(在信息-重置中)."
+            "添加了 Wind 風格的配置文件和導入按鍵(信息-重置).",
+            "添加訂閱彈出式廣告的框架美化."
         },
         ["enUS"] = {
-            "Add Wind style profiles and buttons for importing. (Information - Reset)"
+            "Add Wind style profiles and buttons for importing. (Information - Reset)",
+            "Add new skin for Subscription Interstitial Frame."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "修正更新说明中的一些错误.",
             "添加聊天条的间隔设定.",
+            "添加 TinyInspect 窗体的移动功能.",
             "[施法条] 修复了切换配置时可能导致的报错.",
-            "[游戏条] 修复了切换配置时可能导致的报错."
+            "[游戏条] 修复了切换配置时可能导致的报错.",
+            "[切换按钮] 修复了有时通报按钮选中状态错误的问题."
         },
         ["zhTW"] = {
             "修正更新說明的一些錯誤.",
             "添加聊天條的間隔設定.",
+            "添加 TinyInspect 框架的移動功能.",
             "[施法條] 修復了切換設定時可能導致的報錯.",
             "[遊戲條] 修復了切換設定時可能導致的報錯.",
+            "[切換按鈕] 修復了有時通報按鍵選中狀態錯誤的問題.",
         },
         ["enUS"] = {
             "Ammend the changelog.",
             "Add spacing option of chat bar.",
+            "Now TinyInspect frames can be draged and moved.",
             "[Cast Bar] Fix the errors may occur after profile changed.",
-            "[Game Bar] Fix the errors may occur after profile changed."
+            "[Game Bar] Fix the errors may occur after profile changed.",
+            "[Switch Button] Fix the status display of Announce Button.",
         }
     }
 }

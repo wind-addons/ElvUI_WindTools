77d50415d15cf81fcca7e03c26b1a9944a19644a
Zhou Fang <fang2hou@outlook.com>
2020-10-15 20:15:11 +0900

    整理

--- Core/Changelog/2.01.lua
+++ Core/Changelog/2.01.lua
@@ -16,15 +16,30 @@ W.Changelog[201] = {
     NEW = {
         ["zhCN"] = {
             "添加 Wind 风格的配置文件和导入按钮(信息-重置).",
-            "添加月卡弹出广告的窗体美化."
+            "添加月卡弹出广告的窗体美化.",
+            "[聊天条] 添加了聊天按键的间隔选项.",
+            "[游戏条] 添加了一直显示系统信息文字的功能和选项.",
+            "[游戏条] 添加了附加文字的字体选项.",
+            "[游戏条] 添加了鼠标滑过显示的功能和选项.",
+            "[聊天文字] 添加了自定义缩写频道文字的功能."
         },
         ["zhTW"] = {
             "添加了 Wind 風格的配置文件和導入按鍵(信息-重置).",
-            "添加包月彈出式廣告的框架美化."
+            "添加包月彈出式廣告的框架美化.",
+            "[聊天條] 添加了聊天按鍵的間隔設定.",
+            "[遊戲條] 添加了一直顯示系統信息文字的功能和設定.",
+            "[遊戲條] 添加了附加文字的字體選項.",
+            "[遊戲條] 添加了滑鼠經過時顯示的功能和設定.",
+            "[聊天文字] 添加了自訂頻道縮寫的功能."
         },
         ["enUS"] = {
             "Add Wind style profiles and buttons for importing. (Information - Reset)",
-            "Add new skin for Subscription Interstitial Frame."
+            "Add new skin for Subscription Interstitial Frame.",
+            "[Chat Bar] Add spacing option.",
+            "[Game Bar] Add the feature and options for always showing system information.",
+            "[Game Bar] Add the options for additional text font style.",
+            "[Game Bar] Add the feature and options of mouseover.",
+            "[Chat Text] Add the feature of channel abbreviation customization."
         }
     },
     IMPROVEMENT = {
@@ -33,48 +48,33 @@ W.Changelog[201] = {
             "调整了部分框架的层级以更好的显示内容.",
             "添加 TinyInspect 窗体的移动功能.",
             "[施法条] 修复了切换配置时可能导致的报错.",
-            "[聊天条] 添加了聊天按键的间隔选项.",
             "[额外物品条] 添加了按钮数量选项.",
             "[游戏条] 修复了切换配置时可能导致的报错.",
-            "[游戏条] 添加了一直显示系统信息文字的功能和选项.",
-            "[游戏条] 添加了附加文字的字体选项.",
-            "[游戏条] 添加了鼠标滑过显示的功能和选项.",
             "[游戏条] 好友计数将会统计战网好友了.",
             "[切换按钮] 修复了有时通报按钮选中状态错误的问题.",
-            "[鼠标提示] 艾泽里特装备相关 API 函数更新到 9.0.",
-            "[聊天文字] 添加了自定义缩写频道文字的功能."
+            "[鼠标提示] 艾泽里特装备相关 API 函数更新到 9.0."
         },
         ["zhTW"] = {
             "修正更新說明的一些錯誤.",
             "調整了部分框架的層級以更好的顯示內容.",
             "添加 TinyInspect 框架的移動功能.",
             "[施法條] 修復了切換設定時可能導致的報錯.",
-            "[聊天條] 添加了聊天按鍵的間隔設定.",
             "[额外物品条] 添加了按鍵數量的設定.",
             "[遊戲條] 修復了切換設定時可能導致的報錯.",
-            "[遊戲條] 添加了一直顯示系統信息文字的功能和設定.",
-            "[遊戲條] 添加了附加文字的字體選項.",
-            "[遊戲條] 添加了滑鼠經過時顯示的功能和設定.",
             "[遊戲條] 好友計數支持戰網好友.",
             "[切換按鈕] 修復了有時通報按鍵選中狀態錯誤的問題.",
-            "[浮動提示] 艾澤裡特裝備相關 API 函數更新至 9.0.",
-            "[聊天文字] 添加了自訂頻道縮寫的功能."
+            "[浮動提示] 艾澤裡特裝備相關 API 函數更新至 9.0."
         },
         ["enUS"] = {
             "Ammend the changelog.",
             "Change the strata of several frames for better display.",
             "Now TinyInspect frames can be dragged and moved.",
             "[Cast Bar] Fix the errors may occur after profile changed.",
-            "[Chat Bar] Add spacing option.",
             "[Extra Item Bar] Add the number of buttons option.",
             "[Game Bar] Fix the errors may occur after profile changed.",
-            "[Game Bar] Add the feature and options for always showing system information.",
-            "[Game Bar] Add the options for additional text font style.",
-            "[Game Bar] Add the feature and options of mouseover.",
             "[Game Bar] The number of friend button will count BN friends.",
             "[Switch Button] Fix the status display of Announce Button.",
-            "[Tooltips] Update API of Azerite equips to 9.0.",
-            "[Chat Text] Add the feature of channel abbreviation customization."
+            "[Tooltips] Update API of Azerite equips to 9.0."
         }
     }
 }

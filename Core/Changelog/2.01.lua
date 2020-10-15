a1062ce6e9a4afea365fedfe1108a5aa68bbc739
Zhou Fang <fang2hou@outlook.com>
2020-10-15 14:13:14 +0900

    更新文档

--- Core/Changelog/2.01.lua
+++ Core/Changelog/2.01.lua
@@ -2,6 +2,17 @@ local W = unpack(select(2, ...))
 
 W.Changelog[201] = {
     RELEASE_DATE = "TBC",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "除非你手动开启除错模式, 除错信息将不再显示.",
+        },
+        ["zhTW"] = {
+            "除非你手動開啟除錯模式, 除錯信息將不再顯示.",
+        },
+        ["enUS"] = {
+            "The debug message will no longer show unless you enable the debug mode manully.",
+        }
+    },
     NEW = {
         ["zhCN"] = {
             "添加 Wind 风格的配置文件和导入按钮(信息-重置).",
@@ -23,7 +34,9 @@ W.Changelog[201] = {
             "添加 TinyInspect 窗体的移动功能.",
             "[施法条] 修复了切换配置时可能导致的报错.",
             "[游戏条] 修复了切换配置时可能导致的报错.",
-            "[切换按钮] 修复了有时通报按钮选中状态错误的问题."
+            "[游戏条] 添加了一直显示系统信息文字的功能和选项.",
+            "[切换按钮] 修复了有时通报按钮选中状态错误的问题.",
+            "[鼠标提示] 艾泽里特装备相关 API 函数更新到 9.0."
         },
         ["zhTW"] = {
             "修正更新說明的一些錯誤.",
@@ -31,7 +44,9 @@ W.Changelog[201] = {
             "添加 TinyInspect 框架的移動功能.",
             "[施法條] 修復了切換設定時可能導致的報錯.",
             "[遊戲條] 修復了切換設定時可能導致的報錯.",
+            "[遊戲條] 添加了一直顯示系統信息文字的功能和選項.",
             "[切換按鈕] 修復了有時通報按鍵選中狀態錯誤的問題.",
+            "[浮動提示] 艾澤裡特裝備相關 API 函數更新至 9.0."
         },
         ["enUS"] = {
             "Ammend the changelog.",
@@ -39,7 +54,9 @@ W.Changelog[201] = {
             "Now TinyInspect frames can be draged and moved.",
             "[Cast Bar] Fix the errors may occur after profile changed.",
             "[Game Bar] Fix the errors may occur after profile changed.",
+            "[Game Bar] Add the feature and options for always showing system information.",
             "[Switch Button] Fix the status display of Announce Button.",
+            "[Tooltips] Update API of Azerite equips to 9.0.",
         }
     }
 }

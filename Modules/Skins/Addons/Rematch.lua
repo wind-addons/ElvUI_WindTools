b8b561277100ffa6951dec250597ec401e76401a
Zhou Fang <fang2hou@outlook.com>
2020-10-16 20:12:49 +0900

    Rematch 大部分控件美化

--- Modules/Skins/Addons/Rematch.lua
+++ Modules/Skins/Addons/Rematch.lua
@@ -61,6 +61,98 @@ function S:Rematch()
         F.SetFontOutline(PetCount.UniqueLabel)
     end
 
+    -- 右上按钮
+    local function HandleIconButton(frame)
+        frame:StyleButton(nil, true)
+        if frame.IconBorder then
+            frame.IconBorder:Hide()
+        end
+        if frame.Icon then
+            frame.Icon:CreateBackdrop()
+        end
+    end
+
+    HandleIconButton(_G.RematchHealButton)
+    HandleIconButton(_G.RematchBandageButton)
+    HandleIconButton(_G.RematchToolbar.SafariHat)
+    HandleIconButton(_G.RematchLesserPetTreatButton)
+    HandleIconButton(_G.RematchPetTreatButton)
+    HandleIconButton(_G.RematchToolbar.SummonRandom)
+
+    -- 左半边
+    for _, region in pairs {_G.RematchPetPanel.Top:GetRegions()} do
+        region:Hide()
+    end
+    ES:HandleEditBox(_G.RematchPetPanel.Top.SearchBox)
+    _G.RematchPetPanel.Top.SearchBox:ClearAllPoints()
+    _G.RematchPetPanel.Top.SearchBox:Point("LEFT", _G.RematchPetPanel.Top.Toggle, "RIGHT", 2, 0)
+    _G.RematchPetPanel.Top.SearchBox:Point("RIGHT", _G.RematchPetPanel.Top.Filter, "LEFT", -2, 0)
+
+    ES:HandleButton(_G.RematchPetPanel.Top.Toggle)
+    _G.RematchPetPanel.Top.Toggle:StripTextures()
+
+    _G.RematchPetPanel.Top.Toggle.Texture = _G.RematchPetPanel.Top.Toggle:CreateTexture(nil, "OVERLAY")
+    _G.RematchPetPanel.Top.Toggle.Texture:Point("CENTER")
+    _G.RematchPetPanel.Top.Toggle.Texture:SetTexture(E.Media.Textures.ArrowUp)
+    _G.RematchPetPanel.Top.Toggle.Texture:Size(14, 14)
+
+    self:SecureHook(
+        _G.Rematch,
+        "SetTopToggleButton",
+        function()
+            if _G.RematchPetPanel.Top.Toggle.up then
+                _G.RematchPetPanel.Top.Toggle.Texture:SetRotation(ES.ArrowRotation["up"])
+            else
+                _G.RematchPetPanel.Top.Toggle.Texture:SetRotation(ES.ArrowRotation["down"])
+            end
+        end
+    )
+
+    _G.RematchPetPanel.Top.Toggle:HookScript(
+        "OnEnter",
+        function(self)
+            if self.Texture then
+                self.Texture:SetVertexColor(unpack(E.media.rgbvaluecolor))
+            end
+        end
+    )
+
+    _G.RematchPetPanel.Top.Toggle:HookScript(
+        "OnLeave",
+        function(self)
+            if self.Texture then
+                self.Texture:SetVertexColor(1, 1, 1)
+            end
+        end
+    )
+
+    _G.RematchPetPanel.Top.Toggle:HookScript(
+        "OnClick",
+        function(self)
+            self:SetNormalTexture("")
+            self:SetPushedTexture("")
+            if self.up then
+                self.Texture:SetRotation(ES.ArrowRotation["up"])
+            else
+                self.Texture:SetRotation(ES.ArrowRotation["down"])
+            end
+        end
+    )
+
+    ES:HandleButton(_G.RematchPetPanel.Top.Filter)
+
+    _G.RematchPetPanel.Top.Filter.Arrow:SetTexture(E.Media.Textures.ArrowUp)
+    _G.RematchPetPanel.Top.Filter.Arrow:SetRotation(ES.ArrowRotation.right)
+
+    -- 中间
+    ES:HandleButton(_G.RematchLoadoutPanel.Target.TargetButton)
+    ES:HandleButton(_G.RematchLoadoutPanel.TargetPanel.Top.BackButton)
+
+    -- 右边
+    ES:HandleButton(_G.RematchTeamPanel.Top.Teams)
+    _G.RematchTeamPanel.Top.Teams.Arrow:SetTexture(E.Media.Textures.ArrowUp)
+    _G.RematchTeamPanel.Top.Teams.Arrow:SetRotation(ES.ArrowRotation.right)
+
     -- 右侧队伍标签页
     self:SecureHook(
         _G.RematchTeamTabs,
@@ -91,6 +183,12 @@ function S:Rematch()
         F.SetFontOutline(tab.Text)
         self:CreateBackdropShadowAfterElvUISkins(tab)
     end
+
+    -- 下方按钮
+    ES:HandleButton(_G.RematchBottomPanel.SummonButton)
+    ES:HandleButton(_G.RematchBottomPanel.SaveButton)
+    ES:HandleButton(_G.RematchBottomPanel.SaveAsButton)
+    ES:HandleButton(_G.RematchBottomPanel.FindBattleButton)
 
     -- 移动支持
     if MF and MF.db and MF.db.moveBlizzardFrames then

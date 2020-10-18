4ab10a1cd3b8ce7f48d166796868fd56ff627a2b
Zhou Fang <fang2hou@outlook.com>
2020-10-18 21:34:58 +0900

    Init new 2.02 changelog

---
+++ Core/Changelog/2.02.lua
@@ -0,0 +1,44 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[202] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "placeholder"
+        },
+        ["zhTW"] = {
+            "placeholder"
+        },
+        ["enUS"] = {
+            "placeholder"
+        }
+    },
+    NEW = {
+        ["zhCN"] = {
+            "placeholder"
+        },
+        ["zhTW"] = {
+            "placeholder"
+        },
+        ["enUS"] = {
+            "placeholder"
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[本地化] 修复了简体中文版本提示.",
+            "[游戏条] 修复了开关游戏条可能带来的错误.",
+            "[聊天条] 修复了未设定频道名时部分按钮不显示的问题."
+        },
+        ["zhTW"] = {
+            "[本地化] 修復了簡體中文版版本提示.",
+            "[遊戲條] 修復了開關遊戲條可能帶來的錯誤.",
+            "[聊天條] 修復了未設定頻道名時部分按鈕不顯示的問題."
+        },
+        ["enUS"] = {
+            "[Localization] Fix the version message in zhCN translation.",
+            "[Game Bar] Fix the bug occurred when player try to toggle the game bar.",
+            "[Chat Bar] Fix the bug that several buttons do not show if the name of world channel not be set."
+        }
+    }
+}

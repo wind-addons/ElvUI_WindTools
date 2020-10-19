8e50122e04270b6cffee249e2ed6ac16fffcb00e
Zhou Fang <fang2hou@outlook.com>
2020-10-19 13:12:03 +0900

    Update changelog

--- Core/Changelog/2.02.lua
+++ Core/Changelog/2.02.lua
@@ -28,17 +28,23 @@ W.Changelog[202] = {
         ["zhCN"] = {
             "[本地化] 修复了简体中文版本提示.",
             "[游戏条] 修复了开关游戏条可能带来的错误.",
-            "[聊天条] 修复了未设定频道名时部分按钮不显示的问题."
+            "[聊天条] 修复了未设定频道名时部分按钮不显示的问题.",
+            "[好友列表] 修复了使命召唤: 现代战争 2 的图标显示.",
+            "[无汉字计数] 修复数据库错误."
         },
         ["zhTW"] = {
             "[本地化] 修復了簡體中文版版本提示.",
             "[遊戲條] 修復了開關遊戲條可能帶來的錯誤.",
-            "[聊天條] 修復了未設定頻道名時部分按鈕不顯示的問題."
+            "[聊天條] 修復了未設定頻道名時部分按鈕不顯示的問題.",
+            "[好友列表] 修復了決勝時刻: 現代戰爭 2 的圖標顯示.",
+            "[無漢字計數] 修復數據庫錯誤."
         },
         ["enUS"] = {
             "[Localization] Fix the version message in zhCN translation.",
             "[Game Bar] Fix the bug occurred when player try to toggle the game bar.",
-            "[Chat Bar] Fix the bug that several buttons do not show if the name of world channel not be set."
+            "[Chat Bar] Fix the bug that several buttons do not show if the name of world channel not be set.",
+            "[Friend List] Fix the icon of COD MW2.",
+            "[No Kanji Math] Fix the db error."
         }
     }
 }

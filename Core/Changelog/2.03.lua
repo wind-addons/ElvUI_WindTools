6eaebc40c39baa650bd5d3f1af4a8f1eb72e6fc2
Zhou Fang <fang2hou@outlook.com>
2020-10-20 23:08:47 +0900

    Update changelog

--- Core/Changelog/2.03.lua
+++ Core/Changelog/2.03.lua
@@ -4,13 +4,22 @@ W.Changelog[203] = {
     RELEASE_DATE = "TBD",
     IMPROVEMENT = {
         ["zhCN"] = {
-            "修复更新记录提示函数."
+            "修复更新记录提示函数.",
+            "[游戏条] 部分按钮支持战斗中点击.",
+            "[移动框体] 修复了可能出现的 ElvUI 背包拖拽提示不消失问题.",
+            "[美化皮肤] 修复了保持宽高比的 WeakAuras 图标美化会导致材质拉伸的问题."
         },
         ["zhTW"] = {
-            "修復更新記錄提示函數."
+            "修復更新記錄提示函數.",
+            "[遊戲條] 部分按鈕支持戰鬥中點擊",
+            "[移動框架] 修復了可能出現的 ElvUI 背包拖拽提示不消失的問題.",
+            "[美化皮膚] 修復了保持寬高比的 WeakAura 圖標美化會導致材質拉伸的問題."
         },
         ["enUS"] = {
-            "Fix changelog alert function."
+            "Fix changelog alert function.",
+            "[Game Bar] A part of buttons support click in combat.",
+            "[Move Frames] Fix the bug that tooltips on ElvUI Bags may not hide.",
+            '[Skins] Fix the bug that WeakAuras Icons option "keep aspect ratio" not worked. '
         }
     }
 }

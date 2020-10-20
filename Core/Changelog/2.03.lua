6b5dcddf6d827ed25fc46e218f7b99429443b4ea
Zhou Fang <fang2hou@outlook.com>
2020-10-21 01:54:29 +0900

    Update changlog

--- Core/Changelog/2.03.lua
+++ Core/Changelog/2.03.lua
@@ -7,19 +7,25 @@ W.Changelog[203] = {
             "修复更新记录提示函数.",
             "[游戏条] 部分按钮支持战斗中点击.",
             "[移动框体] 修复了可能出现的 ElvUI 背包拖拽提示不消失问题.",
-            "[美化皮肤] 修复了保持宽高比的 WeakAuras 图标美化会导致材质拉伸的问题."
+            "[美化皮肤] 修复了保持宽高比的 WeakAuras 图标美化会导致材质拉伸的问题.",
+            "[右键菜单] 公会邀请可以邀请战网好友了.",
+            "[右键菜单] 修复了向战网好友发送属性统计的功能"
         },
         ["zhTW"] = {
             "修復更新記錄提示函數.",
             "[遊戲條] 部分按鈕支持戰鬥中點擊",
             "[移動框架] 修復了可能出現的 ElvUI 背包拖拽提示不消失的問題.",
-            "[美化皮膚] 修復了保持寬高比的 WeakAura 圖標美化會導致材質拉伸的問題."
+            "[美化皮膚] 修復了保持寬高比的 WeakAura 圖標美化會導致材質拉伸的問題.",
+            "[右鍵選單] 公會邀請可以邀請戰網好友了.",
+            "[右鍵選單] 修復了向戰網好友發送屬性統計的功能."
         },
         ["enUS"] = {
             "Fix changelog alert function.",
             "[Game Bar] A part of buttons support click in combat.",
             "[Move Frames] Fix the bug that tooltips on ElvUI Bags may not hide.",
-            '[Skins] Fix the bug that WeakAuras Icons option "keep aspect ratio" not worked. '
+            '[Skins] Fix the bug that WeakAuras Icons option "keep aspect ratio" not worked. ',
+            "[Context Menu] Guild invite now available on Battle.net friends.",
+            "[Context Menu] Fix Report Stats function on Battle.net friends."
         }
     }
 }

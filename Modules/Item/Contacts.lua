43b8a00c60003972656f082be05f38b465ef52da
Zhou Fang <fang2hou@outlook.com>
2020-10-22 13:56:16 +0900

    Create page controller and name buttons

--- Modules/Item/Contacts.lua
+++ Modules/Item/Contacts.lua
@@ -29,43 +29,43 @@ function CT:ConstructFrame()
     self.frame = frame
 end
 
+local function SetButtonTexture(button, texture)
+    local normalTex = button:CreateTexture(nil, "ARTWORK")
+    normalTex:Point("CENTER")
+    normalTex:Size(button:GetSize())
+    normalTex:SetTexture(texture)
+    normalTex:SetVertexColor(1, 1, 1)
+    button.normalTex = normalTex
+
+    local hoverTex = button:CreateTexture(nil, "ARTWORK")
+    hoverTex:Point("CENTER")
+    hoverTex:Size(button:GetSize())
+    hoverTex:SetTexture(texture)
+    hoverTex:SetVertexColor(unpack(E.media.rgbvaluecolor))
+    hoverTex:SetAlpha(0)
+    button.hoverTex = hoverTex
+
+    button:SetScript(
+        "OnEnter",
+        function()
+            E:UIFrameFadeIn(button.hoverTex, (1 - button.hoverTex:GetAlpha()) * 0.62, button.hoverTex:GetAlpha(), 1)
+        end
+    )
+
+    button:SetScript(
+        "OnLeave",
+        function()
+            E:UIFrameFadeOut(button.hoverTex, button.hoverTex:GetAlpha() * 0.62, button.hoverTex:GetAlpha(), 0)
+        end
+    )
+end
+
 function CT:ConstructButtons()
-    local function SetButtonTexture(button, texture)
-        local normalTex = button:CreateTexture(nil, "ARTWORK")
-        normalTex:Point("CENTER")
-        normalTex:Size(button:GetSize())
-        normalTex:SetTexture(texture)
-        normalTex:SetVertexColor(1, 1, 1)
-        button.normalTex = normalTex
-
-        local hoverTex = button:CreateTexture(nil, "ARTWORK")
-        hoverTex:Point("CENTER")
-        hoverTex:Size(button:GetSize())
-        hoverTex:SetTexture(texture)
-        hoverTex:SetVertexColor(unpack(E.media.rgbvaluecolor))
-        hoverTex:SetAlpha(0)
-        button.hoverTex = hoverTex
-
-        button:SetScript(
-            "OnEnter",
-            function()
-                E:UIFrameFadeIn(button.hoverTex, (1 - button.hoverTex:GetAlpha()) * 0.62, button.hoverTex:GetAlpha(), 1)
-            end
-        )
-
-        button:SetScript(
-            "OnLeave",
-            function()
-                E:UIFrameFadeOut(button.hoverTex, button.hoverTex:GetAlpha() * 0.62, button.hoverTex:GetAlpha(), 0)
-            end
-        )
-    end
-    
     -- Toggle frame
     local toggleButton = CreateFrame("Button", "WTContactsToggleButton", _G.SendMailFrame, "SecureActionButtonTemplate")
     toggleButton:Size(24)
     SetButtonTexture(toggleButton, W.Media.Icons.list)
-    toggleButton:Point("BOTTOMRIGHT", _G.MailFrame, "BOTTOMRIGHT", -24, 36)
+    toggleButton:Point("BOTTOMRIGHT", _G.MailFrame, "BOTTOMRIGHT", -24, 38)
     toggleButton:RegisterForClicks("AnyUp")
 
     toggleButton:SetScript(
@@ -153,6 +153,96 @@ function CT:ConstructButtons()
     self.friendsButton = friendsButton
     self.guildButton = guildButton
     self.favoriteButton = favoriteButton
+end
+
+function CT:ConstructNameButtons()
+    self.frame.nameButtons = {}
+    for i = 1, 14 do
+        local button = CreateFrame("Button", "WTContactsNameButton" .. i, self.frame, "UIPanelButtonTemplate")
+        button:Size(140, 20)
+
+        if i == 1 then
+            button:Point("TOP", self.frame, "TOP", 0, -45)
+        else
+            button:Point("TOP", self.frame.nameButtons[i - 1], "BOTTOM", 0, -5)
+        end
+
+        button:SetText("123")
+        button.class = "MONK"
+        button.name = "Tabimonk"
+        button.realm = "暗影之月"
+        button:RegisterForClicks("LeftButtonDown", "RightButtonDown")
+        ES:HandleButton(button)
+        S:CreateShadow(button.backdrop, 2, 1, 1, 1, true)
+        if button.backdrop.shadow then
+            button.backdrop.shadow:Hide()
+        end
+
+        button:SetScript(
+            "OnClick",
+            function(self, mouseButton)
+                if mouseButton == "LeftButton" then
+                    if _G.SendMailNameEditBox then
+                        local playerName = self.name
+                        if self.realm ~= E.myrealm then
+                            playerName = playerName .. "-" .. self.realm
+                        end
+
+                        _G.SendMailNameEditBox:SetText(playerName)
+                    end
+                end
+            end
+        )
+
+        button:SetScript(
+            "OnEnter",
+            function(self)
+                CT:SetButtonTooltip(self)
+                if self.backdrop.shadow then
+                    self.backdrop.shadow:Show()
+                end
+            end
+        )
+
+        button:SetScript(
+            "OnLeave",
+            function(self)
+                GameTooltip:Hide()
+                if self.backdrop.shadow then
+                    self.backdrop.shadow:Hide()
+                end
+            end
+        )
+
+        self.frame.nameButtons[i] = button
+    end
+end
+
+function CT:ConstructPageController()
+    local pagePrevButton = CreateFrame("Button", "WTContactsToggleButton", self.frame, "SecureActionButtonTemplate")
+    pagePrevButton:Size(14)
+    SetButtonTexture(pagePrevButton, E.Media.Textures.ArrowUp)
+    pagePrevButton.normalTex:SetRotation(ES.ArrowRotation.left)
+    pagePrevButton.hoverTex:SetRotation(ES.ArrowRotation.left)
+    pagePrevButton:Point("BOTTOMLEFT", self.frame, "BOTTOMLEFT", 8, 8)
+    pagePrevButton:RegisterForClicks("AnyUp")
+
+    local pageNextButton = CreateFrame("Button", "WTContactsToggleButton", self.frame, "SecureActionButtonTemplate")
+    pageNextButton:Size(14)
+    SetButtonTexture(pageNextButton, E.Media.Textures.ArrowUp)
+    pageNextButton.normalTex:SetRotation(ES.ArrowRotation.right)
+    pageNextButton.hoverTex:SetRotation(ES.ArrowRotation.right)
+    pageNextButton:Point("BOTTOMRIGHT", self.frame, "BOTTOMRIGHT", -8, 8)
+    pageNextButton:RegisterForClicks("AnyUp")
+end
+
+function CT:SetButtonTooltip(button)
+    GameTooltip:ClearLines()
+    GameTooltip:SetOwner(button, "ANCHOR_BOTTOMRIGHT", 8, 20)
+    GameTooltip:SetText(button.name)
+    GameTooltip:AddDoubleLine(L["Name"], button.name, 1, 1, 1, GetClassColor(button.class))
+    GameTooltip:AddDoubleLine(L["Realm"], button.realm, 1, 1, 1, unpack(E.media.rgbvaluecolor))
+    GameTooltip:Show()
 end
 
 function CT:Initialize()

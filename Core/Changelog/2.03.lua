580b578df3ce55ef120194706e1915ed8ee7ee63
Zhou Fang <fang2hou@outlook.com>
2020-10-23 19:55:01 +0900

    Update docs

--- Core/Changelog/2.03.lua
+++ Core/Changelog/2.03.lua
@@ -29,6 +29,7 @@ W.Changelog[203] = {
             "[美化皮肤] 修复了可能发生的 ElvUI 动作条皮肤美化错误.",
             "[右键菜单] 公会邀请可以邀请战网好友了.",
             "[右键菜单] 修复了向战网好友发送属性统计的功能.",
+            "[右键菜单] 修复了与 RaiderIO 一起使用时可能发生的重叠.",
             "[聊天链接] 修复了坐骑链接无法预览的问题.",
             "[进度] 修复了观察对象时可能发生的报错.",
             "[巅峰声望] 修复了一处设定索引错误.",
@@ -37,6 +38,7 @@ W.Changelog[203] = {
             "[矩形小地图] 添加了对移动窗体的限制.",
             "[交接] 修复了部分多任务时可能不交接的问题.",
             "[团队标记] 修复了战斗中可能发生的错误.",
+            "[额外物品条] 修复了当玩家持有药水数量大于 12 时可能发生的错误."
         },
         ["zhTW"] = {
             "修復更新記錄提示函數.",
@@ -47,6 +49,7 @@ W.Changelog[203] = {
             "[美化皮膚] 修復了可能发生的 ElvUI 动作条皮肤美化错误.",
             "[右鍵選單] 公會邀請可以邀請戰網好友了.",
             "[右鍵選單] 修復了向戰網好友發送屬性統計的功能.",
+            "[右鍵選單] 修復了同時載入 RaiderIO 時發生的錯位.",
             "[聊天鏈接] 修復了坐騎鏈接無法預覽的問題.",
             "[進度] 修復了觀察對象時可能發生的錯誤.",
             "[巔峰聲望] 修復了一處設定索引錯誤.",
@@ -54,17 +57,19 @@ W.Changelog[203] = {
             "[巔峰聲望] 支援 9.0 新陣營.",
             "[矩形小地图] 新增了對移動框架的限制.",
             "[交接] 修復了多任務時可能不交接的問題.",
-            "[團隊標記] 修復了戰鬥中可能發生的錯誤."
+            "[團隊標記] 修復了戰鬥中可能發生的錯誤.",
+            "[額外物品條] 修復了當玩家持有藥水數大於 12 時可能發生的錯誤."
         },
         ["enUS"] = {
             "Fix changelog alert function.",
             "[Game Bar] A part of buttons support click in combat.",
             "[Move Frames] Fix the bug that tooltips on ElvUI Bags may not hide.",
             "[Move Frames] Fix the bug that the module try to handle ElvUI Bags even it not exist.",
-            '[Skins] Fix the bug that WeakAuras Icons option "keep aspect ratio" not worked. ',
-            "[Skins] Fix the bug that Skins try to show/hide shadow even the shadow was not added. ",
+            '[Skins] Fix the bug that WeakAuras Icons option "keep aspect ratio" not worked.',
+            "[Skins] Fix the bug that Skins try to show/hide shadow even the shadow was not added.",
             "[Context Menu] Guild invite now available on Battle.net friends.",
             "[Context Menu] Fix Report Stats function on Battle.net friends.",
+            "[Context Menu] Fix the postion when Raider.IO also loaded.",
             "[Chat Link] Fix the problem that you cannot preview the mount via clicking the link.",
             "[Progression] Fix the bug occurred when you try to inspect other players.",
             "[Paragon Reputation] Fix the option key error.",
@@ -72,7 +77,8 @@ W.Changelog[203] = {
             "[Paragon Reputation] Add support for SL.",
             "[Rectangle Minimap] Add the limit for minimap mover.",
             "[Turn In] Fix the bugs that the automation stops on multiple quests.",
-            "[Raid Markers] Fix the errors in combat."
+            "[Raid Markers] Fix the errors in combat.",
+            "[Extra Item Bar] Fix the bug occurred if the number of potions over 12."
         }
     }
 }

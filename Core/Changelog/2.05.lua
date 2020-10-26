0ab6629b11aac444cd050c0f815096c9c772ce23
Zhou Fang <fang2hou@outlook.com>
2020-10-26 23:05:50 +0900

    Changlog

--- Core/Changelog/2.05.lua
+++ Core/Changelog/2.05.lua
@@ -4,35 +4,35 @@ W.Changelog[205] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
-            "",
+            "placeholder",
         },
         ["zhTW"] = {
-            "",
+            "placeholder",
         },
         ["enUS"] = {
-            "",
+            "placeholder",
         }
     },
     NEW = {
         ["zhCN"] = {
-            "",
+            "[美化皮肤] 添加了 Bigwigs 进入副本倒计时美化.",
         },
         ["zhTW"] = {
-            "",
+            "[美化皮膚] 新增了 Bigwigs 進入副本倒計時美化.",
         },
         ["enUS"] = {
-            "",
+            "[Skins] Add skin for Bigwigs queue timer.",
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "",
+            "兼容性检查工具更新.",
         },
         ["zhTW"] = {
-            "",
+            "兼容性确认工具更新.",
         },
         ["enUS"] = {
-            "",
+            "Compatiblility check updated.",
         }
     }
 }

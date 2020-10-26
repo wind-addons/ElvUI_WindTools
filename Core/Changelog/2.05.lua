5773f8139ab9373d322d0585b828b47eefb92e0b
Zhou Fang <fang2hou@outlook.com>
2020-10-26 17:44:06 +0900

    Init 2.05 changelog

---
+++ Core/Changelog/2.05.lua
@@ -0,0 +1,38 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[205] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "",
+        },
+        ["zhTW"] = {
+            "",
+        },
+        ["enUS"] = {
+            "",
+        }
+    },
+    NEW = {
+        ["zhCN"] = {
+            "",
+        },
+        ["zhTW"] = {
+            "",
+        },
+        ["enUS"] = {
+            "",
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "",
+        },
+        ["zhTW"] = {
+            "",
+        },
+        ["enUS"] = {
+            "",
+        }
+    }
+}

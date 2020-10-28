5a9e80fc80e6fbc6b24dfc2bcec9020bfaaf3932
Zhou Fang <fang2hou@outlook.com>
2020-10-28 19:38:09 +0900

    changelog

--- Core/Changelog/2.05.lua
+++ Core/Changelog/2.05.lua
@@ -28,7 +28,9 @@ W.Changelog[205] = {
             "[美化皮肤] 添加了克罗米时间线选择框体美化.",
             "[美化皮肤] 添加了艾泽里特特质重置框体美化.",
             "[美化皮肤] 添加了艾泽里特特质装备框体美化.",
-            "[天赋管家] 添加了物品按钮到天赋面板."
+            "[天赋管家] 添加了物品按钮到天赋面板.",
+            "[游戏条] 添加了音量按钮.",
+            "[游戏条] 添加了专业技能按钮."
         },
         ["zhTW"] = {
             "[裝備觀察] 新增了字體風格的設定.",
@@ -37,7 +39,9 @@ W.Changelog[205] = {
             "[美化皮膚] 新增了克羅米時間線選擇框架美化.",
             "[美化皮膚] 新增了艾澤里特特質重置框架美化.",
             "[美化皮膚] 新增了艾澤里特特質裝備框架美化.",
-            "[天賦管家] 新增了物品按鍵到天賦面板."
+            "[天賦管家] 新增了物品按鍵到天賦面板.",
+            "[遊戲條] 新增了音量按鍵.",
+            "[遊戲條] 新增了專業技能按鍵."
         },
         ["enUS"] = {
             "[Inspect] Add options of font styles.",
@@ -46,7 +50,9 @@ W.Changelog[205] = {
             "[Skins] Add skin for Chromie Time.",
             "[Skins] Add skin for Azerite empowered item.",
             "[Skins] Add skin for Azerite Respec.",
-            "[Talent Manager] Add item buttons to talent frame."
+            "[Talent Manager] Add item buttons to talent frame.",
+            "[Game Bar] Add Volume button.",
+            "[Game Bar] Add Profession button."
         },
         ["koKR"] = {
             "[살펴보기] 글꼴 스타일 옵션을 추가합니다.",
@@ -55,7 +61,9 @@ W.Changelog[205] = {
             "[스킨] Bigwigs 큐 타이머 스킨 추가.",
             "[스킨] 아제라이트 장비 스킨 추가.",
             "[스킨] 아제라이트 재연마 스킨 추가.",
-            "[특성관리자] 특성 창에 아이템 버튼이 추가되었습니다."
+            "[특성관리자] 특성 창에 아이템 버튼이 추가되었습니다.",
+            "[Game Bar] Add Volume button.",
+            "[Game Bar] Add Profession button."
         }
     },
     IMPROVEMENT = {
@@ -63,6 +71,7 @@ W.Changelog[205] = {
             "媒体文件更新.",
             "兼容性检查工具更新.",
             "[游戏条] 现在不会挡住全屏的世界地图了.",
+            "[游戏条] 更换了炉石列表的生成逻辑.",
             "[美化皮肤] 优化了跳过过场动画的提示背景美化.",
             "[装备观察] 优化了人物头像的风格."
         },
@@ -70,6 +79,7 @@ W.Changelog[205] = {
             "媒體文件更新.",
             "兼容性确认工具更新.",
             "[遊戲條] 現在不會遮擋全屏世界地圖了.",
+            "[遊戲條] 更換了爐石列表的生成邏輯.",
             "[美化皮膚] 優化了跳過過場動畫的提示背景美化.",
             "[裝備觀察] 優化了人物頭像的風格."
         },
@@ -77,6 +87,7 @@ W.Changelog[205] = {
             "Media files updated.",
             "Compatibility check updated.",
             "[Game Bar] The world map in full-screen mode will display in front of the game bar.",
+            "[Game Bar] New logic for generating the table of HearthStones.",
             "[Skins] Better skin for skip cut scene confirmation dialog.",
             "[Inspect] Better style for the portrait."
         },
@@ -84,6 +95,7 @@ W.Changelog[205] = {
             "미디어 파일이 업데이트되었습니다.",
             "호환성 검사가 업데이트되었습니다.",
             "[게임 바] 이제 전체 화면 모드의 월드 맵을 게임 바가 가리지 않습니다.",
+            "[Game Bar] New logic for generating the table of HearthStones.",
             "[스킨] 컷신 건너 뛰기 확인 대화 상자의 스킨을 최적화 했습니다.",
             "[살펴보기] 캐릭터 초상화 스타일을 최적화했습니다."
         }

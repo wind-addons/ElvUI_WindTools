4a6f267d201bfb5c30da8426c73dca7ef8e91dea
Zhou Fang <fang2hou@outlook.com>
2020-10-29 13:50:56 +0900

    Changelog

--- Core/Changelog/2.05.lua
+++ Core/Changelog/2.05.lua
@@ -6,21 +6,25 @@ W.Changelog[205] = {
         ["zhCN"] = {
             "感谢 헬리오스의방패, 불광불급옹, 다크어쎄신 及 크림슨프릴 的辛苦付出, 韩语已经获得全面的支持.",
             "支持使用 MerathilisUI 皮肤模块美化 Wind 工具箱内的模块窗口.",
+            "修复且优化了默认配置 (Fang2houUI) 的导入. 你可以在 [信息]-[重置] 中找到它.",
             "[美化皮肤] 添加了全新的字体子模块."
         },
         ["zhTW"] = {
             "感謝 헬리오스의방패, 불광불급옹, 다크어쎄신 及 크림슨프릴 的辛苦付出, 韓文已獲得完整支援.",
             "支援 MerathilisUI 皮膚模組美化 Wind 工具箱內的模組窗口",
+            "修復且優化了默認配置 (Fang2houUI) 的導入. 你可以在 [信息]-[重置] 中找到它.",
             "[美化皮膚] 新增了全新的字體子模組."
         },
         ["enUS"] = {
             "Thanks to the excellent work by 헬리오스의방패, 불광불급옹, 다크어쎄신 and 크림슨프릴. Korean is now fully supported.",
             "Add support of MerathilisUI Skins on frames created by WindTools.",
+            "Fixed and updated the default profile (Fang2houUI) importing. You can find it in [Information]-[Reset].",
             "[Skins] Add new font submodule."
         },
         ["koKR"] = {
             "이제 한국어가 완전히 지원됩니다. 헬리오스의방패, 불광불급옹, 다크어쎄신, 크림슨프릴의 훌륭한 작업 덕분입니다.",
             "WindTools의 모듈 프레임에 MerathilisUI 스킨 지원을 추가합니다.",
+            "Fixed and updated the default profile (Fang2houUI) importing. You can find it in [Information]-[Reset].",
             "[스킨] 새로운 글꼴 서브 모듈을 추가합니다."
         }
     },
@@ -82,7 +86,8 @@ W.Changelog[205] = {
             "[游戏条] 更换了炉石列表的生成逻辑.",
             "[美化皮肤] 优化了跳过过场动画的提示背景美化.",
             "[美化皮肤] 优化了鼠标提示美化.",
-            "[装备观察] 优化了人物头像的风格."
+            "[装备观察] 优化了人物头像的风格.",
+            "[好友列表] 支持新的 COD 游戏."
         },
         ["zhTW"] = {
             "媒體文件更新.",
@@ -91,7 +96,8 @@ W.Changelog[205] = {
             "[遊戲條] 更換了爐石列表的生成邏輯.",
             "[美化皮膚] 優化了跳過過場動畫的提示背景美化.",
             "[美化皮膚] 優化了浮動提示美化.",
-            "[裝備觀察] 優化了人物頭像的風格."
+            "[裝備觀察] 優化了人物頭像的風格.",
+            "[好友列表] 支援新的 COD 遊戲."
         },
         ["enUS"] = {
             "Media files updated.",
@@ -100,7 +106,8 @@ W.Changelog[205] = {
             "[Game Bar] New logic for generating the table of HearthStones.",
             "[Skins] Better skin for skip cut scene confirmation dialog.",
             "[Skins] The skin for tooltips have been optimized.",
-            "[Inspect] Better style for the portrait."
+            "[Inspect] Better style for the portrait.",
+            "[Friend List] Add support for new COD game."
         },
         ["koKR"] = {
             "미디어 파일이 업데이트되었습니다.",
@@ -109,7 +116,8 @@ W.Changelog[205] = {
             "[게임 바] 귀환석 목록 생성 로직이 변경되었습니다.",
             "[스킨] 컷신 건너 뛰기 확인 대화 상자의 스킨을 최적화 했습니다.",
             "[스킨] 툴팁 스킨이 최적화되었습니다.",
-            "[살펴보기] 캐릭터 초상화 스타일을 최적화했습니다."
+            "[살펴보기] 캐릭터 초상화 스타일을 최적화했습니다.",
+            "[Friend List] Add support for new COD game."
         }
     }
 }

04a69c7e12ea65dfe2d1f1f5ffd1746957055c22
Zhou Fang <fang2hou@outlook.com>
2020-10-30 11:19:11 +0900

    Init 2.06 changelog

---
+++ Core/Changelog/2.06.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[206] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "兼容性检查现可适用于任何版本的其他插件."
+        },
+        ["zhTW"] = {
+            "兼容性確認現在可以適用於任何版本的其他插件."
+        },
+        ["enUS"] = {
+            "Compatibility check now can work with any version of other addons."
+        },
+        ["koKR"] = {
+            "Compatibility check now can work with any version of other addons."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[天赋管家] 修复了有时候无法显示物品按钮的问题."
+        },
+        ["zhTW"] = {
+            "[天賦管家] 修復了有時候無法顯示物品按鍵的問題."
+        },
+        ["enUS"] = {
+            "[Talent Manager] Fix the bug that item buttons may not been displayed."
+        },
+        ["koKR"] = {
+            "[Talent Manager] Fix the bug that item buttons may not been displayed."
+        }
+    }
+}

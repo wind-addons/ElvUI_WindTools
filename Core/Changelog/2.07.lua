48ad005181c8ea4e4854ea2c1d3757d3ffa201e7
Zhou Fang <fang2hou@outlook.com>
2020-11-01 12:55:33 +0900

    Init 2.07 changelog

---
+++ Core/Changelog/2.07.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[207] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "清理代码",
+            "默认配置优化.",
+            "[额外物品条] 可以选择继承 ElvUI 全局渐隐了."
+        },
+        ["zhTW"] = {
+            "清理代碼",
+            "默認配置優化",
+            "[額外物品條] 可以選擇繼承 ElvUI 全局漸隱了."
+        },
+        ["enUS"] = {
+            "Cleanup codes.",
+            "Update default profile.",
+            "[Extra Item Bar] Now you can choose to inherit the ElvUI global fade."
+        },
+        ["koKR"] = {
+            "Cleanup codes.",
+            "Update default profile.",
+            "[Extra Item Bar] Now you can choose to inherit the ElvUI global fade."
+        }
+    }
+}

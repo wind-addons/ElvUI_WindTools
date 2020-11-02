c7ad28695a6b3994ebf1c4ad6b0d424d491ce442
Zhou Fang <fang2hou@outlook.com>
2020-11-02 16:59:45 +0900

    changelog

--- Core/Changelog/2.07.lua
+++ Core/Changelog/2.07.lua
@@ -29,7 +29,8 @@ W.Changelog[207] = {
             "兼容性检查工具更新.",
             "[好友列表] 修复了有时候无法获取名字时的报错.",
             "[其他] 跳过过场动画在跳过后会显示提示, 支持点击链接重播.",
-            "[额外物品条] 可以选择继承 ElvUI 全局渐隐了."
+            "[额外物品条] 可以选择继承 ElvUI 全局渐隐了.",
+            "[任务列表] 修复了修改风格可能带来的污染."
         },
         ["zhTW"] = {
             "清理代碼",
@@ -37,7 +38,8 @@ W.Changelog[207] = {
             "相容性確認工具更新.",
             "[好友名單] 修復了有時無法獲取名字時的報錯.",
             "[其他] 跳過過場動畫在跳過後會顯示提示, 支持點擊鏈接重播.",
-            "[額外物品條] 可以選擇繼承 ElvUI 全局漸隱了."
+            "[額外物品條] 可以選擇繼承 ElvUI 全局漸隱了.",
+            "[任務列表] 修復了修改風格可能帶來的污染."
         },
         ["enUS"] = {
             "Cleanup codes.",
@@ -45,7 +47,8 @@ W.Changelog[207] = {
             "Update compatibility check.",
             "[Friend List] Fix the bug occurred if the name cannot been obtained.",
             "[Misc] Skip Cut Scene will display a message after skipping, you can click the link inside the message to replay the cut scene.",
-            "[Extra Item Bar] Now you can choose to inherit the ElvUI global fade."
+            "[Extra Item Bar] Now you can choose to inherit the ElvUI global fade.",
+            "[Objective Tracker] Fix the taint by changing style."
         },
         ["koKR"] = {
             "코드 정리.",
@@ -53,7 +56,8 @@ W.Changelog[207] = {
             "호환성 검사 업데이트.",
             "[친구 목록] 가끔 이름을 얻을 수 없는 오류를 수정했습니다.",
             "[기타] 컷신을 건너뛰면 메시지가 표시되며 링크를 클릭하여 리플레이할 수 있습니다.",
-            "[아이템 바] ElvUI의 페이드 설정을 상속 하도록 선택할 수 있습니다."
+            "[아이템 바] ElvUI의 페이드 설정을 상속 하도록 선택할 수 있습니다.",
+            "[Objective Tracker] Fix the taint by changing style."
         }
     }
 }

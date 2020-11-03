a36e33203ddd9fefb4654e696b79c882efeec7e4
Zhou Fang <fang2hou@outlook.com>
2020-11-03 14:29:58 +0900

    changelog

--- Core/Changelog/2.07.lua
+++ Core/Changelog/2.07.lua
@@ -33,7 +33,9 @@ W.Changelog[207] = {
             "[任务列表] 修复了修改风格可能带来的污染.",
             "[切换按钮] 修复了有可能被暴雪追踪框体挡住的问题.",
             "[美化皮肤] 添加天赋界面的图标美化.",
-            "[移动框体] 重新启用地下城手册的移动."
+            "[移动框体] 重新启用地下城手册的移动.",
+            "[游戏条] 修复了音量的鼠标提示.",
+            "[天赋管家] 在传送后自动更新专精."
         },
         ["zhTW"] = {
             "清理代碼",
@@ -45,19 +47,23 @@ W.Changelog[207] = {
             "[任務列表] 修復了修改風格可能帶來的污染.",
             "[切換按鈕] 修復了有可能被暴雪追蹤框架擋住的問題.",
             "[美化皮膚] 新增了天賦介面的圖示美化.",
-            "[移動框架] 重新啟用地下城手冊的移動."
+            "[移動框架] 重新啟用地下城手冊的移動.",
+            "[遊戲條] 修復了音量的浮動提示.",
+            "[天賦管家] 在傳送後自動更新專精.",
         },
         ["enUS"] = {
             "Cleanup codes.",
             "Update default profile.",
             "Update compatibility check.",
-            "[Friend List] Fix the bug occurred if the name cannot been obtained.",
+            "[Friend List] Fix the bug occurred if the name cannot be obtained.",
             "[Misc] Skip Cut Scene will display a message after skipping, you can click the link inside the message to replay the cut scene.",
             "[Extra Item Bar] Now you can choose to inherit the ElvUI global fade.",
             "[Objective Tracker] Fix the taint by changing style.",
-            "[Switch Buttons] Fix the bug that sometimes it might be hided by Blizzard Objective Tracker.",
+            "[Switch Buttons] Fix the bug that sometimes it might be hidden by Blizzard Objective Tracker.",
             "[Skins] Apply new role icons in Talent UI.",
-            "[Move Frames] Reenable the moving of encounter journal."
+            "[Move Frames] Reenable the moving of encounter journal.",
+            "[Game Bar] Fix the tooltip of volume.",
+            "[Talent Manager] Auto-update the spec after transport."
         },
         ["koKR"] = {
             "코드 정리.",
@@ -69,7 +75,9 @@ W.Changelog[207] = {
             "[퀘스트 추적기] 스타일 수정으로 발생할 수 있는 오염 문제 수정",
             "[스위치 버튼] 블리자드 기본 퀘스트 추적기에 의해 차단될 수 있는 문제를 수정했습니다.",
             "[스킨] 특성 UI에 새로운 역할 아이콘을 적용합니다.",
-            "[프레임 이동] 모험 안내서 창 이동을 다시 활성화했습니다."
+            "[프레임 이동] 모험 안내서 창 이동을 다시 활성화했습니다.",
+            "[Game Bar] Fix the tooltip of volume.",
+            "[Talent Manager] Auto-update the spec after transport."
         }
     }
 }

3a1cd586afacc694c257604038d5c72f4aca64ac
Zhou Fang <fang2hou@outlook.com>
2020-11-05 17:40:41 +0900

    changelog

--- Core/Changelog/2.07.lua
+++ Core/Changelog/2.07.lua
@@ -18,16 +18,20 @@ W.Changelog[207] = {
     },
     NEW = {
         ["zhCN"] = {
-            "[美化皮肤] 添加了自定义厚实阴影的支持."
+            "[美化皮肤] 添加了自定义厚实阴影的支持.",
+            "[美化皮肤] 添加了暴雪按键绑定框体美化.",
         },
         ["zhTW"] = {
-            "[美化皮膚] 新增了自訂厚實陰影的支援."
+            "[美化皮膚] 新增了自訂厚實陰影的支援.",
+            "[美化皮膚] 新增了暴雪按鍵綁定框架美化.",
         },
         ["enUS"] = {
-            "[Skins] Add options for thicker shadow."
+            "[Skins] Add options for thicker shadow.",
+            "[Skins] Add new skin for Blizzard Binding UI."
         },
         ["koKR"] = {
-            "[스킨] 그림자 두께 조절 옵션을 추가합니다."
+            "[스킨] 그림자 두께 조절 옵션을 추가합니다.",
+            "[Skins] Add new skin for Blizzard Binding UI."
         }
     },
     IMPROVEMENT = {
@@ -43,13 +47,14 @@ W.Changelog[207] = {
             "[切换按钮] 修复了有可能被暴雪追踪框体挡住的问题.",
             "[美化皮肤] 添加天赋界面的图标美化.",
             "[美化皮肤] 微调 ElvUI 额外能量条的文字位置.",
+            "[美化皮肤] 移除 ElvUI 按键绑定皮肤.",
             "[移动框体] 重新启用地下城手册的移动.",
             "[游戏条] 修复了音量的鼠标提示.",
             "[天赋管家] 在传送后自动更新专精.",
             "[聊天条] 修复了一个潜在的错误.",
             "[聊天链接] 修复了一个潜在的错误.",
             "[世界地图] 更新了迷雾去除数据库.",
-            "[世界地图] 修复了部分地区因为迷雾去除无法显示地图的错误."
+            "[世界地图] 修复了部分地区因为迷雾去除无法显示地图的错误.",
         },
         ["zhTW"] = {
             "清理代碼",
@@ -63,6 +68,7 @@ W.Changelog[207] = {
             "[切換按鈕] 修復了有可能被暴雪追蹤框架擋住的問題.",
             "[美化皮膚] 新增了天賦介面的圖示美化.",
             "[美化皮膚] 微調 ElvUI 額外能量條的文字位置.",
+            "[美化皮膚] 移除 ElvUI 按鍵綁定皮膚.",
             "[移動框架] 重新啟用地下城手冊的移動.",
             "[遊戲條] 修復了音量的浮動提示.",
             "[天賦管家] 在傳送後自動更新專精.",
@@ -83,6 +89,7 @@ W.Changelog[207] = {
             "[Switch Buttons] Fix the bug that sometimes it might be hidden by Blizzard Objective Tracker.",
             "[Skins] Apply new role icons in Talent UI.",
             "[Skins] Modify the position of text in ElvUI alt power bar.",
+            "[Skins] Remove the skin for ElvUI key binding frame.",
             "[Move Frames] Reenable the moving of encounter journal.",
             "[Game Bar] Fix the tooltip of volume.",
             "[Talent Manager] Auto-update the spec after transport.",
@@ -103,6 +110,7 @@ W.Changelog[207] = {
             "[스위치 버튼] 블리자드 기본 퀘스트 추적기에 의해 차단될 수 있는 문제를 수정했습니다.",
             "[스킨] 특성 UI에 새로운 역할 아이콘을 적용합니다.",
             "[스킨] ElvUI 특수 자원 바의 텍스트의 위치를 미세 조정하였습니다.",
+            "[Skins] Remove the skin for ElvUI key binding frame.",
             "[프레임 이동] 모험 안내서 창 이동을 다시 활성화했습니다.",
             "[게임 바] 볼륨의 툴팁을 수정합니다.",
             "[특성 관리자] 전문화 변경 시 자동으로 업데이트됩니다.",

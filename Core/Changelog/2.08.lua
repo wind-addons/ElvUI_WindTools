f897b894bf4758c4129421876291e9c2d5dc2224
Zhou Fang <fang2hou@outlook.com>
2020-11-06 21:48:57 +0900

    changelog

--- Core/Changelog/2.08.lua
+++ Core/Changelog/2.08.lua
@@ -28,32 +28,36 @@ W.Changelog[208] = {
             "[其他] 新增了預組隊伍彩色定位圖示的功能."
         },
         ["enUS"] = {
-            "[Skins] Add skin for NetEase Meeting Stone.",
-            "[Skins] Add skin for NetEase Meeting Stone Plus.",
+            "[Skins] Add new skin for NetEase Meeting Stone.",
+            "[Skins] Add new skin for NetEase Meeting Stone Plus.",
             "[Misc] Add new feature that using a colorful icon replaces the Blizzard LFG role icons."
         },
         ["koKR"] = {
-            "[Skins] Add skin for NetEase Meeting Stone.",
-            "[Skins] Add skin for NetEase Meeting Stone Plus.",
+            "[Skins] Add new skin for NetEase Meeting Stone.",
+            "[Skins] Add new skin for NetEase Meeting Stone Plus.",
             "[Misc] Add new feature that using a colorful icon replaces the Blizzard LFG role icons."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "[队伍信息] 支持网易集合石.",
-            "[美化皮肤] 优化 Ace3 皮肤."
+            "[美化皮肤] 优化 Ace3 皮肤.",
+            "[美化皮肤] 优化职业大厅条皮肤.",
         },
         ["zhTW"] = {
             "[隊伍信息] 支援網易集合石.",
-            "[美化皮膚] 優化 Ace3 皮膚."
+            "[美化皮膚] 優化 Ace3 皮膚.",
+            "[美化皮膚] 優化職業大廳皮膚.",
         },
         ["enUS"] = {
             "[Party Info] Add support of NetEase Meeting Stone.",
-            "[Skins] Optimize Ace3 skins."
+            "[Skins] Optimize Ace3 skins.",
+            "[Skins] Optimize order hall bar skin.",
         },
         ["koKR"] = {
             "[Party Info] Add support of NetEase Meeting Stone.",
-            "[Skins] Optimize Ace3 skins."
+            "[Skins] Optimize Ace3 skins.",
+            "[Skins] Optimize order hall bar skin.",
         }
     }
 }

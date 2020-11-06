edc5fee617b59af8c8cbedf97da91d424c36ea23
Zhou Fang <fang2hou@outlook.com>
2020-11-06 20:21:18 +0900

    changelog

---
+++ Core/Changelog/2.08.lua
@@ -0,0 +1,43 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[208] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[美化皮肤] 添加了网易集合石的美化.",
+            "[其他] 添加了预组队伍彩色定位图标的功能."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了網易集合石的美化.",
+            "[其他] 新增了預組隊伍彩色定位圖示的功能."
+        },
+        ["enUS"] = {
+            "[Skins] Add skin for NetEase Meeting Stone.",
+            "[Misc] Add new feature that using a colorful icon replaces the Blizzard LFG role icons."
+        },
+        ["koKR"] = {
+            "[Skins] Add skin for NetEase Meeting Stone.",
+            "[Misc] Add new feature that using a colorful icon replaces the Blizzard LFG role icons."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[队伍信息] 支持网易集合石."
+        },
+        ["zhTW"] = {
+            "[隊伍信息] 支援網易集合石."
+        },
+        ["enUS"] = {
+            "[Party Info] Add support of NetEase Meeting Stone."
+        },
+        ["koKR"] = {
+            "[Party Info] Add support of NetEase Meeting Stone."
+        }
+    }
+}

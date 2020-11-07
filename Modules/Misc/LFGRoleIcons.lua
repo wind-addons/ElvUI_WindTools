e42f89b3bd182304199a185bca3d48b96b50ff12
Zhou Fang <fang2hou@outlook.com>
2020-11-07 21:02:55 +0900

    Also support for Meeting Stone

--- Modules/Misc/LFGRoleIcons.lua
+++ Modules/Misc/LFGRoleIcons.lua
@@ -3,8 +3,13 @@ local M = W:GetModule("Misc")
 local UF = E:GetModule("UnitFrames")
 
 local hooksecurefunc = hooksecurefunc
+local pairs = pairs
 local tinsert = tinsert
 local tremove = tremove
+local type = type
+
+local IsAddOnLoaded = IsAddOnLoaded
+local LibStub = LibStub
 
 local C_LFGList_GetSearchResultInfo = C_LFGList.GetSearchResultInfo
 local C_LFGList_GetSearchResultMemberInfo = C_LFGList.GetSearchResultMemberInfo
@@ -39,52 +44,72 @@ local function ReskinIcon(parent, icon, role, class)
 end
 
 function M:LFGRoleIcons()
-   if E.private.WT.misc.lfgRoleIcons then
-      hooksecurefunc(
-         "LFGListGroupDataDisplayEnumerate_Update",
-         function(Enumerate)
-            local button = Enumerate:GetParent():GetParent()
-            if not button.resultID then
-               return
-            end
+   if not E.private.WT.misc.lfgRoleIcons then
+      return
+   end
 
-            local result = C_LFGList_GetSearchResultInfo(button.resultID)
+   -- Raw hook for NetEase Meeting Stone libs
+   if IsAddOnLoaded("MeetingStone") or IsAddOnLoaded("MeetingStonePlus") then
+      local NetEaseEnv = LibStub("NetEaseEnv-1.0")
 
-            if not result then
-               return
-            end
-
-            local cache = {
-               TANK = {},
-               HEALER = {},
-               DAMAGER = {}
-            }
-
-            for i = 1, result.numMembers do
-               local role, class = C_LFGList_GetSearchResultMemberInfo(button.resultID, i)
-               tinsert(cache[role], class)
-            end
-
-            for i = 5, 1, -1 do -- The index of icon starts from right
-               local icon = Enumerate["Icon" .. i]
-               if icon and icon.SetTexture then
-                  if #cache.TANK > 0 then
-                     ReskinIcon(Enumerate, icon, "TANK", cache.TANK[1])
-                     tremove(cache.TANK, 1)
-                  elseif #cache.HEALER > 0 then
-                     ReskinIcon(Enumerate, icon, "HEALER", cache.HEALER[1])
-                     tremove(cache.HEALER, 1)
-                  elseif #cache.DAMAGER > 0 then
-                     ReskinIcon(Enumerate, icon, "DAMAGER", cache.DAMAGER[1])
-                     tremove(cache.DAMAGER, 1)
-                  else
-                     ReskinIcon(Enumerate, icon)
-                  end
+      for k in pairs(NetEaseEnv._NSInclude) do
+         if type(k) == "table" then
+            local module = k.Addon and k.Addon.GetClass and k.Addon:GetClass("MemberDisplay")
+            if module and module.SetActivity then
+               local original = module.SetActivity
+               module.SetActivity = function(self, activity)
+                  self.resultID = activity and activity.GetID and activity:GetID() or nil
+                  original(self, activity)
                end
             end
          end
-      )
+      end
    end
+
+   hooksecurefunc(
+      "LFGListGroupDataDisplayEnumerate_Update",
+      function(Enumerate)
+         local button = Enumerate:GetParent():GetParent()
+         if not button.resultID then
+            return
+         end
+
+         local result = C_LFGList_GetSearchResultInfo(button.resultID)
+
+         if not result then
+            return
+         end
+
+         local cache = {
+            TANK = {},
+            HEALER = {},
+            DAMAGER = {}
+         }
+
+         for i = 1, result.numMembers do
+            local role, class = C_LFGList_GetSearchResultMemberInfo(button.resultID, i)
+            tinsert(cache[role], class)
+         end
+
+         for i = 5, 1, -1 do -- The index of icon starts from right
+            local icon = Enumerate["Icon" .. i]
+            if icon and icon.SetTexture then
+               if #cache.TANK > 0 then
+                  ReskinIcon(Enumerate, icon, "TANK", cache.TANK[1])
+                  tremove(cache.TANK, 1)
+               elseif #cache.HEALER > 0 then
+                  ReskinIcon(Enumerate, icon, "HEALER", cache.HEALER[1])
+                  tremove(cache.HEALER, 1)
+               elseif #cache.DAMAGER > 0 then
+                  ReskinIcon(Enumerate, icon, "DAMAGER", cache.DAMAGER[1])
+                  tremove(cache.DAMAGER, 1)
+               else
+                  ReskinIcon(Enumerate, icon)
+               end
+            end
+         end
+      end
+   )
 end
 
 M:AddCallback("LFGRoleIcons")

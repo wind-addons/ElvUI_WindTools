0e4e6e3e63ca963ac6b6d54dacb83f3b923c04a9
Zhou Fang <fang2hou@outlook.com>
2020-11-07 16:53:42 +0900

    Ignore multiple events

--- Modules/Maps/WhoClicked.lua
+++ Modules/Maps/WhoClicked.lua
@@ -5,12 +5,11 @@ local _G = _G
 
 local max = max
 local select = select
+local tonumber = tonumber
 
 local UnitClass = UnitClass
 local UnitName = UnitName
 local InCombatLockdown = InCombatLockdown
-
-local C_Timer_After = C_Timer.After
 
 local hideTimes = 0
 function WC:TryFadeOut()
@@ -24,39 +23,52 @@ function WC:TryFadeOut()
     end
     hideTimes = max(hideTimes - 1, 0)
 end
-
-function WC:MINIMAP_PING(_, unit)
-    if self.db and self.db.onlyOnCombat and not InCombatLockdown() then
-        return
-    end
-
-    local englishClass = select(2, UnitClass(unit))
-    local name, realm = UnitName(unit)
-
-    if realm and self.db.addRealm and realm ~= E.myrealm then
-        name = name .. " - " .. realm
-    end
-
-    if self.db.classColor then
-        name = F.CreateClassColorString(name, englishClass)
-    else
-        name = F.CreateColorString(name, self.db.customColor)
-    end
-
-    self.text:SetText(name)
-    if self.db.fadeInTime == 0 then
-        self.text:Show()
-    else
-        E:UIFrameFadeIn(self.text, WC.db.fadeInTime, 0, 1)
-    end
-    hideTimes = hideTimes + 1
-
-    C_Timer_After(
-        WC.db.stayTime,
-        function()
-            WC:TryFadeOut()
+do
+    local temp = {}
+    function WC:MINIMAP_PING(_, unit, x, y)
+        if self.db and self.db.onlyOnCombat and not InCombatLockdown() then
+            return
         end
-    )
+
+        local time = time()
+        if temp then
+            if temp.unit == unit and temp.x == x and temp.y == y then
+                if time < 3 + temp.time then
+                    return
+                end
+            end
+        end
+
+        temp = {
+            unit = unit,
+            x = x,
+            y = y,
+            time = time
+        }
+
+        local englishClass = select(2, UnitClass(unit))
+        local name, realm = UnitName(unit)
+
+        if realm and self.db.addRealm and realm ~= E.myrealm then
+            name = name .. " - " .. realm
+        end
+
+        if self.db.classColor then
+            name = F.CreateClassColorString(name, englishClass)
+        else
+            name = F.CreateColorString(name, self.db.customColor)
+        end
+
+        self.text:SetText(name)
+        if self.db.fadeInTime == 0 then
+            self.text:Show()
+        else
+            E:UIFrameFadeIn(self.text, WC.db.fadeInTime, 0, 1)
+        end
+        hideTimes = hideTimes + 1
+
+        E:Delay(WC.db.stayTime, WC.TryFadeOut, WC)
+    end
 end
 
 function WC:UpdateText()

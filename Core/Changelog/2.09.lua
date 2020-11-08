fe2cd7fe2b46f4ce31c110a416870e96f18417cf
Zhou Fang <fang2hou@outlook.com>
2020-11-08 14:52:45 +0900

    changelog

---
+++ Core/Changelog/2.09.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[209] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[美化皮肤] 添加了 Premade Groups Filter 的美化."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了 Premade Groups Filter 的美化."
+        },
+        ["enUS"] = {
+            "[Skins] Add new skin for Premade Groups Filter."
+        },
+        ["koKR"] = {
+            "[스킨] Premade Groups Filter 애드온 스킨 추가."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[预组列表] 修复了图标背景无法正确隐藏的问题."
+        },
+        ["zhTW"] = {
+            "[預組列表] 修復了圖示背景無法正確隱藏的問題."
+        },
+        ["enUS"] = {
+            "[LFG List] Fix the bug that the backdrop of the icon cannot be hidden properly."
+        },
+        ["koKR"] = {
+            "[파티 찾기 목록] Fix the bug that the backdrop of the icon cannot be hidden properly."
+        }
+    }
+}

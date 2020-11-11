1a6be25f0720910b382260bba4ad02b803e7e1db
Zhou Fang <fang2hou@outlook.com>
2020-11-11 22:20:32 +0900

    Changelog

--- Core/Changelog/2.09.lua
+++ Core/Changelog/2.09.lua
@@ -30,22 +30,34 @@ W.Changelog[209] = {
         ["zhCN"] = {
             "兼容性检查工具更新.",
             "[预组列表] 修复了图标背景无法正确隐藏的问题.",
-            "[美化皮肤] 优化了网易集合石的美化."
+            "[美化皮肤] 优化了网易集合石的美化.",
+            "[通告] 回调队伍判定.",
+            "[小地图按钮] 除外 Narcissus 小地图图标. 感谢 @ryanfys",
+            "[小地图按钮] 添加对 BagSync 的支持. 感谢 @ryanfys"
         },
         ["zhTW"] = {
             "相容性確認工具更新.",
             "[預組列表] 修復了圖示背景無法正確隱藏的問題.",
-            "[美化皮膚] 優化網易集合石皮膚."
+            "[美化皮膚] 優化網易集合石皮膚.",
+            "[通告] 回調隊伍判定.",
+            "[小地圖按鍵] 除外 Narcissus 小地圖圖示. 感謝 @ryanfys",
+            "[小地圖按鍵] 添加對 BagSync 的支持. 感謝 @ryanfys"
         },
         ["enUS"] = {
             "Update compatibility check.",
             "[LFG List] Fix the bug that the backdrop of the icon cannot be hidden properly.",
-            "[Skins] Optimize NetEase Meeting Stone skin."
+            "[Skins] Optimize NetEase Meeting Stone skin.",
+            "[Announcement] Roll back the status detection of party.",
+            "[Minimap Buttons] Exclude Narcissus minimap icon. Thanks @ryanfys",
+            "[Minimap Buttons] Add support of BagSync. Thanks @ryanfys"
         },
         ["koKR"] = {
             "호환성 검사 업데이트.",
             "[파티 찾기 목록] 아이콘 배경이 제대로 숨겨지지 않던 문제를 수정했습니다.",
-            "[스킨] NetEase Meeting Stone 스킨 최적화."
+            "[스킨] NetEase Meeting Stone 스킨 최적화.",
+            "[Announcement] Roll back the status detection of party.",
+            "[Minimap Buttons] Exclude Narcissus minimap icon. Thanks @ryanfys",
+            "[Minimap Buttons] Add support of BagSync. Thanks @ryanfys"
         }
     }
 }

a43a1ca4fc26dfa7da8aaac5e4bbbc3ac733936e
Zhou Fang <fang2hou@outlook.com>
2020-11-13 14:59:01 +0900

    changelog

--- Core/Changelog/2.09.lua
+++ Core/Changelog/2.09.lua
@@ -19,19 +19,23 @@ W.Changelog[209] = {
     NEW = {
         ["zhCN"] = {
             "[美化皮肤] 添加了 Premade Groups Filter 的美化.",
-            "[美化皮肤] 添加了 REHack 的美化."
+            "[美化皮肤] 添加了 REHack 的美化.",
+            "[美化皮肤] 添加了神器界面的美化.",
         },
         ["zhTW"] = {
             "[美化皮膚] 新增了 Premade Groups Filter 的美化.",
-            "[美化皮膚] 新增了 REHack 的美化."
+            "[美化皮膚] 新增了 REHack 的美化.",
+            "[美化皮膚] 新增了神兵武器介面的美化.",
         },
         ["enUS"] = {
             "[Skins] Add new skin for Premade Groups Filter.",
-            "[Skins] Add new skin for REHack."
+            "[Skins] Add new skin for REHack.",
+            "[Skins] Add new skin for Artifact.",
         },
         ["koKR"] = {
             "[스킨] Premade Groups Filter 애드온 스킨 추가.",
-            "[스킨] REHack 애드온 스킨 추가."
+            "[스킨] REHack 애드온 스킨 추가.",
+            "[Skins] Add new skin for Artifact.",
         }
     },
     IMPROVEMENT = {

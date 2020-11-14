f5e9cb070ec2b48b05a6f5e5a52db44200606b5d
Zhou Fang <fang2hou@outlook.com>
2020-11-14 20:29:24 +0900

    changelog

--- Core/Changelog/2.10.lua
+++ Core/Changelog/2.10.lua
@@ -18,22 +18,26 @@ W.Changelog[210] = {
         ["zhCN"] = {
             "[小地图图标] 以新风格兼容 Narcissus 图标, 如果你不喜欢可以在 Narcissus 设置中取消加入条.",
             "[美化皮肤] 优化 WeakAuras 设定美化的性能.",
-            "[美化皮肤] 优化好友界面的美化."
+            "[美化皮肤] 优化好友界面的美化.",
+            "[美化皮肤] 优化社区界面的美化."
         },
         ["zhTW"] = {
             "[小地圖圖標] 以新風格兼容 Narcissus 圖標, 如果你不喜歡可以在 Narcissus 設置中取消加入條.",
             "[美化皮膚] 優化 WeakAuras 設定美化的性能.",
-            "[美化皮膚] 優化好友框架美化."
+            "[美化皮膚] 優化好友框架美化.",
+            "[美化皮膚] 優化社群框架美化."
         },
         ["enUS"] = {
             "[Minimap Buttons] Handle Narcissus icon with new style, you can disable handling in Narcissus configuration if you want.",
             "[Skins] Optimize the performance of WeakAuras Options skins.",
-            "[Skins] Optimize Friends skin."
+            "[Skins] Optimize Friends skin.",
+            "[Skins] Optimize Communities skin."
         },
         ["koKR"] = {
             "[Minimap Buttons] Handle Narcissus icon with new style, you can disable handling in Narcissus configuration if you want.",
             "[Skins] Optimize the performance of WeakAuras Options skins.",
-            "[Skins] Optimize Friends skin."
+            "[Skins] Optimize Friends skin.",
+            "[Skins] Optimize Communities skin."
         }
     }
 }

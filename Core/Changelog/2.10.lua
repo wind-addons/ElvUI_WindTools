213078c7068c1173a688eb36d4901b57e0688ff2
Zhou Fang <fang2hou@outlook.com>
2020-11-14 23:28:08 +0900

    disable announcement if solo

--- Core/Changelog/2.10.lua
+++ Core/Changelog/2.10.lua
@@ -20,28 +20,32 @@ W.Changelog[210] = {
             "[小地图图标] 以新风格兼容 Narcissus 图标, 如果你不喜欢可以在 Narcissus 设置中取消加入条.",
             "[美化皮肤] 优化 WeakAuras 设定美化的性能.",
             "[美化皮肤] 优化好友界面的美化.",
-            "[美化皮肤] 优化社区界面的美化."
+            "[美化皮肤] 优化社区界面的美化.",
+            "[通告] 单人情况下部分设定默认不再通报."
         },
         ["zhTW"] = {
             "[預組隊伍] 修復條顯示不正確的問題.",
             "[小地圖圖標] 以新風格兼容 Narcissus 圖標, 如果你不喜歡可以在 Narcissus 設置中取消加入條.",
             "[美化皮膚] 優化 WeakAuras 設定美化的性能.",
             "[美化皮膚] 優化好友框架美化.",
-            "[美化皮膚] 優化社群框架美化."
+            "[美化皮膚] 優化社群框架美化.",
+            "[通告] 單人情況下部分設定默認不再通報."
         },
         ["enUS"] = {
             "[LFG List] Fix the bug that the line does not hide properly.",
             "[Minimap Buttons] Handle Narcissus icon with the new style, you can disable handling in Narcissus configuration if you want.",
             "[Skins] Optimize the performance of WeakAuras Options skins.",
             "[Skins] Optimize Friends skin.",
-            "[Skins] Optimize Communities skin."
+            "[Skins] Optimize Communities skin.",
+            "[Announcement] Change the default channel setting of solo to NONE."
         },
         ["koKR"] = {
             "[LFG List] Fix the bug that the line does not hide properly.",
             "[Minimap Buttons] Handle Narcissus icon with the new style, you can disable handling in Narcissus configuration if you want.",
             "[Skins] Optimize the performance of WeakAuras Options skins.",
             "[Skins] Optimize Friends skin.",
-            "[Skins] Optimize Communities skin."
+            "[Skins] Optimize Communities skin.",
+            "[Announcement] Change the default channel setting of solo to NONE."
         }
     }
 }

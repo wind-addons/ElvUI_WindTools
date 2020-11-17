adb3bf34977a51468e2da45572a585e5054a2d51
Zhou Fang <fang2hou@outlook.com>
2020-11-18 01:24:36 +0900

    changelog

--- Core/Changelog/2.11.lua
+++ Core/Changelog/2.11.lua
@@ -17,18 +17,22 @@ W.Changelog[211] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "[美化皮肤] 优化了 Rematch 美化.",
+            "[美化皮肤] 修复了任务物品的美化.",
             "[游戏条] 公会按钮现在可以显示未读提示了."
         },
         ["zhTW"] = {
             "[美化皮膚] 優化了 Rematch 美化.",
+            "[美化皮膚] 修复了任務物品美化.",
             "[遊戲條] 公會按鍵現在可以顯示未讀提示了."
         },
         ["enUS"] = {
             "[Skins] Optimize Rematch skin.",
+            "[Skins] Fix the quest item skin.",
             "[Game Bar] Add an indicator of unread messages to Guild button."
         },
         ["koKR"] = {
             "[스킨] Rematch 스킨 최적화.",
+            "[Skins] Fix the quest item skin.",
             "[Game Bar] Add an indicator of unread messages to Guild button."
         }
     }

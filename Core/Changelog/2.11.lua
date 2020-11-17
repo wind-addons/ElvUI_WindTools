5c861e0f886c1ae1e317312aafcec6cf1a615612
Zhou Fang <fang2hou@outlook.com>
2020-11-17 23:37:44 +0900

    changelog

---
+++ Core/Changelog/2.11.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[211] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[游戏条] 公会按钮现在可以显示未读提示了."
+        },
+        ["zhTW"] = {
+            "[遊戲條] 公會按鍵現在可以顯示未讀提示了."
+        },
+        ["enUS"] = {
+            "[Game Bar] Add an indicator of unread messages to Guild button."
+        },
+        ["koKR"] = {
+            "[Game Bar] Add an indicator of unread messages to Guild button."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[美化皮肤] 优化了 Rematch 美化."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 優化了 Rematch 美化."
+        },
+        ["enUS"] = {
+            "[Skins] Optimize Rematch skin."
+        },
+        ["koKR"] = {
+            "[스킨] Rematch 스킨 최적화."
+        }
+    }
+}

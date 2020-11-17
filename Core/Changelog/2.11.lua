170a052b7fe2e765091dbbdf7b02a69ec7f1f4d0
Zhou Fang <fang2hou@outlook.com>
2020-11-17 23:43:36 +0900

    move to improvement

--- Core/Changelog/2.11.lua
+++ Core/Changelog/2.11.lua
@@ -9,31 +9,27 @@ W.Changelog[211] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {
-            "[游戏条] 公会按钮现在可以显示未读提示了."
-        },
-        ["zhTW"] = {
-            "[遊戲條] 公會按鍵現在可以顯示未讀提示了."
-        },
-        ["enUS"] = {
-            "[Game Bar] Add an indicator of unread messages to Guild button."
-        },
-        ["koKR"] = {
-            "[Game Bar] Add an indicator of unread messages to Guild button."
-        }
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "[美化皮肤] 优化了 Rematch 美化."
+            "[美化皮肤] 优化了 Rematch 美化.",
+            "[游戏条] 公会按钮现在可以显示未读提示了."
         },
         ["zhTW"] = {
-            "[美化皮膚] 優化了 Rematch 美化."
+            "[美化皮膚] 優化了 Rematch 美化.",
+            "[遊戲條] 公會按鍵現在可以顯示未讀提示了."
         },
         ["enUS"] = {
-            "[Skins] Optimize Rematch skin."
+            "[Skins] Optimize Rematch skin.",
+            "[Game Bar] Add an indicator of unread messages to Guild button."
         },
         ["koKR"] = {
-            "[스킨] Rematch 스킨 최적화."
+            "[스킨] Rematch 스킨 최적화.",
+            "[Game Bar] Add an indicator of unread messages to Guild button."
         }
     }
 }

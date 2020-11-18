b9f20c8c05a632e056ecfa8c659d4f93cc70aa36
Zhou Fang <fang2hou@outlook.com>
2020-11-18 20:05:51 +0900

    changelog

--- Core/Changelog/2.11.lua
+++ Core/Changelog/2.11.lua
@@ -9,35 +9,51 @@ W.Changelog[211] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[美化皮肤] 添加了公会邀请窗口的美化.",
+            "[美化皮肤] 添加了队伍等级同步窗口的美化.",
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了公會邀請窗口的美化.",
+            "[美化皮膚] 新增了隊伍等級同步窗口的美化.",
+        },
+        ["enUS"] = {
+            "[Skins] Add new skin for guild invite dialog.",
+            "[Skins] Add new skin for group level sync dialog.",
+        },
+        ["koKR"] = {
+            "[스킨] Add new skin for guild invite dialog.",
+            "[스킨] Add new skin for group level sync dialog.",
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "[交接] 优化了一些逻辑. 感谢 siweia @ NDui.",
             "[美化皮肤] 优化了 Rematch 美化.",
             "[美化皮肤] 修复了任务物品的美化.",
-            "[游戏条] 公会按钮现在可以显示未读提示了."
+            "[游戏条] 公会按钮现在可以显示未读提示了.",
+            "[任务列表] 修复了部分设定报错.",
         },
         ["zhTW"] = {
             "[交接] 優化了一些邏輯. 感谢 siweia @ NDui.",
             "[美化皮膚] 優化了 Rematch 美化.",
             "[美化皮膚] 修复了任務物品美化.",
-            "[遊戲條] 公會按鍵現在可以顯示未讀提示了."
+            "[遊戲條] 公會按鍵現在可以顯示未讀提示了.",
+            "[任務列表] 修復了部分設定報錯."
         },
         ["enUS"] = {
             "[Turn In] Optimize the logic of auto turn in. Thanks siweia @ NDui",
             "[Skins] Optimize Rematch skin.",
             "[Skins] Fix the quest item skin.",
-            "[Game Bar] Add an indicator of unread messages to Guild button."
+            "[Game Bar] Add an indicator of unread messages to Guild button.",
+            "[Objective Tracker] Fix some bugs in options."
         },
         ["koKR"] = {
             "[자동 수락] 자동 수락 로직을 최적화합니다. Thanks siweia @ NDui",
             "[스킨] Rematch 스킨 최적화.",
             "[스킨] 퀘스트 아이템 스킨 수정.",
-            "[게임 바] 읽지 않은 길드 메시지가 있는 경우 길드 버튼 위에 알림 아이콘을 표시합니다."
+            "[게임 바] 읽지 않은 길드 메시지가 있는 경우 길드 버튼 위에 알림 아이콘을 표시합니다.",
+            "[Objective Tracker] Fix some bugs in options."
         }
     }
 }

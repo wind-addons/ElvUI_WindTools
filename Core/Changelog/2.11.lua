e9b446e9d237bbfb17f87c1a0f2c7267310a9995
Zhou Fang <fang2hou@outlook.com>
2020-11-18 21:55:31 +0900

    changelog

--- Core/Changelog/2.11.lua
+++ Core/Changelog/2.11.lua
@@ -3,27 +3,35 @@ local W = unpack(select(2, ...))
 W.Changelog[211] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[地图]-[世界地图] 追踪点 => [地图]-[超级追踪], 并新增了一些自定义功能."
+        },
+        ["zhTW"] = {
+            "[地圖]-[世界地圖] 追蹤點 => [地圖]-[超級追蹤], 同時新增了一些自訂功能."
+        },
+        ["enUS"] = {
+            "[Maps]-[World Map] Waypoint => [Maps]-[Super Tracker], also some customizable options been added."
+        },
+        ["koKR"] = {
+            "[Maps]-[World Map] Waypoint => [Maps]-[Super Tracker], also some customizable options been added."
+        }
     },
     NEW = {
         ["zhCN"] = {
             "[美化皮肤] 添加了公会邀请窗口的美化.",
-            "[美化皮肤] 添加了队伍等级同步窗口的美化.",
+            "[美化皮肤] 添加了队伍等级同步窗口的美化."
         },
         ["zhTW"] = {
             "[美化皮膚] 新增了公會邀請窗口的美化.",
-            "[美化皮膚] 新增了隊伍等級同步窗口的美化.",
+            "[美化皮膚] 新增了隊伍等級同步窗口的美化."
         },
         ["enUS"] = {
             "[Skins] Add new skin for guild invite dialog.",
-            "[Skins] Add new skin for group level sync dialog.",
+            "[Skins] Add new skin for group level sync dialog."
         },
         ["koKR"] = {
             "[스킨] Add new skin for guild invite dialog.",
-            "[스킨] Add new skin for group level sync dialog.",
+            "[스킨] Add new skin for group level sync dialog."
         }
     },
     IMPROVEMENT = {
@@ -32,7 +40,7 @@ W.Changelog[211] = {
             "[美化皮肤] 优化了 Rematch 美化.",
             "[美化皮肤] 修复了任务物品的美化.",
             "[游戏条] 公会按钮现在可以显示未读提示了.",
-            "[任务列表] 修复了部分设定报错.",
+            "[任务列表] 修复了部分设定报错."
         },
         ["zhTW"] = {
             "[交接] 優化了一些邏輯. 感谢 siweia @ NDui.",

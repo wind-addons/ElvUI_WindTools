3d077fc68f7dac1effa797e150dd31176ac491f7
Zhou Fang <fang2hou@outlook.com>
2020-11-19 12:40:43 +0900

    changelog

--- Core/Changelog/2.11.lua
+++ Core/Changelog/2.11.lua
@@ -4,16 +4,24 @@ W.Changelog[211] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
-            "[地图]-[世界地图] 追踪点 => [地图]-[超级追踪]."
+            "支持 9.0.2 游戏版本.",
+            "[地图]-[世界地图] 追踪点 => [地图]-[超级追踪].",
+            "[删除] 由于暴雪 API 的改动, 按下 Delete 键后现在还需要点击确认."
         },
         ["zhTW"] = {
-            "[地圖]-[世界地圖] 追蹤點 => [地圖]-[超級追蹤]."
+            "支援 9.0.2 遊戲版本.",
+            "[地圖]-[世界地圖] 追蹤點 => [地圖]-[超級追蹤].",
+            "[刪除] 由於暴雪 API 的改動, 按下 Delete 鍵後現在仍舊需要點擊確認."
         },
         ["enUS"] = {
-            "[Maps]-[World Map] Waypoint => [Maps]-[Super Tracker]."
+            "Support WoW build 9.0.2.",
+            "[Maps]-[World Map] Waypoint => [Maps]-[Super Tracker].",
+            "[Delete] Due to the change of Blizzard API, after pressing Delete you also need to confirm by click."
         },
         ["koKR"] = {
-            "[지도]-[월드맵]의 <지도 핀> 기능이 [지도]-[슈퍼 트래커]의 새로운 기능으로 변경되었습니다."
+            "Support WoW build 9.0.2.",
+            "[지도]-[월드맵]의 <지도 핀> 기능이 [지도]-[슈퍼 트래커]의 새로운 기능으로 변경되었습니다.",
+            "[Delete] Due to the change of Blizzard API, after pressing Delete you also need to confirm by click."
         }
     },
     NEW = {
@@ -48,28 +56,36 @@ W.Changelog[211] = {
             "[美化皮肤] 优化了 Rematch 美化.",
             "[美化皮肤] 修复了任务物品的美化.",
             "[游戏条] 公会按钮现在可以显示未读提示了.",
-            "[任务列表] 修复了部分设定报错."
+            "[任务列表] 修复了部分设定报错.",
+            "[游戏条] 好友按钮现在可以在战斗中打开.",
+            "[好友列表] 重新制作了游戏图标, 添加 COD CW 及 WoW 暗影国度版本图标."
         },
         ["zhTW"] = {
             "[交接] 優化了一些邏輯. 感谢 siweia @ NDui.",
             "[美化皮膚] 優化了 Rematch 美化.",
             "[美化皮膚] 修复了任務物品美化.",
             "[遊戲條] 公會按鍵現在可以顯示未讀提示了.",
-            "[任務列表] 修復了部分設定報錯."
+            "[任務列表] 修復了部分設定報錯.",
+            "[遊戲條] 好友按鍵現在p可以在戰鬥中開啟介面.",
+            "[好友列表] 重新製作了遊戲圖示, 添加 COD CW 及 WoW 暗影之境版本圖示."
         },
         ["enUS"] = {
             "[Turn In] Optimize the logic of auto turn in. Thanks siweia @ NDui",
             "[Skins] Optimize Rematch skin.",
             "[Skins] Fix the quest item skin.",
             "[Game Bar] Add an indicator of unread messages to Guild button.",
-            "[Objective Tracker] Fix some bugs in options."
+            "[Objective Tracker] Fix some bugs in options.",
+            "[Game Bar] Friend button now can be used in combat.",
+            "[Friend List] Renew the game icons, add new icons for COD CW and WoW Shadowlands."
         },
         ["koKR"] = {
             "[자동 수락] 자동 수락 로직을 최적화합니다. Thanks siweia @ NDui",
             "[스킨] Rematch 스킨 최적화.",
             "[스킨] 퀘스트 아이템 스킨 수정.",
             "[게임 바] 읽지 않은 길드 메시지가 있는 경우 길드 버튼 위에 알림 아이콘을 표시합니다.",
-            "[퀘스트 추적기] 옵션의 일부 버그를 수정하였습니다."
+            "[퀘스트 추적기] 옵션의 일부 버그를 수정하였습니다.",
+            "[Game Bar] Friend button now can be used in combat.",
+            "[Friend List] Renew the game icons, add new icons for COD CW and WoW Shadowlands."
         }
     }
 }

dbc9756f96cd132308990ffa8b84a5c061e2518c
Zhou Fang <fang2hou@outlook.com>
2020-11-21 13:38:59 +0900

    changelog

--- Core/Changelog/2.12.lua
+++ Core/Changelog/2.12.lua
@@ -9,23 +9,51 @@ W.Changelog[212] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[标签] 添加了 [smart-power], 比如潜行者会自动变为完整能量(120), 法师会变为百分数(100%).",
+            "[标签] 添加了 [smart-power-nosign], 比如潜行者会自动变为完整能量(120), 法师会变为百分数(100)."
+        },
+        ["zhTW"] = {
+            "[標籤] 新增了 [smart-power], 盜賊等職業會自動變為完整格式(120), 法師會變為百分數(100%).",
+            "[標籤] 新增了 [smart-power-nosign], 盜賊等職業會自動變為完整格式(120), 法師會變為百分數(100)."
+        },
+        ["enUS"] = {
+            "[Tags] Add [smart-power], e.g. rogue is current power (120), mage is percentage(100%).",
+            "[Tags] Add [smart-power-nosign], e.g. rogue is current power (120), mage is percentage(100)."
+        },
+        ["koKR"] = {
+            "[Tags] Add [smart-power], e.g. rogue is current power (120), mage is percentage(100%).",
+            "[Tags] Add [smart-power-nosign], e.g. rogue is current power (120), mage is percentage(100)."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "[游戏条] 好友按钮回滚到上个版本."
+            "[矩形小地图] 修复了有时上方会有一条横线的问题.",
+            "[额外物品条] 就算条中没有按钮, 现在也会进行尺寸更新了.",
+            "[游戏条] 好友按钮回滚到上个版本.",
+            "[美化皮肤] 添加遗漏的 ElvUI 聊天面板选项.",
+            "[聊天链接] 修复了切换配置可能导致的错误."
         },
         ["zhTW"] = {
-            "[遊戲條] 好友按鍵回滾到上個版本."
+            "[矩形小地圖] 修復了有時上方會有一條橫線的問題.",
+            "[額外物品條] 就算條中沒有按鍵, 現在也會進行尺寸更新了.",
+            "[遊戲條] 好友按鍵回滾到上個版本.",
+            "[美化皮膚] 添加遺漏的 ElvUI 對話框架設置.",
+            "[聊天鏈接] 修復了切換配置可能導致的錯誤."
         },
         ["enUS"] = {
-            "[Game Bar] Roll back the change of Friends button."
+            "[Rectangle Minimap] Fix the bug that sometimes there is a line in the top of the minimap.",
+            "[Extra Item Bar] Resize the bar even if there is no button on it.",
+            "[Game Bar] Roll back the change of Friends button.",
+            "[Skins] Add the missing option of ElvUI Chat Panels.",
+            "[Chat Link] Fix the error occurred in switching profile."
         },
         ["koKR"] = {
-            "[게임 바] 친구 버튼이 이전 버전으로 롤백 되었습니다."
+            "[Rectangle Minimap] Fix the bug that sometimes there is a line in the top of the minimap.",
+            "[Extra Item Bar] Resize the bar even if there is no button on it.",
+            "[게임 바] 친구 버튼이 이전 버전으로 롤백 되었습니다.",
+            "[Skins] Add the missing option of ElvUI Chat Panels.",
+            "[Chat Link] Fix the error occurred in switching profile."
         }
     }
 }

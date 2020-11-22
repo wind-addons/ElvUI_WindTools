a65050f18a4c649cade890e9804182a10fca1c43
Zhou Fang <fang2hou@outlook.com>
2020-11-22 22:19:07 +0900

    changelog

--- Core/Changelog/2.12.lua
+++ Core/Changelog/2.12.lua
@@ -4,18 +4,22 @@ W.Changelog[212] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
-            "Tip: If you want to level up with Turn In, don't forget to uncheck the auto select reward!",
-            "Fang2hou UI have been updated! You can find import button in [Information]-[Reset]"
-        },
-        ["zhTW"] = {
+            "提醒: 如果你要看新的过场动画, 别忘了去 [其他] 关掉 [跳过过场动画]!",
             "提醒: 如果你打算使用自动交接来升级, 别忘了取消勾选自动选择奖励!",
             "Fang2hou UI 更新了! 你可以在 [信息]-[重置] 界面中找到导入按钮."
         },
-        ["enUS"] = {
+        ["zhTW"] = {
+            "提醒: 如果你要看新的過場動畫, 別忘記去 [其他] 關掉 [跳過過場動畫]!",
             "提醒: 如果你打算使用自動交接來升級, 別忘記取消勾選自動選擇獎勵!",
             "Fang2hou UI 更新了! 你可以在 [信息]-[重置] 介面中找到導入按鍵."
         },
+        ["enUS"] = {
+            "Tip: Don't forgot disabling the [Skip Cutscene] in [Misc], if you want to watch new scenes!",
+            "Tip: If you want to level up with Turn In, don't forget to uncheck the auto select reward!",
+            "Fang2hou UI have been updated! You can find import button in [Information]-[Reset]"
+        },
         ["koKR"] = {
+            "Tip: Don't forgot disabling the [Skip Cutscene] in [Misc], if you want to watch new scenes!",
             "팁: 레벨업 중 자동 수락을 사용할 생각이라면, 보상 자동 선택 옵션을 꺼두는 것을 잊지 마세요!",
             "Fang2hou UI가 업데이트되었습니다! 가져오기 버튼은 [정보]-[리셋]에서 찾을 수 있습니다."
         }
@@ -25,24 +29,28 @@ W.Changelog[212] = {
             "[标签] 添加了 [smart-power], 比如潜行者会自动变为完整能量(120), 法师会变为百分数(100%).",
             "[标签] 添加了 [smart-power-nosign], 比如潜行者会自动变为完整能量(120), 法师会变为百分数(100).",
             "[其他] 添加了在人物面板显示移动速度的功能.",
+            "[美化皮肤] 添加了 Myslot 的美化.",
             "添加 [地图]-[副本难度] 模块, 支持美化副本难度为文字风格. 感谢 Merathilis."
         },
         ["zhTW"] = {
             "[標籤] 新增了 [smart-power], 盜賊等職業會自動變為完整格式(120), 法師會變為百分數(100%).",
             "[標籤] 新增了 [smart-power-nosign], 盜賊等職業會自動變為完整格式(120), 法師會變為百分數(100).",
             "[其他] 新增了在人物面板顯示移動速度的功能.",
+            "[美化皮膚] 新增了 Myslot 的美化.",
             "新增 [地圖]-[副本難度] 模組, 支持美化副本難度為文字風格. 感謝 Merathilis."
         },
         ["enUS"] = {
             "[Tags] Add [smart-power], e.g. rogue is current power (120), mage is percentage(100%).",
             "[Tags] Add [smart-power-nosign], e.g. rogue is current power (120), mage is percentage(100).",
             "[Misc] Add new feature that showing the move speed in character panel.",
+            "[Skins] Add new skin for Myslot.",
             "New module [Maps]-[Instance Difficulty] supports reskin the difficulty indicator to text style. Thanks Merathilis."
         },
         ["koKR"] = {
             "[태그] 신규 태그 [smart-power]가 자원 카테고리에 추가되었습니다. 적용 예 : 도적은 (120), 마법사는 (100%)",
             "[태그] 신규 태그 [smart-power-nosign]이 자원 카테고리에 추가되었습니다. 적용 예 : 도적은 (120), 마법사는 (100)",
             "[기타] 캐릭터 창에 이동 속도를 보여주는 새로운 기능을 추가합니다.",
+            "[스킨] Myslot 애드온 스킨 추가.",
             "신규 모듈 [지도]-[인스턴스 난이도]가 추가되었습니다. 미니맵의 인스턴스 난이도를 문자 형식으로 변경합니다. Thanks Merathilis."
         }
     },

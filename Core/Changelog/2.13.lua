282ee60daf771e84fa0eda614d964055aef8db5b
Zhou Fang <fang2hou@outlook.com>
2020-11-24 23:47:12 +0900

    2.13 start

---
+++ Core/Changelog/2.13.lua
@@ -0,0 +1,31 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[213] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[重置] 更新 [其他] - [一般设定] 重置."
+        },
+        ["zhTW"] = {
+            "[重置] 更新 [其他] - [一般设定] 重置."
+        },
+        ["enUS"] = {
+            "[Reset] Update [Misc] - [General] resetting."
+        },
+        ["koKR"] = {
+            "[Reset] Update [Misc] - [General] resetting."
+        }
+    }
+}

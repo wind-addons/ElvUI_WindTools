a1f617569ae69535ad34dc8cf1769236d18776c0
Zhou Fang <fang2hou@outlook.com>
2020-11-25 18:47:39 +0900

    changelog

--- Core/Changelog/2.13.lua
+++ Core/Changelog/2.13.lua
@@ -10,37 +10,64 @@ W.Changelog[213] = {
     },
     NEW = {
         ["zhCN"] = {
-            "[任务列表] 提供一个显示爬塔 Buff 在右边的选项."
+            "[任务列表] 提供一个显示爬塔 Buff 在右边的选项.",
+            "[美化皮肤] 添加了盟约预览界面的美化.",
+            "[美化皮肤] 添加了盟约圣所界面的美化.",
+            "[美化皮肤] 添加了盟约名望界面的美化.",
+            "[美化皮肤] 添加了灵魂羁绊界面的美化."
         },
         ["zhTW"] = {
-            "[任務列表] 提供一個顯示爬塔 Buff 在右邊的選項."
+            "[任務列表] 提供一個顯示爬塔 Buff 在右邊的選項.",
+            "[美化皮膚] 新增了誓盟預覽介面的美化.",
+            "[美化皮膚] 新增了誓盟聖所介面的美化.",
+            "[美化皮膚] 新增了誓盟名望介面的美化.",
+            "[美化皮膚] 新增了魂絆介面的美化."
         },
         ["enUS"] = {
-            "[Objective Frame] Add new option for moving maw buff frame to right."
+            "[Objective Frame] Add new option for moving maw buff frame to right.",
+            "[Skins] Add new skin for Blizzard Covenant Preview.",
+            "[Skins] Add new skin for Blizzard Covenant Renown.",
+            "[Skins] Add new skin for Blizzard Covenant Sanctum.",
+            "[Skins] Add new skin for Blizzard Soulbinds."
         },
         ["koKR"] = {
-            "[Objective Frame] Add new option for moving maw buff frame to right."
+            "[Objective Frame] Add new option for moving maw buff frame to right.",
+            "[Skins] Add new skin for Blizzard Covenant Preview.",
+            "[Skins] Add new skin for Blizzard Covenant Renown.",
+            "[Skins] Add new skin for Blizzard Covenant Sanctum.",
+            "[Skins] Add new skin for Blizzard Soulbinds."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "[美化皮肤] 优化了托加斯特能力选择界面的美化.",
+            "[美化皮肤] 优化了对话界面的美化.",
+            "[美化皮肤] 优化了任务界面的美化.",
             "[天赋管家] 修复了战斗中打开可能造成的污染.",
+            "[天赋管家] 从格里恩誓约仆从获得天赋重置 Buff 时, 现在可以正确的更新状态图标了.",
             "[重置] 更新 [其他] - [一般设定] 重置."
         },
         ["zhTW"] = {
             "[美化皮膚] 優化了托迦司能力選擇介面的美化.",
+            "[美化皮膚] 優化了對話介面的美化.",
+            "[美化皮膚] 優化了任務介面的美化.",
             "[天賦管家] 修復了戰鬥中開啟可能造成的污染.",
+            "[天賦管家] 從琪瑞安誓盟僕從處獲得天賦重置 Buff 時, 現在可以正確的更新狀態圖示了.",
             "[重置] 更新 [其他] - [一般设定] 重置."
         },
         ["enUS"] = {
             "[Skins] Optimize the skins on Torghast ability choice.",
+            "[Skins] Optimize the skins on Blizzard Quest Frame.",
+            "[Skins] Optimize the skins on Blizzard Gossip Frame.",
             "[Talent Manager] Fix the taint may occurred when you toggle talent UI in combat",
             "[Reset] Update [Misc] - [General] resetting."
         },
         ["koKR"] = {
             "[Skins] Optimize the skins on Torghast ability choice.",
+            "[Skins] Optimize the skins on Blizzard Quest Frame.",
+            "[Skins] Optimize the skins on Blizzard Gossip Frame.",
             "[Talent Manager] Fix the taint may occurred when you toggle talent UI in combat",
+            "[Talent Manager] Now after gains the buff from the Kyrian Steward, the status icon will be updated.",
             "[Reset] Update [Misc] - [General] resetting."
         }
     }

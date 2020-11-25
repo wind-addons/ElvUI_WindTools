95959e91a79fbccbedde21193fe6bbd713db0525
Zhou Fang <fang2hou@outlook.com>
2020-11-25 15:03:14 +0900

    changelog

--- Core/Changelog/2.13.lua
+++ Core/Changelog/2.13.lua
@@ -9,22 +9,38 @@ W.Changelog[213] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[任务列表] 提供一个显示爬塔 Buff 在右边的选项."
+        },
+        ["zhTW"] = {
+            "[任務列表] 提供一個顯示爬塔 Buff 在右邊的選項."
+        },
+        ["enUS"] = {
+            "[Objective Frame] Add new option for moving maw buff frame to right."
+        },
+        ["koKR"] = {
+            "[Objective Frame] Add new option for moving maw buff frame to right."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[美化皮肤] 优化了托加斯特能力选择界面的美化.",
+            "[天赋管家] 修复了战斗中打开可能造成的污染.",
             "[重置] 更新 [其他] - [一般设定] 重置."
         },
         ["zhTW"] = {
+            "[美化皮膚] 優化了托迦司能力選擇介面的美化.",
+            "[天賦管家] 修復了戰鬥中開啟可能造成的污染.",
             "[重置] 更新 [其他] - [一般设定] 重置."
         },
         ["enUS"] = {
+            "[Skins] Optimize the skins on Torghast ability choice.",
+            "[Talent Manager] Fix the taint may occurred when you toggle talent UI in combat",
             "[Reset] Update [Misc] - [General] resetting."
         },
         ["koKR"] = {
+            "[Skins] Optimize the skins on Torghast ability choice.",
+            "[Talent Manager] Fix the taint may occurred when you toggle talent UI in combat",
             "[Reset] Update [Misc] - [General] resetting."
         }
     }

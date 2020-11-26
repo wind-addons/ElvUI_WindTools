242a24984f778d900416cdb1983dbc9ab66eeaf0
Zhou Fang <fang2hou@outlook.com>
2020-11-26 20:36:17 +0900

    changelog

--- Core/Changelog/2.13.lua
+++ Core/Changelog/2.13.lua
@@ -15,7 +15,8 @@ W.Changelog[213] = {
             "[美化皮肤] 添加了盟约预览界面的美化.",
             "[美化皮肤] 添加了盟约圣所界面的美化.",
             "[美化皮肤] 添加了盟约名望界面的美化.",
-            "[美化皮肤] 添加了灵魂羁绊界面的美化."
+            "[美化皮肤] 添加了灵魂羁绊界面的美化.",
+            "[美化皮肤] 添加了心能传导器界面的美化."
         },
         ["zhTW"] = {
             "[任務列表] 提供一個顯示爬塔 Buff 在右邊的選項. 默認關閉",
@@ -23,7 +24,8 @@ W.Changelog[213] = {
             "[美化皮膚] 新增了誓盟預覽介面的美化.",
             "[美化皮膚] 新增了誓盟聖所介面的美化.",
             "[美化皮膚] 新增了誓盟名望介面的美化.",
-            "[美化皮膚] 新增了魂絆介面的美化."
+            "[美化皮膚] 新增了魂絆介面的美化.",
+            "[美化皮膚] 新增了靈魄傳導器介面的美化."
         },
         ["enUS"] = {
             "[Objective Frame] Add new option for moving maw buff frame to right. Disabled by default.",
@@ -31,7 +33,8 @@ W.Changelog[213] = {
             "[Skins] Add new skin for Blizzard Covenant Preview.",
             "[Skins] Add new skin for Blizzard Covenant Renown.",
             "[Skins] Add new skin for Blizzard Covenant Sanctum.",
-            "[Skins] Add new skin for Blizzard Soulbinds."
+            "[Skins] Add new skin for Blizzard Soulbinds.",
+            "[Skins] Add new skin for Blizzard Anima Diversion."
         },
         ["koKR"] = {
             "[퀘스트 추적기] 령 능력 버프 창을 오른쪽으로 열어주는 새로운 옵션을 추가합니다. 기본적으로 비활성화되어 있습니다.",
@@ -39,7 +42,8 @@ W.Changelog[213] = {
             "[스킨] Blizzard 성약단 공물 보기 프레임에 새로운 스킨을 추가합니다.",
             "[스킨] Blizzard 성약단 영예 프레임에 새로운 스킨을 추가합니다.",
             "[스킨] Blizzard 성약단 성소 프레임에 새로운 스킨을 추가합니다.",
-            "[스킨] Blizzard 성약단 영혼결속 프레임에 새로운 스킨을 추가합니다."
+            "[스킨] Blizzard 성약단 영혼결속 프레임에 새로운 스킨을 추가합니다.",
+            "[Skins] Add new skin for Blizzard Anima Diversion."
         }
     },
     IMPROVEMENT = {
@@ -47,6 +51,8 @@ W.Changelog[213] = {
             "[美化皮肤] 优化了托加斯特能力选择界面的美化.",
             "[美化皮肤] 优化了对话界面的美化.",
             "[美化皮肤] 优化了任务界面的美化.",
+            "[美化皮肤] 优化了死亡回顾界面的美化.",
+            "[美化皮肤] 优化了飞行地图界面的美化.",
             "[天赋管家] 修复了战斗中打开可能造成的污染.",
             "[天赋管家] 从格里恩誓约仆从获得天赋重置 Buff 时, 现在可以正确的更新状态图标了.",
             "[矩形小地图] 优化了托加斯特内的显示.",
@@ -56,6 +62,8 @@ W.Changelog[213] = {
             "[美化皮膚] 優化了托迦司能力選擇介面的美化.",
             "[美化皮膚] 優化了對話介面的美化.",
             "[美化皮膚] 優化了任務介面的美化.",
+            "[美化皮膚] 優化了死亡回顧介面的美化.",
+            "[美化皮膚] 優化了飛行地圖介面的美化.",
             "[天賦管家] 修復了戰鬥中開啟可能造成的污染.",
             "[天賦管家] 從琪瑞安誓盟僕從處獲得天賦重置 Buff 時, 現在可以正確的更新狀態圖示了.",
             "[矩形小地圖] 優化了托迦司內的顯示.",
@@ -63,8 +71,10 @@ W.Changelog[213] = {
         },
         ["enUS"] = {
             "[Skins] Optimize the skins on Torghast ability choice.",
-            "[Skins] Optimize the skins on Blizzard Quest Frame.",
             "[Skins] Optimize the skins on Blizzard Gossip Frame.",
+            "[Skins] Optimize the skins on Blizzard Quest Frame.",
+            "[Skins] Optimize the skins on Blizzard Death Recap Frame.",
+            "[Skins] Optimize the skins on Blizzard FlightMap Frame.",
             "[Talent Manager] Fix the taint may occur when you toggle talent UI in combat",
             "[Talent Manager] Now after gains the buff from the Kyrian Steward, the status icon will be updated.",
             "[Rectangle Minimap] Optimize the display in Torghast.",
@@ -72,8 +82,10 @@ W.Changelog[213] = {
         },
         ["koKR"] = {
             "[스킨] 토르가스트 령 능력 선택 스킨을 최적화 합니다.",
-            "[스킨] Blizzard 퀘스트 프레임 스킨을 최적화 합니다.",
             "[스킨] Blizzard NPC 대화 창 스킨을 최적화 합니다.",
+            "[스킨] Blizzard 퀘스트 프레임 스킨을 최적화 합니다.",
+            "[Skins] Optimize the skins on Blizzard Death Recap Frame.",
+            "[Skins] Optimize the skins on Blizzard FlightMap Frame.",
             "[특성 관리자] 전투 중 특성 UI를 전환할 때 오류가 발생할 수 있는 문제를 수정하였습니다.",
             "[특성 관리자] 이제 키리안 청지기에게 특성 변경 버프를 받을 때 상태 아이콘이 올바르게 업데이트됩니다.",
             "[미니맵 비율 조정] 토르가스트에서 디스플레이를 최적화했습니다.",

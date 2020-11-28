f9f6db15888cb2769490fb36b9d5a4be28a4fd76
Zhou Fang <fang2hou@outlook.com>
2020-11-28 12:54:07 +0900

    changelog

--- Core/Changelog/2.14.lua
+++ Core/Changelog/2.14.lua
@@ -15,9 +15,21 @@ W.Changelog[214] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[进度] 修复了赤红深渊的传奇击杀次数统计.",
+            "[额外物品条] 修复了设定时鼠标提示中法师制作食物的文本."
+        },
+        ["zhTW"] = {
+            "[進度] 修復了血紅深淵的傳奇極差次數統計.",
+            "[額外物品條] 修復了設定時浮動提示中法師製造食物的文字."
+        },
+        ["enUS"] = {
+            "[Progression] Fix the statastic of Sanguine Depths.",
+            "[Extra Item Bar] Fix the text of food crafted by mage in item group tooltips."
+        },
+        ["koKR"] = {
+            "[Progression] Fix the statastic of Sanguine Depths.",
+            "[Extra Item Bar] Fix the text of food crafted by mage in item group tooltips."
+        }
     }
 }

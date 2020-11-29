25092d34952184cd10edb00b10e6ecbff1bcfd21
Zhou Fang <fang2hou@outlook.com>
2020-11-29 10:27:01 +0900

    changelog

--- Core/Changelog/2.14.lua
+++ Core/Changelog/2.14.lua
@@ -17,19 +17,35 @@ W.Changelog[214] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "[进度] 修复了赤红深渊的传奇击杀次数统计.",
-            "[额外物品条] 修复了设定时鼠标提示中法师制作食物的文本."
+            "[额外物品条] 修复了设定时鼠标提示中法师制作食物的文本.",
+            "[美化皮肤] 优化了小宠物鼠标提示的美化.",
+            "[美化皮肤] 优化了 LFD 准备提示的美化.",
+            "[聊天条] 修复了一个潛在的报错.",
+            "[交接] 修复了一个潛在的报错."
         },
         ["zhTW"] = {
             "[進度] 修復了血紅深淵的傳奇極差次數統計.",
-            "[額外物品條] 修復了設定時浮動提示中法師製造食物的文字."
+            "[額外物品條] 修復了設定時浮動提示中法師製造食物的文字.",
+            "[美化皮膚] 優化了小寵物浮動提示的美化.",
+            "[美化皮膚] 優化了 LFD 準備提示的美化.",
+            "[聊天條] 修復了一個潛在的錯誤.",
+            "[交接] 修復了一個潛在的錯誤."
         },
         ["enUS"] = {
             "[Progression] Fix the statastic of Sanguine Depths.",
-            "[Extra Item Bar] Fix the text of food crafted by mage in item group tooltips."
+            "[Extra Item Bar] Fix the text of food crafted by mage in item group tooltips.",
+            "[Skins] Optimize the skins on Pet Tooltip.",
+            "[Skins] Optimize the skins on LFD Ready Popup.",
+            "[Chat Bar] Fix a potential bug.",
+            "[Turn In] Fix a potential bug."
         },
         ["koKR"] = {
             "[진행 상태] 핏빛 심연의 통계치를 수정합니다.",
-            "[아이템 바] 아이템 그룹 툴팁에서 마법사 창조 음식 텍스트를 수정했습니다."
+            "[아이템 바] 아이템 그룹 툴팁에서 마법사 창조 음식 텍스트를 수정했습니다.",
+            "[Skins] Optimize the skins on Pet Tooltip.",
+            "[Skins] Optimize the skins on LFD Ready Popup.",
+            "[Chat Bar] Fix a potential bug.",
+            "[Turn In] Fix a potential bug."
         }
     }
 }

540744acc0f4850eef92f8b4c23f4dd98cd8516b
Zhou Fang <fang2hou@outlook.com>
2020-12-04 11:27:29 +0900

    changelog

--- Core/Changelog/2.14.lua
+++ Core/Changelog/2.14.lua
@@ -11,15 +11,19 @@ W.Changelog[214] = {
     NEW = {
         ["zhCN"] = {
             "[额外物品条] 添加了塔内坐骑.",
+            '[额外物品条] 添加了 "FOODVENDOR" (商人食物) 物品分类.'
         },
         ["zhTW"] = {
-            "[額外物品條] 新增了塔內坐騎."
+            "[額外物品條] 新增了塔內坐騎.",
+            '[額外物品條] 新增了 "FOODVENDOR" (商人食物) 物品分組.'
         },
         ["enUS"] = {
-            "[Extra Item Bar] Add the mount in Torghast."
+            "[Extra Item Bar] Add the mount in Torghast.",
+            '[Extra Item Bar] Add new item group "FOODVENDOR" (food sold by vendors).'
         },
         ["koKR"] = {
-            "[아이템 바] 토르가스트 탈것 추가"
+            "[아이템 바] 토르가스트 탈것 추가",
+            '[Extra Item Bar] Add new item group "FOODVENDOR" (food sold by vendors).'
         }
     },
     IMPROVEMENT = {
@@ -28,32 +32,40 @@ W.Changelog[214] = {
             "[额外物品条] 修复了设定时鼠标提示中法师制作食物的文本.",
             "[美化皮肤] 优化了小宠物鼠标提示的美化.",
             "[美化皮肤] 优化了 LFD 准备提示的美化.",
+            "[美化皮肤] 修复了盟约任务完成提示框体图标不显示的错误.",
             "[聊天条] 修复了一个潛在的报错.",
-            "[交接] 修复了一个潛在的报错."
+            "[交接] 修复了一个潛在的报错.",
+            "[游戏条] 现在可以设定家按钮为虫洞发生器了."
         },
         ["zhTW"] = {
             "[進度] 修復了血紅深淵的傳奇極差次數統計.",
             "[額外物品條] 修復了設定時浮動提示中法師製造食物的文字.",
             "[美化皮膚] 優化了小寵物浮動提示的美化.",
             "[美化皮膚] 優化了 LFD 準備提示的美化.",
+            "[美化皮膚] 修復了誓盟任務完成提示框架圖示不顯示的錯誤.",
             "[聊天條] 修復了一個潛在的錯誤.",
-            "[交接] 修復了一個潛在的錯誤."
+            "[交接] 修復了一個潛在的錯誤.",
+            "[遊戲條] 現在可以設定按鈕為蟲洞產生器了."
         },
         ["enUS"] = {
             "[Progression] Fix the statastic of Sanguine Depths.",
             "[Extra Item Bar] Fix the text of food crafted by mage in item group tooltips.",
             "[Skins] Optimize the skins on Pet Tooltip.",
             "[Skins] Optimize the skins on LFD Ready Popup.",
+            "[Skins] Fix the icon in misson complate alerts.",
             "[Chat Bar] Fix a potential bug.",
-            "[Turn In] Fix a potential bug."
+            "[Turn In] Fix a potential bug.",
+            "[Game Bar] You can set Home button to wormhole generators now."
         },
         ["koKR"] = {
             "[진행 상태] 핏빛 심연의 통계치를 수정합니다.",
             "[아이템 바] 아이템 그룹 툴팁에서 마법사 창조 음식 텍스트를 수정했습니다.",
             "[스킨] 펫 툴팁 최적화.",
             "[스킨] 던전 찾기 준비 확인 팝업 스킨 최적화.",
+            "[Skins] Fix the icon in misson complate alerts.",
             "[채팅 바] 잠재적인 버그를 수정하였습니다.",
-            "[자동 수락] 잠재적인 버그를 수정하였습니다."
+            "[자동 수락] 잠재적인 버그를 수정하였습니다.",
+            "[Game Bar] You can set Home button to wormhole generators now."
         }
     }
 }

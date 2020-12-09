5c6014745a570c1cdc52152d5810292ec59c17ed
Zhou Fang <fang2hou@outlook.com>
2020-12-09 15:23:26 +0900

    2.15 changelog init

---
+++ Core/Changelog/2.15.lua
@@ -0,0 +1,43 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[215] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[副本难度] 添加了一个文字对齐选项."
+        },
+        ["zhTW"] = {
+            "[副本難度] 新增了一個文字對齊選項."
+        },
+        ["enUS"] = {
+            "[Instance Difficulty] Add a new option of text-align."
+        },
+        ["koKR"] = {
+            "[Instance Difficulty] Add a new option of text-align."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "修正了一些之前更新记录的文本.",
+            "[额外物品条] UTILITY (实用工具) 分组新增了护甲片等暗影国度制作物品."
+        },
+        ["zhTW"] = {
+            "修正了一些之前的更新記錄的文本.",
+            "[額外物品條] UTILITY (實用工具) 分組新增了護甲片等暗影之境製造物品."
+        },
+        ["enUS"] = {
+            "Amend some parts of the previous changelog.",
+            "[Extra Item Bar] UTILITY (utilities) category add some Shadowlands crafting items like armor kit."
+        },
+        ["koKR"] = {
+            "Amend some parts of the previous changelog.",
+            "[Extra Item Bar] UTILITY (utilities) category add some Shadowlands crafting items like armor kit."
+        }
+    }
+}

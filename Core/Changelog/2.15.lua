e4c7b24627a1bccbbe95762709f14252f0261cee
Zhou Fang <fang2hou@outlook.com>
2020-12-11 14:50:14 +0900

    changelog

--- Core/Changelog/2.15.lua
+++ Core/Changelog/2.15.lua
@@ -25,19 +25,27 @@ W.Changelog[215] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "修正了一些之前更新记录的文本.",
-            "[额外物品条] UTILITY (实用工具) 分组新增了护甲片等暗影国度制作物品."
+            "[美化皮肤] 修复了团队工具皮肤可能造成的错误.",
+            "[额外物品条] UTILITY (实用工具) 分组新增了护甲片等暗影国度制作物品.",
+            "[观察] 修复了暗影国度的附魔信息. 感谢 loudsoul"
         },
         ["zhTW"] = {
             "修正了一些之前的更新記錄的文本.",
-            "[額外物品條] UTILITY (實用工具) 分組新增了護甲片等暗影之境製造物品."
+            "[美化皮膚] 修復了團隊工具皮膚可能造成的錯誤.",
+            "[額外物品條] UTILITY (實用工具) 分組新增了護甲片等暗影之境製造物品.",
+            "[觀察] 修復了暗影之境的附魔訊息. 感謝 loudsoul"
         },
         ["enUS"] = {
             "Amend some parts of the previous changelog.",
-            "[Extra Item Bar] UTILITY (utilities) category add some Shadowlands crafting items like armor kit."
+            "[Skins] Fix a potential bug of Raid Utility skin.",
+            "[Extra Item Bar] UTILITY (utilities) category add some Shadowlands crafting items like armor kit.",
+            "[Inpect] Fix the Shadowlands enchant info. Thanks loudsoul."
         },
         ["koKR"] = {
             "이전 변경 로그의 일부 수정.",
-            "[아이템 바] UTILITY (유틸리티) 카테고리에 방어구 강화, 기름, 숫돌 같은 어둠땅 제작 아이템이 추가 되였습니다."
+            "[Skins] Fix a potential bug of Raid Utility skin.",
+            "[아이템 바] UTILITY (유틸리티) 카테고리에 방어구 강화, 기름, 숫돌 같은 어둠땅 제작 아이템이 추가 되였습니다.",
+            "[Inpect] Fix the Shadowlands enchant info. Thanks loudsoul."
         }
     }
 }

16ec8352a67de0968a2bde1e100f053796f8c560
Zhou Fang <fang2hou@outlook.com>
2020-12-14 16:04:53 +0900

    changelog

--- Core/Changelog/2.15.lua
+++ Core/Changelog/2.15.lua
@@ -10,41 +10,65 @@ W.Changelog[215] = {
     },
     NEW = {
         ["zhCN"] = {
+            "[美化皮肤] 添加了每周奖励宝库的界面美化.",
             "[副本难度] 添加了一个文字对齐选项."
         },
         ["zhTW"] = {
+            "[美化皮膚] 新增了每週獎勵寶庫的界面美化.",
             "[副本難度] 新增了一個文字對齊選項."
         },
         ["enUS"] = {
+            "[Skins] Add skin for weekly rewards.",
             "[Instance Difficulty] Add a new option of text-align."
         },
         ["koKR"] = {
+            "[Skins] Add skin for weekly rewards.",
             "[인스턴스 난이도] 텍스트 정렬 옵션을 추가하였습니다."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "修正了一些之前更新记录的文本.",
+            "[通告] 修复了在离队后可能出现的错误.",
+            "[游戏条] 右键冒险手册现在将会打开每周奖励宝库.",
+            "[移动框体] 添加了每周奖励宝库界面的支持.",
             "[美化皮肤] 修复了团队工具皮肤可能造成的错误.",
+            "[美化皮肤] 优化了传奇钥石界面美化.",
             "[额外物品条] UTILITY (实用工具) 分组新增了护甲片等暗影国度制作物品.",
+            "[额外物品条] 更新托加斯特物品列表.",
             "[观察] 修复了暗影国度的附魔信息. 感谢 loudsoul"
         },
         ["zhTW"] = {
             "修正了一些之前的更新記錄的文本.",
+            "[通告] 修復了可能出現的離隊後錯誤.",
+            "[遊戲條] 右鍵地城導覽手冊按鍵現在將會開啟每週獎勵寶庫.",
+            "[移動框架] 支援每週獎勵寶庫介面.",
             "[美化皮膚] 修復了團隊工具皮膚可能造成的錯誤.",
+            "[美化皮膚] 優化了傳奇鑰石介面美化.",
             "[額外物品條] UTILITY (實用工具) 分組新增了護甲片等暗影之境製造物品.",
+            "[額外物品條] 更新托伽司物品列表.",
             "[觀察] 修復了暗影之境的附魔訊息. 感謝 loudsoul"
         },
         ["enUS"] = {
             "Amend some parts of the previous changelog.",
+            "[Announcement] Fix the bug that may occur after leaving from a group.",
+            "[Game Bar] Right-click Encounter Journal button now will open Weekly Rewards.",
+            "[Move Frame] Add support for weekly rewards.",
             "[Skins] Fix a potential bug of Raid Utility skin.",
+            "[Skins] Optimize Mythic Dungeon Keystone skin.",
             "[Extra Item Bar] UTILITY (utilities) category add some Shadowlands crafting items like armor kit.",
+            "[Extra Item Bar] Update Torghast item list.",
             "[Inpect] Fix the Shadowlands enchant info. Thanks loudsoul."
         },
         ["koKR"] = {
             "이전 변경 로그의 일부 수정.",
+            "[Announcement] Fix the bug that may occur after leaving from a group.",
+            "[Game Bar] Right-click Encounter Journal button now will open Weekly Rewards.",
+            "[Move Frame] Add support for weekly rewards.",
             "[스킨] 공격대 도구 스킨의 잠재적인 버그를 수정하였습니다.",
+            "[Skins] Optimize Mythic Dungeon Keystone skin.",
             "[아이템 바] UTILITY (유틸리티) 카테고리에 방어구 강화, 기름, 숫돌 같은 어둠땅 제작 아이템이 추가 되였습니다.",
+            "[Extra Item Bar] Update Torghast item list.",
             "[살펴보기] 어둠땅 마법 부여 정보를 수정하였습니다. Thanks loudsoul."
         }
     }

eb38f18ec72437fcd6b56605f2c6fb293dfabcb8
Zhou Fang <fang2hou@outlook.com>
2020-12-19 19:48:26 +0900

    start 2.16

---
+++ Core/Changelog/2.16.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[216] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[静音] 添加了渊誓猎魂犬的选项."
+        },
+        ["zhTW"] = {
+            "[靜音] 新增了淵誓獵魂犬的選項."
+        },
+        ["enUS"] = {
+            "[Mute] support Mawsworn Soulhunter."
+        },
+        ["koKR"] = {
+            "[Mute] support Mawsworn Soulhunter."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[美化皮肤] 新增了自定义数据文字面板的美化."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了自訂資訊文字面板的美化."
+        },
+        ["enUS"] = {
+            "[Skins] Add skins for custom DataText panels."
+        },
+        ["koKR"] = {
+            "[Skins] Add skins for custom DataText panels."
+        }
+    }
+}

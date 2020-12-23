f12a59fab6561e8065114ecca6059b368dedd944
Zhou Fang <fang2hou@outlook.com>
2020-12-23 14:24:13 +0900

    changelog

--- Core/Changelog/2.16.lua
+++ Core/Changelog/2.16.lua
@@ -10,30 +10,38 @@ W.Changelog[216] = {
     },
     NEW = {
         ["zhCN"] = {
-            "[静音] 添加了渊誓猎魂犬的选项."
+            "[静音] 添加了渊誓猎魂犬的选项.",
+            "[额外物品条] 为每个条添加了鼠标提示的选项."
         },
         ["zhTW"] = {
-            "[靜音] 新增了淵誓獵魂犬的選項."
+            "[靜音] 新增了淵誓獵魂犬的選項.",
+            "[額外物品條] 為每個條添加了鼠標提示的選項."
         },
         ["enUS"] = {
-            "[Mute] support Mawsworn Soulhunter."
+            "[Mute] support Mawsworn Soulhunter.",
+            "[Extra Item Bar] Add new tooltip options for each bar."
         },
         ["koKR"] = {
-            "[음소거] 나락살이 영혼사냥꾼 지원"
+            "[음소거] 나락살이 영혼사냥꾼 지원",
+            "[Extra Item Bar] Add new tooltip options for each bar."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "[美化皮肤] 新增了自定义数据文字面板的美化."
+            "[美化皮肤] 新增了自定义数据文字面板的美化.",
+            "[聊天链接] 适配更多的频道."
         },
         ["zhTW"] = {
-            "[美化皮膚] 新增了自訂資訊文字面板的美化."
+            "[美化皮膚] 新增了自訂資訊文字面板的美化.",
+            "[聊天鏈接] 適配更多頻道."
         },
         ["enUS"] = {
-            "[Skins] Add skins for custom DataText panels."
+            "[Skins] Add skins for custom DataText panels.",
+            "[Chat Link] Add support for more channels."
         },
         ["koKR"] = {
-            "[스킨] 사용자 정보 문자 스킨을 추가하였습니다."
+            "[스킨] 사용자 정보 문자 스킨을 추가하였습니다.",
+            "[Chat Link] Add support for more channels."
         }
     }
 }

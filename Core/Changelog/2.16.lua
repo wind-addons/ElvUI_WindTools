3c96d36994ebb71b74c83a7cefd673c4a402ade6
Zhou Fang <fang2hou@outlook.com>
2020-12-24 13:19:15 +0900

    changelog

--- Core/Changelog/2.16.lua
+++ Core/Changelog/2.16.lua
@@ -28,18 +28,26 @@ W.Changelog[216] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "兼容性检查工具更新.",
+            "[额外物品条] 更新托加斯特物品列表.",
             "[美化皮肤] 新增了自定义数据文字面板的美化.",
             "[聊天链接] 适配更多的频道."
         },
         ["zhTW"] = {
+            "相容性確認工具更新.",
+            "[額外物品條] 更新托伽司物品列表.",
             "[美化皮膚] 新增了自訂資訊文字面板的美化.",
             "[聊天鏈接] 適配更多頻道."
         },
         ["enUS"] = {
+            "Update compatibility check.",
+            "[Extra Item Bar] Update Torghast item list.",
             "[Skins] Add skins for custom DataText panels.",
             "[Chat Link] Add support for more channels."
         },
         ["koKR"] = {
+            "호환성 검사 업데이트.",
+            "[아이템 바] 토르가스트 아이템 리스트 업데이트.",
             "[스킨] 사용자 정보 문자 스킨을 추가하였습니다.",
             "[체팅 링크] 더 많은 채널을 추가하였습니다."
         }

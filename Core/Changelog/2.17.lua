4ac537305fde5c25d60e5acb379d48a9efbbfe6f
Zhou Fang <fang2hou@outlook.com>
2021-01-11 10:37:34 +0900

    2.17 changelog init

---
+++ Core/Changelog/2.17.lua
@@ -0,0 +1,47 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[217] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[通告] 添加了新史诗钥石的通报.",
+            "[美化皮肤] 添加了新的 UI 组件美化. (比如: 赎罪老一充能)"
+        },
+        ["zhTW"] = {
+            "[通告] 新增了新的傳奇鑰石通報.",
+            "[美化皮膚] 新增了 UI 組件美化. (比如: 贖罪一王充能)"
+        },
+        ["enUS"] = {
+            "[Announcement] Add support for new keystone announcement.",
+            "[Skins] Add new skins for UI widget. (e.g. charge of HoS first boss)"
+        },
+        ["koKR"] = {
+            "[Announcement] Add support for new keystone announcement.",
+            "[Skins] Add new skins for UI widget. (e.g. charge of HoS first boss)"
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "优化设定文本.",
+            "[额外物品条] 更新托加斯特物品列表.",
+        },
+        ["zhTW"] = {
+            "優化設定文字.",
+            "[額外物品條] 更新托伽司物品列表.",
+        },
+        ["enUS"] = {
+            "Edited some option description.",
+            "[Extra Item Bar] Update Torghast item list.",
+        },
+        ["koKR"] = {
+            "Edited some option description.",
+            "[아이템 바] 토르가스트 아이템 리스트 업데이트.",
+        }
+    }
+}

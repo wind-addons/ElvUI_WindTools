defb31410a799bb587ec095cb08f838bfb4b13db
Zhou Fang <fang2hou@outlook.com>
2021-01-22 18:42:12 +0900

    changelog

--- Core/Changelog/2.17.lua
+++ Core/Changelog/2.17.lua
@@ -30,22 +30,34 @@ W.Changelog[217] = {
         ["zhCN"] = {
             "优化设定文本.",
             "[进度] 移除了过时的副本.",
+            "[游戏条] 禁止战斗中开启日历造成污染.",
+            "[游戏条] 家按钮的鼠标提示将会显示物品可用次数.",
             "[额外物品条] 更新托加斯特物品列表.",
+            "[额外物品条] 优化了条更新的逻辑."
         },
         ["zhTW"] = {
             "優化設定文字.",
             "[進度] 移除了過時的副本.",
+            "[遊戲條] 禁止在戰鬥中開啟行事曆導致污染.",
+            "[遊戲條] 家按鍵的滑鼠提示將會顯示物品的可用次數.",
             "[額外物品條] 更新托伽司物品列表.",
+            "[額外物品條] 優化了條更新的邏輯."
         },
         ["enUS"] = {
             "Edited some option description.",
             "[Progression] Remove outdated instances.",
+            "[Game Bar] Prevent the taint by toggling calendar in combat.",
+            "[Game Bar] Add charge inforamtion into the tooltip of Home button.",
             "[Extra Item Bar] Update Torghast item list.",
+            "[Extra Item Bar] Optimize the update of bars."
         },
         ["koKR"] = {
             "일부 옵션의 설명을 수정했습니다.",
             "[진행상태] 오래된 인스턴스 삭제.",
+            "[Game Bar] Prevent the taint by toggling calendar in combat.",
+            "[Game Bar] Add charge inforamtion into the tooltip of Home button.",
             "[아이템 바] 토르가스트 아이템 리스트 업데이트.",
+            "[Extra Item Bar] Optimize the update of bars."
         }
     }
 }

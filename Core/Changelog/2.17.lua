d16163b2fc87efad7efbf125f790e6f88a912d0b
Zhou Fang <fang2hou@outlook.com>
2021-01-25 16:02:22 +0900

    changelog

--- Core/Changelog/2.17.lua
+++ Core/Changelog/2.17.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[217] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "新增了对西班牙语和葡萄牙语的支持. 任何翻译问题可随时来 Discord 反馈."
+        },
+        ["zhTW"] = {
+            "支援西班牙語及葡萄牙語. 任何翻譯問題可隨時來 Discord 進行回報."
+        },
+        ["enUS"] = {
+            "Add basic support for Spanish and Portuguese. You can report the translation problem in our Discord anytime."
+        },
+        ["koKR"] = {
+            "Add basic support for Spanish and Portuguese. You can report the translation problem in our Discord anytime."
+        }
     },
     NEW = {
         ["zhCN"] = {
@@ -29,35 +37,47 @@ W.Changelog[217] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "优化设定文本.",
+            "[通告] 韩语客户端中宠物主人现在可以被正确识别.",
+            "[通告] 修复了宠物打断无法通告的问题.",
             "[进度] 移除了过时的副本.",
             "[游戏条] 禁止战斗中开启日历造成污染.",
             "[游戏条] 家按钮的鼠标提示将会显示物品可用次数.",
             "[额外物品条] 更新托加斯特物品列表.",
-            "[额外物品条] 优化了条更新的逻辑."
+            "[额外物品条] 优化了条更新的逻辑.",
+            '[美化皮肤] 修复了设定能量条为"偏移"风格时, 阴影位置不正确的问题.'
         },
         ["zhTW"] = {
             "優化設定文字.",
+            "[通告] 韓文客戶端中寵物主人現可被正確識別.",
+            "[通告] 修復了寵物打斷無法通告的問題.",
             "[進度] 移除了過時的副本.",
             "[遊戲條] 禁止在戰鬥中開啟行事曆導致污染.",
             "[遊戲條] 家按鍵的滑鼠提示將會顯示物品的可用次數.",
             "[額外物品條] 更新托伽司物品列表.",
-            "[額外物品條] 優化了條更新的邏輯."
+            "[額外物品條] 優化了條更新的邏輯.",
+            '[美化皮膚] 修復了設定能量條為"偏移"風格的時候, 陰影位置不正確的問題.'
         },
         ["enUS"] = {
             "Edited some option description.",
+            "[Announcement] The owner of the pet can be recognized correctly in Korean client.",
+            "[Announcement] Fix the announcement of the interrupts by pets.",
             "[Progression] Remove outdated instances.",
             "[Game Bar] Prevent the taint by toggling calendar in combat.",
-            "[Game Bar] Add charge inforamtion into the tooltip of Home button.",
+            "[Game Bar] Add charge information into the tooltip of Home button.",
             "[Extra Item Bar] Update Torghast item list.",
-            "[Extra Item Bar] Optimize the update of bars."
+            "[Extra Item Bar] Optimize the update of bars.",
+            '[Skins] Fix the issue that the shadow not in right place with the ElvUI power bar in "OFFSET" style.'
         },
         ["koKR"] = {
             "일부 옵션의 설명을 수정했습니다.",
+            "[Announcement] The owner of the pet can be recognized correctly in Korean client.",
+            "[Announcement] Fix the announcement of the interrupts by pets.",
             "[진행상태] 오래된 인스턴스 삭제.",
             "[게임 바] 전투 중 달력을 열수 없도록 수정.",
             "[게임 바] 홈 버튼의 툴팁에 사용 가능한 개수를 표시합니다.",
             "[아이템 바] 토르가스트 아이템 리스트 업데이트.",
-            "[아이템 바] 업데이트 로직을 최적화했습니다."
+            "[아이템 바] 업데이트 로직을 최적화했습니다.",
+            '[Skins] Fix the issue that the shadow not in right place with the ElvUI power bar in "OFFSET" style.'
         }
     }
 }

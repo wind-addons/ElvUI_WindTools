81b950b9abab2254b842a5fd06bd1e7bae6712e2
Zhou Fang <fang2hou@outlook.com>
2021-02-13 15:23:23 +0900

    format

--- Media/Media.lua
+++ Media/Media.lua
@@ -42,22 +42,21 @@ do
 end
 
 do
-	
-    function F.GetRoleTexCoord(role)
+	function F.GetRoleTexCoord(role)
 		if role == "TANK" then
-			return .32/9.03, 2.04/9.03, 2.65/9.03, 4.3/9.03
+			return .32 / 9.03, 2.04 / 9.03, 2.65 / 9.03, 4.3 / 9.03
 		elseif role == "DPS" or role == "DAMAGER" then
-			return 2.68/9.03, 4.4/9.03, 2.65/9.03, 4.34/9.03
+			return 2.68 / 9.03, 4.4 / 9.03, 2.65 / 9.03, 4.34 / 9.03
 		elseif role == "HEALER" then
-			return 2.68/9.03, 4.4/9.03, .28/9.03, 1.98/9.03
+			return 2.68 / 9.03, 4.4 / 9.03, .28 / 9.03, 1.98 / 9.03
 		elseif role == "LEADER" then
-			return .32/9.03, 2.04/9.03, .28/9.03, 1.98/9.03
+			return .32 / 9.03, 2.04 / 9.03, .28 / 9.03, 1.98 / 9.03
 		elseif role == "READY" then
-			return 5.1/9.03, 6.76/9.03, .28/9.03, 1.98/9.03
+			return 5.1 / 9.03, 6.76 / 9.03, .28 / 9.03, 1.98 / 9.03
 		elseif role == "PENDING" then
-			return 5.1/9.03, 6.76/9.03, 2.65/9.03, 4.34/9.03
+			return 5.1 / 9.03, 6.76 / 9.03, 2.65 / 9.03, 4.34 / 9.03
 		elseif role == "REFUSE" then
-			return 2.68/9.03, 4.4/9.03, 5.02/9.03, 6.7/9.03
+			return 2.68 / 9.03, 4.4 / 9.03, 5.02 / 9.03, 6.7 / 9.03
 		end
 	end
 

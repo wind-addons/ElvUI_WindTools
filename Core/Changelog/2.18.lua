9a1007cd928f7aa51b8ba9e0f2aeb8d3aea158f6
Zhou Fang <fang2hou@outlook.com>
2021-02-23 12:37:13 +0900

    changelog

--- Core/Changelog/2.18.lua
+++ Core/Changelog/2.18.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[218] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "德语翻译大幅更新. 感谢 Dlarge"
+        },
+        ["zhTW"] = {
+            "德語翻譯大幅更新. 感謝 Dlarge"
+        },
+        ["enUS"] = {
+            "German translation updated. Thanks Dlarge"
+        },
+        ["koKR"] = {
+            "German translation updated. Thanks Dlarge"
+        }
     },
     NEW = {
         ["zhCN"] = {},
@@ -15,9 +23,21 @@ W.Changelog[218] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "兼容性检查工具更新.",
+            "[装备观察] 更新附魔数据库."
+        },
+        ["zhTW"] = {
+            "相容性確認工具更新.",
+            "[裝備觀察] 更新附魔資料庫."
+        },
+        ["enUS"] = {
+            "Update compatibility check.",
+            "[Inspect] Update enchant database."
+        },
+        ["koKR"] = {
+            "호환성 검사 업데이트.",
+            "[Inspect] Update enchant database."
+        }
     }
 }

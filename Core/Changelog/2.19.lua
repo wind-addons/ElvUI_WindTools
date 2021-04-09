6ba03ef29ed7eb4a588b044b1a528de9a265bbaf
Zhou Fang <fang2hou@outlook.com>
2021-04-10 03:07:18 +0900

    changelog

--- Core/Changelog/2.19.lua
+++ Core/Changelog/2.19.lua
@@ -15,9 +15,22 @@ W.Changelog[219] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[额外物品条] 添加销魂治疗药水到 POTION (药水) 分类.",
+            "[额外物品条] 补全了鼠标提示中自定义物品相关的设定说明."
+        },
+        ["zhTW"] = {
+            
+            "[額外物品條] 新增了靈性治療藥水到 POTION (藥水) 分類.",
+            "[額外物品條] 補全了滑鼠提示中自訂物品相關的設定說明."
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Add Soulful Healing Potion to POTION list.",
+            "[Extra Item Bar] Readd the description of CUSTOM group in the tooltip."
+        },
+        ["koKR"] = {
+            "[Extra Item Bar] Add Soulful Healing Potion(get name by macro '/run print(GetItemInfo(180317))') to POTION list.",
+            "[Extra Item Bar] Readd the description of CUSTOM group in the tooltip."
+        }
     }
 }

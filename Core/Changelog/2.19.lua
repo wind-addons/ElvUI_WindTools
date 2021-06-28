62db55386dd8648787e76b30d0374e029031a0ff
Zhou Fang <fang2hou@outlook.com>
2021-06-28 15:57:22 +0900

    bump toc

--- Core/Changelog/2.19.lua
+++ Core/Changelog/2.19.lua
@@ -1,7 +1,7 @@
 local W = unpack(select(2, ...))
 
 W.Changelog[219] = {
-    RELEASE_DATE = "TBD",
+    RELEASE_DATE = "2021/06/29",
     IMPORTANT = {
         ["zhCN"] = {
             "新增了法语支持. 感谢 xan2622",

f72a57cc9ddb09a88c643abd24cb7e37023517e4
Zhou Fang <fang2hou@outlook.com>
2021-06-30 12:35:59 +0900

    Fix PlayerChoice Skin

--- Modules/Skins/Blizzard/PlayerChoice.lua
+++ Modules/Skins/Blizzard/PlayerChoice.lua
@@ -3,7 +3,7 @@ local S = W:GetModule("Skins")
 
 local _G = _G
 
-function S:Blizzard_PlayerChoiceUI()
+function S:Blizzard_PlayerChoice()
     if not self:CheckDB("playerChoice") then
         return
     end
@@ -12,7 +12,7 @@ function S:Blizzard_PlayerChoiceUI()
     if not frame then
         return
     end
-    self:CreateBackdropShadow(_G.PlayerChoiceFrame)
+    self:CreateShadow(_G.PlayerChoiceFrame)
 end
 
-S:AddCallbackForAddon("Blizzard_PlayerChoiceUI")
+S:AddCallbackForAddon("Blizzard_PlayerChoice")

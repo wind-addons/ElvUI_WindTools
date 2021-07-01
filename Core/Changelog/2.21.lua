b226e664bb0aeb19ee5f66d19806a27ae51842f3
Zhou Fang <fang2hou@outlook.com>
2021-07-02 03:05:18 +0900

    2.21 start!

---
+++ Core/Changelog/2.21.lua
@@ -0,0 +1,55 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[221] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "修复 Premade Groups Filter 的皮肤.",
+            "修复网易集合石的皮肤."
+        },
+        ["zhTW"] = {
+            "修復了 Premade Groups Filter 的美化.",
+            "修復了網易集合石的美化."
+        },
+        ["enUS"] = {
+            "Fixed Premade Groups Filter Skin.",
+            "Fixed NetEase MeetingStone Skin."
+        },
+        ["koKR"] = {
+            "Premade Groups Filter 스킨 수정.",
+            "NetEase MeetingStone 스킨 수정."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[移动框体] 添加了玩家選擇框体的支持."
+        },
+        ["zhTW"] = {
+            "[移動框架] 支援玩家選擇框架."
+        },
+        ["enUS"] = {
+            "[Move Frames] Add support of Player Choice Frame."
+        },
+        ["koKR"] = {
+            "[Move Frames] Add support of Player Choice Frame."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[额外物品条] 新增了一些克希亚物品.",
+            "[巅峰声望] 修改默认格式为暴雪风格."
+        },
+        ["zhTW"] = {
+            "[額外物品條] 新增了一些科西亞物品.",
+            "[巔峰聲望] 修改默認格式為暴雪風格."
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Add some items from Korthia.",
+            "[Paragon Reputation] Change the default format to Blizzard style."
+        },
+        ["koKR"] = {
+            "[Extra Item Bar] Add some items from Korthia.",
+            "[Paragon Reputation] Change the default format to Blizzard style."
+        }
+    }
+}

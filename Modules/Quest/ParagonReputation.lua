08cdd95ad2920207a6e6c4a04c8fcae2af93be48
Zhou Fang <fang2hou@outlook.com>
2021-07-02 02:37:56 +0900

    Import some changes from original PR

--- Modules/Quest/ParagonReputation.lua
+++ Modules/Quest/ParagonReputation.lua
@@ -42,41 +42,506 @@ local ACTIVE_TOAST = false
 local WAITING_TOAST = {}
 
 local PARAGON_QUEST_ID = {
-	--[questID] = {factionID,rewardID}
 	--Legion
-	[48976] = {2170, 152922}, -- Argussian Reach
-	[46777] = {2045, 152108}, -- Armies of Legionfall
-	[48977] = {2165, 152923}, -- Army of the Light
-	[46745] = {1900, 152102}, -- Court of Farondis
-	[46747] = {1883, 152103}, -- Dreamweavers
-	[46743] = {1828, 152104}, -- Highmountain Tribes
-	[46748] = {1859, 152105}, -- The Nightfallen
-	[46749] = {1894, 152107}, -- The Wardens
-	[46746] = {1948, 152106}, -- Valarjar
+	[48976] = {
+		-- Argussian Reach
+		factionID = 2170,
+		cache = 152922
+	},
+	[46777] = {
+		-- Armies of Legionfall
+		factionID = 2045,
+		cache = 152108,
+		rewards = {
+			{
+				-- Orphaned Felbat
+				type = PET,
+				itemID = 147841
+			}
+		}
+	},
+	[48977] = {
+		-- Army of the Light
+		factionID = 2165,
+		cache = 152923,
+		rewards = {
+			{
+				-- Avenging Felcrushed
+				type = MOUNT,
+				itemID = 153044,
+				mountID = 985
+			},
+			{
+				-- Blessed Felcrushed
+				type = MOUNT,
+				itemID = 153043,
+				mountID = 984
+			},
+			{
+				-- Glorious Felcrushed
+				type = MOUNT,
+				itemID = 153042,
+				mountID = 983
+			},
+			{
+				-- Holy Lightsphere
+				type = TOY,
+				itemID = 153182
+			}
+		}
+	},
+	[46745] = {
+		-- Court of Farondis
+		factionID = 1900,
+		cache = 152102,
+		rewards = {
+			{
+				-- Cloudwing Hippogryph
+				type = MOUNT,
+				itemID = 147806,
+				mountID = 943
+			}
+		}
+	},
+	[46747] = {
+		-- Dreamweavers
+		factionID = 1883,
+		cache = 152103,
+		rewards = {
+			{
+				-- Wild Dreamrunner
+				type = MOUNT,
+				itemID = 147804,
+				mountID = 942
+			}
+		}
+	},
+	[46743] = {
+		-- Highmountain Tribes
+		factionID = 1828,
+		cache = 152104,
+		rewards = {
+			{
+				-- Highmountain Elderhorn
+				type = MOUNT,
+				itemID = 147807,
+				mountID = 941
+			}
+		}
+	},
+	[46748] = {
+		-- The Nightfallen
+		factionID = 1859,
+		cache = 152105,
+		rewards = {
+			{
+				-- Leywoven Flying Carpet
+				type = MOUNT,
+				itemID = 143764,
+				mountID = 905
+			}
+		}
+	},
+	[46749] = {
+		-- The Wardens
+		factionID = 1894,
+		cache = 152107,
+		rewards = {
+			{
+				-- Sira's Extra Cloak
+				type = TOY,
+				itemID = 147843
+			}
+		}
+	},
+	[46746] = {
+		-- Valarjar
+		factionID = 1948,
+		cache = 152106,
+		rewards = {
+			{
+				-- Valarjar Stormwing
+				type = MOUNT,
+				itemID = 147805,
+				mountID = 944
+			}
+		}
+	},
 	--Battle for Azeroth
 	--Neutral
-	[54453] = {2164, 166298}, --Champions of Azeroth
-	[58096] = {2415, 174483}, --Rajani
-	[55348] = {2391, 170061}, --Rustbolt Resistance
-	[54451] = {2163, 166245}, --Tortollan Seekers
-	[58097] = {2417, 174484}, --Uldum Accord
+	[54453] = {
+		--Champions of Azeroth
+		factionID = 2164,
+		cache = 166298,
+		rewards = {
+			{
+				-- Azerite Firework Launcher
+				type = TOY,
+				itemID = 166877
+			}
+		}
+	},
+	[58096] = {
+		--Rajani
+		factionID = 2415,
+		cache = 174483,
+		rewards = {
+			{
+				-- Jade Defender
+				type = PET,
+				itemID = 174479
+			}
+		}
+	},
+	[55348] = {
+		--Rustbolt Resistance
+		factionID = 2391,
+		cache = 170061,
+		rewards = {
+			{
+				-- Blueprint: Microbot XD
+				type = BINDING_HEADER_OTHER,
+				itemID = 169171,
+				questID = 55079
+			},
+			{
+				-- Blueprint: Holographic Digitalization Relay
+				type = BINDING_HEADER_OTHER,
+				itemID = 168906,
+				questID = 56086
+			},
+			{
+				-- Blueprint: Rustbolt Resistance Insignia
+				type = BINDING_HEADER_OTHER,
+				itemID = 168494,
+				questID = 55073
+			}
+		}
+	},
+	[54451] = {
+		--Tortollan Seekers
+		factionID = 2163,
+		cache = 166245,
+		rewards = {
+			{
+				-- Bowl of Glowing Pufferfish
+				type = TOY,
+				itemID = 166704
+			}
+		}
+	},
+	[58097] = {
+		--Uldum Accord
+		factionID = 2417,
+		cache = 174484,
+		rewards = {
+			{
+				-- Cursed Dune Watcher
+				type = PET,
+				itemID = 174481
+			}
+		}
+	},
 	--Horde
-	[54460] = {2156, 166282}, --Talanji's Expedition
-	[54455] = {2157, 166299}, --The Honorbound
-	[53982] = {2373, 169940}, --The Unshackled
-	[54461] = {2158, 166290}, --Voldunai
-	[54462] = {2103, 166292}, --Zandalari Empire
+	[54460] = {
+		--Talanji's Expedition
+		factionID = 2156,
+		cache = 166282,
+		rewards = {
+			{
+				-- For da Blood God!
+				type = TOY,
+				itemID = 166308
+			},
+			{
+				-- Pair of Tiny Bat Wings
+				type = PET,
+				itemID = 166716
+			}
+		}
+	},
+	[54455] = {
+		--The Honorbound
+		factionID = 2157,
+		cache = 166299,
+		rewards = {
+			{
+				-- Rallying War Banner
+				type = TOY,
+				itemID = 166879
+			}
+		}
+	},
+	[53982] = {
+		--The Unshackled
+		factionID = 2373,
+		cache = 169940,
+		rewards = {
+			{
+				-- Royal Snapdragon
+				type = MOUNT,
+				itemID = 169198,
+				mountID = 1237
+			},
+			{
+				-- Flopping Fish
+				type = TOY,
+				itemID = 170203
+			},
+			{
+				-- Memento of the Deeps
+				type = TOY,
+				itemID = 170469
+			}
+		}
+	},
+	[54461] = {
+		--Voldunai
+		factionID = 2158,
+		cache = 166290,
+		rewards = {
+			{
+				-- Goldtusk Inn Breakfast Buffet
+				type = TOY,
+				itemID = 166703
+			},
+			{
+				-- Meerah's Jukebox
+				type = TOY,
+				itemID = 166880
+			}
+		}
+	},
+	[54462] = {
+		--Zandalari Empire
+		factionID = 2103,
+		cache = 166292,
+		rewards = {
+			{
+				-- Warbeast Kraal Dinner Bell
+				type = TOY,
+				itemID = 166701
+			}
+		}
+	},
 	--Alliance
-	[54456] = {2161, 166297}, --Order of Embers
-	[54458] = {2160, 166295}, --Proudmoore Admiralty
-	[54457] = {2162, 166294}, --Storm's Wake
-	[54454] = {2159, 166300}, --The 7th Legion
-	[55976] = {2400, 169939}, --Waveblade Ankoan
+	[54456] = {
+		--Order of Embers
+		factionID = 2161,
+		cache = 166297,
+		rewards = {
+			{
+				-- Bewitching Tea Set
+				type = TOY,
+				itemID = 166808
+			},
+			{
+				-- Cobalt Raven Hatchling
+				type = PET,
+				itemID = 166718
+			}
+		}
+	},
+	[54458] = {
+		--Proudmoore Admiralty
+		factionID = 2160,
+		cache = 166295,
+		rewards = {
+			{
+				-- Proudmoore Music Box
+				type = TOY,
+				itemID = 166702
+			},
+			{
+				-- Albatross Feather
+				type = PET,
+				itemID = 166714
+			}
+		}
+	},
+	[54457] = {
+		--Storm's Wake
+		factionID = 2162,
+		cache = 166294,
+		rewards = {
+			{
+				-- Violet Abyssal Eel
+				type = PET,
+				itemID = 166719
+			}
+		}
+	},
+	[54454] = {
+		--The 7th Legion
+		factionID = 2159,
+		cache = 166300,
+		rewards = {
+			{
+				-- Rallying War Banner
+				type = TOY,
+				itemID = 166879
+			}
+		}
+	},
+	[55976] = {
+		--Waveblade Ankoan
+		factionID = 2400,
+		cache = 169939,
+		rewards = {
+			{
+				-- Royal Snapdragon
+				type = MOUNT,
+				itemID = 169198,
+				mountID = 1237
+			},
+			{
+				-- Flopping Fish
+				type = TOY,
+				itemID = 170203
+			},
+			{
+				-- Memento of the Deeps
+				type = TOY,
+				itemID = 170469
+			}
+		}
+	},
 	--Shadowlands
-	[61100] = {2413, 180648}, --Court of Harvesters
-	[61097] = {2407, 180647}, --The Ascended
-	[61095] = {2410, 180646}, --The Undying Army
-	[61098] = {2465, 180649} --The Wild Hunt
+	[61100] = {
+		--Court of Harvesters
+		factionID = 2413,
+		cache = 180648,
+		rewards = {
+			{
+				-- Stonewing Dredwing Pup
+				type = PET,
+				itemID = 180601
+			}
+		}
+	},
+	[64012] = {
+		--Death's Advance
+		factionID = 2470,
+		cache = 186650,
+		rewards = {
+			{
+				-- Beryl Shardhide
+				type = MOUNT,
+				itemID = 186644,
+				mountID = 1455
+			},
+			{
+				-- Fierce Razorwing
+				type = MOUNT,
+				itemID = 186649,
+				mountID = 1508
+			},
+			{
+				-- Mosscoated Hopper
+				type = PET,
+				itemID = 186541
+			}
+		}
+	},
+	[64266] = {
+		--The Archivist's Codex
+		factionID = 2472,
+		cache = 187028,
+		rewards = {
+			{
+				-- Tamed Mauler
+				type = MOUNT,
+				itemID = 186641,
+				mountID = 1454
+			},
+			{
+				-- Gnashtooth
+				type = PET,
+				itemID = 186538
+			}
+		}
+	},
+	[61097] = {
+		--The Ascended
+		factionID = 2407,
+		cache = 180647,
+		rewards = {
+			{
+				-- Malfunctioning Goliath Gauntlet
+				type = TOY,
+				itemID = 184396
+			},
+			{
+				-- Mark of Purity
+				type = TOY,
+				itemID = 184435
+			},
+			{
+				-- Larion Cub
+				type = PET,
+				itemID = 184399
+			}
+		}
+	},
+	[61095] = {
+		--The Undying Army
+		factionID = 2410,
+		cache = 180646,
+		rewards = {
+			{
+				-- Reins of the Colossal Slaughterclaw
+				type = MOUNT,
+				itemID = 182081,
+				mountID = 1350
+			},
+			{
+				-- Infested Arachnid Casing
+				type = TOY,
+				itemID = 184495
+			},
+			{
+				-- Micromancer's Mystical Cowl
+				type = PET,
+				itemID = 181269
+			}
+		}
+	},
+	[61098] = {
+		--The Wild Hunt
+		factionID = 2465,
+		cache = 180649,
+		rewards = {
+			{
+				-- Amber Ardenmoth
+				type = MOUNT,
+				itemID = 183800,
+				mountID = 1428
+			},
+			{
+				-- Hungry Burrower
+				type = PET,
+				itemID = 180635
+			}
+		}
+	},
+	[64267] = {
+		--Ve'nari
+		factionID = 2432,
+		cache = 187029,
+		rewards = {
+			{
+				-- Soulbound Gloomcharger's Reins
+				type = MOUNT,
+				itemID = 186657,
+				mountID = 1501
+			},
+			{
+				-- Rook
+				type = PET,
+				itemID = 186552
+			}
+		}
+	}
 }
 
 function PR:ColorWatchbar(bar)
@@ -110,12 +575,13 @@ function PR:SetupParagonTooltip(tt)
 end
 
 function PR:Tooltip(bar, event)
-	if not bar.questID then
+	if not bar.questID or not PARAGON_QUEST_ID[bar.questID] then
 		return
 	end
+
 	if event == "OnEnter" then
-		local _, link = GetItemInfo(PARAGON_QUEST_ID[bar.questID][2])
-		if link ~= nil then
+		local name, link, quality = GetItemInfo(PARAGON_QUEST_ID[bar.questID].cache)
+		if link then
 			_G.GameTooltip:SetOwner(bar, "ANCHOR_NONE")
 			_G.GameTooltip:SetPoint("LEFT", bar, "RIGHT", 10, 0)
 			_G.GameTooltip:SetHyperlink(link)
@@ -227,7 +693,7 @@ end
 
 function PR:QUEST_ACCEPTED(event, questID)
 	if self.db.toast.enable and PARAGON_QUEST_ID[questID] then
-		local name = GetFactionInfoByID(PARAGON_QUEST_ID[questID][1])
+		local name = GetFactionInfoByID(PARAGON_QUEST_ID[questID].factionID)
 		local text = GetQuestLogCompletionText(C_QuestLog_GetLogIndexForQuestID(questID))
 		if ACTIVE_TOAST then
 			WAITING_TOAST[#WAITING_TOAST + 1] = {name, text} --Toast is already active, put this info on the line.

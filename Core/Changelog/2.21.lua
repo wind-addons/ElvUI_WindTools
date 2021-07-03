49e6647859eeb62e68b0ea8c5b0ce57a34100979
Zhou Fang <fang2hou@outlook.com>
2021-07-03 12:02:29 +0900

    changelog

--- Core/Changelog/2.21.lua
+++ Core/Changelog/2.21.lua
@@ -1,53 +1,69 @@
 local W = unpack(select(2, ...))
 
 W.Changelog[221] = {
-    RELEASE_DATE = "TBD",
+    RELEASE_DATE = "2021/07/03",
     IMPORTANT = {
         ["zhCN"] = {
-            "修复 Premade Groups Filter 的皮肤.",
-            "修复网易集合石的皮肤."
+            "[过滤器] 修复解锁后无法通过战网组队的问题."
         },
         ["zhTW"] = {
-            "修復了 Premade Groups Filter 的美化.",
-            "修復了網易集合石的美化."
+            "[過濾器] 修復了解鎖後無法通過戰網組隊的問題."
         },
         ["enUS"] = {
-            "Fixed Premade Groups Filter Skin.",
-            "Fixed NetEase MeetingStone Skin."
+            "[Filter] Fix the bug that player cannot join party via Battle.net."
         },
         ["koKR"] = {
-            "Premade Groups Filter 스킨 수정.",
-            "NetEase MeetingStone 스킨 수정."
+            "[Filter] Fix the bug that player cannot join party via Battle.net."
         }
     },
     NEW = {
         ["zhCN"] = {
-            "[移动框体] 添加了玩家選擇框体的支持."
+            "[移动框体] 添加了玩家選擇框体的支持.",
+            "[世界地图] 添加了 9.1 新区域的去除迷雾支持."
         },
         ["zhTW"] = {
-            "[移動框架] 支援玩家選擇框架."
+            "[移動框架] 支援玩家選擇框架.",
+            "[世界地圖] 支援 9.1 新的區域去除地圖迷霧."
         },
         ["enUS"] = {
-            "[Move Frames] Add support of Player Choice Frame."
+            "[Move Frames] Add support of Player Choice Frame.",
+            "[World Map] Reveal map can handle new areas in 9.1."
         },
         ["koKR"] = {
-            "[Move Frames] Add support of Player Choice Frame."
+            "[Move Frames] Add support of Player Choice Frame.",
+            "[World Map] Reveal map can handle new areas in 9.1."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[美化皮肤] 修复公会页面皮肤",
+            "[美化皮肤] 修复 Premade Groups Filter 的皮肤.",
+            "[美化皮肤] 修复网易集合石的皮肤.",
+            "[美化皮肤] 修复开发者工具的皮肤.",
             "[额外物品条] 新增了一些克希亚物品.",
             "[巅峰声望] 修改默认格式为暴雪风格."
         },
         ["zhTW"] = {
+            "[美化皮膚] 修復了公會頁面皮膚",
+            "[美化皮膚] 修復了 Premade Groups Filter 的美化.",
+            "[美化皮膚] 修復了網易集合石的美化.",
+            "[美化皮膚] 修復了開發者工具皮膚",
             "[額外物品條] 新增了一些科西亞物品.",
             "[巔峰聲望] 修改默認格式為暴雪風格."
         },
         ["enUS"] = {
+            "[Skins] Fixed Guild Skin.",
+            "[Skins] Fixed Premade Groups Filter Skin.",
+            "[Skins] Fixed NetEase MeetingStone Skin.",
+            "[Skins] Fixed skins for debug tools.",
             "[Extra Item Bar] Add some items from Korthia.",
             "[Paragon Reputation] Change the default format to Blizzard style."
         },
         ["koKR"] = {
+            "[Skins] Fixed Guild Skin.",
+            "[Skins] Premade Groups Filter 스킨 수정.",
+            "[Skins] NetEase MeetingStone 스킨 수정.",
+            "[Skins] Fixed skins for debug tools.",
             "[Extra Item Bar] Add some items from Korthia.",
             "[Paragon Reputation] Change the default format to Blizzard style."
         }

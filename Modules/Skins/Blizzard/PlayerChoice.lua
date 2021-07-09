36556944daefff8041a6a6250a8d99386fb6f697
Zhou Fang <fang2hou@outlook.com>
2021-07-09 15:21:22 +0900

    Fix player choice frame in tower

--- Modules/Skins/Blizzard/PlayerChoice.lua
+++ Modules/Skins/Blizzard/PlayerChoice.lua
@@ -2,17 +2,30 @@ local W, F, E, L = unpack(select(2, ...))
 local S = W:GetModule("Skins")
 
 local _G = _G
+local hooksecurefunc = hooksecurefunc
+
+local IsInJailersTower = IsInJailersTower
+
+local function SetupOptions()
+    if not _G.PlayerChoiceFrame.windStyle then
+        S:CreateShadow(_G.PlayerChoiceFrame)
+    end
+
+    local inTower = IsInJailersTower()
+
+    if inTower then
+        _G.PlayerChoiceFrame.shadow:Hide()
+    else
+        _G.PlayerChoiceFrame.shadow:Show()
+    end
+end
 
 function S:Blizzard_PlayerChoice()
     if not self:CheckDB("playerChoice") then
         return
     end
 
-    local frame = _G.PlayerChoiceFrame
-    if not frame then
-        return
-    end
-    self:CreateShadow(_G.PlayerChoiceFrame)
+    hooksecurefunc(_G.PlayerChoiceFrame, "SetupOptions", SetupOptions)
 end
 
 S:AddCallbackForAddon("Blizzard_PlayerChoice")

a905489b1a2ae42f847b1f57375e6e1a04f215ba
talmus76 <73485793+talmus76@users.noreply.github.com>
2021-07-09 16:16:30 +0900

    KoKR  Changelog update

--- Core/Changelog/2.22.lua
+++ Core/Changelog/2.22.lua
@@ -31,9 +31,9 @@ W.Changelog[222] = {
             "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast."
         },
         ["koKR"] = {
-            "[Extra Item Bar] Add more items from Korthia.",
-            "[Chat Text] Be compatible with latest ElvUI changes.",
-            "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast."
+            "[아이템 바] 더 많은 코르시아 아이템이 추가되였습니다.",
+            "[채팅 문자] 최신 버전의 ElvUI 채팅 모듈과 호환됩니다.",
+            "[스킨] 토르가스트에서 령 능력 선택시 테두리가 나오는 문제를 수정하였습니다."
         }
     }
 }

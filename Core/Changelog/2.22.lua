5cfbd94eb9c93db1c091d608c214a50d3bced21a
Zhou Fang <fang2hou@outlook.com>
2021-07-09 15:57:46 +0900

    2.22 start

---
+++ Core/Changelog/2.22.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[222] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[额外物品条] 新增更多刻希亚物品.",
+            "[聊天文字] 适配最新版本的 ElvUI 聊天模块.",
+            "[美化皮肤] 修复了塔内心能选择时的边框皮肤错误."
+        },
+        ["zhTW"] = {
+            "[額外物品條] 新增更多科西亞物品.",
+            "[聊天文字] 適配最新版本的 ElvUI 聊天模組.",
+            "[美化皮膚] 修復了塔內心能選擇時的邊框皮膚錯誤."
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Add more items from Korthia.",
+            "[Chat Text] Be compatible with latest ElvUI changes.",
+            "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast."
+        },
+        ["koKR"] = {
+            "[Extra Item Bar] Add more items from Korthia.",
+            "[Chat Text] Be compatible with latest ElvUI changes.",
+            "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast."
+        }
+    }
+}

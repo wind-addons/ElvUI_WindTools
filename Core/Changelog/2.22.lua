878d26eb02636500af8deddc1c4956cbf9b38069
Zhou Fang <fang2hou@outlook.com>
2021-07-19 09:32:16 +0900

    changelog

--- Core/Changelog/2.22.lua
+++ Core/Changelog/2.22.lua
@@ -18,22 +18,26 @@ W.Changelog[222] = {
         ["zhCN"] = {
             "[额外物品条] 新增更多刻希亚物品.",
             "[聊天文字] 适配最新版本的 ElvUI 聊天模块.",
-            "[美化皮肤] 修复了塔内心能选择时的边框皮肤错误."
+            "[美化皮肤] 修复了塔内心能选择时的边框皮肤错误.",
+            "[天赋管家] 修复了偶尔在战斗中更新按钮导致的错误."
         },
         ["zhTW"] = {
             "[額外物品條] 新增更多科西亞物品.",
             "[聊天文字] 適配最新版本的 ElvUI 聊天模組.",
-            "[美化皮膚] 修復了塔內心能選擇時的邊框皮膚錯誤."
+            "[美化皮膚] 修復了塔內心能選擇時的邊框皮膚錯誤.",
+            "[天賦管家] 修復了偶爾在戰鬥中更新按鍵導致的錯誤."
         },
         ["enUS"] = {
             "[Extra Item Bar] Add more items from Korthia.",
             "[Chat Text] Be compatible with latest ElvUI changes.",
-            "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast."
+            "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast.",
+            "[Talent Manager] Fix the bug that updating buttons in combat."
         },
         ["koKR"] = {
             "[아이템 바] 더 많은 코르시아 아이템이 추가되였습니다.",
             "[채팅 문자] 최신 버전의 ElvUI 채팅 모듈과 호환됩니다.",
-            "[스킨] 토르가스트에서 령 능력 선택시 테두리가 나오는 문제를 수정하였습니다."
+            "[스킨] 토르가스트에서 령 능력 선택시 테두리가 나오는 문제를 수정하였습니다.",
+            "[Talent Manager] Fix the bug that updating buttons in combat."
         }
     }
 }

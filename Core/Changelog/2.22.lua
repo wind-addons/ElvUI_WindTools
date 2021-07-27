0f5747eec5e0bff56885d90f28c873687cb75e13
Zhou Fang <fang2hou@outlook.com>
2021-07-27 10:03:45 +0900

    changelog

--- Core/Changelog/2.22.lua
+++ Core/Changelog/2.22.lua
@@ -9,34 +9,54 @@ W.Changelog[222] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[进度] 新增了统御圣所, 集市的支持."
+        },
+        ["zhTW"] = {
+            "[進度] 新增支援統御聖所, 市集."
+        },
+        ["enUS"] = {
+            "[Progression] Add support for Sanctum of Domination and Tazavesh, the Veiled Market."
+        },
+        ["koKR"] = {
+            "[Progression] Add support for Sanctum of Domination and Tazavesh, the Veiled Market."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
             "[额外物品条] 新增更多刻希亚物品.",
+            "[额外物品条] 新增更多奖励箱子物品.",
             "[聊天文字] 适配最新版本的 ElvUI 聊天模块.",
             "[美化皮肤] 修复了塔内心能选择时的边框皮肤错误.",
+            "[美化皮肤] 修复了单位框体阴影不正确的错误.",
+            "[美化皮肤] 修复了 ReHack 的皮肤.",
             "[天赋管家] 修复了偶尔在战斗中更新按钮导致的错误."
         },
         ["zhTW"] = {
             "[額外物品條] 新增更多科西亞物品.",
+            "[額外物品條] 新增更多獎勵箱子物品.",
             "[聊天文字] 適配最新版本的 ElvUI 聊天模組.",
             "[美化皮膚] 修復了塔內心能選擇時的邊框皮膚錯誤.",
+            "[美化皮膚] 修復了單位框架陰影不正確的錯誤.",
+            "[美化皮膚] 修復了 ReHack 的皮膚.",
             "[天賦管家] 修復了偶爾在戰鬥中更新按鍵導致的錯誤."
         },
         ["enUS"] = {
             "[Extra Item Bar] Add more items from Korthia.",
+            "[Extra Item Bar] Add more reward boxes.",
             "[Chat Text] Be compatible with latest ElvUI changes.",
-            "[Skins] Fix the bug that Player Choice Frame show an unexcepted border in Torghast.",
-            "[Talent Manager] Fix the bug that updating buttons in combat."
+            "[Skins] Fixed the bug that Player Choice Frame show an unexcepted border in Torghast.",
+            "[Skins] Fixed the position of shadows on unitframes.",
+            "[Skins] Fixed ReHack Skin.",
+            "[Talent Manager] Fixed the bug that updating buttons in combat."
         },
         ["koKR"] = {
             "[아이템 바] 더 많은 코르시아 아이템이 추가되였습니다.",
+            "[Extra Item Bar] Add more reward boxes.",
             "[채팅 문자] 최신 버전의 ElvUI 채팅 모듈과 호환됩니다.",
             "[스킨] 토르가스트에서 령 능력 선택시 테두리가 나오는 문제를 수정하였습니다.",
+            "[Skins] Fix the position of shadows on unitframes.",
+            "[Skins] Fixed ReHack Skin.",
             "[특성 관리자] 전투 중 간헐적으로 버튼 업데이트로 인해 발생하는 문제를 수정하였습니다."
         }
     }

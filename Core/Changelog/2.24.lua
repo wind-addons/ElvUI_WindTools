6002475fc7d3dde43c45bbb1b093c1ae1f37ea96
Zhou Fang <fang2hou@outlook.com>
2021-08-08 08:34:13 +0900

    changelog

---
+++ Core/Changelog/2.24.lua
@@ -0,0 +1,35 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[224] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[标签] 改善标签的运行性能.",
+            "[美化皮肤] 修复大秘境内的玩家选择框体美化."
+        },
+        ["zhTW"] = {
+            "[標籤] 改善標籤的執行性能.",
+            "[美化皮膚] 修復傳奇鑰石地下城中的玩家選擇框架美化."
+        },
+        ["enUS"] = {
+            "[Tags] Optimize the performance of additional tags.",
+            "[Skins] Fix the shadows of PlayerChoiceFrame in mythic dungeons."
+        },
+        ["koKR"] = {
+            "[Tags] Optimize the performance of additional tags.",
+            "[Skins] Fix the shadows of PlayerChoiceFrame in mythic dungeons."
+        }
+    }
+}

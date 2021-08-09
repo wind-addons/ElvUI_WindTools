dc8d77ce34c3ba94bdb90d1d1c57fd6286d99ccc
talmus76 <73485793+talmus76@users.noreply.github.com>
2021-08-09 21:22:29 +0900

    Updated koKR changelog

--- Core/Changelog/2.24.lua
+++ Core/Changelog/2.24.lua
@@ -17,7 +17,7 @@ W.Changelog[224] = {
         },
         ["koKR"] = {
             "[스킨] Meeting Stone EX 스킨 추가.",
-            "[Objective Progress] Update mob data to latest version."
+            "[목표 진행] 몬스터 데이터베이스가 최신 버전으로 업데이트되었습니다."
         }
     },
     NEW = {
@@ -37,9 +37,9 @@ W.Changelog[224] = {
             "[Friend List] Add an option to hide level information completely." 
         },
         ["koKR"] = {
-            "[Misc] Add new features to close world map after player enters combat.",
-            "[Skins] Add new skin for Blizzard Event Trace.",
-            "[Friend List] Add an option to hide level information completely."
+            "[기타] 전투 진입 시 자동으로 월드맵을 닫는 기능을 추가하였습니다.",
+            "[스킨] Blizzard Event Trace 스킨 추가.",
+            "[친구 목록] 레벨을 완전히 숨기는 옵션이 추가되었습니다."
         }
     },
     IMPROVEMENT = {
@@ -61,7 +61,7 @@ W.Changelog[224] = {
         ["koKR"] = {
             "[태그] 추가된 태그의 성능을 최적화하였습니다.",
             "[스킨] 쐐기돌 던전에서 령 능력 선택 창의 그림자를 수정합니다.",
-            "[Turn In] Update ignore NPCs list."
+            "[자동 수락] 무시할 NPC 목록을 업데이트하였습니다."
         }
     }
 }

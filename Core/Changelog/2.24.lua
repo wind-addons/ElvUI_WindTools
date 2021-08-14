9a9f147bcb38184b121d308480d2c83101951e23
Zhou Fang <fang2hou@outlook.com>
2021-08-14 18:17:27 +0900

    changelog

--- Core/Changelog/2.24.lua
+++ Core/Changelog/2.24.lua
@@ -4,18 +4,22 @@ W.Changelog[224] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
+            "部分颜色选项支持了 Alpha 通道.",
             "[美化皮肤] 支持了 Meeting Stone EX 组件.",
             "[任务信息] 怪物数据库更新到最新."
         },
         ["zhTW"] = {
+            "部分色彩選項支援 Alpha 通道.",
             "[美化皮膚] 支援 Meeting Stone EX 組件.",
             "[任務信息] 怪物數據資料更新到最新."
         },
         ["enUS"] = {
+            "Some color options now support alpha channel.",
             "[Skins] Add support of Meeting Stone EX.",
             "[Objective Progress] Update mob data to latest version."
         },
         ["koKR"] = {
+            "Some color options now support alpha channel.",
             "[스킨] Meeting Stone EX 스킨 추가.",
             "[목표 진행] 몬스터 데이터베이스가 최신 버전으로 업데이트되었습니다."
         }
@@ -25,25 +29,29 @@ W.Changelog[224] = {
             "[其他] 添加了进入战斗自动关闭世界地图功能.",
             "[美化皮肤] 添加了事件追踪工具的美化.",
             "[好友列表] 添加了完全隐藏等级的选项.",
-            "[静音] 添加了闷燃之心的选项."
+            "[静音] 添加了闷燃之心的选项.",
+            "[地图] 支持对战争迷雾进行染色."
         },
         ["zhTW"] = {
             "[其他] 新增了進入戰鬥自動關閉世界地圖功能.",
             "[美化皮膚] 新增了事件追蹤工具的美化.",
             "[好友列表] 新增了一個完全隱藏等級的選項.",
-            "[靜音] 新增了燃心的選項."
+            "[靜音] 新增了燃心的選項.",
+            "[世界地圖] 新增染色戰爭迷霧的選項."
         },
         ["enUS"] = {
             "[Misc] Add new features to close world map after player enters combat.",
             "[Skins] Add new skin for Blizzard Event Trace.",
             "[Friend List] Add an option to hide level information completely.",
-            "[Mute] Add an new option for Smolderheart."
+            "[Mute] Add an new option for Smolderheart.",
+            "[World Map] Add color option of Fog of War."
         },
         ["koKR"] = {
             "[기타] 전투 진입 시 자동으로 월드맵을 닫는 기능을 추가하였습니다.",
             "[스킨] Blizzard Event Trace 스킨 추가.",
             "[친구 목록] 레벨을 완전히 숨기는 옵션이 추가되었습니다.",
-            "[음소거] 이글심장 관련 옵션을 추가하였습니다."
+            "[음소거] 이글심장 관련 옵션을 추가하였습니다.",
+            "[World Map] Add color option of Fog of War."
         }
     },
     IMPROVEMENT = {
@@ -52,6 +60,7 @@ W.Changelog[224] = {
             "[美化皮肤] 修复大秘境内的玩家选择框体美化.",
             "[美化皮肤] 适配最新版本网易集合石.",
             "[美化皮肤] 修复 WeakAuras 的进度条内图标背景错误.",
+            "[美化皮肤] 适当缩小 Immersion 任务文本字体大小.",
             "[交接] 更新了忽略 NPC 列表."
         },
         ["zhTW"] = {
@@ -59,6 +68,7 @@ W.Changelog[224] = {
             "[美化皮膚] 修復傳奇鑰石地下城中的玩家選擇框架美化.",
             "[美化皮膚] 支援最新版本的網易集合石.",
             "[美化皮膚] 修復 WeakAuras 進度條內圖示背景錯誤.",
+            "[美化皮膚] 適當縮小 Immersion 任務文本字體尺寸.",
             "[交接] 更新了忽略 NPC 列表."
         },
         ["enUS"] = {
@@ -66,6 +76,7 @@ W.Changelog[224] = {
             "[Skins] Fix the shadows of PlayerChoiceFrame in mythic dungeons.",
             "[Skins] Optimize the skin of NetEase Meeting Stone.",
             "[Skins] Fix the backdrop of icons inside WeakAuras aurabars.",
+            "[Skins] Scale down the font size of Immersion quest text.",
             "[Turn In] Update ignore NPCs list."
         },
         ["koKR"] = {
@@ -73,6 +84,7 @@ W.Changelog[224] = {
             "[스킨] 쐐기돌 던전에서 령 능력 선택 창의 그림자를 수정합니다.",
             "[스킨] NetEase Meeting Stone 스킨 최적화.",
             "[스킨] WeakAuras 오라바 내부의 아이콘 배경을 수정했습니다.",
+            "[Skins] Scale down the font size of Immersion quest text.",
             "[자동 수락] 무시할 NPC 목록을 업데이트하였습니다."
         }
     }

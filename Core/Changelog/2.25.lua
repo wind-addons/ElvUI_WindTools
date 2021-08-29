8428f2561181d643758c897d81092d9b89a35238
Zhou Fang <fang2hou@outlook.com>
2021-08-29 11:09:09 +0900

    2.25 start

---
+++ Core/Changelog/2.25.lua
@@ -0,0 +1,31 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[225] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[其他] 添加了进入战斗自动关闭 ElvUI 背包功能."
+        },
+        ["zhTW"] = {
+            "[其他] 新增了進入戰鬥自動關閉 ElvUI 背包功能."
+        },
+        ["enUS"] = {
+            "[Misc] Add new features to close ElvUI Bag after player enters combat."
+        },
+        ["koKR"] = {
+            "[Misc] Add new features to close ElvUI Bag after player enters combat."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    }
+}

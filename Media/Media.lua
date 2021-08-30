ef339d22b38ea996d8e012705ede11b747511a07
Zhou Fang <fang2hou@outlook.com>
2021-08-30 16:29:12 +0900

    add forward icon

--- Media/Media.lua
+++ Media/Media.lua
@@ -134,6 +134,7 @@ AddMedia("buttonLock", "Button/Lock.tga", "Icons")
 AddMedia("buttonUnlock", "Button/Unlock.tga", "Icons")
 AddMedia("buttonMinus", "Button/Minus.tga", "Icons")
 AddMedia("buttonPlus", "Button/Plus.tga", "Icons")
+AddMedia("buttonForward", "Button/Forward.tga", "Icons")
 
 AddMedia("inspectGemBG", "InspectGemBG.blp", "Textures")
 AddMedia("exchange", "Exchange.tga", "Textures")

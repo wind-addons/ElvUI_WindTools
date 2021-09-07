165b40d16b4bd9095df4de46afb9aa10c4999971
Zhou Fang <fang2hou@outlook.com>
2021-09-07 11:28:19 +0900

    optimize the shadow if icon attached

--- Modules/Skins/ElvUI/CastBars.lua
+++ Modules/Skins/ElvUI/CastBars.lua
@@ -5,10 +5,53 @@ local UF = E:GetModule("UnitFrames")
 local _G = _G
 
 function S:ElvUI_UnitFrames_SkinCastBar(_, frame)
-    self:CreateBackdropShadow(frame.Castbar, true)
-    if not frame.db.castbar.iconAttached then
-        self:CreateShadow(frame.Castbar.ButtonIcon.bg)
+    if not frame.Castbar then
+        return
     end
+
+    local db = frame.db.castbar
+
+    if not frame.Castbar.windShadowBackdrop then
+        frame.Castbar.windShadowBackdrop = CreateFrame("Frame", nil, frame.Castbar)
+        frame.Castbar.windShadowBackdrop:SetFrameStrata(frame.Castbar.backdrop:GetFrameStrata())
+        frame.Castbar.windShadowBackdrop:SetFrameLevel(frame.Castbar.backdrop:GetFrameLevel() or 1)
+    end
+
+    local windBg = frame.Castbar.windShadowBackdrop
+    local iconBg = frame.Castbar.ButtonIcon.bg
+
+    if not db.iconAttached then
+        if not windBg.mode or windBg.mode ~= "NotAttach" then
+            -- Icon shadow
+            self:CreateShadow(frame.Castbar.ButtonIcon.bg)
+            if frame.Castbar.ButtonIcon.bg.shadow then
+                frame.Castbar.ButtonIcon.bg.shadow:Show()
+            end
+
+            -- Bar shadow
+            windBg:ClearAllPoints()
+            windBg:SetAllPoints(frame.Castbar.backdrop)
+            windBg.mode = "NotAttach"
+        end
+    else
+        if not windBg.mode or windBg.mode ~= "Attach" then
+            -- Disable icon shadow
+            if frame.Castbar.ButtonIcon.bg.shadow then
+                frame.Castbar.ButtonIcon.bg.shadow:Hide()
+            end
+
+            -- |-- Icon --| ---------------- Time Bar ---------------|
+            -- |---------------- windShadowBackdrop -----------------|
+            windBg:ClearAllPoints()
+            windBg:Point("TOPRIGHT", frame.Castbar.backdrop, "TOPRIGHT")
+            windBg:Point("BOTTOMRIGHT", frame.Castbar.backdrop, "BOTTOMRIGHT")
+            windBg:Point("TOPLEFT", iconBg, "TOPLEFT")
+            windBg:Point("BOTTOMLEFT", iconBg, "BOTTOMLEFT")
+            windBg.mode = "Attach"
+        end
+    end
+
+    self:CreateShadow(windBg)
 end
 
 function S:ElvUI_CastBars()

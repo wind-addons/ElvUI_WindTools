0515dd11d9526b28d705ecbbef43ea8d3c63add9
Zhou Fang <fang2hou@outlook.com>
2021-09-07 19:51:28 +0900

    new maw buff skin

--- Modules/Skins/Blizzard/Scenario.lua
+++ Modules/Skins/Blizzard/Scenario.lua
@@ -3,6 +3,32 @@ local S = W:GetModule("Skins")
 
 local _G = _G
 
+function S:SkinMawBuffsContainer(container)
+    container:StripTextures()
+    container:GetHighlightTexture():Kill()
+    container:GetPushedTexture():Kill()
+    local pushed = container:CreateTexture()
+    self:Reposition(pushed, container, 0, -11, -11, -17, -4)
+    pushed:SetBlendMode("ADD")
+    local vr, vg, vb = unpack(E.media.rgbvaluecolor)
+    pushed:SetColorTexture(vr, vg, vb, 0.2)
+    container:SetPushedTexture(pushed)
+    container.SetHighlightAtlas = E.noop
+    container.SetPushedAtlas = E.noop
+    container.SetWidth = E.noop
+    container.SetPushedTextOffset = E.noop
+
+    container:CreateBackdrop("Transparent")
+    self:Reposition(container.backdrop, container, 1, -10, -10, -16, -3)
+    self:CreateBackdropShadow(container)
+
+    local blockList = container.List
+    blockList:StripTextures()
+    blockList:CreateBackdrop("Transparent")
+    self:Reposition(blockList.backdrop, blockList, 1, -11, -11, -6, -6)
+    self:CreateBackdropShadow(blockList)
+end
+
 function S:ScenarioStage_CustomizeBlock(stageBlock, scenarioType, widgetSetID, textureKitID)
     stageBlock.NormalBG:SetTexture("")
     stageBlock.FinalBG:SetTexture("")
@@ -29,10 +55,14 @@ function S:ScenarioStageWidgetContainer()
         local bar = widgetFrame.TimerBar
 
         if bar and not bar.windStyle then
-            hooksecurefunc(bar, "SetStatusBarAtlas", function(frame)
-                frame:SetStatusBarTexture(E.media.normTex)
-                frame:SetStatusBarColor(unpack(E.media.rgbvaluecolor))
-            end)
+            hooksecurefunc(
+                bar,
+                "SetStatusBarAtlas",
+                function(frame)
+                    frame:SetStatusBarTexture(E.media.normTex)
+                    frame:SetStatusBarColor(unpack(E.media.rgbvaluecolor))
+                end
+            )
             bar:CreateBackdrop("Transparent")
             bar.windStyle = true
         end
@@ -48,8 +78,6 @@ function S:ScenarioStageWidgetContainer()
     end
 end
 
-
-
 function S:ScenarioStage()
     if not self:CheckDB(nil, "scenario") then
         return
@@ -57,6 +85,8 @@ function S:ScenarioStage()
 
     self:SecureHook("ScenarioStage_CustomizeBlock")
     self:SecureHook(_G.SCENARIO_CONTENT_TRACKER_MODULE, "Update", "ScenarioStageWidgetContainer")
+    self:SkinMawBuffsContainer(ScenarioBlocksFrame.MawBuffsBlock.Container)
+    self:SkinMawBuffsContainer(MawBuffsBelowMinimapFrame.Container)
 end
 
 S:AddCallback("ScenarioStage")
--- Modules/Skins/Core.lua
+++ Modules/Skins/Core.lua
@@ -393,6 +393,14 @@ do
     end
 end
 
+function S:Reposition(frame, target, border, top, bottom, left, right)
+    frame:ClearAllPoints()
+    frame:SetPoint("TOPLEFT", target, "TOPLEFT", -left - border, top + border)
+    frame:SetPoint("TOPRIGHT", target, "TOPRIGHT", right + border, top + border)
+    frame:SetPoint("BOTTOMLEFT", target, "BOTTOMLEFT", -left - border, -bottom - border)
+    frame:SetPoint("BOTTOMRIGHT", target, "BOTTOMRIGHT", right + border, -bottom - border)
+end
+
 -- 初始化，将不需要监视插件载入情况的函数全部进行执行
 function S:Initialize()
     if not E.private.WT.skins.enable then

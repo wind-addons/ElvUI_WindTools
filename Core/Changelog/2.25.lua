1017bb931e92b6ac3551dbbfa24f7114a6fdd0f1
Zhou Fang <fang2hou@outlook.com>
2021-09-07 11:32:47 +0900

    changelog

--- Core/Changelog/2.25.lua
+++ Core/Changelog/2.25.lua
@@ -22,16 +22,24 @@ W.Changelog[225] = {
     },
     NEW = {
         ["zhCN"] = {
-            "[其他] 添加了进入战斗自动关闭背包功能."
+            "[其他] 添加了进入战斗自动关闭背包功能.",
+            "[美化皮肤] 新增了 ElvUI 光环条的美化.",
+            "[美化皮肤] 优化并新增了 ElvUI 施法条图标在内时的美化."
         },
         ["zhTW"] = {
-            "[其他] 新增了進入戰鬥自動關閉背包功能."
+            "[其他] 新增了進入戰鬥自動關閉背包功能.",
+            "[美化皮膚] 新增了 ElvUI 光環條的美化.",
+            "[美化皮膚] 優化并新增了 ElvUI 施法條圖示在內時的美化."
         },
         ["enUS"] = {
-            "[Misc] Add new features to close bags after player enters combat."
+            "[Misc] Add new features to close bags after player enters combat.",
+            "[Skins] Add the skin of aura bars for ElvUI unit frames.",
+            "[Skins] Optimize the skin of ElvUI cast bars which contains an attached icon."
         },
         ["koKR"] = {
-            "[기타] 전투가 시작되면 자동으로 가방을 닫는 기능 추가."
+            "[기타] 전투가 시작되면 자동으로 가방을 닫는 기능 추가.",
+            "[Skins] Add the skin of aura bars for ElvUI unit frames.",
+            "[Skins] Optimize the skin of ElvUI cast bars which contains an attached icon."
         }
     },
     IMPROVEMENT = {
@@ -41,7 +49,10 @@ W.Changelog[225] = {
             "[游戏条] 修复了不启用自动缩放时背景错位的问题.",
             "[团队标记] 修复了不启用自动缩放时背景错位的问题.",
             "[小地图按钮] 修复了不启用自动缩放时背景错位的问题.",
-            "[战斗提醒] 修复了不启用自动缩放时错位的问题."
+            "[战斗提醒] 修复了不启用自动缩放时错位的问题.",
+            "[美化皮肤] 修复了 ElvUI 单位框体的光环美化",
+            "[美化皮肤] 修复了 ElvUI 动作条飞出按钮的美化.",
+            "[任务列表] 优化了标题缩写功能."
         },
         ["zhTW"] = {
             "[其他] 部分選項更改後毋須重載介面.",
@@ -49,21 +60,32 @@ W.Changelog[225] = {
             "[遊戲條] 修復了不啟用自動縮放時背景錯位的問題.",
             "[團隊標記] 修復了不啟用自動縮放時背景錯位的問題.",
             "[小地圖按鍵] 修復了不啟用自動縮放時背景錯位的問題.",
-            "[戰鬥提醒] 修復了不啟用自動縮放時錯位的問題."
+            "[戰鬥提醒] 修復了不啟用自動縮放時錯位的問題.",
+            "[美化皮膚] 修復了 ElvUI 單位框架的光環美化.",
+            "[美化皮膚] 修復了 ElvUI 快捷列飛出按鍵的美化.",
+            "[任務列表] 優化了標題縮寫功能."
         },
         ["enUS"] = {
             "[Misc] Some options now can be changed without reloading user interface.",
             "[Extra Item Bar] Fix the backdrop scale issue when auto-scale disabled.",
             "[Game Bar] Fix the backdrop scale issue when auto-scale disabled.",
             "[Raid Markers] Fix the backdrop scale issue when auto-scale disabled.",
-            "[Combat Alert] Fix the scale issue when auto-scale disabled."
+            "[Minimap Buttons] Fix the backdrop scale issue when auto-scale disabled.",
+            "[Combat Alert] Fix the scale issue when auto-scale disabled.",
+            "[Skins] Fix the skin for auras on ElvUI unit frames.",
+            "[Skins] Fix the skin for flyout buttons in ElvUI action bar.",
+            "[Objective Tracker] Optimize the abbreviation feature for long titles."
         },
         ["koKR"] = {
             "[기타] 이제 일부 옵션은 사용자 인터페이스를 다시 로드하지 않고 변경할 수 있습니다.",
             "[아이템 바] 자동 크기 조정이 활성화되지 않았을 때 배경이 어긋나는 문제를 수정했습니다.",
             "[게임 바] 자동 크기 조정이 활성화되지 않았을 때 배경이 어긋나는 문제를 수정했습니다.",
             "[공격대 징표] 자동 크기 조정이 활성화되지 않았을 때 배경이 어긋나는 문제를 수정했습니다.",
-            "[전투 알림] 자동 크기 조정이 활성화되지 않았을 때 크기 문제를 수정했습니다."
+            "[Minimap Buttons] Fix the backdrop scale issue when auto-scale disabled.",
+            "[전투 알림] 자동 크기 조정이 활성화되지 않았을 때 크기 문제를 수정했습니다.",
+            "[Skins] Fix the skin for auras on ElvUI unit frames.",
+            "[Skins] Fix the skin for flyout buttons in ElvUI action bar.",
+            "[Objective Tracker] Optimize the abbreviation feature for long titles."
         }
     }
 }

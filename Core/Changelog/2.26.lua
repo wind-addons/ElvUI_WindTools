2ae07e038344e081258539a49038413eb90c3758
Zhou Fang <fang2hou@outlook.com>
2021-09-14 11:08:30 +0900

    changelog

--- Core/Changelog/2.26.lua
+++ Core/Changelog/2.26.lua
@@ -3,21 +3,37 @@ local W = unpack(select(2, ...))
 W.Changelog[226] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
+        ["zhCN"] = {
+            "核心函数库更新."
+        },
+        ["zhTW"] = {
+            "核心函數庫更新."
+        },
+        ["enUS"] = {
+            "Update functions in core library."
+        },
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
+        ["zhCN"] = {
+            "핵심 기능 라이브러리 업데이트."
+        },
         ["zhTW"] = {},
         ["enUS"] = {},
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "兼容性检查工具更新."
+        },
+        ["zhTW"] = {
+            "相容性確認工具更新."
+        },
+        ["enUS"] = {
+            "Compatibility check updated."
+        },
+        ["koKR"] = {
+            "호환성 검사가 업데이트되었습니다."
+        }
     }
 }

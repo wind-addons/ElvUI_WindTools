f691df78a18348917efbe19700ce9a17fafe4091
Zhou Fang <fang2hou@outlook.com>
2021-11-01 11:34:52 +0900

    9.1.5 api change

--- Modules/Maps/SuperTracker.lua
+++ Modules/Maps/SuperTracker.lua
@@ -36,10 +36,8 @@ function ST:HookPin()
         return
     end
 
-    local EnumerateAllPins = _G.WorldMapFrame:EnumerateAllPins()
-    local pin = EnumerateAllPins()
-    while pin do
-        if pin.pinTemplate and pin.pinTemplate == "WaypointLocationPinTemplate" then
+    if _G.WorldMapFrame:GetNumActivePinsByTemplate("WaypointLocationPinTemplate") ~= 0 then
+        for pin in _G.WorldMapFrame:EnumeratePinsByTemplate("WaypointLocationPinTemplate") do
             if not self:IsHooked(pin, "OnMouseClickAction") then
                 self:SecureHook(
                     pin,
@@ -51,9 +49,7 @@ function ST:HookPin()
                     end
                 )
             end
-            break
         end
-        pin = EnumerateAllPins()
     end
 end
 

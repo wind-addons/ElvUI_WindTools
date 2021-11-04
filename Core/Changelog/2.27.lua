0210b4d618e1a9e789e81e790ab8c69ce479b3f9
Zhou Fang <fang2hou@outlook.com>
2021-11-04 10:17:59 +0900

    changelog

---
+++ Core/Changelog/2.27.lua
@@ -0,0 +1,31 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[227] = {
+    RELEASE_DATE = "2021/11/04",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "修复小地图图标材质错位问题."
+        },
+        ["zhTW"] = {
+            "修復小地圖圖示材質錯位的問題."
+        },
+        ["enUS"] = {
+            "Fix texture misplacement of minimap icon."
+        },
+        ["koKR"] = {
+            "Fix texture misplacement of minimap icon."
+        }
+    }
+}

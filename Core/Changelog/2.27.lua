91400af304ae295356ecc897be2a8de0e6cdfa7f
Zhou Fang <fang2hou@outlook.com>
2021-11-05 14:52:06 +0900

    changelog

--- Core/Changelog/2.27.lua
+++ Core/Changelog/2.27.lua
@@ -1,12 +1,20 @@
 local W = unpack(select(2, ...))
 
 W.Changelog[227] = {
-    RELEASE_DATE = "2021/11/04",
+    RELEASE_DATE = "2021/11/05",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "暂时屏蔽了收藏品界面(坐骑, 玩具箱等)的移动功能."
+        },
+        ["zhTW"] = {
+            "暫時屏蔽了收藏品介面(坐騎, 玩具箱等)的移動功能."
+        },
+        ["enUS"] = {
+            "Remove moving feature of collections journal for 9.1.5."
+        },
+        ["koKR"] = {
+            "Remove moving feature of collections journal for 9.1.5."
+        }
     },
     NEW = {
         ["zhCN"] = {},
@@ -16,16 +24,20 @@ W.Changelog[227] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "修复小地图图标材质错位问题."
+            "修复小地图图标材质错位问题.",
+            "修复已知物品染色."
         },
         ["zhTW"] = {
-            "修復小地圖圖示材質錯位的問題."
+            "修復小地圖圖示材質錯位的問題.",
+            "修復已知物品染色."
         },
         ["enUS"] = {
-            "Fix texture misplacement of minimap icon."
+            "Fix texture misplacement of minimap icon.",
+            "Fix Already Known."
         },
         ["koKR"] = {
-            "미니맵 아이콘의 잘못된 텍스처 위치를 수정합니다."
+            "미니맵 아이콘의 잘못된 텍스처 위치를 수정합니다.",
+            "Fix Already Known."
         }
     }
 }

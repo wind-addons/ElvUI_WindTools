78374a8aa425e7cbf9905f1d61fe2bbee42630b0
Zhou Fang <fang2hou@outlook.com>
2021-11-11 12:04:21 +0900

    changelog

--- Core/Changelog/2.28.lua
+++ Core/Changelog/2.28.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[228] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "修复了兼容性检查的设定. 你可以在 (信息)-(帮助) 的底部找到它."
+        },
+        ["zhTW"] = {
+            "修復了相容性檢查的設定. 你可以在 (信息)-(幫助) 的底部找到它."
+        },
+        ["enUS"] = {
+            "Fix compatibility check option. You can find it in the bottom of (Information)-(Help)."
+        },
+        ["koKR"] = {
+            "Fix compatibility check option. You can find it in the bottom of (Information)-(Help)."
+        }
     },
     NEW = {
         ["zhCN"] = {},
@@ -15,9 +23,21 @@ W.Changelog[228] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[信息] 更新日志的日期显示针对各语言进行了优化.",
+            "[进度] 修复了部分语言翻译带来的格式化错误."
+        },
+        ["zhTW"] = {
+            "[信息] 更新記錄的日期顯示針對各語言進行了優化.",
+            "[進度] 修復了部分語言翻譯帶來的格式化錯誤."
+        },
+        ["enUS"] = {
+            "[Information] Optimize the date formats for various languages.",
+            "[Progression] Fix the formatting issue caused by translation."
+        },
+        ["koKR"] = {
+            "[Information] Optimize the date formats for various languages.",
+            "[Progression] Fix the formatting issue caused by translation."
+        }
     }
 }

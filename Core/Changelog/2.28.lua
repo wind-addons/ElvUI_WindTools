b0b99077a84cf87bd79c6e85f99d23045f9ea489
Zhou Fang <fang2hou@outlook.com>
2021-11-19 20:08:52 +0900

    changelog

--- Core/Changelog/2.28.lua
+++ Core/Changelog/2.28.lua
@@ -25,19 +25,31 @@ W.Changelog[228] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "[信息] 更新日志的日期显示针对各语言进行了优化.",
-            "[进度] 修复了部分语言翻译带来的格式化错误."
+            "[进度] 修复了部分语言翻译带来的格式化错误.",
+            "[美化皮肤] 修复了下拉菜单的背景透明度.",
+            "[美化皮肤] 修复了 WeakAura 设定皮肤.",
+            '[好友列表] 新增了 "使命召唤: 先锋" 的游戏图标.'
         },
         ["zhTW"] = {
             "[信息] 更新記錄的日期顯示針對各語言進行了優化.",
-            "[進度] 修復了部分語言翻譯帶來的格式化錯誤."
+            "[進度] 修復了部分語言翻譯帶來的格式化錯誤.",
+            "[美化皮膚] 修復了下拉選單的背景透明度.",
+            "[美化皮膚] 修復了 WeakAura 設定皮膚.",
+            '[好友列表] 新增了 "決戰時刻: 先鋒" 的遊戲圖示.'
         },
         ["enUS"] = {
             "[Information] Optimize the date formats for various languages.",
-            "[Progression] Fix the formatting issue caused by translation."
+            "[Progression] Fix the formatting issue caused by translation.",
+            "[Skins] Fix transparent support of dropdown menus.",
+            "[Skins] Fix the skin of WeakAura Options.",
+            '[Friend List] Add a new game icon for "Call of Duty: Vanguard".'
         },
         ["koKR"] = {
             "[Information] Optimize the date formats for various languages.",
-            "[Progression] Fix the formatting issue caused by translation."
+            "[Progression] Fix the formatting issue caused by translation.",
+            "[Skins] Fix transparent support of dropdown menus.",
+            "[Skins] Fix the skin of WeakAura Options.",
+            '[Friend List] Add a new game icon for "Call of Duty: Vanguard".'
         }
     }
 }

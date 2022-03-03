66fdd98dc7f23bb090ecc26a5c1703644fc24894
Zhou Fang <fang2hou@outlook.com>
2022-03-03 18:24:32 +0900

    changelog

--- Core/Changelog/2.30.lua
+++ Core/Changelog/2.30.lua
@@ -9,15 +9,39 @@ W.Changelog[230] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[额外物品条] 新增了 9.2 版本中的新周常奖励物品. 感谢 mcc1"
+        },
+        ["zhTW"] = {
+            "[額外物品條] 新增了 9.2 版本中的新周常獎勵物品. 感謝 mcc1"
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Added new weekly reward items in 9.2 version. Thanks mcc1"
+        },
+        ["koKR"] = {
+            "[Extra Item Bar] Added new weekly reward items in 9.2 version. Thanks mcc1"
+        }
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[皮肤] 修复了寻求组队按钮的皮肤.",
+            "[任务列表] 修复了可能会产生预组队界面污染的问题.",
+            '[额外物品条] 使用缩写 "B" 来替代 "BUTTON".'
+        },
+        ["zhTW"] = {
+            "[皮膚] 修復了尋求隊伍按鍵的皮膚.",
+            "[任務列表] 修復了可能會產生預組隊伍介面污染的問題.",
+            "[額外物品條] 使用縮寫「B」來替代「BUTTON」."
+        },
+        ["enUS"] = {
+            "[Skin] Fixed the skin of the group finder button.",
+            "[Objective Tracker] Fixed the possible problem of causing premade group UI taint.",
+            '[Extra Item Bar] Use the abbreviation "B" instead of "BUTTON".'
+        },
+        ["koKR"] = {
+            "[Skin] Fixed the skin of the group finder button.",
+            "[Objective Tracker] Fixed the possible problem of causing premade group UI taint.",
+            '[Extra Item Bar] Use the abbreviation "B" instead of "BUTTON".'
+        }
     }
 }

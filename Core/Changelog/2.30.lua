769fc4bd2ee1b282aeed617f2e7cea5b7dd16672
Zhou Fang <fang2hou@outlook.com>
2022-03-25 12:59:08 +0900

    Update 2.30.lua

--- Core/Changelog/2.30.lua
+++ Core/Changelog/2.30.lua
@@ -24,21 +24,25 @@ W.Changelog[230] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[聊天文字] 适配代码到 ElvUI 12.72 版本.",
             "[皮肤] 修复了寻求组队按钮的皮肤.",
             "[任务列表] 修复了可能会产生预组队界面污染的问题.",
             '[额外物品条] 使用缩写 "B" 来替代 "BUTTON".'
         },
         ["zhTW"] = {
+            "[聊天文字] 适配代码到 ElvUI 12.72 版本.",
             "[皮膚] 修復了尋求隊伍按鍵的皮膚.",
             "[任務列表] 修復了可能會產生預組隊伍介面污染的問題.",
             "[額外物品條] 使用縮寫「B」來替代「BUTTON」."
         },
         ["enUS"] = {
+            "[Chat Text] Update with codes from ElvUI 12.72.",
             "[Skin] Fixed the skin of the group finder button.",
             "[Objective Tracker] Fixed the possible problem of causing premade group UI taint.",
             '[Extra Item Bar] Use the abbreviation "B" instead of "BUTTON".'
         },
         ["koKR"] = {
+            "[Chat Text] Update with codes from ElvUI 12.72.",
             "[스킨] 파티 찾기 버튼의 스킨을 수정했습니다.",
             "[퀘스트 추적기] 파티 구성하기 인터페이스의 오염을 일으킬 수 있는 문제를 수정했습니다.",
             '[아이템 바] 단축키에 마우스 버튼을 포함하여 설정할 경우 아이템 바에 표시되는 "BUTTON"을 "B"로 축약하여 표시합니다.'

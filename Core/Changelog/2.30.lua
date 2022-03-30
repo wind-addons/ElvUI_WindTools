b6e54f957fad25eaf86bfe4fa14a3a8969a389ae
Zhou Fang <fang2hou@outlook.com>
2022-03-30 15:44:59 +0900

    changelog

--- Core/Changelog/2.30.lua
+++ Core/Changelog/2.30.lua
@@ -4,20 +4,24 @@ W.Changelog[230] = {
     RELEASE_DATE = "2022/03/30",
     IMPORTANT = {
         ["zhCN"] = {
+            "[其他] 新增禁用战斗后拾取信息面板的功能.",
             '[进度] 新增"暗影国度钥石大师: 第三赛季"到进度追踪, "暗影国度钥石大师: 第一赛季"的追踪将默认关闭.',
             "[进度] 新增初诞者圣墓支持."
         },
         ["zhTW"] = {
+            "[其他] 新增禁用戰鬥後拾取訊息面板的功能.",
             '[进度] 新增"暗影之境鑰石王: 第三季"到進度追蹤, "暗影之境鑰石王: 第一季"的追蹤將預設關閉.',
             "[进度] 新增首創者聖塚支援."
         },
         ["enUS"] = {
-            '[Progression] Add "Shadowlands Keystone Master: Season Three" to tracking list, the tracking of "Shadowlands Keystone Master: Season One" will be disabled by default.',
-            "[Progression] Add the support of Sepulcher of the First Ones."
+            "[Other] Added a new feature of disabling loot info panel that auto showing after a combat.",
+            '[Progression] Added "Shadowlands Keystone Master: Season Three" to tracking list, the tracking of "Shadowlands Keystone Master: Season One" will be disabled by default.',
+            "[Progression] Added the support of Sepulcher of the First Ones."
         },
         ["koKR"] = {
-            '[Progression] Add "Shadowlands Keystone Master: Season Three" to tracking list, the tracking of "Shadowlands Keystone Master: Season One" will be disabled by default.',
-            "[Progression] Add the support of 태초의 존재의 매장터."
+            "[Other] Added a new feature of disabling loot info panel that auto showing after a combat.",
+            '[Progression] Added "Shadowlands Keystone Master: Season Three" to tracking list, the tracking of "Shadowlands Keystone Master: Season One" will be disabled by default.',
+            "[Progression] Added the support of 태초의 존재의 매장터."
         }
     },
     NEW = {
@@ -50,24 +54,28 @@ W.Changelog[230] = {
         ["zhCN"] = {
             "[聊天文字] 适配代码到 ElvUI 12.73 版本.",
             "[皮肤] 修复了寻求组队按钮的皮肤.",
+            "[皮肤] 修复了理发店的皮肤.",
             "[任务列表] 修复了可能会产生预组队界面污染的问题.",
             '[额外物品条] 使用缩写 "B" 来替代 "BUTTON".'
         },
         ["zhTW"] = {
             "[聊天文字] 适配代码到 ElvUI 12.73 版本.",
             "[皮膚] 修復了尋求隊伍按鍵的皮膚.",
+            "[皮膚] 修復了美容沙龍的皮膚.",
             "[任務列表] 修復了可能會產生預組隊伍介面污染的問題.",
             "[額外物品條] 使用縮寫「B」來替代「BUTTON」."
         },
         ["enUS"] = {
             "[Chat Text] Update with codes from ElvUI 12.73.",
             "[Skin] Fixed the skin of the group finder button.",
+            "[Skin] Fixed the skin of barber shop.",
             "[Objective Tracker] Fixed the possible problem of causing premade group UI taint.",
             '[Extra Item Bar] Use the abbreviation "B" instead of "BUTTON".'
         },
         ["koKR"] = {
             "[채팅 문자] ElvUI 12.72 버전의 코드로 업데이트했습니다.",
             "[스킨] 파티 찾기 버튼의 스킨을 수정했습니다.",
+            "[스킨] 미용실의 스킨을 수정했습니다.",
             "[퀘스트 추적기] 파티 구성하기 인터페이스의 오염을 일으킬 수 있는 문제를 수정했습니다.",
             '[아이템 바] 단축키에 마우스 버튼을 포함하여 설정할 경우 아이템 바에 표시되는 "BUTTON"을 "B"로 축약하여 표시합니다.'
         }

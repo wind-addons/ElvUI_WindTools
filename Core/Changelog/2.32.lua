07ab90a5136bf7b410534d908d44fbac15b3e4a2
Zhou Fang <fang2hou@outlook.com>
2022-04-11 00:01:22 +0900

    start 2.32

---
+++ Core/Changelog/2.32.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[232] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[额外物品条] 新增了掮灵开悟者补给品. 感谢 mcc1"
+        },
+        ["zhTW"] = {
+            "[額外物品條] 新增了受啟迪的仲介者物資. 感謝 mcc1"
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Added enlightened broker supplies. Thanks mcc1",
+        },
+        ["koKR"] = {
+            "[Extra Item Bar] Added 깨달음을 얻은 중개자 보급품. Thanks mcc1",
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[好友列表] 现在可以显示不同账号地区的信息了."
+        },
+        ["zhTW"] = {
+            "[好友列表] 現在可以顯示不同賬號地區的資訊了."
+        },
+        ["enUS"] = {
+            "[Friend List] Now you can show different account region info."
+        },
+        ["koKR"] = {
+            "[Friend List] Now you can show different account region info."
+        },
+    }
+}

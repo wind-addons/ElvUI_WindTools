cf68f89f59e73e5940a27c01180382bdd815cba0
Zhou Fang <fang2hou@outlook.com>
2022-04-14 15:23:20 +0900

    changelog

--- Core/Changelog/2.32.lua
+++ Core/Changelog/2.32.lua
@@ -10,30 +10,42 @@ W.Changelog[232] = {
     },
     NEW = {
         ["zhCN"] = {
-            "[额外物品条] 新增了掮灵开悟者补给品. 感谢 mcc1"
+            "[其他] 新增了一些额外的按键绑定功能, 可以在暴雪的快捷键绑定中设置.",
+            "[额外物品条] 新增了掮灵开悟者补给品. 感谢 mcc1",
+            "[美化皮肤] 新增了点击绑定界面的美化皮肤."
         },
         ["zhTW"] = {
-            "[額外物品條] 新增了受啟迪的仲介者物資. 感謝 mcc1"
+            "[其他] 新增了一些額外的按鍵綁定功能, 可以在暴雪的按鍵綁定中設置.",
+            "[額外物品條] 新增了受啟迪的仲介者物資. 感謝 mcc1",
+            "[美化皮膚] 新增了點擊綁定介面的美化皮膚."
         },
         ["enUS"] = {
+            "[Misc] Add some extra key binding features, you can change configuration in Blizzard Key Binding.",
             "[Extra Item Bar] Added enlightened broker supplies. Thanks mcc1",
+            "[Skins] Added new skin for click binding frame ."
         },
         ["koKR"] = {
+            "[Misc] Add some extra key binding features, you can change configuration in Blizzard Key Binding.",
             "[Extra Item Bar] Added 깨달음을 얻은 중개자 보급품. Thanks mcc1",
+            "[Skins] Added new skin for click binding frame ."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[任务列表] 修复了在停用取消标记功能时标记和完成图标重叠的问题.",
             "[好友列表] 现在可以显示不同账号地区的信息了."
         },
         ["zhTW"] = {
+            "[任務列表] 修復了在停用取消標記功能時標記和完成图示重疊的問題.",
             "[好友列表] 現在可以顯示不同賬號地區的資訊了."
         },
         ["enUS"] = {
+            '[Quest List] Fixed the problem that the checked icon and dash overlap while the option "No Dash" is disabling.',
             "[Friend List] Now you can show different account region info."
         },
         ["koKR"] = {
+            '[Quest List] Fixed the problem that the checked icon and dash overlap while the option "No Dash" is disabling.',
             "[Friend List] Now you can show different account region info."
-        },
+        }
     }
 }

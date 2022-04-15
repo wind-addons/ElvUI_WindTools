aeb8af0cbfa68b009972896dd33ac8156bfeeba6
Zhou Fang <fang2hou@outlook.com>
2022-04-16 00:49:51 +0900

    changelog

--- Core/Changelog/2.32.lua
+++ Core/Changelog/2.32.lua
@@ -3,13 +3,22 @@ local W = unpack(select(2, ...))
 W.Changelog[232] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[移动框体] 提升了移动功能的稳定性."
+        },
+        ["zhTW"] = {
+            "[移動框架] 提升了移動功能的穩定性."
+        },
+        ["enUS"] = {
+            "[Move Frames] Improve the stability of the moving function."
+        },
+        ["koKR"] = {
+            "[Move Frames] Improve the stability of the moving function."
+        }
     },
     NEW = {
         ["zhCN"] = {
+            "[移动框体] 新增了一些新框体.",
             "[其他] 新增了一些额外的按键绑定功能, 可以在暴雪的快捷键绑定中设置.",
             "[额外物品条] 新增了掮灵开悟者补给品. 感谢 mcc1",
             "[美化皮肤] 新增了暴雪点击绑定界面的美化皮肤.",
@@ -18,6 +27,7 @@ W.Changelog[232] = {
             "[美化皮肤] 新增了社区设定界面皮肤."
         },
         ["zhTW"] = {
+            "[移動框架] 新增了一些新框架.",
             "[其他] 新增了一些額外的按鍵綁定功能, 可以在暴雪的按鍵綁定中設置.",
             "[額外物品條] 新增了受啟迪的仲介者物資. 感謝 mcc1",
             "[美化皮膚] 新增了暴雪點擊綁定介面的美化皮膚.",
@@ -26,6 +36,7 @@ W.Changelog[232] = {
             "[美化皮膚] 新增了 ElvUI 按鍵綁定界面皮膚."
         },
         ["enUS"] = {
+            "[Move Frames] Add support of more frames.",
             "[Misc] Add some extra key binding features, you can change configuration in Blizzard Key Binding.",
             "[Extra Item Bar] Added enlightened broker supplies. Thanks mcc1",
             "[Skins] Added new skin for Blizzard click binding frame.",
@@ -34,6 +45,7 @@ W.Changelog[232] = {
             "[Skins] Added new skin for community settings."
         },
         ["koKR"] = {
+            "[Move Frames] Add support of more frames.",
             "[기타] Blizzard 단축키 설정에 몇가지 추가 단축키 설정을 추가 했습니다.",
             "[아이템 바] 깨달음을 얻은 중개자 보급품이 추가 되였습니다. Thanks mcc1",
             "[스킨] Blizzard 클릭 바인딩 스킨을 추가 하였습니다.",
@@ -46,22 +58,26 @@ W.Changelog[232] = {
         ["zhCN"] = {
             "[装备观察] 新增多种附魔显示, 优化图标显示逻辑.",
             "[任务列表] 修复了在停用取消标记功能时标记和完成图标重叠的问题.",
-            "[好友列表] 现在可以显示不同账号地区的信息了."
+            "[好友列表] 现在可以显示不同账号地区的信息了.",
+            "[美化皮膚] 优化了 ElvUI 动作条皮肤."
         },
         ["zhTW"] = {
             "[裝備觀察] 新增多種附魔顯示, 優化圖示顯示逻輯.",
             "[任務列表] 修復了在停用取消標記功能時標記和完成图示重疊的問題.",
-            "[好友列表] 現在可以顯示不同賬號地區的資訊了."
+            "[好友列表] 現在可以顯示不同賬號地區的資訊了.",
+            "[美化皮膚] 優化了 ElvUI 快捷列皮膚."
         },
         ["enUS"] = {
             "[Inspect] Added more enchants support, optimized icon display logic.",
             '[Quest List] Fixed the problem that the checked icon and dash overlap while the option "No Dash" is disabling.',
-            "[Friend List] Now you can show different account region info."
+            "[Friend List] Now you can show different account region info.",
+            "[Skins] Optimized ElvUI action bar skin."
         },
         ["koKR"] = {
             "[Inspect] Added more enchants support, optimized icon display logic.",
             "[퀘스트 목록] 표시 해제가 비활성화된 경우 표시 및 완료 아이콘이 겹치는 문제가 수정되었습니다.",
-            "[친구 목록] 이제 다른 계정 지역에 대한 정보를 표시할 수 있습니다."
+            "[친구 목록] 이제 다른 계정 지역에 대한 정보를 표시할 수 있습니다.",
+            "[Skins] Optimized ElvUI action bar skin."
         }
     }
 }

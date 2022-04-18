bc4224a2ee2c93ffcca0d565c1b2cf7b64814f7f
Zhou Fang <fang2hou@outlook.com>
2022-04-18 22:38:44 +0900

    set progression priority to 2

--- Modules/Tooltips/Progression.lua
+++ Modules/Tooltips/Progression.lua
@@ -558,4 +558,4 @@ function T:INSPECT_ACHIEVEMENT_READY(event, GUID)
     self:UnregisterEvent("INSPECT_ACHIEVEMENT_READY")
 end
 
-T:AddInspectInfoCallback(1, "Progression")
+T:AddInspectInfoCallback(2, "Progression")

28574d976689b35f33fdc17568341c369440749a
Zhou Fang <fang2hou@outlook.com>
2022-04-24 03:08:13 +0900

    changelog

--- Core/Changelog/2.33.lua
+++ Core/Changelog/2.33.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[233] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "为亚洲语系客户端引入全部的 ElvUI 拉丁字体."
+        },
+        ["zhTW"] = {
+            "为亞洲語系客戶端引入全部的 ElvUI 拉丁字型."
+        },
+        ["enUS"] = {
+            "Import all ElvUI latin fonts to clients in asian."
+        },
+        ["koKR"] = {
+            "Import all ElvUI latin fonts to clients in asian."
+        }
     },
     NEW = {
         ["zhCN"] = {
@@ -32,19 +40,27 @@ W.Changelog[233] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[矩形小地图] 提升了稳定性.",
             "[小地图按钮] 修复了日历按钮的材质无法去除的错误.",
+            "[小地图按钮] 修复了要塞/盟约按钮错位的问题.",
             "[美化皮肤] 移除了强制 ElvUI 动作条背景为透明的代码."
         },
         ["zhTW"] = {
+            "[矩形小地圖] 提升了穩定性.",
             "[小地圖按鍵] 修復了行事曆按鍵的材質無法去除的錯誤.",
+            "[小地圖按鍵] 修復了要塞/盟約按鍵錯位的問題.",
             "[美化皮膚] 移除了強制 ElvUI 快捷列背景為透明的代碼."
         },
         ["enUS"] = {
+            "[Rectangle Minimap] Improved stability.",
             "[Minimap Buttons] Fixed the texture not removed problem within skinned calendar icon.",
+            "[Minimap Buttons] Fixed the position of garrison/covenant buttons.",
             "[Skins] Removed the code that making ElvUI action bar background to be transparent."
         },
         ["koKR"] = {
+            "[Rectangle Minimap] Improved stability.",
             "[Minimap Buttons] Fixed the texture not removed problem within skinned calendar icon.",
+            "[Minimap Buttons] Fixed the position of garrison/covenant buttons.",
             "[Skins] Removed the code that making ElvUI action bar background to be transparent."
         }
     }

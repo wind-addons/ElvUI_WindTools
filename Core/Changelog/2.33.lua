b82572cb682121392f6892c47db62d121c18ab6e
Zhou Fang <fang2hou@outlook.com>
2022-04-24 00:26:40 +0900

    changelog

--- Core/Changelog/2.33.lua
+++ Core/Changelog/2.33.lua
@@ -1,6 +1,6 @@
 local W = unpack(select(2, ...))
 
-W.Changelog[999] = {
+W.Changelog[233] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {},
@@ -9,15 +9,43 @@ W.Changelog[999] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "新增了对 ElvUI Enhanced Again 的兼容性检查.",
+            "[美化皮肤] 新增了客服工单状态框体的皮肤.",
+            "[美化皮肤] 新增了 ElvUI 聊天语音面板的皮肤."
+        },
+        ["zhTW"] = {
+            "新增了對 ElvUI Enhanced Again 的兼容性檢查.",
+            "[美化皮膚] 新增了 GM 工單狀態框架的皮膚.",
+            "[美化皮膚] 新增了 ElvUI 對話語音面板的皮膚."
+        },
+        ["enUS"] = {
+            "Added compatibility check for ElvUI Enhanced Again.",
+            "[Skins] Added a skin for the GM ticket status frame.",
+            "[Skins] Added a skin for the ElvUI chat voice panel."
+        },
+        ["koKR"] = {
+            "ElvUI Enhanced Again에 대한 호환성 검사를 추가합니다.",
+            "[Skins] Added a skin for the GM ticket status frame.",
+            "[Skins] Added a skin for the ElvUI chat voice panel."
+        }
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[小地图按钮] 修复了日历按钮的材质无法去除的错误.",
+            "[美化皮肤] 移除了强制 ElvUI 动作条背景为透明的代码."
+        },
+        ["zhTW"] = {
+            "[小地圖按鍵] 修復了行事曆按鍵的材質無法去除的錯誤.",
+            "[美化皮膚] 移除了強制 ElvUI 快捷列背景為透明的代碼."
+        },
+        ["enUS"] = {
+            "[Minimap Buttons] Fixed the texture not removed problem within skinned calendar icon.",
+            "[Skins] Removed the code that making ElvUI action bar background to be transparent."
+        },
+        ["koKR"] = {
+            "[Minimap Buttons] Fixed the texture not removed problem within skinned calendar icon.",
+            "[Skins] Removed the code that making ElvUI action bar background to be transparent."
+        }
     }
 }

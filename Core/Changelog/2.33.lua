e302e5ffc7643841bef8a354444abf829b3298a1
Zhou Fang <fang2hou@outlook.com>
2022-04-25 17:37:11 +0900

    changelog

--- Core/Changelog/2.33.lua
+++ Core/Changelog/2.33.lua
@@ -4,38 +4,58 @@ W.Changelog[233] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
-            "为亚洲语系客户端引入全部的 ElvUI 拉丁字体."
+            "更新 LibOpenRaid 到版本 30.",
+            "为亚洲语系客户端引入全部的 ElvUI 拉丁字体.",
+            "[快速焦点] 现在默认为不开启."
         },
         ["zhTW"] = {
-            "为亞洲語系客戶端引入全部的 ElvUI 拉丁字型."
+            "更新 LibOpenRaid 到版本 30.",
+            "为亞洲語系客戶端引入全部的 ElvUI 拉丁字型.",
+            "[快速焦點] 現在預設為不開啟."
         },
         ["enUS"] = {
-            "Import all ElvUI latin fonts to clients in asian."
+            "Update LibOpenRaid to version 30.",
+            "Import all ElvUI latin fonts to clients in asian.",
+            "[Quick Focus] Change the default status of Quick Focus module to disabled."
         },
         ["koKR"] = {
-            "아시아 사용자들은 불러오지 못했던 ElvUI의 모든 라틴 글꼴들을 불러옵니다."
+            "Update LibOpenRaid to version 30.",
+            "아시아 사용자들은 불러오지 못했던 ElvUI의 모든 라틴 글꼴들을 불러옵니다.",
+            "[Quick Focus] Change the default status of Quick Focus module to disabled."
         }
     },
     NEW = {
         ["zhCN"] = {
             "新增了对 ElvUI Enhanced Again 的兼容性检查.",
             "[美化皮肤] 新增了客服工单状态框体的皮肤.",
-            "[美化皮肤] 新增了 ElvUI 聊天语音面板的皮肤."
+            "[美化皮肤] 新增了 ElvUI 聊天语音面板的皮肤.",
+            "[鼠标提示] 新增了玩家阵营的图标. 感谢 Merathilis",
+            "[鼠标提示] 新增了宠物种类的图标. 感谢 Merathilis",
+            "[鼠标提示] 新增了宠物物种 ID. 感谢 Merathilis"
         },
         ["zhTW"] = {
             "新增了對 ElvUI Enhanced Again 的兼容性檢查.",
             "[美化皮膚] 新增了 GM 工單狀態框架的皮膚.",
-            "[美化皮膚] 新增了 ElvUI 對話語音面板的皮膚."
+            "[美化皮膚] 新增了 ElvUI 對話語音面板的皮膚.",
+            "[滑鼠提示] 新增了玩家陣營的图示. 感謝 Merathilis",
+            "[滑鼠提示] 新增了寵物種類的图示. 感謝 Merathilis",
+            "[滑鼠提示] 新增了寵物物種 ID. 感謝 Merathilis"
         },
         ["enUS"] = {
             "Added compatibility check for ElvUI Enhanced Again.",
             "[Skins] Added a skin for the GM ticket status frame.",
-            "[Skins] Added a skin for the ElvUI chat voice panel."
+            "[Skins] Added a skin for the ElvUI chat voice panel.",
+            "[Tooltip] Added a faction icon for players. Thanks Merathilis",
+            "[Tooltip] Added a species icon for pets. Thanks Merathilis",
+            "[Tooltip] Added a species ID for pets. Thanks Merathilis"
         },
         ["koKR"] = {
             "ElvUI Enhanced Again에 대한 호환성 검사를 추가하였습니다.",
             "[스킨] GM 대기표 상태 스킨을 추가하였습니다.",
-            "[스킨] ElvUI 채팅 보이스 패널 스킨을 추가하였습니다."
+            "[스킨] ElvUI 채팅 보이스 패널 스킨을 추가하였습니다.",
+            "[Tooltip] Added a faction icon for players. Thanks Merathilis",
+            "[Tooltip] Added a species icon for pets. Thanks Merathilis",
+            "[Tooltip] Added a species ID for pets. Thanks Merathilis"
         }
     },
     IMPROVEMENT = {
@@ -43,25 +63,29 @@ W.Changelog[233] = {
             "[矩形小地图] 提升了稳定性.",
             "[小地图按钮] 修复了日历按钮的材质无法去除的错误.",
             "[小地图按钮] 修复了要塞/盟约按钮错位的问题.",
-            "[美化皮肤] 移除了强制 ElvUI 动作条背景为透明的代码."
+            "[美化皮肤] 移除了强制 ElvUI 动作条背景为透明的代码.",
+            "[鼠标提示] 进度显示标题支持自定义."
         },
         ["zhTW"] = {
             "[矩形小地圖] 提升了穩定性.",
             "[小地圖按鍵] 修復了行事曆按鍵的材質無法去除的錯誤.",
             "[小地圖按鍵] 修復了要塞/盟約按鍵錯位的問題.",
-            "[美化皮膚] 移除了強制 ElvUI 快捷列背景為透明的代碼."
+            "[美化皮膚] 移除了強制 ElvUI 快捷列背景為透明的代碼.",
+            "[滑鼠提示] 进度顯示標題支援自定義."
         },
         ["enUS"] = {
             "[Rectangle Minimap] Improved stability.",
             "[Minimap Buttons] Fixed the texture not removed problem within skinned calendar icon.",
             "[Minimap Buttons] Fixed the position of garrison/covenant buttons.",
-            "[Skins] Removed the code that making ElvUI action bar background to be transparent."
+            "[Skins] Removed the code that making ElvUI action bar background to be transparent.",
+            "[Tooltip] Support the customization of the progression display title."
         },
         ["koKR"] = {
             "[미니맵 비율 조정] 안정성 향상.",
             "[미니맵 버튼 통합 바] 달력 버튼의 텍스처를 제거할 수 없는 버그를 수정했습니다.",
             "[미니맵 버튼 통합 바] 주둔지/성약 버튼이 잘못된 위치에 있던 문제를 수정했습니다.",
-            "[스킨] ElvUI 작업 표시줄 배경을 투명하게 하는 코드를 제거했습니다."
+            "[스킨] ElvUI 작업 표시줄 배경을 투명하게 하는 코드를 제거했습니다.",
+            "[Tooltip] Support the customization of the progression display title."
         }
     }
 }

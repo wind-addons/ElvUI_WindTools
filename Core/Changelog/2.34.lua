edd9f5e927f24d1243f12f0645d801cf44ab06a7
Zhou Fang <fang2hou@outlook.com>
2022-04-30 21:57:47 +0900

    changelog

--- Core/Changelog/2.34.lua
+++ Core/Changelog/2.34.lua
@@ -15,9 +15,33 @@ W.Changelog[234] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[交接] 大幅优化了模块代码.",
+            "[交接] 支持自定义暂停修饰键.",
+            "[交接] 支持仅开启接受任务或仅开启完成任务.",
+            "[交接] 新增了智能聊天选项功能, 同时并入原有的盗贼职业大厅传送功能.",
+            "[美化皮肤] 修复了跳过动画框体的阴影."
+        },
+        ["zhTW"] = {
+            "[交接] 大幅优化了模組代碼.",
+            "[交接] 支援自訂暫停修飾鍵.",
+            "[交接] 支援仅開啟接受任務或仅開啟完成任務.",
+            "[交接] 新增了智能聊天選項功能, 同時也并入原有的盜賊職業大廳傳送功能.",
+            "[美化皮膚] 修復了跳過動畫框架的陰影."
+        },
+        ["enUS"] = {
+            "[Turn In] Greatly optimized module code.",
+            "[Turn In] Support customization of pause modifier key.",
+            "[Turn In] Support only auto-accepting or only auto-completing.",
+            "[Turn In] New feature: Smart Chat, also included the old option of Rogue Class Hall Insignia.",
+            "[Skin] Fixed the shadow of the skip cutscene frame."
+        },
+        ["koKR"] = {
+            "[Turn In] Greatly optimized module code.",
+            "[Turn In] Support customization of pause modifier key.",
+            "[Turn In] Support only auto-accepting or only auto-completing.",
+            "[Turn In] New feature: Smart Chat, also included the old option of Rogue Class Hall Insignia.",
+            "[Skin] Fixed the shadow of the skip cutscene frame."
+        }
     }
 }

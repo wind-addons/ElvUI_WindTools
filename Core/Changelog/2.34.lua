bdd8b07fd970cefbe7f601acc58fcfc9d60bf57c
Zhou Fang <fang2hou@outlook.com>
2022-05-03 00:27:20 +0900

    missing changelog

--- Core/Changelog/2.34.lua
+++ Core/Changelog/2.34.lua
@@ -30,6 +30,9 @@ W.Changelog[234] = {
             "[交接] 新增了自动选择最高奖励物品并完成任务的功能.",
             "[交接] 新增了智能聊天选项功能, 同时并入原有的盗贼职业大厅传送功能.",
             "[美化皮肤] 修复了跳过动画框体的阴影.",
+            "[美化皮肤] 修复了举报玩家框体的阴影.",
+            "[美化皮肤] 修复了灵魂医者框体的阴影.",
+            "[美化皮肤] 修复了分离物品框体的阴影.",
             "[游戏条] 立即截图时会自动隐藏鼠标提示.",
             "[游戏条] 修复了 Ctrl + Shift + 点击时间区域无法开启 CPU 占用统计的问题."
         },
@@ -40,6 +43,9 @@ W.Changelog[234] = {
             "[交接] 新增了自動選擇最高獎勵物品並完成任務的功能.",
             "[交接] 新增了智能聊天選項功能, 同時也并入原有的盜賊職業大廳傳送功能.",
             "[美化皮膚] 修復了跳過動畫框架的陰影.",
+            "[美化皮膚] 修復了舉報玩家框架的陰影.",
+            "[美化皮膚] 修復了靈魂醫者框架的陰影.",
+            "[美化皮膚] 修復了分離物品框架的陰影.",
             "[游戏條] 立即截圖時會自動隱藏滑鼠提示.",
             "[游戏條] 修復了 Ctrl + Shift + 點擊時間區域無法開啟 CPU 占用統計的問題."
         },
@@ -50,6 +56,9 @@ W.Changelog[234] = {
             "[Turn In] New feature: Get Best Reward, completing the quest with the most valuable reward.",
             "[Turn In] New feature: Smart Chat, also included the old option of Rogue Class Hall Insignia.",
             "[Skin] Fixed the shadow of the skip cutscene frame.",
+            "[Skin] Fixed the shadow of the report player frame.",
+            "[Skin] Fixed the shadow of the spirit healer frame.",
+            "[Skin] Fixed the shadow of the separate item frame.",
             "[Game Bar] Automatically hide the tooltip when taking a screenshot.",
             "[Game Bar] Fixed the issue of Ctrl + Shift + click on the time area."
         },
@@ -60,6 +69,9 @@ W.Changelog[234] = {
             "[Turn In] New feature: Get Best Reward, completing the quest with the most valuable reward.",
             "[Turn In] New feature: Smart Chat, also included the old option of Rogue Class Hall Insignia.",
             "[Skin] Fixed the shadow of the skip cutscene frame.",
+            "[Skin] Fixed the shadow of the report player frame.",
+            "[Skin] Fixed the shadow of the spirit healer frame.",
+            "[Skin] Fixed the shadow of the separate item frame.",
             "[Game Bar] Automatically hide the tooltip when taking a screenshot.",
             "[Game Bar] Fixed the issue of Ctrl + Shift + click on the time area."
         }

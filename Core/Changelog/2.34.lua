9698465438a71fd84d8e9dbb0826f8c25229e899
Zhou Fang <fang2hou@outlook.com>
2022-05-04 01:07:55 +0900

    changelog

--- Core/Changelog/2.34.lua
+++ Core/Changelog/2.34.lua
@@ -4,20 +4,24 @@ W.Changelog[234] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
+            "插件设计风格焕新.",
             "新增意大利语支持",
-            "[移动框体] 配置文件表位置更改, 更新后会自动拷贝旧配置并清理.",
+            "[移动框体] 配置文件表位置更改, 更新后会自动拷贝旧配置并清理."
         },
         ["zhTW"] = {
+            "插件設計風格更新.",
             "新增義大利語支援",
-            "[移動框架] 配置文件表位置更改, 更新後會自動拷貝舊配置並清理.",
+            "[移動框架] 配置文件表位置更改, 更新後會自動拷貝舊配置並清理."
         },
         ["enUS"] = {
+            "Updated the overall designs.",
             "Added Italian language support",
-            "[Move Frames] The location of settings are changed, the old settings will be copied and cleaned.",
+            "[Move Frames] The location of settings are changed, the old settings will be copied and cleaned."
         },
         ["koKR"] = {
+            "Updated the overall designs.",
             "이탈리아어 지원 추가",
-            "[프레임 이동] 설정 위치가 변경되면 이전 구성이 자동으로 복사되어 정리됩니다.",
+            "[프레임 이동] 설정 위치가 변경되면 이전 구성이 자동으로 복사되어 정리됩니다."
         }
     },
     NEW = {
@@ -49,7 +53,8 @@ W.Changelog[234] = {
             "[美化皮肤] 修复了灵魂医者框体的阴影.",
             "[美化皮肤] 修复了分离物品框体的阴影.",
             "[游戏条] 立即截图时会自动隐藏鼠标提示.",
-            "[游戏条] 修复了 Ctrl + Shift + 点击时间区域无法开启 CPU 占用统计的问题."
+            "[游戏条] 修复了 Ctrl + Shift + 点击时间区域无法开启 CPU 占用统计的问题.",
+            "[额外物品条] 新增了圣墓宝物箱(英雄难度). 感谢 mcc1"
         },
         ["zhTW"] = {
             "優化了部分設置項的顯示.",
@@ -65,7 +70,8 @@ W.Changelog[234] = {
             "[美化皮膚] 修復了靈魂醫者框架的陰影.",
             "[美化皮膚] 修復了分離物品框架的陰影.",
             "[游戏條] 立即截圖時會自動隱藏滑鼠提示.",
-            "[游戏條] 修復了 Ctrl + Shift + 點擊時間區域無法開啟 CPU 占用統計的問題."
+            "[游戏條] 修復了 Ctrl + Shift + 點擊時間區域無法開啟 CPU 占用統計的問題.",
+            "[額外物品條] 新增了聖塚寶藏箱(英雄難度). 感謝 mcc1"
         },
         ["enUS"] = {
             "Improved some options.",
@@ -81,7 +87,8 @@ W.Changelog[234] = {
             "[Skin] Fixed the shadow of the spirit healer frame.",
             "[Skin] Fixed the shadow of the separate item frame.",
             "[Game Bar] Automatically hide the tooltip when taking a screenshot.",
-            "[Game Bar] Fixed the issue of Ctrl + Shift + click on the time area."
+            "[Game Bar] Fixed the issue of Ctrl + Shift + click on the time area.",
+            "[Extra Item Bar] Added cache of sepulcher treasures (Heroic). Thanks mcc1"
         },
         ["koKR"] = {
             "일부 설정 항목의 표시 최적화.",
@@ -97,7 +104,8 @@ W.Changelog[234] = {
             "[스킨] Spirit healer 프레임의 그림자 수정.",
             "[스킨] Separate item 프레임의 그림자 수정.",
             "[게임 바] 스크린샷을 찍을 때 마우스 툴팁이 자동으로 숨겨집니다.",
-            "[게임 바] Ctrl + Shift + 시계를 클릭하면 CPU 사용량 통계가 활성화되지 않는 문제가 수정되었습니다."
+            "[게임 바] Ctrl + Shift + 시계를 클릭하면 CPU 사용량 통계가 활성화되지 않는 문제가 수정되었습니다.",
+            "[아이템 바] 올리아 보관함(영웅 난이도)이 추가되였습니다. Thanks mcc1"
         }
     }
 }

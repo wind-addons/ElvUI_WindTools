875fc78c99a1eced296091ee5349b62409d23ac1
Zhou Fang <fang2hou@outlook.com>
2022-05-06 15:34:52 +0900

    changelog

--- Core/Changelog/2.34.lua
+++ Core/Changelog/2.34.lua
@@ -31,7 +31,8 @@ W.Changelog[234] = {
             "[静音] 新增了全种族哭声的静音.",
             "[静音] 新增了永恒者的挽歌的静音.",
             "[进度追踪] 支持自定义顶部文字颜色.",
-            "[进度追踪] 新增了装饰条功能, 默认开启."
+            "[进度追踪] 新增了装饰条功能, 默认开启.",
+            "[其他] 新增了显示键位于冷却动画之上的功能."
         },
         ["zhTW"] = {
             "新增模組: [吸收], 可以自定義 ElvUI 吸收盾材質, 同時增加暴雪風格的吸收火花.",
@@ -39,7 +40,8 @@ W.Changelog[234] = {
             "[靜音] 新增了全種族哭聲的靜音.",
             "[靜音] 新增了永恆輓歌的靜音.",
             "[進度追蹤] 支援自定義頂部文字顏色.",
-            "[進度追蹤] 新增了裝飾條功能, 預設開啟."
+            "[進度追蹤] 新增了裝飾條功能, 預設開啟.",
+            "[其他] 新增了顯示鍵位於冷卻動畫之上的功能."
         },
         ["enUS"] = {
             "New module [Absorb], you can customize the texture of ElvUI absorb shield, and add Blizzard style absorb spark.",
@@ -47,7 +49,8 @@ W.Changelog[234] = {
             "[Mute] Added a new option for crying sounds of all races.",
             "[Mute] You can mute Elegy of the Eternals sound effect now.",
             "[Objective Tracker] Support customize the color of header text.",
-            "[Objective Tracker] Add new feature of cosmetic bar. It is ON by default."
+            "[Objective Tracker] Add new feature of cosmetic bar. It is ON by default.",
+            "[Misc] Added a new option to show action bar hotkey above the cooldown animation."
         },
         ["koKR"] = {
             "새로운 모듈 [피해흡수], ElvUI 유닛프레임 체력바의 피해흡수 텍스처를 커스터마이징 할 수 있으며, 블리자드 스타일의 효과를 추가할 수 있습니다.",
@@ -55,7 +58,8 @@ W.Changelog[234] = {
             "[음소거] 모든 종족의 울음 소리를 음소거 옵션에 추가하였습니다.",
             "[음소거] 영원한 존재의 비가의 사운드 효과를 음소거 옵션에 추가하였습니다.",
             "[퀘스트 추적기] 제목 글자색을 커스터마이징 할 수 있습니다.",
-            "[퀘스트 추적기] 새로운 기능인 장씩띠를 추가하였습니다. 기본적으로 활성화되어 있습니다."
+            "[퀘스트 추적기] 새로운 기능인 장씩띠를 추가하였습니다. 기본적으로 활성화되어 있습니다.",
+            "[Misc] Added a new option to show action bar hotkey above the cooldown animation."
         }
     },
     IMPROVEMENT = {
@@ -63,6 +67,7 @@ W.Changelog[234] = {
             "优化了部分设定项的显示",
             "优化了更新助手.",
             "配置重置模块更新.",
+            "[移动框体] 按住冒险手册中的 Boss 模型不再进行移动.",
             "[装备观察] 重新采集并整合了数据, 当前版本应该不会有任何的附魔效果被遗漏了.",
             "[装备观察] 不再显示副手物品的附魔提示.",
             "[交接] 大幅优化了模块代码.",
@@ -76,6 +81,7 @@ W.Changelog[234] = {
             "[美化皮肤] 修复了举报玩家框体的阴影.",
             "[美化皮肤] 修复了灵魂医者框体的阴影.",
             "[美化皮肤] 修复了分离物品框体的阴影.",
+            "[美化皮肤] 优化了冒险手册的皮肤.",
             "[游戏条] 立即截图时会自动隐藏鼠标提示.",
             "[游戏条] 修复了 Ctrl + Shift + 点击时间区域无法开启 CPU 占用统计的问题.",
             "[额外物品条] 新增了圣墓宝物箱(英雄难度). 感谢 mcc1",
@@ -85,6 +91,7 @@ W.Changelog[234] = {
             "優化了部分設置項的顯示.",
             "優化了更新助手.",
             "重置模組功能更新.",
+            "[移動框架] 按住冒險手冊中的首領模型不再進行移動.",
             "[裝備觀察] 重新采集並整合了數據, 当前版本應該不會有任何的附魔效果被遺漏了.",
             "[裝備觀察] 不再顯示副手物品的附魔提示.",
             "[交接] 大幅優化了模組代碼.",
@@ -98,6 +105,7 @@ W.Changelog[234] = {
             "[美化皮膚] 修復了舉報玩家框架的陰影.",
             "[美化皮膚] 修復了靈魂醫者框架的陰影.",
             "[美化皮膚] 修復了分離物品框架的陰影.",
+            "[美化皮膚] 優化了冒險手冊的皮膚.",
             "[游戏條] 立即截圖時會自動隱藏滑鼠提示.",
             "[游戏條] 修復了 Ctrl + Shift + 點擊時間區域無法開啟 CPU 占用統計的問題.",
             "[額外物品條] 新增了聖塚寶藏箱(英雄難度). 感謝 mcc1",
@@ -107,6 +115,7 @@ W.Changelog[234] = {
             "Improved some options.",
             "Improved upgrade assistant.",
             "Update the module reset buttons with latest changes.",
+            "[Move Frames] The encounter journal will not be moved when you checking the boss model.",
             "[Inspect] Rebuild the enchantment database, there is no enchantment lost anymore.",
             "[Inspect] No longer show the enchantment icon on the offhand item.",
             "[Turn In] Greatly optimized module code.",
@@ -120,6 +129,7 @@ W.Changelog[234] = {
             "[Skin] Fixed the shadow of the report player frame.",
             "[Skin] Fixed the shadow of the spirit healer frame.",
             "[Skin] Fixed the shadow of the separate item frame.",
+            "[Skin] Optimized the skin of encounter journal.",
             "[Game Bar] Automatically hide the tooltip when taking a screenshot.",
             "[Game Bar] Fixed the issue of Ctrl + Shift + click on the time area.",
             "[Extra Item Bar] Added cache of sepulcher treasures (Heroic). Thanks mcc1",
@@ -129,6 +139,7 @@ W.Changelog[234] = {
             "일부 설정 항목의 표시 최적화.",
             "업데이트 도우미 최적화.",
             "구성 재설정 모듈 업데이트.",
+            "[Move Frames] The encounter journal will not be moved when you checking the boss model.",
             "[살펴보기] 마법부여 데이터베이스를 재구축 하였습니다. 더 이상 잃어버린 마법은 없습니다.",
             "[살펴보기] 더 이상 보조 아이템에 마법 부여 아이콘을 표시하지 않습니다.",
             "[자동 수락] 모듈 코드 최적화. ",
@@ -142,6 +153,7 @@ W.Changelog[234] = {
             "[스킨] Report player 프레임의 그림자 수정.",
             "[스킨] Spirit healer 프레임의 그림자 수정.",
             "[스킨] Separate item 프레임의 그림자 수정.",
+            "[Skin] Optimized the skin of encounter journal.",
             "[게임 바] 스크린샷을 찍을 때 마우스 툴팁이 자동으로 숨겨집니다.",
             "[게임 바] Ctrl + Shift + 시계를 클릭하면 CPU 사용량 통계가 활성화되지 않는 문제가 수정되었습니다.",
             "[아이템 바] 올리아 보관함(영웅 난이도)이 추가되었습니다. Thanks mcc1",

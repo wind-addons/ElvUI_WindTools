a730b9549ac7edf398677c58800487708cd9b906
Zhou Fang <fang2hou@outlook.com>
2022-05-09 14:03:04 +0900

    changelog

--- Core/Changelog/2.35.lua
+++ Core/Changelog/2.35.lua
@@ -15,9 +15,17 @@ W.Changelog[235] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[美化皮肤] 优化了拍卖行皮肤."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 優化了拍賣行皮膚."
+        },
+        ["enUS"] = {
+            "[Skins] Optimized Auction House skin."
+        },
+        ["koKR"] = {
+            "[Skins] Optimized Auction House skin."
+        }
     }
 }

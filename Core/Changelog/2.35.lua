757fc967b9460769225f393c39be152c3d6f903d
Zhou Fang <fang2hou@outlook.com>
2022-05-09 16:15:20 +0900

    changelog

--- Core/Changelog/2.35.lua
+++ Core/Changelog/2.35.lua
@@ -16,16 +16,20 @@ W.Changelog[235] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "[美化皮肤] 优化了拍卖行皮肤."
+            "[美化皮肤] 优化了拍卖行皮肤.",
+            "[移动框体] 新增一个选项用于提升与 Trade Skill Master 的兼容性."
         },
         ["zhTW"] = {
-            "[美化皮膚] 優化了拍賣行皮膚."
+            "[美化皮膚] 優化了拍賣行皮膚.",
+            "[移動框架] 新增一個選項用於提升與 Trade Skill Master 的相容性."
         },
         ["enUS"] = {
-            "[Skins] Optimized Auction House skin."
+            "[Skins] Optimized Auction House skin.",
+            "[Move Frames] New option to improve compatibility with Trade Skill Master."
         },
         ["koKR"] = {
-            "[Skins] Optimized Auction House skin."
+            "[Skins] Optimized Auction House skin.",
+            "[Move Frames] New option to improve compatibility with Trade Skill Master."
         }
     }
 }

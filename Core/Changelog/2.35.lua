1d8fda730c26a8f1612e23b88ea733f874a12d57
Zhou Fang <fang2hou@outlook.com>
2022-05-11 00:55:04 +0900

    changelog

--- Core/Changelog/2.35.lua
+++ Core/Changelog/2.35.lua
@@ -3,40 +3,64 @@ local W = unpack(select(2, ...))
 W.Changelog[235] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "LibOpenRaid 升级到 33 版本.",
+            "修复了未通过账号认证的玩家无法创建预组建队伍的问题."
+        },
+        ["zhTW"] = {
+            "LibOpenRaid 升级到 33 版本.",
+            "修復了未通過帳號認證的玩家無法創建預組隊伍的問題."
+        },
+        ["enUS"] = {
+            "Update LibOpenRaid to version 33.",
+            "Fixed a bug where unauthenticated players could not create a group."
+        },
+        ["koKR"] = {
+            "Update LibOpenRaid to version 33.",
+            "Fixed a bug where unauthenticated players could not create a group."
+        }
     },
     NEW = {
         ["zhCN"] = {
+            "[通告] (任务) 新增一个选项用于屏蔽暴雪自带的任务进度信息.",
             "[标签] 增加了一些新的不同小数位数缩写的生命值标签."
         },
         ["zhTW"] = {
+            "[通告] (任務) 新增一個選項用於屏蔽暴雪自帶的任務進度訊息.",
             "[標籤] 增加了一些新的不同小數位數縮寫的生命值標籤."
         },
         ["enUS"] = {
+            "[Announcement] (Quest) Added an option to disable Blizzard quest progress info.",
             "[Tags] Added some new different health tags with different decimal lengths."
         },
         ["koKR"] = {
+            "[Announcement] (Quest) Added an option to disable Blizzard quest progress info.",
             "[Tags] Added some new different health tags with different decimal lengths."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[交接] 修复了有时任务无法正常借取完成的问题",
             "[美化皮肤] 优化了拍卖行皮肤.",
+            "[美化皮肤] 优化了暴雪 UI 组件皮肤.",
             "[移动框体] 新增一个选项用于提升与 Trade Skill Master 的兼容性."
         },
         ["zhTW"] = {
+            "[交接] 修復了有時任務無法正常借取完成的問題",
             "[美化皮膚] 優化了拍賣行皮膚.",
+            "[美化皮膚] 優化了暴雪 UI 組件皮膚.",
             "[移動框架] 新增一個選項用於提升與 Trade Skill Master 的相容性."
         },
         ["enUS"] = {
+            "[Turn In] Fixed a bug that sometimes the module can not accept or complete quests.",
             "[Skins] Optimized Auction House skin.",
+            "[Skins] Optimized Blizzard UI Widgets skin.",
             "[Move Frames] New option to improve compatibility with Trade Skill Master."
         },
         ["koKR"] = {
+            "[Turn In] Fixed a bug that sometimes the module can not accept or complete quests.",
             "[Skins] Optimized Auction House skin.",
+            "[Skins] Optimized Blizzard UI Widgets skin.",
             "[Move Frames] New option to improve compatibility with Trade Skill Master."
         }
     }

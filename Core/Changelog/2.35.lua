e6ce88961ffe3708c409fac7c3e4be745b984e9c
Zhou Fang <fang2hou@outlook.com>
2022-05-11 20:49:37 +0900

    changelog

--- Core/Changelog/2.35.lua
+++ Core/Changelog/2.35.lua
@@ -40,36 +40,44 @@ W.Changelog[235] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[右键菜单] 修复了属性报告功能.",
             "[超级追踪] 由于可能产生的污染, 移除了纯数字的选项.",
             "[交接] 修复了有时任务无法正常借取完成的问题",
             "[美化皮肤] 优化了拍卖行皮肤.",
             "[美化皮肤] 优化了暴雪 UI 组件皮肤.",
             "[美化皮肤] 优化了成就进度皮肤.",
-            "[移动框体] 新增一个选项用于提升与 Trade Skill Master 的兼容性."
+            "[移动框体] 新增一个选项用于提升与 Trade Skill Master 的兼容性.",
+            "[吸收] 修复了战斗中重载界面可能导致的污染."
         },
         ["zhTW"] = {
+            "[右鍵選單] 修復了屬性報告功能.",
             "[超級追蹤] 由於可能產生的汙染, 移除了僅數字的選項.",
             "[交接] 修復了有時任務無法正常借取完成的問題",
             "[美化皮膚] 優化了拍賣行皮膚.",
             "[美化皮膚] 優化了暴雪 UI 組件皮膚.",
             "[美化皮膚] 優化了成就進度皮膚.",
-            "[移動框架] 新增一個選項用於提升與 Trade Skill Master 的相容性."
+            "[移動框架] 新增一個選項用於提升與 Trade Skill Master 的相容性.",
+            "[吸收] 修復了戰鬥中重載介面可能導致的汙染."
         },
         ["enUS"] = {
+            "[Context Menu] Fixed a bug in stats report.",
             "[Super Tracker] Bacause it may cause taint, the option of only number has been removed.",
             "[Turn In] Fixed a bug that sometimes the module can not accept or complete quests.",
             "[Skins] Optimized Auction House skin.",
             "[Skins] Optimized Blizzard UI Widgets skin.",
             "[Skins] Optimized achievement progress skin.",
-            "[Move Frames] New option to improve compatibility with Trade Skill Master."
+            "[Move Frames] New option to improve compatibility with Trade Skill Master.",
+            "[Absorb] Fixed a bug that the module may cause taint if reload ui in combat."
         },
         ["koKR"] = {
+            "[Context Menu] Fixed a bug in stats report.",
             "[Super Tracker] Bacause it may cause taint, the option of only number has been removed.",
             "[Turn In] Fixed a bug that sometimes the module can not accept or complete quests.",
             "[Skins] Optimized Auction House skin.",
             "[Skins] Optimized Blizzard UI Widgets skin.",
             "[Skins] Optimized 업적 진행 상황 skin.",
-            "[Move Frames] New option to improve compatibility with Trade Skill Master."
+            "[Move Frames] New option to improve compatibility with Trade Skill Master.",
+            "[Absorb] Fixed a bug that the module may cause taint if reload ui in combat."
         }
     }
 }

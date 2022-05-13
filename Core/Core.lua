946f4e933d7e9b6ad13dae0c5e1d8cc6a5c738f5
Zhou Fang <fang2hou@outlook.com>
2022-05-13 11:24:10 +0900

    new updater

--- Core/Core.lua
+++ Core/Core.lua
@@ -15,20 +15,37 @@ local InCombatLockdown = InCombatLockdown
 local ACCEPT = _G.ACCEPT
 local CANCEL = _G.CANCEL
 
--- 一些常量
 W.Title = L["WindTools"]
 W.Locale = GetLocale()
 W.ChineseLocale = strsub(W.Locale, 0, 2) == "zh"
 W.MaxLevelForPlayerExpansion = GetMaxLevelForPlayerExpansion()
 W.SupportElvUIVersion = 12.75
 
--- 模块部分
 W.RegisteredModules = {}
-
--- 更新记录
 W.Changelog = {}
 
--- 快捷键注册
+-- Alerts
+E.PopupDialogs.WINDTOOLS_ELVUI_OUTDATED = {
+    text = format(
+        "%s\n%s",
+        format(L["%s not been loaded since you are using an outdated version of ElvUI."], W.Title),
+        format(L["Please upgrade your ElvUI to %2.2f or newer version!"], W.SupportElvUIVersion)
+    ),
+    button1 = ACCEPT,
+    hideOnEscape = 1
+}
+
+E.PopupDialogs.WINDTOOLS_OPEN_CHANGELOG = {
+    text = format(L["Welcome to %s %s!"], L["WindTools"], W.Version),
+    button1 = L["Open Changelog"],
+    button2 = CANCEL,
+    OnAccept = function(self)
+        E:ToggleOptionsUI("WindTools,information,changelog")
+    end,
+    hideOnEscape = 1
+}
+
+-- Keybinds
 _G.BINDING_CATEGORY_ELVUI_WINDTOOLS = L["WindTools"]
 for i = 1, 5 do
     _G["BINDING_HEADER_WTEXTRAITEMSBAR" .. i] =
@@ -45,7 +62,7 @@ _G["BINDING_NAME_CLICK WTExtraBindingButtonLeaveGroup:LeftButton"] = L["Leave Pa
 _G["BINDING_NAME_CLICK WTExtraBindingButtonLeavePartyIfSoloing:LeftButton"] = L["Leave Party if soloing"]
 
 --[[
-    注册 WindTools 模块
+    WindTools module registration
     @param {string} name 模块名
 ]]
 function W:RegisterModule(name)
@@ -60,7 +77,7 @@ function W:RegisterModule(name)
     end
 end
 
--- 初始化 WindTools 模块
+-- WindTools module initialization
 function W:InitializeModules()
     for _, moduleName in pairs(W.RegisteredModules) do
         local module = self:GetModule(moduleName)
@@ -70,7 +87,7 @@ function W:InitializeModules()
     end
 end
 
--- 配置更改后的模块更新
+-- WindTools module update after profile switch
 function W.UpdateModules()
     for _, moduleName in pairs(W.RegisteredModules) do
         local module = W:GetModule(moduleName)
@@ -79,16 +96,6 @@ function W.UpdateModules()
         end
     end
 end
-
-E.PopupDialogs.WINDTOOLS_ELVUI_OUTDATED = {
-    text = format(
-        "%s\n%s",
-        format(L["%s not been loaded since you are using an outdated version of ElvUI."], W.Title),
-        format(L["Please upgrade your ElvUI to %2.2f or newer version!"], W.SupportElvUIVersion)
-    ),
-    button1 = ACCEPT,
-    hideOnEscape = 1
-}
 
 -- Check ElvUI version, if not matched, show a popup to user
 function W:CheckElvUIVersion()
@@ -99,148 +106,33 @@ function W:CheckElvUIVersion()
     return true
 end
 
-E.PopupDialogs.WINDTOOLS_OPEN_CHANGELOG = {
-    text = format(L["Welcome to %s %s!"], L["WindTools"], W.Version),
-    button1 = L["Open Changelog"],
-    button2 = CANCEL,
-    OnAccept = function(self)
-        E:ToggleOptionsUI("WindTools,information,changelog")
-    end,
-    hideOnEscape = 1
-}
-
-function W:UpdateScripts(oldVersion, currentVersion)
-    if not oldVersion or not currentVersion then
+-- Check install version, show changelog and run update scripts
+function W:CheckInstalledVersion()
+    if InCombatLockdown() then
         return
     end
-
-    currentVersion = tonumber(currentVersion)
-    oldVersion = tonumber(oldVersion)
-
-    local isFirstLine = true
-
-    local function UpdateMessage(text)
-        if isFirstLine then
-            isFirstLine = false
-            print(
-                E:TextGradient(
-                    "----------------------------------",
-                    0.910,
-                    0.314,
-                    0.357,
-                    0.976,
-                    0.835,
-                    0.431,
-                    0.953,
-                    0.925,
-                    0.761,
-                    0.078,
-                    0.694,
-                    0.671
-                )
-            )
-            F.Print(format("%s (|cff00a8ff%.2f|r -> |cff00a8ff%.2f|r)", L["Update"], oldVersion, currentVersion))
-        end
-
-        print(text)
+    if not E.global.WT.version or E.global.WT.version ~= W.Version then
+        E:StaticPopup_Show("WINDTOOLS_OPEN_CHANGELOG")
     end
 
-    local doneIcon = format(" |T%s:0|t", W.Media.Icons.accept)
-
-    -- Clear the history of move frames.
-    if oldVersion >= 2.27 and oldVersion <= 2.31 then
-        E.private.WT.misc.framePositions = {}
-        UpdateMessage(L["Move Frames"] .. " - " .. L["Clear History"] .. doneIcon)
-    end
-
-    -- Copy old move frames options to its new db.
-    if oldVersion <= 2.33 then
-        local miscDB = E.private.WT.misc
-        miscDB.moveFrames.enable = miscDB.moveBlizzardFrames or miscDB.moveFrames.enable
-        miscDB.moveFrames.elvUIBags = miscDB.moveElvUIBags or miscDB.moveFrames.elvUIBags
-        miscDB.moveFrames.rememberPositions = miscDB.rememberPositions or miscDB.moveFrames.rememberPositions
-        miscDB.moveFrames.framePositions = miscDB.framePositions or miscDB.moveFrames.framePositions
-
-        miscDB.moveBlizzardFrames = nil
-        miscDB.moveElvUIBags = nil
-        miscDB.rememberPositions = nil
-        miscDB.framePositions = nil
-
-        UpdateMessage(L["Move Frames"] .. " - " .. L["Update Database"] .. "..." .. doneIcon)
-    end
-
-    if oldVersion <= 2.34 then
-        local miscDB = E.db.WT.misc
-        miscDB.automation.hideBagAfterEnteringCombat =
-            miscDB.autoHideBag or miscDB.automation.hideBagAfterEnteringCombat
-        miscDB.automation.hideWorldMapAfterEnteringCombat =
-            miscDB.autoHideWorldMap or miscDB.automation.hideWorldMapAfterEnteringCombat
-
-        miscDB.autoHideBag = nil
-        miscDB.autoHideWorldMap = nil
-
-        UpdateMessage(L["Automation"] .. " - " .. L["Update Database"] .. "..." .. doneIcon)
-    end
-
-    if not isFirstLine then
+    if E.private.WT.core.loginMessage then
+        local icon = F.GetIconString(W.Media.Textures.smallLogo, 14)
         print(
-            E:TextGradient(
-                "----------------------------------",
-                0.910,
-                0.314,
-                0.357,
-                0.976,
-                0.835,
-                0.431,
-                0.953,
-                0.925,
-                0.761,
-                0.078,
-                0.694,
-                0.671
+            format(
+                icon ..
+                    " " ..
+                        L["%s %s Loaded."] ..
+                            " " .. L["You can send your suggestions or bugs via %s, %s, %s, and the thread in %s."],
+                L["WindTools"],
+                W.Version,
+                L["QQ Group"],
+                L["Discord"],
+                L["Github"],
+                L["NGA.cn"]
             )
         )
     end
-end
-
-function W:ForBetaUser()
-    local miscDB = E.private.WT.misc
-
-    miscDB.moveFrames.enable = miscDB.moveBlizzardFrames or miscDB.moveFrames.enable
-    miscDB.moveFrames.elvUIBags = miscDB.moveElvUIBags or miscDB.moveFrames.elvUIBags
-    miscDB.moveFrames.rememberPositions = miscDB.rememberPositions or miscDB.moveFrames.rememberPositions
-    miscDB.moveFrames.framePositions = miscDB.framePositions or miscDB.moveFrames.framePositions
-
-    miscDB.moveBlizzardFrames = nil
-    miscDB.moveElvUIBags = nil
-    miscDB.rememberPositions = nil
-    miscDB.framePositions = nil
-end
-
--- 检查安装版本, 提示更新记录
-function W:CheckInstalledVersion()
-    if not InCombatLockdown() then
-        W:ForBetaUser()
-        if not E.global.WT.Version or E.global.WT.Version ~= W.Version then
-            E:StaticPopup_Show("WINDTOOLS_OPEN_CHANGELOG")
-            W:UpdateScripts(E.global.WT.Version, W.Version)
-            E.global.WT.Version = W.Version
-        elseif E.private.WT.core.loginMessage then
-            local icon = F.GetIconString(W.Media.Textures.smallLogo, 14)
-            print(
-                format(
-                    icon ..
-                        " " ..
-                            L["%s %s Loaded."] ..
-                                " " .. L["You can send your suggestions or bugs via %s, %s, %s, and the thread in %s."],
-                    L["WindTools"],
-                    W.Version,
-                    L["QQ Group"],
-                    L["Discord"],
-                    L["Github"],
-                    L["NGA.cn"]
-                )
-            )
-        end
-    end
+    
+    W:ForBetaUser()
+    W:UpdateScripts()
 end
---
+++ Core/Update.lua
@@ -0,0 +1,120 @@
+local W, F, E, L, V, P, G = unpack(select(2, ...))
+
+local isFirstLine = true
+
+local doneIcon = format(" |T%s:0|t", W.Media.Icons.accept)
+
+local function UpdateMessage(text, from, to)
+    if isFirstLine then
+        isFirstLine = false
+        print(
+            E:TextGradient(
+                "----------------------------------",
+                0.910,
+                0.314,
+                0.357,
+                0.976,
+                0.835,
+                0.431,
+                0.953,
+                0.925,
+                0.761,
+                0.078,
+                0.694,
+                0.671
+            )
+        )
+        F.Print(L["Update"])
+    end
+
+    print(text .. format("(|cff00a8ff%.2f|r -> |cff00a8ff%.2f|r)...", from, to) .. doneIcon)
+end
+
+function W:ForBetaUser()
+    local miscDB = E.db.WT.misc
+    miscDB.automation.hideBagAfterEnteringCombat = miscDB.autoHideBag or miscDB.automation.hideBagAfterEnteringCombat
+    miscDB.automation.hideWorldMapAfterEnteringCombat =
+        miscDB.autoHideWorldMap or miscDB.automation.hideWorldMapAfterEnteringCombat
+
+    miscDB.autoHideBag = nil
+    miscDB.autoHideWorldMap = nil
+end
+
+function W:UpdateScripts()
+    local cv = tonumber(W.Version) -- Installed WindTools Version
+    local gv = tonumber(E.global.WT.version or "0") -- Version in ElvUI Global
+
+    -- from old updater
+    if gv == 0 then
+        gv = tonumber(E.global.WT.Version or "0")
+        E.global.WT.Version = nil
+    end
+
+    local dv = tonumber(E.db.WT.version or gv) -- Version in ElvUI Profile
+    local pv = tonumber(E.private.WT.version or gv) -- Version in ElvUI Private
+
+    if gv == cv and dv == cv and pv == cv then
+        return
+    end
+
+    isFirstLine = true
+
+    -- Clear the history of move frames.
+    if gv >= 2.27 and gv <= 2.31 then
+        E.private.WT.misc.framePositions = {}
+        UpdateMessage(L["Move Frames"] .. " - " .. L["Clear History"], gv, cv)
+    end
+
+    -- Copy old move frames options to its new db.
+    if gv <= 2.33 then
+        local miscDB = E.private.WT.misc
+        miscDB.moveFrames.enable = miscDB.moveBlizzardFrames or miscDB.moveFrames.enable
+        miscDB.moveFrames.elvUIBags = miscDB.moveElvUIBags or miscDB.moveFrames.elvUIBags
+        miscDB.moveFrames.rememberPositions = miscDB.rememberPositions or miscDB.moveFrames.rememberPositions
+        miscDB.moveFrames.framePositions = miscDB.framePositions or miscDB.moveFrames.framePositions
+
+        miscDB.moveBlizzardFrames = nil
+        miscDB.moveElvUIBags = nil
+        miscDB.rememberPositions = nil
+        miscDB.framePositions = nil
+
+        UpdateMessage(L["Move Frames"] .. " - " .. L["Update Database"], gv, cv)
+    end
+
+    if dv <= 2.34 then
+        local miscDB = E.db.WT.misc
+        miscDB.automation.hideBagAfterEnteringCombat =
+            miscDB.autoHideBag or miscDB.automation.hideBagAfterEnteringCombat
+        miscDB.automation.hideWorldMapAfterEnteringCombat =
+            miscDB.autoHideWorldMap or miscDB.automation.hideWorldMapAfterEnteringCombat
+
+        miscDB.autoHideBag = nil
+        miscDB.autoHideWorldMap = nil
+
+        UpdateMessage(L["Automation"] .. " - " .. L["Update Database"], dv, cv)
+    end
+
+    if not isFirstLine then
+        print(
+            E:TextGradient(
+                "----------------------------------",
+                0.910,
+                0.314,
+                0.357,
+                0.976,
+                0.835,
+                0.431,
+                0.953,
+                0.925,
+                0.761,
+                0.078,
+                0.694,
+                0.671
+            )
+        )
+    end
+
+    E.global.WT.version = W.Version
+    E.db.WT.version = W.Version
+    E.private.WT.version = W.Version
+end

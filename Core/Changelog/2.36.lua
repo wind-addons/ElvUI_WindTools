8b6be7a4091bb1f032682029e2d19eb56bb10b65
Zhou Fang <fang2hou@outlook.com>
2022-05-16 23:45:08 +0900

    changelog

--- Core/Changelog/2.36.lua
+++ Core/Changelog/2.36.lua
@@ -3,21 +3,53 @@ local W = unpack(select(2, ...))
 W.Changelog[236] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "新增了命令 /wtdebug 可以用于开启除错模式. 提交 Bug 前请务必使用这个命令进行测试."
+        },
+        ["zhTW"] = {
+            "新增了 /wtdebug 命令可以用於開啓除錯模式. 提交 Bug 前請務必使用此命令進行測試."
+        },
+        ["enUS"] = {
+            "Added /wtdebug command to enable debug mode. Please use this command test once before submitting a bug report."
+        },
+        ["koKR"] = {
+            "Added /wtdebug command to enable debug mode. Please use this command test once before submitting a bug report."
+        }
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[美化皮肤] 新增了组件美化功能, 可修改 ElvUI 美化的全局设置."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了組件美化功能, 可修改 ElvUI 美化的全局設定."
+        },
+        ["enUS"] = {
+            "[Skins] Add new widgets skins, you can use it to change the global settings of ElvUI skin."
+        },
+        ["koKR"] = {
+            "[Skins] Add new widgets skins, you can use it to change the global settings of ElvUI skin."
+        }
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[任务列表] 新增了更多顶部缩写支持.",
+            "[联系人] 按钮划过特效从阴影更改为 ElvUI 风格.",
+            "[天赋管家] 按钮划过特效从阴影更改为 ElvUI 风格."
+        },
+        ["zhTW"] = {
+            "[任務列表] 新增了更多頂部縮寫支援.",
+            "[聯絡人] 按鈕划過特效從陰影更改為 ElvUI 風格.",
+            "[天賦管家] 按鈕划過特效從陰影更改為 ElvUI 風格."
+        },
+        ["enUS"] = {
+            "[Objective Tracker] Added more abbreviations support of header text.",
+            "[Contact] Button hover effect changed to ElvUI style.",
+            "[Talent Manager] Button hover effect changed to ElvUI style."
+        },
+        ["koKR"] = {
+            "[Objective Tracker] Added more abbreviations support of header text.",
+            "[Contacts] Button hover effect changed to ElvUI style.",
+            "[Talent Manager] Button hover effect changed to ElvUI style."
+        }
     }
 }

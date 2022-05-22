43b7522aa905aa6ff962f65e0c5f51d1a88d9639
Zhou Fang <fang2hou@outlook.com>
2022-05-22 17:45:28 +0900

    changelog

--- Core/Changelog/2.36.lua
+++ Core/Changelog/2.36.lua
@@ -32,6 +32,7 @@ W.Changelog[236] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[标签] 修正了一些标签说明.",
             "[任务列表] 新增了更多顶部缩写支持.",
             "[联系人] 按钮划过特效从阴影更改为 ElvUI 风格.",
             "[天赋管家] 按钮划过特效从阴影更改为 ElvUI 风格.",
@@ -39,6 +40,7 @@ W.Changelog[236] = {
             "[美化皮肤] 修复了公会银行的美化."
         },
         ["zhTW"] = {
+            "[標籤] 修正了一些標籤說明.",
             "[任務列表] 新增了更多頂部縮寫支援.",
             "[聯絡人] 按鈕划過特效從陰影更改為 ElvUI 風格.",
             "[天賦管家] 按鈕划過特效從陰影更改為 ElvUI 風格.",
@@ -46,6 +48,7 @@ W.Changelog[236] = {
             "[美化皮膚] 修復了公會銀行的美化."
         },
         ["enUS"] = {
+            "[Tags] Fix some tags description.",
             "[Objective Tracker] Added more abbreviations support of header text.",
             "[Contact] Button hover effect changed to ElvUI style.",
             "[Talent Manager] Button hover effect changed to ElvUI style.",
@@ -53,6 +56,7 @@ W.Changelog[236] = {
             "[Skins] Fixed the skin of guild bank."
         },
         ["koKR"] = {
+            "[Tags] Fix some tags description.",
             "[퀘스트 추적기] 제목에 더 많은 약어 지원을 추가했습니다.",
             "[우편 연락처] 버튼 효과가 ElvUI 스타일로 변경되었습니다.",
             "[특성 관리자] 버튼 효과가 ElvUI 스타일로 변경되었습니다.",

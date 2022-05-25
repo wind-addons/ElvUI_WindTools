a702695e2aa8e3cca6d83985dfb495cce821cd27
Zhou Fang <fang2hou@outlook.com>
2022-05-25 09:25:30 +0900

    changelog

--- Core/Changelog/2.37.lua
+++ Core/Changelog/2.37.lua
@@ -15,9 +15,21 @@ W.Changelog[237] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "修复了测试环境命令无法按照预期工作的一些问题.",
+            "[美化皮肤] 修复了可能出现的数据库读取错误."
+        },
+        ["zhTW"] = {
+            "修復了測試環境命令無法按照預期工作的一些問題.",
+            "[美化皮膚] 修復了可能出現的資料庫讀取錯誤."
+        },
+        ["enUS"] = {
+            "Fixed a bug that caused the debug environment command to not work as expected.",
+            "[Skins] Fixed possible database read error."
+        },
+        ["koKR"] = {
+            "Fixed a bug that caused the debug environment command to not work as expected.",
+            "[Skins] 예상치 못한 데이터베이스 읽기 오류를 수정하였습니다."
+        },
     }
 }

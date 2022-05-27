18fd56c34c4fabf3bbf9e7de06aa67bbe3efa6da
talmus76 <73485793+talmus76@users.noreply.github.com>
2022-05-27 16:16:02 +0900

    Update 2.37.lua

--- Core/Changelog/2.37.lua
+++ Core/Changelog/2.37.lua
@@ -22,8 +22,8 @@ W.Changelog[237] = {
             "[Chat Link] Support showing the icon of keystone dungeon."
         },
         ["koKR"] = {
-            "[Progression] 최고 점수 던전을 표시하는 옵션을 추가하였습니다.",
-            "[Chat Link] Support showing the icon of keystone dungeon."
+            "[진행 상태] 최고 점수 던전을 표시하는 옵션을 추가하였습니다.",
+            "[채팅 링크] 쐐기돌 던전의 아이콘 표시를 지원합니다."
         }
     },
     IMPROVEMENT = {
@@ -49,11 +49,11 @@ W.Changelog[237] = {
             "[Skins] Fixed possible database read error."
         },
         ["koKR"] = {
-            "Fixed a bug that caused the debug environment command to not work as expected.",
-            "[Extra Item Bar] Item information is now more accurate.",
-            "[Move Frames] Fixed a bug that may cause an database reading error.",
-            "[Announcement] Fix the bug that the Blizzard progress text would not shown even if the entire module was disabled.",
-            "[Skins] 예상치 못한 데이터베이스 읽기 오류를 수정하였습니다."
+            "디버그 명령이 예상대로 작동하지 않는 일부 문제를 수정했습니다.",
+            "[아이템 바] 아이템 정보가 더 정확해졌습니다.",
+            "[프레임 이동] 데이터베이스 읽기 오류를 일으킬 수 있는 문제를 수정했습니다.",
+            "[알림] 전체 모드가 꺼져 있으면 Blizzard 진행 상황 알림이 비활성화되는 문제가 수정되었습니다.",
+            "[스킨] 예상치 못한 데이터베이스 읽기 오류를 수정하였습니다."
         }
     }
 }

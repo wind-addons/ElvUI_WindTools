11fb008c982bd1dedb23eefcd4c35811f9cba40c
Zhou Fang <fang2hou@outlook.com>
2022-05-28 18:11:47 +0900

    changelog

--- Core/Changelog/2.38.lua
+++ Core/Changelog/2.38.lua
@@ -9,22 +9,38 @@ W.Changelog[238] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[美化皮肤] 新增物品插入框体皮肤."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增物品插入框架皮膚."
+        },
+        ["enUS"] = {
+            "[Skins] New item socketing frame skin."
+        },
+        ["koKR"] = {
+            "[Skins] New item socketing frame skin."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[通告] 任务通告现在支持当前地图的世界任务.",
+            "[进度追踪] 修复标记的显示错误.",
             "[美化皮肤] 修正组件延后载入的逻辑."
         },
         ["zhTW"] = {
+            "[通告] 任務通告現在支持當前地圖的世界任務.",
+            "[進度追蹤] 修復標記的顯示錯誤.",
             "[美化皮膚] 修正組件延後載入的邏輯."
         },
         ["enUS"] = {
+            "[Announcement] Now supports the world quests on the current map.",
+            "[Objective Tracker] Fix the display error of the dashes.",
             "[Skins] Fix the logic of widget skins lazy loading."
         },
         ["koKR"] = {
+            "[Announcement] Now supports the world quests on the current map.",
+            "[Objective Tracker] Fix the display error of the dashes.",
             "[Skins] Fix the logic of widget skins lazy loading."
         }
     }

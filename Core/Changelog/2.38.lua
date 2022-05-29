aa068317282e32dffc3d1cedc68b1224fc0c1f78
Zhou Fang <fang2hou@outlook.com>
2022-05-30 01:15:01 +0900

    changelog

--- Core/Changelog/2.38.lua
+++ Core/Changelog/2.38.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[238] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[物品等级] 重写了模块, 以支持所有的飞出物品按钮的等级显示."
+        },
+        ["zhTW"] = {
+            "[物品等級] 重寫了模組, 以支援所有的飛出物品按鈕的等級顯示."
+        },
+        ["enUS"] = {
+            "[Item Level] Rewrote the module, to support all the flyout buttons' item level."
+        },
+        ["koKR"] = {
+            "[Item Level] Rewrote the module, to support all the flyout buttons' item level."
+        }
     },
     NEW = {
         ["zhCN"] = {
@@ -32,33 +40,41 @@ W.Changelog[238] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[矩形小地图] 修复了切换场景时可能产生的错位问题.",
             "[小地图按钮] 修复了 Zygor 小地图按钮的材质显示.",
             "[通告] 任务通告现在支持当前地图的世界任务.",
             "[进度追踪] 修复标记的显示错误.",
+            "[进度追踪] 略微改动了默认装饰条设定.",
             "[美化皮肤] 修正组件延后载入的逻辑.",
             "[美化皮肤] 更新了对 ElvUI MerathilisUI 的兼容.",
             "[美化皮肤] 对于停用的按键, 将不再显示动画效果."
         },
         ["zhTW"] = {
+            "[矩形小地圖] 修復了切換場景時可能產生的錯位問題.",
             "[小地圖按鈕] 修復了 Zygor 小地圖按鍵的材質顯示.",
             "[通告] 任務通告現在支持當前地圖的世界任務.",
             "[進度追蹤] 修復標記的顯示錯誤.",
+            "[進度追蹤] 稍微改動了預設裝飾條設定.",
             "[美化皮膚] 修正組件延後載入的邏輯.",
             "[美化皮膚] 更新了對 ElvUI MerathilisUI 的兼容.",
             "[美化皮膚] 對於停用的按鍵, 將不再顯示動畫效果."
         },
         ["enUS"] = {
+            "[Rectangle Minimap] Fixed the position error of the minimap after changing maps.",
             "[Minimap Button] Fix the Zygor minimap button texture.",
             "[Announcement] Now supports the world quests on the current map.",
             "[Objective Tracker] Fix the display error of the dashes.",
+            "[Objective Tracker] Minor change the default cosmetic bar setting.",
             "[Skins] Fix the logic of widget skins lazy loading.",
             "[Skins] Improved ElvUI MerathilisUI compatibility.",
             "[Skins] No animation effect for disabled buttons."
         },
         ["koKR"] = {
+            "[Rectangle Minimap] Fixed the position error of the minimap after changing maps.",
             "[Minimap Button] Fix the Zygor minimap button texture.",
             "[Announcement] Now supports the world quests on the current map.",
             "[Objective Tracker] Fix the display error of the dashes.",
+            "[Objective Tracker] Minor change the default cosmetic bar setting.",
             "[Skins] Fix the logic of widget skins lazy loading.",
             "[Skins] Improved ElvUI MerathilisUI compatibility.",
             "[Skins] No animation effect for disabled buttons."

90f19fcc0922632d4f0be0d42339b8a55e015764
Zhou Fang <fang2hou@outlook.com>
2022-05-29 20:30:01 +0900

    changelog

--- Core/Changelog/2.38.lua
+++ Core/Changelog/2.38.lua
@@ -11,19 +11,23 @@ W.Changelog[238] = {
     NEW = {
         ["zhCN"] = {
             "[美化皮肤] 新增物品插入框体皮肤.",
-            "[美化皮肤] 新增物品交互框体皮肤."
+            "[美化皮肤] 新增物品交互框体皮肤.",
+            "[美化皮肤] 新增按钮组件选中背景效果."
         },
         ["zhTW"] = {
             "[美化皮膚] 新增物品插入框架皮膚.",
-            "[美化皮膚] 新增物品互動框架皮膚."
+            "[美化皮膚] 新增物品互動框架皮膚.",
+            "[美化皮膚] 新增按鈕組件選中背景效果."
         },
         ["enUS"] = {
             "[Skins] New item socketing frame skin.",
-            "[Skins] New item interaction frame skin."
+            "[Skins] New item interaction frame skin.",
+            "[Skins] New button widget selected background effect."
         },
         ["koKR"] = {
             "[Skins] New item socketing frame skin.",
-            "[Skins] New item interaction frame skin."
+            "[Skins] New item interaction frame skin.",
+            "[Skins] New button widget selected background effect."
         }
     },
     IMPROVEMENT = {
@@ -31,25 +35,29 @@ W.Changelog[238] = {
             "[通告] 任务通告现在支持当前地图的世界任务.",
             "[进度追踪] 修复标记的显示错误.",
             "[美化皮肤] 修正组件延后载入的逻辑.",
-            "[美化皮肤] 更新了对 ElvUI MerathilisUI 的兼容."
+            "[美化皮肤] 更新了对 ElvUI MerathilisUI 的兼容.",
+            "[美化皮肤] 对于停用的按键, 将不再显示动画效果."
         },
         ["zhTW"] = {
             "[通告] 任務通告現在支持當前地圖的世界任務.",
             "[進度追蹤] 修復標記的顯示錯誤.",
             "[美化皮膚] 修正組件延後載入的邏輯.",
-            "[美化皮膚] 更新了對 ElvUI MerathilisUI 的兼容."
+            "[美化皮膚] 更新了對 ElvUI MerathilisUI 的兼容.",
+            "[美化皮膚] 對於停用的按鍵, 將不再顯示動畫效果."
         },
         ["enUS"] = {
             "[Announcement] Now supports the world quests on the current map.",
             "[Objective Tracker] Fix the display error of the dashes.",
             "[Skins] Fix the logic of widget skins lazy loading.",
-            "[Skins] Improved ElvUI MerathilisUI compatibility."
+            "[Skins] Improved ElvUI MerathilisUI compatibility.",
+            "[Skins] No animation effect for disabled buttons."
         },
         ["koKR"] = {
             "[Announcement] Now supports the world quests on the current map.",
             "[Objective Tracker] Fix the display error of the dashes.",
             "[Skins] Fix the logic of widget skins lazy loading.",
-            "[Skins] Improved ElvUI MerathilisUI compatibility."
+            "[Skins] Improved ElvUI MerathilisUI compatibility.",
+            "[Skins] No animation effect for disabled buttons."
         }
     }
 }

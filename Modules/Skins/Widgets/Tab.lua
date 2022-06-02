42a926124006bd566ab945cda692f308b3fb3b04
Zhou Fang <fang2hou@outlook.com>
2022-06-02 23:12:28 +0900

    forgot these two

--- Modules/Skins/Widgets/Tab.lua
+++ Modules/Skins/Widgets/Tab.lua
@@ -75,10 +75,10 @@ function WS:HandleTab(_, tab, noBackdrop, template)
             function(frame, scriptType)
                 if scriptType == "OnEnter" then
                     self:Unhook(frame, "OnEnter")
-                    self:SecureHookScript(frame, "OnEnter", onEnter)
+                    self:SecureHookScript(frame, "OnEnter", button.windAnimation.onEnter)
                 elseif scriptType == "OnLeave" then
                     self:Unhook(frame, "OnLeave")
-                    self:SecureHookScript(frame, "OnLeave", onLeave)
+                    self:SecureHookScript(frame, "OnLeave", button.windAnimation.onLeave)
                 end
             end
         )

95fc0c05141f1daee8dcca4c0db46a6b56fdb555
Zhou Fang <fang2hou@outlook.com>
2022-06-02 21:50:09 +0900

    start 2.41

---
+++ Core/Changelog/2.41.lua
@@ -0,0 +1,19 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[241] = {
+    RELEASE_DATE = "2022/06/02",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "[美化皮肤] 修复了按钮美化的错误."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 修復了按鍵美化的錯誤."
+        },
+        ["enUS"] = {
+            "[Skins] Fix button reskin error."
+        },
+        ["koKR"] = {
+            "[스킨] 버튼 스킨 오류를 수정했습니다."
+        }
+    }
+}

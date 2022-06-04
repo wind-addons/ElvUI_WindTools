3593f9c42da658d20eafdc99fdd3c1ec25027fdc
Zhou Fang <fang2hou@outlook.com>
2022-06-04 13:07:07 +0900

    changelog

--- Core/Changelog/2.43.lua
+++ Core/Changelog/2.43.lua
@@ -3,28 +3,56 @@ local W = unpack(select(2, ...))
 W.Changelog[243] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "根据暴雪 9.2.5 的改动, 移除了部分游戏补丁.",
+            "新增了对于 ElvUI 下拉窗体库支持的补丁. (自动载入, 不可设置)",
+            "新增了修复预组建队伍界面中重复队伍显示的补丁. (自动载入, 不可设置)"
+        },
+        ["zhTW"] = {
+            "根據暴雪 9.2.5 的改动, 移除了部分遊戲补丁.",
+            "新增了對於 ElvUI 下拉窗体庫支持的补丁. (自動載入, 不可設定)",
+            "新增了修復預組隊伍界面中重複隊伍顯示的补丁. (自動載入, 不可設定)"
+        },
+        ["enUS"] = {
+            "Removed some game patches recording to the Blizzard's 9.2.5 code changes.",
+            "Added a patch for ElvUI dropdown menu library support. (Load automatically and not configurable)",
+            "Added a patch for fixing the duplicate team display in the LFG list. (Load automatically and not configurable)"
+        },
+        ["koKR"] = {
+            "Removed some game patches recording to the Blizzard's 9.2.5 code changes.",
+            "Added a patch for ElvUI dropdown menu library support. (Load automatically and not configurable)",
+            "Added a patch for fixing the duplicate team display in the LFG list. (Load automatically and not configurable)"
+        }
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[美化皮肤] 新增了 TLDR Missions 的皮肤."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了 TLDR Missions 的皮膚."
+        },
+        ["enUS"] = {
+            "[Skins] New skin for TLDR Missions."
+        },
+        ["koKR"] = {
+            "[Skins] New skin for TLDR Missions."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[美化皮肤] 移除了部分无用的设置.",
             "[聊天链接] 支持导灵器的链接图标."
         },
         ["zhTW"] = {
+            "[美化皮膚] 移除了部分無用的設定.",
             "[聊天鏈接] 支援靈印的鏈接圖示."
         },
         ["enUS"] = {
+            "[Skins] Removed some useless options.",
             "[Chat Link] Support conduit link icon."
         },
         ["koKR"] = {
+            "[Skins] Removed some useless options.",
             "[Chat Link] Support conduit link icon."
         }
     }

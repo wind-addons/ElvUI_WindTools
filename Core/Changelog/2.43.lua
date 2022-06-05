6819b974fdefb684d707865b4853a2153bdc01d4
Zhou Fang <fang2hou@outlook.com>
2022-06-05 10:38:25 +0900

    changelog

--- Core/Changelog/2.43.lua
+++ Core/Changelog/2.43.lua
@@ -38,22 +38,30 @@ W.Changelog[243] = {
         ["zhCN"] = {
             "兼容了网易集合石开心版最新的改动.",
             "[美化皮肤] 移除了部分无用的设置.",
-            "[聊天链接] 支持导灵器的链接图标."
+            "[聊天链接] 支持导灵器的链接图标.",
+            "[副本难度] 修复随机难度的文本.",
+            "[好友列表] 注释作为名字现在支持离线玩家."
         },
         ["zhTW"] = {
             "兼容了網易集合石开心版最新的改动.",
             "[美化皮膚] 移除了部分無用的設定.",
-            "[聊天鏈接] 支援靈印的鏈接圖示."
+            "[聊天鏈接] 支援靈印的鏈接圖示.",
+            "[副本難度] 修復隨機難度的文本.",
+            "[好友列表] 注記作為名字現在支持離線玩家."
         },
         ["enUS"] = {
             "Compatible with the latest changes of NetEase MeetingStone Happy Edit.",
             "[Skins] Removed some useless options.",
-            "[Chat Link] Support conduit link icon."
+            "[Chat Link] Support conduit link icon.",
+            "[Dungeon Difficulty] Fix the text for LFG difficulty.",
+            "[Friends List] Note as name now supports offline players."
         },
         ["koKR"] = {
             "Compatible with the latest changes of NetEase MeetingStone Happy Edit.",
             "[Skins] Removed some useless options.",
-            "[Chat Link] Support conduit link icon."
+            "[Chat Link] Support conduit link icon.",
+            "[Dungeon Difficulty] Fix the text for LFG difficulty.",
+            "[Friends List] Note as name now supports offline players."
         }
     }
 }

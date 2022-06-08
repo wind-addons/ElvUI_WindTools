0d132289c9cc84e0779fd3ae2d8bfeff0753318a
fang2hou <fang2hou@outlook.com>
2022-06-08 12:14:18 +0900

    error handler & logger

--- Core/Functions/Developer.lua
+++ Core/Functions/Developer.lua
@@ -1,5 +1,6 @@
 local W, F, E, L, V, P, G = unpack(select(2, ...))
 
+local _G = _G
 local format = format
 local pairs = pairs
 local print = print
@@ -9,6 +10,14 @@ local tostring = tostring
 local type = type
 
 F.Developer = {}
+
+local function isDebugMode()
+    if E.global and E.global.WT and E.global.WT.core.debugMode then
+        return true
+    end
+
+    return false
+end
 
 --[[
     高级打印函数
@@ -53,4 +62,43 @@ function F.Developer.Print(object)
     else
         print("(" .. type(object) .. ") " .. tostring(object))
     end
+end
+
+--[[
+    Custom Error Handler
+    @param ...string Error Message
+]]
+function F.Developer.ThrowError(...)
+    if not isDebugMode() then
+        return
+    end
+
+    local message = strjoin(" ", ...)
+    _G.geterrorhandler()(format("%s |cffe74c3c%s|r\n", W.Title, "[ERROR]") .. message)
+end
+
+--[[
+    Custom Logger [INFO]
+    @param ...string Message
+]]
+function F.Developer.LogInfo(...)
+    if not isDebugMode() then
+        return
+    end
+
+    local message = strjoin(" ", ...)
+    print(format("%s |cff3498db%s|r\n", W.Title, "[INFO]") .. message)
+end
+
+--[[
+    Custom Logger [WARNING]
+    @param ...string Message
+]]
+function F.Developer.LogWarning(...)
+    if not isDebugMode() then
+        return
+    end
+
+    local message = strjoin(" ", ...)
+    print(format("%s |cfff1c40f%s|r\n", W.Title, "[WARNING]") .. message)
 end

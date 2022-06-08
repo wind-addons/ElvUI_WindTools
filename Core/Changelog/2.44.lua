76f150b647b7b646dd4620bf0a0aad68d7b4e0b0
fang2hou <fang2hou@outlook.com>
2022-06-09 08:20:42 +0900

    changelog

--- Core/Changelog/2.44.lua
+++ Core/Changelog/2.44.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[244] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "LibOpenRaid 升级到 43 版本."
+        },
+        ["zhTW"] = {
+            "LibOpenRaid 升級到 33 版本."
+        },
+        ["enUS"] = {
+            "Update LibOpenRaid to version 43."
+        },
+        ["koKR"] = {
+            "LibOpenRaid를 버전 43으로 업그레이드하였습니다."
+        }
     },
     NEW = {
         ["zhCN"] = {},

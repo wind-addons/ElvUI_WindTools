b7221e931ea6d7420bd1a881d7998eb4a7d10b25
fang2hou <fang2hou@outlook.com>
2022-06-13 01:13:36 +0900

    changelog

--- Core/Changelog/2.44.lua
+++ Core/Changelog/2.44.lua
@@ -26,21 +26,33 @@ W.Changelog[244] = {
         ["zhCN"] = {
             "更正了日志记录函数注入模块的时机.",
             "[美化外观] 优化了光环的阴影颜色逻辑.",
+            "[美化外观] 优化了 Azeroth Auto Pilot 皮肤",
+            "[美化外观] 优化了 BugSack 皮肤",
+            "[美化外观] 优化了 WeakAuras 皮肤",
             "[交易] 感谢按钮可以对跨服角色产生作用了."
         },
         ["zhTW"] = {
             "更正了日誌記錄函式注入模組的時機.",
             "[美化外觀] 優化了光環的陰影顏色邏輯.",
+            "[美化外觀] 優化了 Azeroth Auto Pilot 皮膚",
+            "[美化外觀] 優化了 BugSack 皮膚",
+            "[美化外觀] 優化了 WeakAuras 皮膚",
             "[交易] 感謝按鈕可以對跨服角色產生作用了."
         },
         ["enUS"] = {
             "Corrected the logger injection to the modules.",
             "[Skins] Optimized the shadow color logic of auras.",
+            "[Skins] Optimized the Azeroth Auto Pilot skin.",
+            "[Skins] Optimized the BugSack skin.",
+            "[Skins] Optimized the WeakAuras skin.",
             "[Trade] Thanks button now works with cross-server players."
         },
         ["koKR"] = {
             "Corrected the logger injection to the modules.",
             "[Skins] Optimized the shadow color logic of auras.",
+            "[Skins] Optimized the Azeroth Auto Pilot skin.",
+            "[Skins] Optimized the BugSack skin.",
+            "[Skins] Optimized the WeakAuras skin.",
             "[Trade] Thanks button now works with cross-server players."
         }
     }

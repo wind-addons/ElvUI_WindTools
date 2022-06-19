2cdad0d095f0d6dded9d6c9a88df56424a21faa3
fang2hou <fang2hou@outlook.com>
2022-06-19 15:40:48 +0900

    changelog

--- Core/Changelog/2.45.lua
+++ Core/Changelog/2.45.lua
@@ -9,10 +9,18 @@ W.Changelog[245] = {
         ["koKR"] = {}
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[超级追踪] 新增了智能解析文本中坐标点并设定的功能."
+        },
+        ["zhTW"] = {
+            "[超級追蹤] 新增了智能解析文本中坐標點並設定的功能."
+        },
+        ["enUS"] = {
+            "[Super Tracker] Added a feature that automatically parses text to extract the coordinates and set the waypoint."
+        },
+        ["koKR"] = {
+            "[슈퍼 트래커] 이제 자동으로 텍스트를 파싱하여 좌표를 추출하고 웨이포인트를 설정할 수 있습니다."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {

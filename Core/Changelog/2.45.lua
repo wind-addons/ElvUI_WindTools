002a64606032fac51436930cda4f0fce0f843c18
fang2hou <fang2hou@outlook.com>
2022-06-21 21:38:26 +0900

    changelog

--- Core/Changelog/2.45.lua
+++ Core/Changelog/2.45.lua
@@ -28,15 +28,23 @@ W.Changelog[245] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[盟约助手] 修复了法夜圣骑士的四季祝福转换的问题.",
+            "[盟约助手] 修复了温西尔技能转换的问题.",
             "[美化外观] 修复了 WeakAuras 的按钮皮肤错误."
         },
         ["zhTW"] = {
+            "[誓盟助手] 修復了暗夜妖精聖騎士的四季祝福轉換的問題.",
+            "[誓盟助手] 修復了汎希爾族技能轉換的問題.",
             "[美化外觀] 修復了 WeakAuras 的按鍵皮膚錯誤."
         },
         ["enUS"] = {
+            "[Covenant Helper] Fixed the bug that caused the Blessing of the Seasons not be set.",
+            "[Covenant Helper] Fixed the bug that caused the Venthyr spell not be set.",
             "[Skins] Fixed the error of the button skin of WeakAuras."
         },
         ["koKR"] = {
+            "[Covenant Helper] Fixed the bug that caused the 사계의 축복 not be set.",
+            "[Covenant Helper] Fixed the bug that caused the 벤티르 spell not be set.",
             "[스킨] WeakAuras 버튼 스킨 오류를 수정했습니다."
         }
     }

297f048297ac204ab5e05c6ca0e138ca1cc4c07c
fang2hou <fang2hou@outlook.com>
2022-07-09 22:16:39 +0900

    changelog

--- Core/Changelog/2.46.lua
+++ Core/Changelog/2.46.lua
@@ -1,7 +1,7 @@
 local W = unpack(select(2, ...))
 
 W.Changelog[246] = {
-    RELEASE_DATE = "TBD",
+    RELEASE_DATE = "2022/07/09",
     IMPORTANT = {
         ["zhCN"] = {},
         ["zhTW"] = {},
@@ -15,9 +15,17 @@ W.Changelog[246] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[美化外观] 优化了 TLDR Missions 皮肤."
+        },
+        ["zhTW"] = {
+            "[美化外觀] 優化了 TLDR Missions 皮膚."
+        },
+        ["enUS"] = {
+            "[Skins] Optimized TLDR Missions skin."
+        },
+        ["koKR"] = {
+            "[스킨] TLDR Missions 스킨 최적화."
+        }
     }
 }

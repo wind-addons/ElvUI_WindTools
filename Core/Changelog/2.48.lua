a8be548116b6787d15bbe603e017833b98f02bde
fang2hou <fang2hou@outlook.com>
2022-08-06 18:54:40 +0900

    changelog

--- Core/Changelog/2.48.lua
+++ Core/Changelog/2.48.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[248] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "修复了可能导致无法举报玩家的错误."
+        },
+        ["zhTW"] = {
+            "修復了可能導致無法举报玩家的錯誤."
+        },
+        ["enUS"] = {
+            "Fixed error that may cause the error while reporting a player."
+        },
+        ["koKR"] = {
+            "Fixed error that may cause the error while reporting a player."
+        }
     },
     NEW = {
         ["zhCN"] = {},
@@ -16,16 +24,20 @@ W.Changelog[248] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "[美化皮肤] 修复 WeakAuras 阴影."
+            "[美化皮肤] 修复 WeakAuras 阴影.",
+            "[美化皮肤] 修复 ElvUI 信息文字框体背景隐藏时阴影依旧显示的问题."
         },
         ["zhTW"] = {
-            "[美化皮膚] 修復 WeakAuras 陰影."
+            "[美化皮膚] 修復 WeakAuras 陰影.",
+            "[美化皮膚] 修復 ElvUI 資訊文字框体背景隱藏時陰影顯示的問題."
         },
         ["enUS"] = {
-            "[Skins] Fix WeakAuras shadow."
+            "[Skins] Fix WeakAuras shadow.",
+            "[Skins] Fix the shadow display of ElvUI datatext panels."
         },
         ["koKR"] = {
-            "[스킨] WeakAuras 그림자 수정."
+            "[스킨] WeakAuras 그림자 수정.",
+            "[Skins] Fix the shadow display of ElvUI datatext panels."
         }
     }
 }

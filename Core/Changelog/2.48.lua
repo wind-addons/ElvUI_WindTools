de97ed2afffaa173c5ced2c44560cc151c8fa99e
fang2hou <fang2hou@outlook.com>
2022-08-13 02:09:32 +0900

    changelog

--- Core/Changelog/2.48.lua
+++ Core/Changelog/2.48.lua
@@ -24,18 +24,34 @@ W.Changelog[248] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[游戏条] 新增新版本炉石的支持. 感谢 Charles Yost",
+            "[自动化] 自动接受召唤时将会在接受后隐藏提示框.",
+            "[自动化] 自动接受召唤调整到 0.6 秒后执行.",
+            "[过滤器] 在启用解锁过滤器功能时, 打开客服界面将会显示提示.",
             "[美化皮肤] 修复 WeakAuras 阴影.",
             "[美化皮肤] 修复 ElvUI 信息文字框体背景隐藏时阴影依旧显示的问题."
         },
         ["zhTW"] = {
+            "[游戏條] 新增新版本炉石的支持. 感謝 Charles Yost",
+            "[自動化] 自動接受召喚時將會在接受後隱藏提示框.",
+            "[自動化] 自動接受召喚调整到 0.6 秒後執行.",
+            "[過濾器] 在啟用解鎖過濾器功能時, 開啟客服界面將會顯示提示.",
             "[美化皮膚] 修復 WeakAuras 陰影.",
             "[美化皮膚] 修復 ElvUI 資訊文字框体背景隱藏時陰影顯示的問題."
         },
         ["enUS"] = {
+            "[GameBar] New heartstone added in latest patch is supported. Thanks Charles Yost",
+            "[Automation] Automatically accept summon will hide the popup after confirming.",
+            "[Automation] Automatically accept summon will be executed after 0.6 seconds.",
+            "[Filter] When enabling the unlock profanity filter, it will show a message when customer service frame opens.",
             "[Skins] Fix WeakAuras shadow.",
             "[Skins] Fix the shadow display of ElvUI datatext panels."
         },
         ["koKR"] = {
+            "[GameBar] New heartstone added in latest patch is supported. Thanks Charles Yost",
+            "[Automation] Automatically accept summon will hide the popup after confirming.",
+            "[Automation] Automatically accept summon will be executed after 0.6 seconds.",
+            "[Filter] When enabling the unlock profanity filter, it will show a message when customer service frame opens.",
             "[스킨] WeakAuras 그림자 수정.",
             "[Skins] Fix the shadow display of ElvUI datatext panels."
         }

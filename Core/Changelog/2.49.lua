cb9173cf2aa44cdad04467d039f1195644cd4e9b
fang2hou <fang2hou@outlook.com>
2022-09-11 03:00:17 +0900

    changelog

--- Core/Changelog/2.49.lua
+++ Core/Changelog/2.49.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[249] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "重构了预组队频道信息获取逻辑."
+        },
+        ["zhTW"] = {
+            "重構了預組隊頻道資訊獲取邏輯."
+        },
+        ["enUS"] = {
+            "Refactor the logic of getting the player information from the LFG list."
+        },
+        ["koKR"] = {
+            "Refactor the logic of getting the player information from the LFG list."
+        }
     },
     NEW = {
         ["zhCN"] = {},
@@ -16,15 +24,19 @@ W.Changelog[249] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[鼠标提示] 队伍信息现在可以显示更多的信息.",
             "[好友列表] 新增了巫妖王之怒版本图标."
         },
         ["zhTW"] = {
+            "[滑鼠提示] 隊伍資訊現在可以顯示更多的資訊.",
             "[好友列表] 新增了巫妖王之怒版本圖示."
         },
         ["enUS"] = {
+            "[Tooltip] The tooltip of the LFG group now can show more information.",
             "[Friend List] Add the new WotLK icon."
         },
         ["koKR"] = {
+            "[Tooltip] The tooltip of the LFG group now can show more information.",
             "[Friend List] Add the new WotLK icon."
         }
     }

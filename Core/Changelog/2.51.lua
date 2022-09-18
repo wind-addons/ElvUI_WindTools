9669641bb3721f21443470d476366c897f3ece91
fang2hou <fang2hou@outlook.com>
2022-09-18 21:30:38 +0900

    docs(changelog): 2.51

---
+++ Core/Changelog/2.51.lua
@@ -0,0 +1,31 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[251] = {
+    RELEASE_DATE = "2022/09/18",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "[预组队玩家信息] 修复了无专精玩家的信息错误."
+        },
+        ["zhTW"] = {
+            "[預組玩家資訊] 修復了無專精玩家的資訊錯誤."
+        },
+        ["enUS"] = {
+            "[Party Info] Fix the error of player with no spec selected."
+        },
+        ["koKR"] = {
+            "[Party Info] Fix the error of player with no spec selected."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    }
+}

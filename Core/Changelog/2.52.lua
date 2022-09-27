92b673936b1a9f34d22b1e6a9c6e8d88c6a8bc0d
fang2hou <fang2hou@outlook.com>
2022-09-28 00:27:30 +0900

    docs(changelog): prepare 2.52

---
+++ Core/Changelog/2.52.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[252] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[其他] 新增一个法术激活提示的自定义模块."
+        },
+        ["zhTW"] = {
+            "[其他] 新增一個法術激活提示的自定義模組."
+        },
+        ["enUS"] = {
+            "[Misc] Add a custom module for spell activation alert."
+        },
+        ["koKR"] = {
+            "[Misc] Add a custom module for spell activation alert."
+        },
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "俄语翻译更新. 感谢 Evgeniy-ONiX."
+        },
+        ["zhTW"] = {
+            "俄語翻譯更新. 感謝 Evgeniy-ONiX."
+        },
+        ["enUS"] = {
+            "Update Russian translation. Thanks Evgeniy-ONiX."
+        },
+        ["koKR"] = {
+            "Update Russian translation. Thanks Evgeniy-ONiX."
+        },
+    }
+}

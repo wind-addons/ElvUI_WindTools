e885d038adbe0266f08c60d50f5dcd2908136695
fang2hou <fang2hou@outlook.com>
2022-09-28 00:50:29 +0900

    docs(changelog): add the abbr change

--- Core/Changelog/2.52.lua
+++ Core/Changelog/2.52.lua
@@ -20,20 +20,24 @@ W.Changelog[252] = {
         },
         ["koKR"] = {
             "[Misc] Add a custom module for spell activation alert."
-        },
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "修正了一个英语缩写错误. 现在「Castle Nathria」能够正确缩写为「CN」了.",
             "俄语翻译更新. 感谢 Evgeniy-ONiX."
         },
         ["zhTW"] = {
+            "修正了一個英語縮寫錯誤. 現在「Castle Nathria」能夠正確縮寫為「CN」了.",
             "俄語翻譯更新. 感謝 Evgeniy-ONiX."
         },
         ["enUS"] = {
+            'Fix a typo in English abbreviation. Now "Castle Nathria" can be abbreviated as "CN".',
             "Update Russian translation. Thanks Evgeniy-ONiX."
         },
         ["koKR"] = {
+            'Fix a typo in English abbreviation. Now "Castle Nathria" can be abbreviated as "CN".',
             "Update Russian translation. Thanks Evgeniy-ONiX."
-        },
+        }
     }
 }

c81d5415895f0e2bce5602c3b2d18b0efcf1e6b2
fang2hou <fang2hou@outlook.com>
2022-10-24 12:37:53 +0900

    fix(skins): elvui minimap

--- Modules/Skins/ElvUI/MiniMap.lua
+++ Modules/Skins/ElvUI/MiniMap.lua
@@ -1,17 +1,19 @@
 local W, F, E, L = unpack(select(2, ...))
+local M = E:GetModule("Minimap")
 local S = W.Modules.Skins
 
 local _G = _G
 
 function S:ElvUI_MiniMap()
-    if not E.private.general.minimap.enable then
+    if not E.private.general.minimap.enable or not M.holder then
         return
     end
+
     if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.miniMap) then
         return
     end
 
-    self:CreateShadow(_G.MMHolder)
+    self:CreateShadow(M.holder)
     self:CreateShadow(_G.MinimapRightClickMenu)
 end
 

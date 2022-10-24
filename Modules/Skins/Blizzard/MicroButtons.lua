5bdc8d05d03003918fc5a2bf6ddc99332009e878
fang2hou <fang2hou@outlook.com>
2022-10-24 14:48:38 +0900

    fix(skins): elvui styled micro bar

--- Modules/Skins/Blizzard/MicroButtons.lua
+++ Modules/Skins/Blizzard/MicroButtons.lua
@@ -2,7 +2,22 @@ local W, F, E, L = unpack(select(2, ...))
 local S = W.Modules.Skins
 
 local _G = _G
-local MICRO_BUTTONS = MICRO_BUTTONS
+local MICRO_BUTTONS =
+    _G.MICRO_BUTTONS or
+    {
+        "CharacterMicroButton",
+        "SpellbookMicroButton",
+        "TalentMicroButton",
+        "AchievementMicroButton",
+        "QuestLogMicroButton",
+        "GuildMicroButton",
+        "LFDMicroButton",
+        "EJMicroButton",
+        "CollectionsMicroButton",
+        "MainMenuMicroButton",
+        "HelpMicroButton",
+        "StoreMicroButton"
+    }
 
 function S:MicroButtons()
     if not self:CheckDB(nil, "microButtons") then
@@ -10,8 +25,11 @@ function S:MicroButtons()
     end
 
     for i = 1, #MICRO_BUTTONS do
-        if _G[MICRO_BUTTONS[i]].backdrop then
-            self:CreateBackdropShadow(_G[MICRO_BUTTONS[i]], true)
+        if _G[MICRO_BUTTONS[i]] then
+            if not _G[MICRO_BUTTONS[i]].backdrop then
+                _G[MICRO_BUTTONS[i]]:CreateBackdrop()
+                self:CreateBackdropShadow(_G[MICRO_BUTTONS[i]], true)
+            end
         end
     end
 end

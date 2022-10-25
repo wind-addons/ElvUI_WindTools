31b94f6ddb83a69dec7650244749ebc0e3ccbd4c
fang2hou <fang2hou@outlook.com>
2022-10-26 02:26:21 +0900

    fix(skins): blizzard options -> settings panel

---
+++ Modules/Skins/Blizzard/SettingsPanel.lua
@@ -0,0 +1,16 @@
+local W, F, E, L = unpack(select(2, ...))
+local S = W.Modules.Skins
+
+local _G = _G
+
+local pairs = pairs
+
+function S:SettingsPanel()
+    if not self:CheckDB("blizzardOptions", "settingsPanel") then
+        return
+    end
+
+    self:CreateShadow(_G.SettingsPanel)
+end
+
+S:AddCallback("SettingsPanel")

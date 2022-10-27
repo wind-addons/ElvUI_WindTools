b36eed0eb44ff7274dbb225612ac3138aff95cad
fang2hou <fang2hou@outlook.com>
2022-10-27 18:01:37 +0900

    docs(readme): 2.56

---
+++ Core/Changelog/2.56.lua
@@ -0,0 +1,51 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[256] = {
+    RELEASE_DATE = "2022/10/27",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "修复了一些选项的错误.",
+            "[美化皮肤] 修复 BigWigs 的美化皮肤.",
+            "[美化皮肤] 修复 TLDRMissions 的美化皮肤.",
+            "[美化皮肤] 修复 TomCats 的美化皮肤.",
+            "[美化皮肤] 修复 WeakAuras 的美化皮肤.",
+            "[美化皮肤] 修复 WeakAuras 选项的美化皮肤.",
+        },
+        ["zhTW"] = {
+            "修復了一些選項的錯誤.",
+            "[美化皮膚] 修復 BigWigs 的美化皮膚.",
+            "[美化皮膚] 修復 TLDRMissions 的美化皮膚.",
+            "[美化皮膚] 修復 TomCats 的美化皮膚.",
+            "[美化皮膚] 修復 WeakAuras 的美化皮膚.",
+            "[美化皮膚] 修復 WeakAuras 選項的美化皮膚.",
+        },
+        ["enUS"] = {
+            "Fix some options' error.",
+            "[Skins] Fix the BigWigs skin.",
+            "[Skins] Fix the TLDRMissions skin.",
+            "[Skins] Fix the TomCats skin.",
+            "[Skins] Fix the WeakAuras skin.",
+            "[Skins] Fix the WeakAuras options skin.",
+        },
+        ["koKR"] = {
+            "Fix some options' error.",
+            "[Skins] Fix the BigWigs skin.",
+            "[Skins] Fix the TLDRMissions skin.",
+            "[Skins] Fix the TomCats skin.",
+            "[Skins] Fix the WeakAuras skin.",
+            "[Skins] Fix the WeakAuras options skin.",
+        }
+    }
+}

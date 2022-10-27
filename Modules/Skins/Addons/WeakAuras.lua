af8dd527f0ce7cbf732e5f2d8a215ff96e114584
fang2hou <fang2hou@outlook.com>
2022-10-27 17:21:35 +0900

    fix(skins): wa icon and aurabar

--- Modules/Skins/Addons/WeakAuras.lua
+++ Modules/Skins/Addons/WeakAuras.lua
@@ -97,9 +97,6 @@ function S:ProfilingWindow_UpdateButtons(frame)
 end
 
 local function Skin_WeakAuras(f, fType)
-    -- Modified from NDui WeakAuras Skins
-    -- 1. Use ElvUI Skins functions
-    -- 2. Fix the TexCoords
     if fType == "icon" then
         if not f.__windSkin then
             f.icon.SetTexCoordOld = f.icon.SetTexCoord
@@ -188,37 +185,18 @@ function S:WeakAuras()
         self:RawHook(_G.WeakAuras, "RegisterRegionOptions", "WeakAuras_RegisterRegionOptions")
     end
 
-    local regionTypes = _G.WeakAuras.regionTypes
-    local Create_Icon, Modify_Icon = regionTypes.icon.create, regionTypes.icon.modify
-    local Create_AuraBar, Modify_AuraBar = regionTypes.aurabar.create, regionTypes.aurabar.modify
-
-    regionTypes.icon.create = function(parent, data)
-        local region = Create_Icon(parent, data)
-        Skin_WeakAuras(region, "icon")
-        return region
+    -- Handle the options region type registration
+    -- from NDui
+    local function OnPrototypeCreate(region)
+        Skin_WeakAuras(region, region.regionType)
     end
 
-    regionTypes.aurabar.create = function(parent)
-        local region = Create_AuraBar(parent)
-        Skin_WeakAuras(region, "aurabar")
-        return region
+    local function OnPrototypeModifyFinish(_, region)
+        Skin_WeakAuras(region, region.regionType)
     end
 
-    regionTypes.icon.modify = function(parent, region, data)
-        Modify_Icon(parent, region, data)
-        Skin_WeakAuras(region, "icon")
-    end
-
-    regionTypes.aurabar.modify = function(parent, region, data)
-        Modify_AuraBar(parent, region, data)
-        Skin_WeakAuras(region, "aurabar")
-    end
-
-    for weakAura, regions in pairs(_G.WeakAuras.regions) do
-        if regions.regionType == "icon" or regions.regionType == "aurabar" then
-            Skin_WeakAuras(regions.region, regions.regionType)
-        end
-    end
+    self:SecureHook(WeakAuras.regionPrototype, "create", OnPrototypeCreate)
+    self:SecureHook(WeakAuras.regionPrototype, "modifyFinish", OnPrototypeModifyFinish)
 
     -- Real Time Profiling Window
     local profilingWindow = _G.WeakAuras.RealTimeProfilingWindow

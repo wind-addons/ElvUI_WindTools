bc053fa71b0a230703fd8e40559ae004d748ef8e
fang2hou <fang2hou@outlook.com>
2022-10-30 01:57:15 +0900

    feat(skins): friend button

--- Modules/Skins/Blizzard/Friends.lua
+++ Modules/Skins/Blizzard/Friends.lua
@@ -4,6 +4,39 @@ local S = W.Modules.Skins
 local _G = _G
 local pairs = pairs
 
+function S:UpdateFriendButton(button)
+    if not button.right then
+        button.right = button:CreateTexture(nil, "BACKGROUND")
+        button.right:SetWidth(button:GetWidth() / 2)
+        button.right:SetHeight(32)
+        button.right:SetPoint("LEFT", button, "CENTER", 0)
+        button.right:SetTexture(E.LSM:Fetch("statusbar", E.media.normTex))
+        button.right:SetGradient("HORIZONTAL", CreateColor(.243, .57, 1, 0), CreateColor(.243, .57, 1, .25))
+
+        if button.gameIcon then
+            button.gameIcon:HookScript(
+                "OnShow",
+                function()
+                    button.right:Show()
+                end
+            )
+
+            button.gameIcon:HookScript(
+                "OnHide",
+                function()
+                    button.right:Hide()
+                end
+            )
+
+            if button.gameIcon:IsShown() then
+                button.right:Show()
+            else
+                button.right:Hide()
+            end
+        end
+    end
+end
+
 function S:FriendsFrame()
     if not self:CheckDB("friends") then
         return
@@ -28,6 +61,8 @@ function S:FriendsFrame()
     for i = 1, 4 do
         self:ReskinTab(_G["FriendsFrameTab" .. i])
     end
+
+    self:SecureHook("FriendsFrame_UpdateFriendButton", "UpdateFriendButton")
 end
 
 S:AddCallback("FriendsFrame")

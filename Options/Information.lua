b369d8d23c232b6a06f06d337eb1c188d7c754ef
fang2hou <fang2hou@outlook.com>
2022-11-08 03:45:28 +0900

    fix(options): color regex

--- Options/Information.lua
+++ Options/Information.lua
@@ -718,18 +718,7 @@ for version, data in pairs(W.Changelog) do
             name = function()
                 local text = ""
                 for index, line in ipairs(importantPart) do
-                    text =
-                        text ..
-                        format("%02d", index) ..
-                            ". " ..
-                                gsub(
-                                    line,
-                                    "%[.+%]",
-                                    function(s)
-                                        return AddColor(s)
-                                    end
-                                ) ..
-                                    "\n"
+                    text = text .. format("%02d", index) .. ". " .. gsub(line, "%[[^%[]+%]", AddColor) .. "\n"
                 end
                 return text .. "\n"
             end,

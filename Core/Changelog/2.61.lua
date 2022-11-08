94accd2d4ef230b70053ebabb5492d4697099128
fang2hou <fang2hou@outlook.com>
2022-11-08 11:17:46 +0900

    docs(changelog): 2.61

---
+++ Core/Changelog/2.61.lua
@@ -0,0 +1,55 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[261] = {
+    RELEASE_DATE = "2022/11/08",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "兼容 PTR 及 Beta 游戏版本."
+        },
+        ["zhTW"] = {
+            "兼容 PTR 及 Beta 遊戲版本."
+        },
+        ["enUS"] = {
+            "Compatible with PTR and Beta WoW."
+        },
+        ["koKR"] = {
+            "Compatible with PTR and Beta WoW."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "优化选项设定部分代码.",
+            "[美化皮肤] 修复地下城手册皮肤.",
+            "[美化皮肤] 修复网易集合石皮肤.",
+            "[移动框体] 优化代码.",
+            "[鼠标提示] 移除了统御等级功能."
+        },
+        ["zhTW"] = {
+            "優化選項設定部分程式碼.",
+            "[美化皮膚] 修復地城導覽手冊皮膚.",
+            "[美化皮膚] 修復網易集合石皮膚.",
+            "[移動框架] 優化程式碼.",
+            "[滑鼠提示] 移除了統御等級功能."
+        },
+        ["enUS"] = {
+            "Optimize the code of the option settings.",
+            "[Skins] Fix Encounter Journal skin.",
+            "[Skins] Fix NetEase Meeting Stone skin.",
+            "[Move Frames] Optimize the code.",
+            "[Tooltips] Remove domination rank support."
+        },
+        ["koKR"] = {
+            "Optimize the code of the option settings.",
+            "[Skins] Fix Encounter Journal skin.",
+            "[Skins] Fix NetEase Meeting Stone skin.",
+            "[Move Frames] Optimize the code.",
+            "[Tooltips] Remove domination rank support."
+        }
+    }
+}

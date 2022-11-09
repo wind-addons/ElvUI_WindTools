811b42044dc789689e1cad31bc3cbaff5d60073c
fang2hou <fang2hou@outlook.com>
2022-11-09 20:02:04 +0900

    docs(changelog): 2.62

---
+++ Core/Changelog/2.62.lua
@@ -0,0 +1,43 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[262] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "最低支持的 ElvUI 版本变更为 12.99.",
+            "按钮修复现在可以自动进行."
+        },
+        ["zhTW"] = {
+            "最低支援的 ElvUI 版本變更為 12.99.",
+            "按鈕修復現在可以自動進行."
+        },
+        ["enUS"] = {
+            "The minimum supported ElvUI version is now 12.99.",
+            "The button fix now can be automatically executed."
+        },
+        ["koKR"] = {
+            "The minimum supported ElvUI version is now 12.99.",
+            "The button fix now can be automatically executed."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[快速焦点] 修复可能导致的无法设置焦点的问题.",
+        },
+        ["zhTW"] = {
+            "[快速焦點] 修復可能導致的無法設置焦點的問題.",
+        },
+        ["enUS"] = {
+            "[Quick Focus] Fix the issue that may cause the focus to be unable to be set.",
+        },
+        ["koKR"] = {
+            "[Quick Focus] Fix the issue that may cause the focus to be unable to be set.",
+        },
+    }
+}

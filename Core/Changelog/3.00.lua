d43a0b52d172899ee9ccdf20fa78412e971ef5dd
fang2hou <fang2hou@outlook.com>
2022-11-11 04:07:05 +0900

    docs(changelog): let's go 3.00

--- Core/Changelog/3.00.lua
+++ Core/Changelog/3.00.lua
@@ -1,6 +1,6 @@
 local W = unpack(select(2, ...))
 
-W.Changelog[262] = {
+W.Changelog[300] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
@@ -25,35 +25,59 @@ W.Changelog[262] = {
         }
     },
     NEW = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "翻新了一下设定相关的图片素材.",
+            "新增了 [高级] 设定分类, 用于调整插件核心设定和游戏修复功能设定."
+        },
+        ["zhTW"] = {
+            "翻新了一下設定相關的圖片素材.",
+            "新增了 [進階] 設定分類, 用於調整插件核心設定和遊戲修復功能設定."
+        },
+        ["enUS"] = {
+            "Redesigned some textures used in options.",
+            "Add new [Advanced] option category for adjusting core settings and game fixes."
+        },
+        ["koKR"] = {
+            "Redesigned some textures used in options.",
+            "Add new [Advanced] option category for adjusting core settings and game fixes."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[快速拾取] 兼容 PTR 和 Beta 客户端.",
+            "[矩形小地图] 修复了一个可能初始化多次的错误.",
             "[任务列表] 修复了标题菜单字体修改无法禁用的问题.",
             "[快速焦点] 修复可能导致的无法设置焦点的问题.",
             "[美化皮肤] 阴影美化不再与 MerathilisUI 冲突.",
-            "[美化皮肤] 由于 Azeroth Auto Pilot 项目关闭, 永久移除 Azeroth Auto Pilot 皮肤."
+            "[美化皮肤] 由于 Azeroth Auto Pilot 项目关闭, 永久移除 Azeroth Auto Pilot 皮肤.",
+            "[联系人] 修复了一个可能导致移动框体错误的问题."
         },
         ["zhTW"] = {
+            "[快速拾取] 兼容 PTR 和 Beta 客戶端.",
+            "[矩形小地圖] 修復了一個可能初始化多次的錯誤.",
             "[任務列表] 修復了標題選單字體修改無法禁用的問題.",
             "[快速焦點] 修復可能導致的無法設置焦點的問題.",
             "[美化皮膚] 陰影美化不再與 MerathilisUI 衝突.",
-            "[美化皮膚] 由於 Azeroth Auto Pilot 專案關閉, 永久移除 Azeroth Auto Pilot 皮膚."
+            "[美化皮膚] 由於 Azeroth Auto Pilot 專案關閉, 永久移除 Azeroth Auto Pilot 皮膚.",
+            "[聯絡人] 修復了一個可能導致移動框體錯誤的問題."
         },
         ["enUS"] = {
+            "[Fast Loot] Compatible with PTR and Beta clients.",
+            "[Rectangle Minimap] Fix a possible error that initializes multiple times.",
             "[Objective Tracker] Fixed the problem that the title menu text modification cannot be disabled.",
             "[Quick Focus] Fix the issue that may cause the focus to be unable to be set.",
             "[Skins] The shadow skin no longer conflicts with MerathilisUI.",
-            "[Skins] Remove the Azeroth Auto Pilot skin permanently due to the Azeroth Auto Pilot project is closed."
+            "[Skins] Remove the Azeroth Auto Pilot skin permanently due to the Azeroth Auto Pilot project is closed.",
+            "[Contacts] Fix a possible error that causes the move frames works not properly."
         },
         ["koKR"] = {
+            "[Fast Loot] Compatible with PTR and Beta clients.",
+            "[Rectangle Minimap] Fix a possible error that initializes multiple times.",
             "[Objective Tracker] Fixed the problem that the title menu text modification cannot be disabled.",
             "[Quick Focus] Fix the issue that may cause the focus to be unable to be set.",
             "[Skins] The shadow skin no longer conflicts with MerathilisUI.",
-            "[Skins] Remove the Azeroth Auto Pilot skin permanently due to the Azeroth Auto Pilot project is closed."
+            "[Skins] Remove the Azeroth Auto Pilot skin permanently due to the Azeroth Auto Pilot project is closed.",
+            "[Contacts] Fix a possible error that causes the move frames works not properly."
         }
     }
 }

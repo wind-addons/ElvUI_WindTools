31b58369e0dbf4c02820e337ce759b8ddcf56381
fang2hou <fang2hou@outlook.com>
2022-11-10 13:14:36 +0900

    docs(changelog): update

--- Core/Changelog/2.62.lua
+++ Core/Changelog/2.62.lua
@@ -4,18 +4,22 @@ W.Changelog[262] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
+            "面向中文用户新增了 KOOK (原开黑啦) 支持社区的邀请链接.",
             "最低支持的 ElvUI 版本变更为 12.99.",
             "按钮修复现在可以自动进行."
         },
         ["zhTW"] = {
+            "面向中文用户新增了 KOOK (原開黑啦) 使用支援社群的邀請連結.",
             "最低支援的 ElvUI 版本變更為 12.99.",
             "按鈕修復現在可以自動進行."
         },
         ["enUS"] = {
+            "Add a new invite link for KOOK (formerly known as KaiHeiLa) support community for Chinese users.",
             "The minimum supported ElvUI version is now 12.99.",
             "The button fix now can be automatically executed."
         },
         ["koKR"] = {
+            "Add a new invite link for KOOK (formerly known as KaiHeiLa) support community for Chinese users.",
             "The minimum supported ElvUI version is now 12.99.",
             "The button fix now can be automatically executed."
         }
@@ -28,16 +32,28 @@ W.Changelog[262] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[任务列表] 修复了标题菜单字体修改无法禁用的问题.",
             "[快速焦点] 修复可能导致的无法设置焦点的问题.",
+            "[美化皮肤] 阴影美化不再与 MerathilisUI 冲突.",
+            "[美化皮肤] 由于 Azeroth Auto Pilot 项目关闭, 永久移除 Azeroth Auto Pilot 皮肤."
         },
         ["zhTW"] = {
+            "[任務列表] 修復了標題選單字體修改無法禁用的問題.",
             "[快速焦點] 修復可能導致的無法設置焦點的問題.",
+            "[美化皮膚] 陰影美化不再與 MerathilisUI 衝突.",
+            "[美化皮膚] 由於 Azeroth Auto Pilot 專案關閉, 永久移除 Azeroth Auto Pilot 皮膚."
         },
         ["enUS"] = {
+            "[Objective Tracker] Fixed the problem that the title menu text modification cannot be disabled.",
             "[Quick Focus] Fix the issue that may cause the focus to be unable to be set.",
+            "[Skins] The shadow skin no longer conflicts with MerathilisUI.",
+            "[Skins] Remove the Azeroth Auto Pilot skin permanently due to the Azeroth Auto Pilot project is closed."
         },
         ["koKR"] = {
+            "[Objective Tracker] Fixed the problem that the title menu text modification cannot be disabled.",
             "[Quick Focus] Fix the issue that may cause the focus to be unable to be set.",
-        },
+            "[Skins] The shadow skin no longer conflicts with MerathilisUI.",
+            "[Skins] Remove the Azeroth Auto Pilot skin permanently due to the Azeroth Auto Pilot project is closed."
+        }
     }
 }

7cbab992a876b8e7ceaa85e13f9f536f298f4c11
fang2hou <fang2hou@outlook.com>
2022-11-11 14:23:36 +0900

    docs(changelog): 3.00

--- Core/Changelog/3.00.lua
+++ Core/Changelog/3.00.lua
@@ -4,46 +4,56 @@ W.Changelog[300] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
-            "面向中文用户新增了 KOOK (原开黑啦) 支持社区的邀请链接.",
+            "坚决抵制插件付费行为! 更新授权以对插件分发进行限制, 今后请勿在任何非授权渠道分发本插件.",
+            "授权渠道现在仅为 CurseForge, Wago, WoWInterface, 以及 178 魔兽插件站. 详见 LICENSE 文件.",
             "最低支持的 ElvUI 版本变更为 13.00.",
             "按钮修复现在可以自动进行."
         },
         ["zhTW"] = {
-            "面向中文用户新增了 KOOK (原開黑啦) 使用支援社群的邀請連結.",
+            "堅決抵制插件付費行為! 更新授權以對插件分發進行限制, 今後請勿在任何非授權渠道分發本插件.",
+            "授權渠道現在僅為 CurseForge, Wago, WoWInterface, 以及 178 魔獸插件站. 詳見 LICENSE 文件.",
             "最低支援的 ElvUI 版本變更為 13.00.",
             "按鈕修復現在可以自動進行."
         },
         ["enUS"] = {
-            "Add a new invite link for KOOK (formerly known as KaiHeiLa) support community for Chinese users.",
+            "Resolutely resist the payment behavior of addons! Update license to restrict the distribution of the addon, please do not distribute this addon in any unauthorized channel in the future.",
+            "The authorized channels are now only CurseForge, Wago, WoWInterface, and 178 WoW Addons. See LICENSE file for details.",
             "The minimum supported ElvUI version is now 13.00.",
             "The button fix now can be automatically executed."
         },
         ["koKR"] = {
-            "Add a new invite link for KOOK (formerly known as KaiHeiLa) support community for Chinese users.",
+            "Resolutely resist the payment behavior of addons! Update license to restrict the distribution of the addon, please do not distribute this addon in any unauthorized channel in the future.",
+            "The authorized channels are now only CurseForge, Wago, WoWInterface, and 178 WoW Addons. See LICENSE file for details.",
             "The minimum supported ElvUI version is now 13.00.",
             "The button fix now can be automatically executed."
         }
     },
     NEW = {
         ["zhCN"] = {
+            "面向中文用户新增了 KOOK (原开黑啦) 支持社区的邀请链接.",
             "翻新了一下设定相关的图片素材.",
             "新增了 [高级] 设定分类, 用于调整插件核心设定和游戏修复功能设定."
         },
         ["zhTW"] = {
+            "面向中文用户新增了 KOOK (原開黑啦) 使用支援社群的邀請連結.",
             "翻新了一下設定相關的圖片素材.",
             "新增了 [進階] 設定分類, 用於調整插件核心設定和遊戲修復功能設定."
         },
         ["enUS"] = {
+            "Add a new invite link for KOOK (formerly known as KaiHeiLa) support community for Chinese users.",
             "Redesigned some textures used in options.",
             "Add new [Advanced] option category for adjusting core settings and game fixes."
         },
         ["koKR"] = {
+            "Add a new invite link for KOOK (formerly known as KaiHeiLa) support community for Chinese users.",
             "Redesigned some textures used in options.",
             "Add new [Advanced] option category for adjusting core settings and game fixes."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[其他]-[自动截图] 修复了在第一次载入游戏时可能发生的截图失败.",
+            "[其他]-[自动截图] 改善了截图的时机选择, 现在会在成就提醒完全载入后再进行截图.",
             "[快速拾取] 兼容 PTR 和 Beta 客户端.",
             "[矩形小地图] 修复了一个可能初始化多次的错误.",
             "[任务列表] 修复了标题菜单字体修改无法禁用的问题.",
@@ -53,6 +63,8 @@ W.Changelog[300] = {
             "[联系人] 修复了一个可能导致移动框体错误的问题."
         },
         ["zhTW"] = {
+            "[其他]-[自動擷圖] 修復了在第一次載入遊戲時可能發生的擷圖失敗.",
+            "[其他]-[自動擷圖] 改善了擷圖的時機選擇, 現在會在成就提醒完全載入後再進行擷圖.",
             "[快速拾取] 兼容 PTR 和 Beta 客戶端.",
             "[矩形小地圖] 修復了一個可能初始化多次的錯誤.",
             "[任務列表] 修復了標題選單字體修改無法禁用的問題.",
@@ -62,6 +74,8 @@ W.Changelog[300] = {
             "[聯絡人] 修復了一個可能導致移動框體錯誤的問題."
         },
         ["enUS"] = {
+            "[Other]-[Auto Screenshot] Fix the issue that the screenshot may fail when the game is loaded for the first time.",
+            "[Other]-[Auto Screenshot] Improve the timing selection of taking screenshots, now it will take screenshots after the achievement alert is fully loaded.",
             "[Fast Loot] Compatible with PTR and Beta clients.",
             "[Rectangle Minimap] Fix a possible error that initializes multiple times.",
             "[Objective Tracker] Fixed the problem that the title menu text modification cannot be disabled.",

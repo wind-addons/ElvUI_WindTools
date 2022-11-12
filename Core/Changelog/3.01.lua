d6ed2ec8e740d653949348d94866f75e0592f7a4
fang2hou <fang2hou@outlook.com>
2022-11-12 15:01:08 +0900

    docs(changelog): 3.01

--- Core/Changelog/3.01.lua
+++ Core/Changelog/3.01.lua
@@ -4,32 +4,32 @@ W.Changelog[301] = {
     RELEASE_DATE = "2022/11/12",
     IMPORTANT = {
         ["zhCN"] = {
-            "(!)此版本为紧急修复版本, 功能更新内容请查看 3.00 更新日志.",
+            "[!] 此版本为紧急修复版本, 功能更新内容请查看 3.00 更新日志.",
             "适配 ElvUI 13.01.",
             "最低支持的 ElvUI 版本变更为 13.01.",
-            "[字符串] 跟随 ElvUI 13.01 版本的库文件变动, 旧的导出字符串将无法使用.",
-            "[字符串] 你可以通过降级 ElvUI 和 ElvUI_WindTools 来继续使用旧的导出字符串, 然后再升级到最新版本."
+            "[配置文件] 跟随 ElvUI 13.01 版本的库文件变动, 旧的导出字符串将无法使用.",
+            "[配置文件] 你可以通过降级 ElvUI 和 ElvUI_WindTools 来继续使用旧的导出字符串, 然后再升级到最新版本."
         },
         ["zhTW"] = {
-            "(!)此版本为紧急修复版本, 功能更新内容请查看 3.00 更新日志.",
+            "[!] 此版本为紧急修复版本, 功能更新内容请查看 3.00 更新日志.",
             "适配 ElvUI 13.01.",
             "最低支持的 ElvUI 版本变更为 13.01.",
-            "[字符串] 跟随 ElvUI 13.01 版本的库文件变动, 旧的导出字符串将无法使用.",
-            "[字符串] 你可以通过降级 ElvUI 和 ElvUI_WindTools 来继续使用旧的导出字符串, 然后再升级到最新版本."
+            "[設定檔] 跟随 ElvUI 13.01 版本的库文件变动, 旧的导出字符串将无法使用.",
+            "[設定檔] 你可以通过降级 ElvUI 和 ElvUI_WindTools 来继续使用旧的导出字符串, 然后再升级到最新版本."
         },
         ["enUS"] = {
-            "(!)This is a hotfix version, please check the 3.00 changelog for new features.",
+            "[!] This is a hotfix version, please check the 3.00 changelog for new features.",
             "ElvUI 13.01 compatibility.",
             "ElvUI 13.01 is the minimum supported version.",
-            "[String] Follow the library file change of ElvUI 13.01, the old export string will not work.",
-            "[String] You can downgrade ElvUI and ElvUI_WindTools to continue using the old export string, then upgrade to the latest version."
+            "[Profile] Follow the library file change of ElvUI 13.01, the old export string will not work.",
+            "[Profile] You can downgrade ElvUI and ElvUI_WindTools to continue using the old export string, then upgrade to the latest version."
         },
         ["koKR"] = {
-            "(!)이것은 긴급 패치 버전입니다. 새로운 기능은 3.00 변경 로그를 확인하십시오.",
+            "[!] 이것은 긴급 패치 버전입니다. 새로운 기능은 3.00 변경 로그를 확인하십시오.",
             "ElvUI 13.01 호환성.",
             "ElvUI 13.01은 최소 지원 버전입니다.",
-            "[문자열] ElvUI 13.01의 라이브러리 파일 변경에 따라 이전 내보내기 문자열이 작동하지 않습니다.",
-            "[문자열] 이전 내보내기 문자열을 계속 사용하려면 ElvUI 및 ElvUI_WindTools를 다운그레이드 한 다음 최신 버전으로 업그레이드 할 수 있습니다."
+            "[프로필] ElvUI 13.01의 라이브러리 파일 변경에 따라 이전 내보내기 문자열이 작동하지 않습니다.",
+            "[프로필] 이전 내보내기 문자열을 계속 사용하려면 ElvUI 및 ElvUI_WindTools를 다운그레이드 한 다음 최신 버전으로 업그레이드 할 수 있습니다."
         }
     },
     NEW = {
@@ -39,9 +39,21 @@ W.Changelog[301] = {
         ["koKR"] = {}
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[信息]-[重置] 现在移动到了 [高级]-[重置].",
+            "[信息]-[设定档] 现在移动到了 [高级]-[设定档]."
+        },
+        ["zhTW"] = {
+            "[信息]-[重置] 现在移动到了 [高级]-[重置].",
+            "[信息]-[設定檔] 现在移动到了 [高级]-[設定檔]."
+        },
+        ["enUS"] = {
+            "[Info]-[Reset] is now moved to [Advanced]-[Reset].",
+            "[Info]-[Profile] is now moved to [Advanced]-[Profile]."
+        },
+        ["koKR"] = {
+            "[정보]-[재설정]이 [Advanced]-[재설정]으로 이동되었습니다.",
+            "[정보]-[프로필]이 [Advanced]-[프로필]으로 이동되었습니다."
+        }
     }
 }

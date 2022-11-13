5f28b89b3302e877954f13d731c15ed5a0e4014c
fang2hou <fang2hou@outlook.com>
2022-11-14 03:29:53 +0900

    docs(changelog): update

---
+++ Core/Changelog/3.02.lua
@@ -0,0 +1,47 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[302] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[预组列表] 添加了显示队长的选项.",
+            "[预组列表] 添加了额外文字 (队长分数, 队长最佳成绩) 的选项.",
+        },
+        ["zhTW"] = {
+            "[預組列表] 添加了顯示隊長的選項.",
+            "[預組列表] 添加了額外文字 (隊長分數, 隊長最佳成績) 的選項.",
+        },
+        ["enUS"] = {
+            "[LFG List] Added an option to show the leader.",
+            "[LFG List] Added an option to show additional text (leader score, leader best score).",
+        },
+        ["koKR"] = {
+            "[LFG List] Added an option to show the leader.",
+            "[LFG List] Added an option to show additional text (leader score, leader best score).",
+        },
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[预组队列表] Premade Groups Filter 载入时将自动禁用.",
+            "[美化皮肤] 修复并优化了 Premade Groups Filter 皮肤."
+        },
+        ["zhTW"] = {
+            "[預組列表] Premade Groups Filter 載入時將自動禁用.",
+            "[美化皮膚] 修復並優化了 Premade Groups Filter 皮膚."
+        },
+        ["enUS"] = {
+            "[LFG List] Premade Groups Filter will be disabled automatically when loaded.",
+            "[Skins] Fixed and optimized the Premade Groups Filter skin."
+        },
+        ["koKR"] = {
+            "[LFG List] Premade Groups Filter will be disabled automatically when loaded.",
+            "[Skins] Fixed and optimized the Premade Groups Filter skin."
+        }
+    }
+}

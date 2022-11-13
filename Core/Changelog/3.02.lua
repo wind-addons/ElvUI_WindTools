8fac73afb5932ec9a99b43650203baa9286b32fc
fang2hou <fang2hou@outlook.com>
2022-11-14 03:50:39 +0900

    docs(changelog): update

--- Core/Changelog/3.02.lua
+++ Core/Changelog/3.02.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[302] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[移动框体] 为了修复可能的错误, 本次更新会重置框体位置记忆."
+        },
+        ["zhTW"] = {
+            "[移動框架] 為了修復可能的錯誤, 本次更新會重置框架位置記憶."
+        },
+        ["enUS"] = {
+            "[Move Frames] To fix possible errors, this update will reset frame positions."
+        },
+        ["koKR"] = {
+            "[Move Frames] To fix possible errors, this update will reset frame positions."
+        }
     },
     NEW = {
         ["zhCN"] = {
@@ -28,18 +36,26 @@ W.Changelog[302] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[小地图按钮] 修复了在不使用 ElvUI 小地图时大厅按键缩放错误的问题",
+            "[矩形小地图] 在 ElvUI 小地图未启用时不再载入.",
             "[预组队列表] Premade Groups Filter 载入时将自动禁用.",
             "[美化皮肤] 修复并优化了 Premade Groups Filter 皮肤."
         },
         ["zhTW"] = {
+            "[小地圖按鍵] 修復了在不使用 ElvUI 小地圖時大廳按鍵縮放錯誤的問題",
+            "[矩形小地圖] 在 ElvUI 小地圖未啟用時不再載入.",
             "[預組列表] Premade Groups Filter 載入時將自動禁用.",
             "[美化皮膚] 修復並優化了 Premade Groups Filter 皮膚."
         },
         ["enUS"] = {
+            "[Minimap Buttons] Fixed the expansion landing page button scaling error when not using ElvUI minimap.",
+            "[Square Minimap] No longer load when ElvUI minimap is not enabled.",
             "[LFG List] Premade Groups Filter will be disabled automatically when loaded.",
             "[Skins] Fixed and optimized the Premade Groups Filter skin."
         },
         ["koKR"] = {
+            "[Minimap Buttons] Fixed the expansion landing page button scaling error when not using ElvUI minimap.",
+            "[Square Minimap] No longer load when ElvUI minimap is not enabled.",
             "[LFG List] Premade Groups Filter will be disabled automatically when loaded.",
             "[Skins] Fixed and optimized the Premade Groups Filter skin."
         }

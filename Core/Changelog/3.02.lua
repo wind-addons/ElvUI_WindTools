77a68145de2a575620802ab0afc4ccdb7cf23f87
fang2hou <fang2hou@outlook.com>
2022-11-14 05:08:43 +0900

    docs(changelog): update

--- Core/Changelog/3.02.lua
+++ Core/Changelog/3.02.lua
@@ -18,42 +18,54 @@ W.Changelog[302] = {
     },
     NEW = {
         ["zhCN"] = {
+            "[鼠标提示] 添加了一个选项用于自定义显示额外信息的按键.",
             "[预组列表] 添加了显示队长的选项.",
             "[预组列表] 添加了额外文字 (队长分数, 队长最佳成绩) 的选项.",
         },
         ["zhTW"] = {
+            "[浮動提示] 添加了一個選項用於自定義顯示額外信息的按鍵.",
             "[預組列表] 添加了顯示隊長的選項.",
             "[預組列表] 添加了額外文字 (隊長分數, 隊長最佳成績) 的選項.",
         },
         ["enUS"] = {
+            "[Tooltips] Added an option to customize the key to show extra information.",
             "[LFG List] Added an option to show the leader.",
             "[LFG List] Added an option to show additional text (leader score, leader best score).",
         },
         ["koKR"] = {
+            "[Tooltips] Added an option to customize the key to show extra information.",
             "[LFG List] Added an option to show the leader.",
             "[LFG List] Added an option to show additional text (leader score, leader best score).",
         },
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[已学配方上色] 修复了一个公会银行中可能出现的错误.",
+            "[其他] 清理部分测试用代码.",
             "[小地图按钮] 修复了在不使用 ElvUI 小地图时大厅按键缩放错误的问题",
             "[矩形小地图] 在 ElvUI 小地图未启用时不再载入.",
             "[预组队列表] Premade Groups Filter 载入时将自动禁用.",
             "[美化皮肤] 修复并优化了 Premade Groups Filter 皮肤."
         },
         ["zhTW"] = {
+            "[已學配方上色] 修復了一個公會銀行中可能出現的錯誤.",
+            "[其他] 清理部分測試用代碼.",
             "[小地圖按鍵] 修復了在不使用 ElvUI 小地圖時大廳按鍵縮放錯誤的問題",
             "[矩形小地圖] 在 ElvUI 小地圖未啟用時不再載入.",
             "[預組列表] Premade Groups Filter 載入時將自動禁用.",
             "[美化皮膚] 修復並優化了 Premade Groups Filter 皮膚."
         },
         ["enUS"] = {
+            "[Already Known] Fixed an error that may occur in the guild bank.",
+            "[Misc] Cleaned up some debug codes.",
             "[Minimap Buttons] Fixed the expansion landing page button scaling error when not using ElvUI minimap.",
             "[Square Minimap] No longer load when ElvUI minimap is not enabled.",
             "[LFG List] Premade Groups Filter will be disabled automatically when loaded.",
             "[Skins] Fixed and optimized the Premade Groups Filter skin."
         },
         ["koKR"] = {
+            "[Already Known] Fixed an error that may occur in the guild bank.",
+            "[Misc] Cleaned up some debug codes.",
             "[Minimap Buttons] Fixed the expansion landing page button scaling error when not using ElvUI minimap.",
             "[Square Minimap] No longer load when ElvUI minimap is not enabled.",
             "[LFG List] Premade Groups Filter will be disabled automatically when loaded.",

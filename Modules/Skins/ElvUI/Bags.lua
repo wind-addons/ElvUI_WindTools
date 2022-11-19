7b200144e864af002990e291ffdc602bcdcec9e8
fang2hou <fang2hou@outlook.com>
2022-11-19 23:03:47 +0900

    fix(skins); selling frame should be skinned

--- Modules/Skins/ElvUI/Bags.lua
+++ Modules/Skins/ElvUI/Bags.lua
@@ -5,18 +5,6 @@ local B = E:GetModule("Bags")
 local _G = _G
 local pairs = pairs
 
-function S:ElvUI_BagBar()
-    if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.bags) then
-        return
-    end
-
-    if E.private.bags.bagBar and B.BagBar and B.BagBar.buttons then
-        for _, buttons in pairs(B.BagBar.buttons) do
-            self:CreateShadow(buttons)
-        end
-    end
-end
-
 function S:ElvUI_Bags()
     if not E.private.bags.enable then
         return
@@ -30,11 +18,30 @@ function S:ElvUI_Bags()
     self:CreateShadow(B.BagFrame.ContainerHolder)
     self:CreateShadow(B.BankFrame)
     self:CreateShadow(B.BankFrame.ContainerHolder)
+end
 
-    if _G.ElvUIVendorGraysFrame then
-        self:CreateShadow(_G.ElvUIVendorGraysFrame)
+function S:ElvUI_BagBar()
+    if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.bags) then
+        return
+    end
+
+    if E.private.bags.bagBar and B.BagBar and B.BagBar.buttons then
+        for _, buttons in pairs(B.BagBar.buttons) do
+            self:CreateShadow(buttons)
+        end
+    end
+end
+
+function S:ElvUI_BagSell()
+    if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.bags) then
+        return
+    end
+
+    if B and B.SellFrame then
+        self:CreateShadow(B.SellFrame)
     end
 end
 
 S:AddCallback("ElvUI_Bags")
 S:AddCallback("ElvUI_BagBar")
+S:AddCallback("ElvUI_BagSell")

56e1986e2a4bab61c1a908188cfb5edf44606f7a
fang2hou <fang2hou@outlook.com>
2022-11-23 19:59:03 +0900

    docs(changelog): 3.05

--- Core/Changelog/3.05.lua
+++ Core/Changelog/3.05.lua
@@ -16,40 +16,56 @@ W.Changelog[305] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[已知配方上色] 重构代码. 参考自 NDui.",
+            "[鼠标提示] 图标最多只会添加一个.",
             "[聊天条] 修复了一个可能导致日志记录无法正常工作的错误.",
             "[移动框体] 专业制造结果框体现在可以被移动.",
+            "[美化皮肤] 更新职业大厅皮肤.",
             "[美化皮肤] 更新社区皮肤.",
+            "[美化皮肤] 更新鼠标提示皮肤.",
             "[美化皮肤] 修复 TinyInspect 皮肤.",
             "[美化皮肤] 更新 TomCats 皮肤.",
             "[美化皮肤] 更新 WeakAuras 皮肤.",
-            "[美化皮肤] 更新 WeakAuras 设定皮肤, 且不再强制载入.",
+            "[美化皮肤] 更新 WeakAuras 设定皮肤, 且不再强制载入."
         },
         ["zhTW"] = {
+            "[已知配方上色] 重構代碼. 參考自 NDui.",
+            "[浮動提示] 圖示最多只會添加一個.",
             "[聊天條] 修復了一個可能導致日誌記錄無法正常工作的錯誤.",
             "[移動框架] 專業製造結果框架現在可以被移動.",
+            "[美化皮膚] 更新職業大廳皮膚.",
             "[美化皮膚] 更新社群皮膚.",
+            "[美化皮膚] 更新浮動提示皮膚.",
             "[美化皮膚] 修復 TinyInspect 皮膚.",
             "[美化皮膚] 更新 TomCats 皮膚.",
             "[美化皮膚] 更新 WeakAuras 皮膚.",
-            "[美化皮膚] 更新 WeakAuras 設定皮膚, 且不再強制載入.",
+            "[美化皮膚] 更新 WeakAuras 設定皮膚, 且不再強制載入."
         },
         ["enUS"] = {
+            "[Already Known] Refactor all code. Refer to NDui.",
+            "[Tooltip] The icon will only add once.",
             "[Chat Bar] Fix a bug that may cause the logger not work properly.",
             "[Move Frames] Profession crafting result frame can now be moved.",
+            "[Skins] Update Class Hall skin.",
             "[Skins] Update community skins.",
+            "[Skins] Update tooltip skins.",
             "[Skins] Fix TinyInspect skin.",
             "[Skins] Update TomCats skin.",
             "[Skins] Update WeakAuras skin.",
-            "[Skins] Update WeakAuras Options skin, and no longer force load.",
+            "[Skins] Update WeakAuras Options skin, and no longer force load."
         },
         ["koKR"] = {
+            "[Already Known] Refactor all code. Refer to NDui.",
+            "[Tooltip] The icon will only add once.",
             "[Chat Bar] Fix a bug that may cause the logger not work properly.",
             "[Move Frames] Profession crafting result frame can now be moved.",
+            "[Skins] Update Class Hall skin.",
             "[Skins] Update community skins.",
+            "[Skins] Update tooltip skins.",
             "[Skins] Fix TinyInspect skin.",
             "[Skins] Update TomCats skin.",
             "[Skins] Update WeakAuras skin.",
-            "[Skins] Update WeakAuras Options skin, and no longer force load.",
-        },
+            "[Skins] Update WeakAuras Options skin, and no longer force load."
+        }
     }
 }

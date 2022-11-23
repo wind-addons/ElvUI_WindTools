f3ba461b5f208ec7c4212ee0d9cf58c5fe626956
fang2hou <fang2hou@outlook.com>
2022-11-23 14:30:37 +0900

    docs(changelog): 3.05

---
+++ Core/Changelog/3.05.lua
@@ -0,0 +1,55 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[305] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[聊天条] 修复了一个可能导致日志记录无法正常工作的错误.",
+            "[移动框体] 专业制造结果框体现在可以被移动.",
+            "[美化皮肤] 更新社区皮肤.",
+            "[美化皮肤] 修复 TinyInspect 皮肤.",
+            "[美化皮肤] 更新 TomCats 皮肤.",
+            "[美化皮肤] 更新 WeakAuras 皮肤.",
+            "[美化皮肤] 更新 WeakAuras 设定皮肤, 且不再强制载入.",
+        },
+        ["zhTW"] = {
+            "[聊天條] 修復了一個可能導致日誌記錄無法正常工作的錯誤.",
+            "[移動框架] 專業製造結果框架現在可以被移動.",
+            "[美化皮膚] 更新社群皮膚.",
+            "[美化皮膚] 修復 TinyInspect 皮膚.",
+            "[美化皮膚] 更新 TomCats 皮膚.",
+            "[美化皮膚] 更新 WeakAuras 皮膚.",
+            "[美化皮膚] 更新 WeakAuras 設定皮膚, 且不再強制載入.",
+        },
+        ["enUS"] = {
+            "[Chat Bar] Fix a bug that may cause the logger not work properly.",
+            "[Move Frames] Profession crafting result frame can now be moved.",
+            "[Skins] Update community skins.",
+            "[Skins] Fix TinyInspect skin.",
+            "[Skins] Update TomCats skin.",
+            "[Skins] Update WeakAuras skin.",
+            "[Skins] Update WeakAuras Options skin, and no longer force load.",
+        },
+        ["koKR"] = {
+            "[Chat Bar] Fix a bug that may cause the logger not work properly.",
+            "[Move Frames] Profession crafting result frame can now be moved.",
+            "[Skins] Update community skins.",
+            "[Skins] Fix TinyInspect skin.",
+            "[Skins] Update TomCats skin.",
+            "[Skins] Update WeakAuras skin.",
+            "[Skins] Update WeakAuras Options skin, and no longer force load.",
+        },
+    }
+}

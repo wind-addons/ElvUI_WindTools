7b2a019a6ea498a8493785cca4344668cef2d9cc
fang2hou <fang2hou@outlook.com>
2022-11-27 23:06:25 +0900

    perf: local variables

--- Modules/Skins/Addons/AdiBags.lua
+++ Modules/Skins/Addons/AdiBags.lua
@@ -2,6 +2,8 @@ local addonName, addon = ...
 local W, F, E, L = unpack(select(2, ...))
 local S = W.Modules.Skins
 
+local _G = _G
+
 function S:AdiBags()
     if not E.private.WT.skins.enable or not E.private.WT.skins.addons.adiBags then
         return

33552c870df85508235855615ab952cb3eeecbb8
fang2hou <fang2hou@outlook.com>
2022-11-28 00:47:40 +0900

    feat(tier-set): new df ids

--- Modules/Tooltips/TierSet.lua
+++ Modules/Tooltips/TierSet.lua
@@ -15,80 +15,87 @@ local UnitGUID = UnitGUID
 local cache = {}
 local locked = {}
 
--- from NDui
-local T29Sets = {
+-- https://www.wowhead.com/guide/vault-incarnates-dragonflight-class-tier-set-overview-primalist-bonuses
+local tierSetsID = {
     -- HUNTER
-    [188856] = true,
-    [188858] = true,
-    [188859] = true,
-    [188860] = true,
-    [188861] = true,
+    [200387] = true,
+    [200389] = true,
+    [200390] = true,
+    [200391] = true,
+    [200392] = true,
     -- WARRIOR
-    [188937] = true,
-    [188938] = true,
-    [188940] = true,
-    [188941] = true,
-    [188942] = true,
+    [200423] = true,
+    [200425] = true,
+    [200426] = true,
+    [200427] = true,
+    [200428] = true,
     -- PALADIN
-    [188928] = true,
-    [188929] = true,
-    [188931] = true,
-    [188932] = true,
-    [188933] = true,
+    [200414] = true,
+    [200416] = true,
+    [200417] = true,
+    [200418] = true,
+    [200419] = true,
     -- ROGUE
-    [188901] = true,
-    [188902] = true,
-    [188903] = true,
-    [188905] = true,
-    [188907] = true,
+    [200369] = true,
+    [200371] = true,
+    [200372] = true,
+    [200373] = true,
+    [200374] = true,
     -- PRIEST
-    [188875] = true,
-    [188878] = true,
-    [188879] = true,
-    [188880] = true,
-    [188881] = true,
+    [200326] = true,
+    [200327] = true,
+    [200328] = true,
+    [200324] = true,
+    [200329] = true,
     -- DK
-    [188863] = true,
-    [188864] = true,
-    [188866] = true,
-    [188867] = true,
-    [188868] = true,
+    [200405] = true,
+    [200407] = true,
+    [200408] = true,
+    [200409] = true,
+    [200410] = true,
     -- SHAMAN
-    [188920] = true,
-    [188922] = true,
-    [188923] = true,
-    [188924] = true,
-    [188925] = true,
+    [200396] = true,
+    [200398] = true,
+    [200399] = true,
+    [200400] = true,
+    [200401] = true,
     -- MAGE
-    [188839] = true,
-    [188842] = true,
-    [188843] = true,
-    [188844] = true,
-    [188845] = true,
+    [200315] = true,
+    [200317] = true,
+    [200318] = true,
+    [200319] = true,
+    [200320] = true,
     -- WARLOCK
-    [188884] = true,
-    [188887] = true,
-    [188888] = true,
-    [188889] = true,
-    [188890] = true,
+    [200335] = true,
+    [200336] = true,
+    [200337] = true,
+    [200333] = true,
+    [200338] = true,
     -- MONK
-    [188910] = true,
-    [188911] = true,
-    [188912] = true,
-    [188914] = true,
-    [188916] = true,
+    [200360] = true,
+    [200362] = true,
+    [200363] = true,
+    [200364] = true,
+    [200365] = true,
     -- DRUID
-    [188847] = true,
-    [188848] = true,
-    [188849] = true,
-    [188851] = true,
-    [188853] = true,
+    [200351] = true,
+    [200353] = true,
+    [200354] = true,
+    [200355] = true,
+    [200356] = true,
     -- DH
-    [188892] = true,
-    [188893] = true,
-    [188894] = true,
-    [188896] = true,
-    [188898] = true
+    [200342] = true,
+    [200344] = true,
+    [200345] = true,
+    [200346] = true,
+    [200347] = true,
+    [200356] = true,
+    -- EVOKER
+    [200378] = true,
+    [200380] = true,
+    [200381] = true,
+    [200382] = true,
+    [200383] = true
 }
 
 local formatSets = {
@@ -115,8 +122,8 @@ function T:ElvUIScanTooltipSetInventoryItem(tt, unit, slot)
         local itemLink = select(2, tt:GetItem())
         local itemID = itemLink and strmatch(itemLink, "item:(%d+):")
         if itemID then
-            itemID = itemID and tonumber(itemID)
-            if T29Sets[tonumber(itemID)] then
+            itemID = tonumber(itemID)
+            if tierSetsID[itemID] then
                 for _, i in pairs(cache[guid]) do
                     if i == itemID then
                         return

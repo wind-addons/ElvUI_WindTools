72cfc1bfeba3a047cf127907536647425aff615d
fang2hou <fang2hou@outlook.com>
2022-11-27 22:59:40 +0900

    docs(changelog): 3.06

---
+++ Core/Changelog/3.06.lua
@@ -0,0 +1,99 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[306] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "移除了 [战斗]-[盟约助手] 模块.",
+            "移除了 [鼠标提示]-[盟约] 功能.",
+            "[超级追踪] 由于暴雪的限制, 快速取消标记从鼠标右键调整为鼠标中键.",
+            "[进度] 默认追踪化身巨龙牢窟.",
+            "[进度] 默认追踪巨龙时代第一赛季传奇钥石地下城.",
+            "[进度] 移除暗影国度第四赛季传奇钥石地下城支持."
+        },
+        ["zhTW"] = {
+            "移除了 [戰鬥]-[盟約助手] 模組.",
+            "移除了 [滑鼠提示]-[盟約] 功能.",
+            "[超級追蹤] 由於暴雪的限制, 快速取消標記從滑鼠右鍵調整為滑鼠中鍵.",
+            "[進度] 默認追蹤洪荒化身牢獄.",
+            "[進度] 默認追蹤巨龍時代第一賽季傳奇鑰石地下城.",
+            "[進度] 移除暗影國度第四賽季傳奇鑰石地下城支持."
+        },
+        ["enUS"] = {
+            "Remove [Combat]-[Covenant Helper] module.",
+            "Remove [Tooltip]-[Covenant] feature.",
+            "[Super Tracker] Due to Blizzard's restriction, pin now can be quickly removed by middle button (right button previously).",
+            "[Progress] Default tracking Vault of the Incarnates.",
+            "[Progress] Default tracking all Dragonflight S1 Mythic+ Dungeons.",
+            "[Progress] Remove tracking support of all Shadowlands S4 Mythic+ Dungeons."
+        },
+        ["koKR"] = {
+            "Remove [Combat]-[Covenant Helper] module.",
+            "Remove [Tooltip]-[Covenant] feature.",
+            "[Super Tracker] Due to Blizzard's restriction, pin now can be quickly removed by middle button (right button previously).",
+            "[Progress] Default tracking Vault of the Incarnates.",
+            "[Progress] Default tracking all Dragonflight S1 Mythic+ Dungeons.",
+            "[Progress] Remove tracking support of all Shadowlands S4 Mythic+ Dungeons."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[美化皮肤] 新增了暴雪原生背包的皮肤."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了暴雪原生背包的皮膚."
+        },
+        ["enUS"] = {
+            "[Skins] Add the skin for Blizzard bag."
+        },
+        ["koKR"] = {
+            "[Skins] Add the skin for Blizzard bag."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[通告] 去除任务进度现在不会造成污染.",
+            "[通告] 修复新钥石无法被通告的问题. 感谢 Urmus",
+            "[交接] 修复存在多个任务时不会自动交接的问题.",
+            "[移动框体] 现在可以临时移动额外投掷框体了.",
+            "[移动框体] 修复一个移动邮箱时可能造成的错误.",
+            "[美化皮肤] 修复镜像计时条的皮肤.",
+            "[美化皮肤] 修复 Angry Keystones 的皮肤.",
+            "[美化皮肤] 更新 Mythic Dungeon Helper 皮肤.",
+            "[美化皮肤] 更新 Immersion 皮肤."
+        },
+        ["zhTW"] = {
+            "[通告] 去除任務進度現在不會造成污染.",
+            "[通告] 修復新鑰石無法被通告的問題. 感謝 Urmus",
+            "[交接] 修復存在多個任務時不會自動交接的問題.",
+            "[移動框架] 現在可以臨時移動額外投擲框架了.",
+            "[移動框架] 修復一個移動郵箱時可能造成的錯誤.",
+            "[美化皮膚] 修復鏡像計時條的皮膚.",
+            "[美化皮膚] 修復 Angry Keystones 的皮膚.",
+            "[美化皮膚] 更新 Mythic Dungeon Helper 皮膚.",
+            "[美化皮膚] 更新 Immersion 皮膚."
+        },
+        ["enUS"] = {
+            "[Announcement] Remove quest progress will not cause pollution now.",
+            "[Announcement] Fix the issue that new keystone cannot be announced. Thanks Urmus",
+            "[Automation] Fix the issue that auto turn in will not work when there are multiple quests.",
+            "[Mover] Now you can temporarily move the bonus roll frame.",
+            "[Mover] Fix the error when moving the mailbox.",
+            "[Skins] Fix the skin of mirror timer.",
+            "[Skins] Fix the skin of Angry Keystones.",
+            "[Skins] Update the skin of Mythic Dungeon Helper.",
+            "[Skins] Update the skin of Immersion."
+        },
+        ["koKR"] = {
+            "[Announcement] Remove quest progress will not cause pollution now.",
+            "[Announcement] Fix the issue that new keystone cannot be announced. Thanks Urmus",
+            "[Automation] Fix the issue that auto turn in will not work when there are multiple quests.",
+            "[Mover] Now you can temporarily move the bonus roll frame.",
+            "[Mover] Fix the error when moving the mailbox.",
+            "[Skins] Fix the skin of mirror timer.",
+            "[Skins] Fix the skin of Angry Keystones.",
+            "[Skins] Update the skin of Mythic Dungeon Helper.",
+            "[Skins] Update the skin of Immersion."
+        }
+    }
+}

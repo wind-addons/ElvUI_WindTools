540af41991060c76e8a92ba563e6abf16db402ba
fang2hou <fang2hou@outlook.com>
2022-12-04 13:11:15 +0900

    docs(changelog): 3.09

---
+++ Core/Changelog/3.09.lua
@@ -0,0 +1,67 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[309] = {
+    RELEASE_DATE = "2022/12/04",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "[高级] 配置文件现在会包含额外物品条呃自定义设定.",
+            "[额外物品条] 条 1 的默认物品组移除了托加司相关物品.",
+            "[额外物品条] 条 2 的默认物品组中的暗影国度药水和药剂更改为巨龙时代药水和药剂.",
+            "[额外物品条] 条 3 的默认物品组中的暗影国度食物更改为巨龙时代食物."
+        },
+        ["zhTW"] = {
+            "[進階] 配置文件現在會包含額外物品條的自訂設定.",
+            "[額外物品條] 條 1 的默認物品組移除了托加斯特相關物品.",
+            "[額外物品條] 條 2 的默認物品組中的暗影之境藥水和藥劑更改為巨龍崛起藥水和藥劑.",
+            "[額外物品條] 條 3 的默認物品組中的暗影之境食物更改為巨龍崛起食物."
+        },
+        ["enUS"] = {
+            "[Advanced] Profile now includes custom list setting for extra action bars.",
+            "[Extra Action Bars] Remove Torghast related items from bar 1's default item group.",
+            "[Extra Action Bars] Change Shadowlands potions and flasks in bar 2's default item group to Dragonflight potions and flasks.",
+            "[Extra Action Bars] Change Shadowlands food in bar 3's default item group to Dragonflight food."
+        },
+        ["koKR"] = {
+            "[Advanced] Profile now includes custom list setting for extra action bars.",
+            "[Extra Action Bars] Remove Torghast related items from bar 1's default item group.",
+            "[Extra Action Bars] Change Shadowlands potions and flasks in bar 2's default item group to Dragonflight potions and flasks.",
+            "[Extra Action Bars] Change Shadowlands food in bar 3's default item group to Dragonflight food."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[额外物品条] 更新巨龙时代版本中的可开启物品. 感谢 mcc1"
+        },
+        ["zhTW"] = {
+            "[額外物品條] 更新巨龍崛起版本中的可開啟物品. 感謝 mcc1"
+        },
+        ["enUS"] = {
+            "[Extra Action Bars] Update openable item list for Dragonflight. Thanks mcc1"
+        },
+        ["koKR"] = {
+            "[Extra Action Bars] Update openable item list for Dragonflight. Thanks mcc1"
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[团队标记] 修复动画计算错误.",
+            "[交接] 修复一个可能的错误.",
+            "[美化皮肤] 修复场景皮肤."
+        },
+        ["zhTW"] = {
+            "[團隊標記] 修復了動效計算錯誤.",
+            "[交接] 修復一個可能的錯誤.",
+            "[美化皮膚] 修復場景皮膚."
+        },
+        ["enUS"] = {
+            "[Raid Markers] Fix animation calculation error.",
+            "[Turn In] Fix a possible error.",
+            "[Skins] Fix scenario skin."
+        },
+        ["koKR"] = {
+            "[Raid Markers] Fix animation calculation error.",
+            "[Turn In] Fix a possible error.",
+            "[Skins] Fix scenario skin."
+        }
+    }
+}

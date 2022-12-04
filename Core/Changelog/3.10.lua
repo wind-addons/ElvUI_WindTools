c99639c3a45b766ab0185f15023a8b3761729737
fang2hou <fang2hou@outlook.com>
2022-12-05 03:58:58 +0900

    docs(changelog): 3.10

---
+++ Core/Changelog/3.10.lua
@@ -0,0 +1,39 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[310] = {
+    RELEASE_DATE = "2022/12/05",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "[美化皮肤] 修复一个逻辑错误."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 修復一個邏輯錯誤."
+        },
+        ["enUS"] = {
+            "[Skins] Fix a logic error."
+        },
+        ["koKR"] = {
+            "[스킨] 로직 오류 수정."
+        },
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[小地图图标] 忽略随机队列图标."
+        },
+        ["zhTW"] = {
+            "[小地圖圖示] 忽略隨機隊列圖示."
+        },
+        ["enUS"] = {
+            "[Minimap Icons] Ignore random queue icons."
+        },
+        ["koKR"] = {
+            "[미니맵 아이콘] 랜덤 대기열 아이콘 무시."
+        },
+    }
+}

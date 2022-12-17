c1cdd4e0c6da3e7b0f4593b674613680efe832fa
fang2hou <fang2hou@outlook.com>
2022-12-17 11:18:57 +0900

    chore: cvar alert default to false

--- Settings/Global.lua
+++ Settings/Global.lua
@@ -2,7 +2,7 @@ local W, F, E, L, V, P, G = unpack(select(2, ...))
 
 G.core = {
     compatibilityCheck = true,
-    cvarAlert = true,
+    cvarAlert = false,
     fixPlaystyle = true,
     logLevel = 2,
     loginMessage = true,

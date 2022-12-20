5a85b486ae25d77370a856fd0257fec707d55d38
fang2hou <fang2hou@outlook.com>
2022-12-20 16:36:13 +0900

    feat(skins): update calendar skins

--- Modules/Skins/Blizzard/Calendar.lua
+++ Modules/Skins/Blizzard/Calendar.lua
@@ -9,7 +9,21 @@ function S:Blizzard_Calendar()
     end
 
     self:CreateBackdropShadow(_G.CalendarFrame)
+
+    self:CreateShadow(_G.CalendarViewRaidFrame)
     self:CreateShadow(_G.CalendarViewHolidayFrame)
+    self:CreateShadow(_G.CalendarMassInviteFrame)
+
+    self:CreateShadow(_G.CalendarViewEventFrame)
+
+    for index in next, CLASS_SORT_ORDER do
+        local button = _G["CalendarClassButton" .. index]
+        if button then
+            self:CreateShadow(button)
+        end
+    end
+
+    self:CreateShadow(_G.CalendarClassTotalsButton)
 end
 
 S:AddCallbackForAddon("Blizzard_Calendar")

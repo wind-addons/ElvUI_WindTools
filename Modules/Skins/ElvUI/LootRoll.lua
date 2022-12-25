fdd76720821a9f9c66b0c5d5a89c3c0f88aa6230
fang2hou <fang2hou@outlook.com>
2022-12-25 17:23:17 +0900

    feat(skins): real impl of loot roll

--- Modules/Skins/ElvUI/LootRoll.lua
+++ Modules/Skins/ElvUI/LootRoll.lua
@@ -4,6 +4,38 @@ local S = W.Modules.Skins
 
 local _G = _G
 
+function S:ElvUI_SkinLootRollFrame(frame)
+    if not frame or frame:IsForbidden() or frame.__windSkin then
+        return
+    end
+
+    frame.button.__Point = frame.button.Point
+    frame.button.Point = function(f, anchor, parent, point, x, y)
+        if anchor == "RIGHT" and parent == frame and point == "LEFT" then
+            f:__Point(anchor, parent, point, x and x - 4, y)
+        else
+            f:__Point(anchor, parent, point, x, y)
+        end
+    end
+
+    points = {}
+    for i = 1, frame.button:GetNumPoints() do
+        tinsert(points, {frame.button:GetPoint(i)})
+    end
+
+    if #points > 0 then
+        frame.button:ClearAllPoints()
+        for _, point in pairs(points) do
+            frame.button:Point(unpack(point))
+        end
+    end
+
+    self:CreateBackdropShadow(frame.button)
+    self:CreateBackdropShadow(frame.status)
+
+    frame.__windSkin = true
+end
+
 function S:ElvUI_LootRoll()
     if not (E.private.general.lootRoll and E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.lootRoll) then
         return
@@ -13,13 +45,21 @@ function S:ElvUI_LootRoll()
         M,
         "LootRoll_Create",
         function(_, index)
-            local frame = _G["ElvUI_LootRollFrame" .. index]
-            if frame and not frame.__windSkin then
-                self:CreateShadow(frame)
-                frame.__windSkin = true
-            end
+            self:ElvUI_SkinLootRollFrame(_G["ElvUI_LootRollFrame" .. index])
         end
     )
+
+    for _, bar in pairs(M.RollBars) do
+        self:ElvUI_SkinLootRollFrame(bar)
+    end
+
+
 end
 
 S:AddCallback("ElvUI_LootRoll")
+
+-- debug note:
+-- itemLink = select(2, GetItemInfo(193652))
+-- texture, name, count, quality, bop, canNeed, canGreed, canDisenchant = 123, 'test', 1, 1, true, true, true, true
+-- call the function
+-- E:GetModule("Misc"):START_LOOT_ROLL(_, 1, 19)

444bf4dc1a04b61f6f790493c6e8dc20cb4a3ae9
fang2hou <fang2hou@outlook.com>
2022-12-30 12:46:08 +0900

    docs(changelog): 3.17

---
+++ Core/Changelog/3.17.lua
@@ -0,0 +1,51 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[317] = {
+    RELEASE_DATE = "2022/12/30",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[事件追踪器] 新增海象人渔网时间追踪.",
+            "[通告] 新增了对 !keys 命令的支持."
+        },
+        ["zhTW"] = {
+            "[事件追蹤器] 新增海象人漁網時間追蹤.",
+            "[通告] 新增了對 !keys 命令的支援."
+        },
+        ["enUS"] = {
+            "[Event Tracker] Add support for Iskaaran Fishing Net.",
+            "[Announce] Add support for !keys command."
+        },
+        ["koKR"] = {
+            "[Event Tracker] Add support for Iskaaran Fishing Net.",
+            "[Announce] Add support for !keys command."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[联系人] 可以通过右键按钮的方式来快速删除小号.",
+            "[美化皮肤] 优化天赋界面皮肤.",
+            "[美化皮肤] 优化拾取历史记录皮肤."
+        },
+        ["zhTW"] = {
+            "[聯絡人] 可以通過右鍵按鈕的方式來快速刪除小號.",
+            "[美化皮膚] 優化天賦介面皮膚.",
+            "[美化皮膚] 優化拾取歷史記錄皮膚."
+        },
+        ["enUS"] = {
+            "[Contacts] You can now delete alts via the context menu (right-click menu).",
+            "[Skins] Optimize class talent skin.",
+            "[Skins] Optimize loot history frame skin."
+        },
+        ["koKR"] = {
+            "[Contacts] You can now delete alts via the context menu (right-click menu).",
+            "[Skins] Optimize class talent skin.",
+            "[Skins] Optimize loot history frame skin."
+        }
+    }
+}

9cb6586890fa16502cea31d95586ff6c7eec1053
fang2hou <fang2hou@outlook.com>
2022-12-30 12:50:45 +0900

    docs(changelog): 3.17

--- Core/Changelog/3.17.lua
+++ Core/Changelog/3.17.lua
@@ -3,10 +3,18 @@ local W = unpack(select(2, ...))
 W.Changelog[317] = {
     RELEASE_DATE = "2022/12/30",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {}
+        ["zhCN"] = {
+            "[额外物品条] 修复了无法正确使用指定品质物品的问题."
+        },
+        ["zhTW"] = {
+            "[額外物品條] 修復了無法正確使用指定品質物品的問題."
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Fix the issue that the specified quality items cannot be used correctly."
+        },
+        ["koKR"] = {
+            "[Extra Item Bar] Fix the issue that the specified quality items cannot be used correctly."
+        }
     },
     NEW = {
         ["zhCN"] = {
@@ -28,24 +36,30 @@ W.Changelog[317] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[额外物品条] 更新可开启物品列表.",
             "[联系人] 可以通过右键按钮的方式来快速删除小号.",
             "[美化皮肤] 优化天赋界面皮肤.",
             "[美化皮肤] 优化拾取历史记录皮肤."
         },
         ["zhTW"] = {
-            "[聯絡人] 可以通過右鍵按鈕的方式來快速刪除小號.",
+            "[額外物品條] 更新可開啟物品列表.",
+            "[聯絡人] 可以通過右鍵按鈕的方式來快速刪除分身.",
             "[美化皮膚] 優化天賦介面皮膚.",
             "[美化皮膚] 優化拾取歷史記錄皮膚."
         },
         ["enUS"] = {
+            "[Extra Item Bar] Update the list of openable items.",
             "[Contacts] You can now delete alts via the context menu (right-click menu).",
             "[Skins] Optimize class talent skin.",
-            "[Skins] Optimize loot history frame skin."
+            "[Skins] Optimize loot history frame skin.",
+            "[Skins] Optimize the skin of leave vehicle button."
         },
         ["koKR"] = {
+            "[Extra Item Bar] Update the list of openable items.",
             "[Contacts] You can now delete alts via the context menu (right-click menu).",
             "[Skins] Optimize class talent skin.",
-            "[Skins] Optimize loot history frame skin."
+            "[Skins] Optimize loot history frame skin.",
+            "[Skins] Optimize the skin of leave vehicle button."
         }
     }
 }

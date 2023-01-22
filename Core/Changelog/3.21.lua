385a487ba7b115bf28c0e489d4bcaf8fe0e48776
fang2hou <fang2hou@outlook.com>
2023-01-22 19:40:29 +0900

    docs(readme): 3.21

---
+++ Core/Changelog/3.21.lua
@@ -0,0 +1,48 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[321] = {
+    RELEASE_DATE = "2023/01/22",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "新年快乐!",
+            "最低 ElvUI 支持版本更改为 13.21.",
+            "移除了针对网易集合石插件的支持."
+        },
+        ["zhTW"] = {
+            "新年快樂!",
+            "最低 ElvUI 支援版本更改為 13.21.",
+            "移除了對於網易集合石插件的支援."
+        },
+        ["enUS"] = {
+            "Happy Spring Festival!",
+            "The minimum ElvUI version is changed to 13.21.",
+            "Remove all support for NetEase Meeting Stone."
+        },
+        ["koKR"] = {
+            "Happy Seollal Festival!",
+            "The minimum ElvUI version is changed to 13.21.",
+            "Remove all support for NetEase Meeting Stone."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {
+        },
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "修复距离数据文字可能产生的错误. 感谢 Azilroka"
+        },
+        ["zhTW"] = {
+            "修復距離資訊文字可能產生的錯誤. 感謝 Azilroka"
+        },
+        ["enUS"] = {
+            "Fix a possible error in the distance datatext. Thanks Azilroka"
+        },
+        ["koKR"] = {
+            "Fix a possible error in the distance datatext. Thanks Azilroka"
+        },
+    }
+}

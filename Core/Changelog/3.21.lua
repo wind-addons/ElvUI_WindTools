b0b1b4e92069d6956236a707fbcc276cf5ba287e
fang2hou <fang2hou@outlook.com>
2023-01-23 19:01:28 +0900

    docs(changelog): 3.21

--- Core/Changelog/3.21.lua
+++ Core/Changelog/3.21.lua
@@ -33,38 +33,48 @@ W.Changelog[321] = {
         ["zhCN"] = {
             "[预组建列表] 新增在传奇钥石界面中显示队友钥石的功能.",
             "[预组建列表] 新增一个右侧面板功能, 用于过滤队伍.",
+            "[其他] 新增查看简体中文队伍的功能."
         },
         ["zhTW"] = {
             "[預組列表] 新增在傳奇鑰石介面中顯示隊友鑰石的功能.",
             "[預組列表] 新增一個右側面板功能, 用於過濾隊伍.",
+            "[其他] 新增查看簡體中文隊伍的功能."
         },
         ["enUS"] = {
             "[LFG List] Add a feature to show the keystone of your party members in the Mythic+ frame.",
-            "[LFG List] Add a right panel feature to filter groups."
+            "[LFG List] Add a right panel feature to filter groups.",
+            "[Misc] Add a feature to view groups created by players who are using Simplified Chinese."
         },
         ["koKR"] = {
             "[LFG List] Add a feature to show the keystone of your party members in the Mythic+ frame.",
-            "[LFG List] Add a right panel feature to filter groups."
+            "[LFG List] Add a right panel feature to filter groups.",
+            "[Misc] Add a feature to view groups created by players who are using Simplified Chinese."
         },
         ["ruRU"] = {
             "[Список LFG] Добавлена возможность показывать ключи членов Вашей группы во фрейме M+.",
-            "[Список LFG] Добавлена возможность фильтровать группы в правой панели."
+            "[Список LFG] Добавлена возможность фильтровать группы в правой панели.",
+            "[Misc] Add a feature to view groups created by players who are using Simplified Chinese."
         }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[皮肤] BigWigs 进本计时条文字现在可以调整位置.",
             "修复距离数据文字可能产生的错误. 感谢 Azilroka"
         },
         ["zhTW"] = {
+            "[皮膚] BigWigs 進本計時條文字現在可以調整位置.",
             "修復距離資訊文字可能產生的錯誤. 感謝 Azilroka"
         },
         ["enUS"] = {
+            "[Skins] The position of the text of BigWigs queue timer bar can now be adjusted.",
             "Fix a possible error in the distance datatext. Thanks Azilroka"
         },
         ["koKR"] = {
+            "[Skins] The position of the text of BigWigs queue timer bar can now be adjusted.",
             "Fix a possible error in the distance datatext. Thanks Azilroka"
         },
         ["ruRU"] = {
+            "[Skins] The position of the text of BigWigs queue timer bar can now be adjusted.",
             "Исправлена возможная ошибка в инфотексте о расстоянии. Спасибо, Azilroka"
         }
     }

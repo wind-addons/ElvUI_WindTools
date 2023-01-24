f995d50378ade5efc76cd0f9ef66fd5642838c33
fang2hou <fang2hou@outlook.com>
2023-01-24 14:32:34 +0900

    docs(changelog): update

--- Core/Changelog/3.22.lua
+++ Core/Changelog/3.22.lua
@@ -31,30 +31,40 @@ W.Changelog[322] = {
             "过期代码清理.",
             "[额外物品条] 新增野性皮革战鼓到实用物品列表.",
             "[预组建列表] 右侧面板现在可以被拖动.",
+            "[预组建列表] 添加更多说明方便使用.",
+            "[预组建列表] 点击每周奖励进度可以打开宝库框体.",
             "[预组建列表] 一起加入更改为职责可用, 单人时也能根据专精进行过滤."
         },
         ["zhTW"] = {
             "過期代碼清理.",
             "[額外物品條] 新增野性皮革戰鼓到實用物品列表.",
             "[預組建列表] 右側面板現在可以被拖動.",
+            "[預組建列表] 添加更多說明方便使用.",
+            "[預組建列表] 點擊每周獎勵進度可以打開寶庫框架.",
             "[預組建列表] 一起加入更改為職責可用, 單人時也能根據專精進行過濾."
         },
         ["enUS"] = {
             "Clean up unused code.",
             "[Extra Item Bar] Add Feral Hide Drums to Utility Items.",
             "[LFG List] The right panel can now be moved by dragging.",
+            "[LFG List] Add more tooltips for easy use.",
+            "[LFG List] Clicking the weekly reward progress can open the treasure frame.",
             "[LFG List] Join Together is now Role Available, and can filter groups by specialization when solo."
         },
         ["koKR"] = {
             "Clean up unused code.",
             "[Extra Item Bar] Add Feral Hide Drums to Utility Items.",
             "[LFG List] The right panel can now be moved by dragging.",
+            "[LFG List] Add more tooltips for easy use.",
+            "[LFG List] Clicking the weekly reward progress can open the treasure frame.",
             "[LFG List] Join Together is now Role Available, and can filter groups by specialization when solo."
         },
         ["ruRU"] = {
             "Clean up unused code.",
             "[Extra Item Bar] Add Feral Hide Drums to Utility Items.",
             "[LFG List] The right panel can now be moved by dragging.",
+            "[LFG List] Add more tooltips for easy use.",
+            "[LFG List] Clicking the weekly reward progress can open the treasure frame.",
             "[LFG List] Join Together is now Role Available, and can filter groups by specialization when solo."
         }
     }

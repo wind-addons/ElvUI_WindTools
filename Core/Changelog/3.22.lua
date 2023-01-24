b452c5ea6786a68258a00ab2da128df7325e4709
fang2hou <fang2hou@outlook.com>
2023-01-24 13:54:17 +0900

    docs(changelog): 3.22

---
+++ Core/Changelog/3.22.lua
@@ -0,0 +1,61 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[322] = {
+    RELEASE_DATE = "2022/01/25",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "默认关闭 PlayStyleString 修复, 如果你遇到错误可自行在 [高级] - [游戏修复] 中开启."
+        },
+        ["zhTW"] = {
+            "預設關閉 PlayStyleString 修復, 如果你遇到錯誤可自行在 [進階] - [遊戲修復] 中開啟."
+        },
+        ["enUS"] = {
+            "Default disable PlayStyleString fix, if you encounter errors, you can enable it in [Advanced] - [Game Fix]."
+        },
+        ["koKR"] = {
+            "Default disable PlayStyleString fix, if you encounter errors, you can enable it in [Advanced] - [Game Fix]."
+        },
+        ["ruRU"] = {
+            "По умолчанию отключено исправление PlayStyleString, если вы столкнетесь с ошибками, вы можете включить его в [Дополнительно] - [Исправление игры]."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "过期代码清理.",
+            "[额外物品条] 新增野性皮革战鼓到实用物品列表.",
+            "[预组建列表] 右侧面板现在可以被拖动.",
+            "[预组建列表] 一起加入更改为职责可用, 单人时也能根据专精进行过滤."
+        },
+        ["zhTW"] = {
+            "過期代碼清理.",
+            "[額外物品條] 新增野性皮革戰鼓到實用物品列表.",
+            "[預組建列表] 右側面板現在可以被拖動.",
+            "[預組建列表] 一起加入更改為職責可用, 單人時也能根據專精進行過濾."
+        },
+        ["enUS"] = {
+            "Clean up unused code.",
+            "[Extra Item Bar] Add Feral Hide Drums to Utility Items.",
+            "[LFG List] The right panel can now be moved by dragging.",
+            "[LFG List] Join Together is now Role Available, and can filter groups by specialization when solo."
+        },
+        ["koKR"] = {
+            "Clean up unused code.",
+            "[Extra Item Bar] Add Feral Hide Drums to Utility Items.",
+            "[LFG List] The right panel can now be moved by dragging.",
+            "[LFG List] Join Together is now Role Available, and can filter groups by specialization when solo."
+        },
+        ["ruRU"] = {
+            "Clean up unused code.",
+            "[Extra Item Bar] Add Feral Hide Drums to Utility Items.",
+            "[LFG List] The right panel can now be moved by dragging.",
+            "[LFG List] Join Together is now Role Available, and can filter groups by specialization when solo."
+        }
+    }
+}

7d8ddbd4d21779fe6936c0ab16558949c0169ea9
fang2hou <fang2hou@outlook.com>
2023-01-26 19:48:09 +0900

    doc(changelog): 3.23 start

---
+++ Core/Changelog/3.23.lua
@@ -0,0 +1,36 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[323] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[美化皮肤] 新增了 RareScanner 的美化皮肤."
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 新增了 RareScanner 的美化皮膚."
+        },
+        ["enUS"] = {
+            "[Skins] Added RareScanner skin."
+        },
+        ["koKR"] = {
+            "[스킨] RareScanner 스킨을 추가했습니다."
+        },
+        ["ruRU"] = {
+            "[Скины] Добавлен скин RareScanner."
+        },
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}

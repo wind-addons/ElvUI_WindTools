ee9db6158885a53ab3d5ba761e29931577b6b1f3
fang2hou <fang2hou@outlook.com>
2023-01-26 19:45:31 +0900

    feat(tooltip): add a small api for adding item tooltips

--- Modules/Tooltips/Icons.lua
+++ Modules/Tooltips/Icons.lua
@@ -5,6 +5,7 @@ local _G = _G
 local hooksecurefunc = hooksecurefunc
 local select = select
 local strfind = strfind
+local tinsert = tinsert
 local unpack = unpack
 
 local GetItemIcon = GetItemIcon
@@ -214,6 +215,12 @@ function T:Icons()
 
     if E.private.WT.tooltips.petId then
         self:AddInspectInfoCallback(3, "AddPetID", false)
+    end
+end
+
+function T:AddIconTooltip(name)
+    if not F.In(name, tooltips) then
+        tinsert(tooltips, name)
     end
 end
 

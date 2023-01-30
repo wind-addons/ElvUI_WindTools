43cc5d520a9187d4b088a979fca3175e14a6f7ca
fang2hou <fang2hou@outlook.com>
2023-01-30 12:44:52 +0900

    feat(game-bar): update for latest elvui dt changes

--- Modules/Misc/GameBar.lua
+++ Modules/Misc/GameBar.lua
@@ -206,11 +206,22 @@ local VirtualDTEvent = {
 
 local VirtualDT = {
     Friends = {
+        name = "Friends",
+        text = {
+            SetFormattedText = E.noop
+        }
+    },
+    System = {
+        name = "System"
+    },
+    Time = {
+        name = "Time",
         text = {
             SetFormattedText = E.noop
         }
     },
     Guild = {
+        name = "Guild",
         text = {
             SetFormattedText = E.noop,
             SetText = E.noop
@@ -1352,6 +1363,12 @@ function GB:Initialize()
     if InCombatLockdown() then
         self:RegisterEvent("PLAYER_REGEN_ENABLED")
         return
+    end
+
+    for name, vDT in pairs(VirtualDT) do
+        if DT.RegisteredDataTexts[name] and DT.RegisteredDataTexts[name].colorUpdate then
+            DT.RegisteredDataTexts[name].colorUpdate(vDT, E.media.hexvaluecolor)
+        end
     end
 
     self:UpdateReknown()

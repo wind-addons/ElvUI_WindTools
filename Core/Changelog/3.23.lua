5c44aa11cb58bb54930df36f41aef959bf46386f
Zhou Fang <fang2hou@outlook.com>
2023-02-02 13:05:53 +0900

    docs(changelog): 3.23

--- Core/Changelog/3.23.lua
+++ Core/Changelog/3.23.lua
@@ -3,44 +3,69 @@ local W = unpack(select(2, ...))
 W.Changelog[323] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {},
-        ["ruRU"] = {}
+        ["zhCN"] = {
+            "适配 ElvUI 13.24 版本. (开发中)"
+        },
+        ["zhTW"] = {
+            "適配 ElvUI 13.24 版本. (開發中)"
+        },
+        ["enUS"] = {
+            "Adapt to ElvUI 13.24. (In development)"
+        },
+        ["koKR"] = {
+            "Adapt to ElvUI 13.24. (In development)"
+        },
+        ["ruRU"] = {
+            "Adapt to ElvUI 13.24. (In development)"
+        }
     },
     NEW = {
         ["zhCN"] = {
-            "[美化皮肤] 新增了 RareScanner 的美化皮肤."
+            "[美化皮肤] 新增了 RareScanner 的美化皮肤.",
+            "[美化皮肤] 新增了 Simple Addon Manager 的美化皮肤.",
+            "[美化皮肤] 新增了贸易站美化皮肤."
         },
         ["zhTW"] = {
-            "[美化皮膚] 新增了 RareScanner 的美化皮膚."
+            "[美化皮膚] 新增了 RareScanner 的美化皮膚.",
+            "[美化皮膚] 新增了 Simple Addon Manager 的美化皮膚.",
+            "[美化皮膚] 新增了貿易站美化皮膚."
         },
         ["enUS"] = {
-            "[Skins] Added RareScanner skin."
+            "[Skins] Add RareScanner skin.",
+            "[Skins] Add Simple Addon Manager skin.",
+            "[Skins] Add skin for The Trading Post."
         },
         ["koKR"] = {
-            "[스킨] RareScanner 스킨을 추가했습니다."
+            "[스킨] RareScanner 스킨을 추가했습니다.",
+            "[스킨] Simple Addon Manager 스킨을 추가했습니다.",
+            "[Skins] Add skin for The Trading Post."
         },
         ["ruRU"] = {
-            "[Скины] Добавлен скин RareScanner."
-        },
+            "[Скины] Добавлен скин RareScanner.",
+            "[Скины] Добавлен скин Simple Addon Manager.",
+            "[Skins] Add skin for The Trading Post."
+        }
     },
     IMPROVEMENT = {
         ["zhCN"] = {
-            "[额外物品条] 更新可开启物品列表."
+            "[额外物品条] 更新可开启物品列表.",
+            "[美化皮肤] 更新地下城手册美化皮肤."
         },
         ["zhTW"] = {
-            "[額外物品條] 更新可開啟物品列表."
+            "[額外物品條] 更新可開啟物品列表.",
+            "[美化皮膚] 更新地城導覽手冊美化皮膚."
         },
         ["enUS"] = {
-            "[Extra Item Bar] Updated the openable item list."
+            "[Extra Item Bar] Update the openable item list.",
+            "[Skins] Update the Encounter Journal skin."
         },
         ["koKR"] = {
-            "[Extra Item Bar] Updated the openable item list."
+            "[Extra Item Bar] Update the openable item list.",
+            "[Skins] Update the Encounter Journal skin."
         },
         ["ruRU"] = {
-            "[Extra Item Bar] Updated the openable item list."
-        },
+            "[Extra Item Bar] Update the openable item list.",
+            "[Skins] Update the Encounter Journal skin."
+        }
     }
 }

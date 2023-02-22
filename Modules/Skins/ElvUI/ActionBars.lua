a19f8d27b0345cf72d13e4e8f56eab6aff4dff0e
Merathilis <neo0608@gmail.com>
2023-02-22 15:22:05 +0100

    Skin also the new ActionBars (13 & 15), also we need to add a check here because bar 11 & 14 don't exist.

--- Modules/Skins/ElvUI/ActionBars.lua
+++ Modules/Skins/ElvUI/ActionBars.lua
@@ -108,9 +108,11 @@ function S:ElvUI_ActionBars()
 
     -- ElvUI action bar
     if not E.private.actionbar.masque.actionbars then
-        for id = 1, 10 do
+        for id = 1, 15 do
             local bar = _G["ElvUI_Bar" .. id]
-            self:ElvUI_ActionBar_SkinBar(bar, "PLAYER")
+            if bar then
+                self:ElvUI_ActionBar_SkinBar(bar, "PLAYER")
+            end
         end
 
         self:SecureHook(AB, "PositionAndSizeBar", "ElvUI_ActionBar_PositionAndSizeBar")

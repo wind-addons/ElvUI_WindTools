47ebd380202ae2d5879d6bebeb5f17bb2d692f5e
Zhou Fang <fang2hou@outlook.com>
2023-02-23 11:18:27 +0900

    docs(changelog): 3.25 start

---
+++ Core/Changelog/3.25.lua
@@ -0,0 +1,46 @@
+local W = unpack(select(2, ...))
+
+W.Changelog[325] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[交接] 修复无法使用修饰键暂停部分对话的问题.",
+            "[美化外观] 更新 Auctionator 皮肤.",
+            "[美化外观] 更新 ElvUI 动作条皮肤."
+        },
+        ["zhTW"] = {
+            "[交接] 修復無法使用修飾鍵暫停部分對話的問題.",
+            "[美化外觀] 更新 Auctionator 皮膚.",
+            "[美化外觀] 更新 ElvUI 動作條皮膚."
+        },
+        ["enUS"] = {
+            "[Turn In] Fix the issue that you cannot pause some conversations with modifier key.",
+            "[Skins] Update Auctionator skin.",
+            "[Skins] Update ElvUI actionbar skin."
+        },
+        ["koKR"] = {
+            "[Turn In] Fix the issue that you cannot pause some conversations with modifier key.",
+            "[Skins] Update Auctionator skin.",
+            "[Skins] Update ElvUI actionbar skin."
+        },
+        ["ruRU"] = {
+            "[Turn In] Fix the issue that you cannot pause some conversations with modifier key.",
+            "[Skins] Update Auctionator skin.",
+            "[Skins] Update ElvUI actionbar skin."
+        }
+    }
+}

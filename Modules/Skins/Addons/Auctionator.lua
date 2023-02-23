08099a1c7960a92099e26fe994cb59f5104c2796
Zhou Fang <fang2hou@outlook.com>
2023-02-23 10:25:15 +0900

    feat(skins): add skin for auctionator objective tracker button

--- Modules/Skins/Addons/Auctionator.lua
+++ Modules/Skins/Addons/Auctionator.lua
@@ -411,6 +411,10 @@ local function configSellingFrame(frame)
     S:ESProxy("HandleButton", frame.UnhideAll)
 end
 
+local function craftingInfoObjectiveTrackerFrame(frame)
+    S:ESProxy("HandleButton", frame.SearchButton)
+end
+
 function S:Auctionator()
     if not E.private.WT.skins.enable or not E.private.WT.skins.addons.auctionator then
         return
@@ -456,6 +460,7 @@ function S:Auctionator()
     hooksecurefunc(_G.AuctionatorListExportFrameMixin, "OnLoad", reskin(listExportFrame))
     hooksecurefunc(_G.AuctionatorListImportFrameMixin, "OnLoad", reskin(listImportFrame))
     hooksecurefunc(_G.AuctionatorItemHistoryFrameMixin, "Init", reskin(itemHistoryFrame))
+    hooksecurefunc(_G.AuctionatorCraftingInfoObjectiveTrackerFrameMixin, "OnLoad", reskin(craftingInfoObjectiveTrackerFrame))
     hooksecurefunc(_G.AuctionatorShoppingItemMixin, "OnLoad", reskin(shoppingItem))
     hooksecurefunc(_G.AuctionatorSplashScreenMixin, "OnLoad", reskin(splashFrame))
 end

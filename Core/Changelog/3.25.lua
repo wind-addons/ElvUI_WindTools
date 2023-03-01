25f192d960bb46bce9e64e5b8e8993fa85a9adec
Zhou Fang <fang2hou@outlook.com>
2023-03-01 09:55:49 +0900

    docs(changelog): 3.25

--- Core/Changelog/3.25.lua
+++ Core/Changelog/3.25.lua
@@ -18,29 +18,49 @@ W.Changelog[325] = {
     },
     IMPROVEMENT = {
         ["zhCN"] = {
+            "[通告] 被地震打断时不再通报自己打断了自己.",
+            "[通告] 在更换钥石时现在也会进行通告.",
             "[交接] 修复无法使用修饰键暂停部分对话的问题.",
             "[美化外观] 更新 Auctionator 皮肤.",
-            "[美化外观] 更新 ElvUI 动作条皮肤."
+            "[美化外观] 更新 ElvUI 动作条皮肤.",
+            "[美化外观] 修复贸易站皮肤的一个错误.",
+            "[吸收] 修复切换配置文件时可能造成的一个错误."
         },
         ["zhTW"] = {
+            "[通告] 被地震打斷時不再通報自己打斷了自己.",
+            "[通告] 在更換鑰石時現在也會進行通報.",
             "[交接] 修復無法使用修飾鍵暫停部分對話的問題.",
             "[美化外觀] 更新 Auctionator 皮膚.",
-            "[美化外觀] 更新 ElvUI 動作條皮膚."
+            "[美化外觀] 更新 ElvUI 動作條皮膚.",
+            "[美化外觀] 修復貿易站皮膚的一個錯誤.",
+            "[吸收] 修復切換配置文件時可能造成的一個錯誤."
         },
         ["enUS"] = {
+            "[Announcement] No longer announce you interrupt yourself by quake.",
+            "[Announcement] Add the announcement after you changed the keystone.",
             "[Turn In] Fix the issue that you cannot pause some conversations with modifier key.",
             "[Skins] Update Auctionator skin.",
-            "[Skins] Update ElvUI actionbar skin."
+            "[Skins] Update ElvUI actionbar skin.",
+            "[Skins] Fix an error in TradeSkill skin.",
+            "[Absorb] Fix an error when switching profile."
         },
         ["koKR"] = {
+            "[Announcement] No longer announce you interrupt yourself by quake.",
+            "[Announcement] Add the announcement after you changed the keystone.",
             "[Turn In] Fix the issue that you cannot pause some conversations with modifier key.",
             "[Skins] Update Auctionator skin.",
-            "[Skins] Update ElvUI actionbar skin."
+            "[Skins] Update ElvUI actionbar skin.",
+            "[Skins] Fix an error in TradeSkill skin.",
+            "[Absorb] Fix an error when switching profile."
         },
         ["ruRU"] = {
+            "[Announcement] No longer announce you interrupt yourself by quake.",
+            "[Announcement] Add the announcement after you changed the keystone.",
             "[Turn In] Fix the issue that you cannot pause some conversations with modifier key.",
             "[Skins] Update Auctionator skin.",
-            "[Skins] Update ElvUI actionbar skin."
+            "[Skins] Update ElvUI actionbar skin.",
+            "[Skins] Fix an error in TradeSkill skin.",
+            "[Absorb] Fix an error when switching profile."
         }
     }
 }

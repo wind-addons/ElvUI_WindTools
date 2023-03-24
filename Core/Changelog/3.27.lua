76cc1c75a91c8583365c2b82a40057fb01fdd532
Zhou Fang <fang2hou@outlook.com>
2023-03-25 01:37:56 +0900

    docs(changelog): 3.27

--- Core/Changelog/3.27.lua
+++ Core/Changelog/3.27.lua
@@ -4,19 +4,29 @@ W.Changelog[327] = {
     RELEASE_DATE = "TBD",
     IMPORTANT = {
         ["zhCN"] = {
-            "西班牙语翻译大幅度更新. 感谢 Keralin"
+            "西班牙语翻译大幅度更新. 感谢 Keralin",
+            "更新库 LibItemInfo.",
+            "更新库 LibObjectiveProgress."
         },
         ["zhTW"] = {
-            "西班牙語翻譯大幅度更新. 感謝 Keralin"
+            "西班牙語翻譯大幅度更新. 感謝 Keralin",
+            "更新庫 LibItemInfo.",
+            "更新庫 LibObjectiveProgress."
         },
         ["enUS"] = {
-            "Spanish translation has been updated. Thanks Keralin"
+            "Spanish translation has been updated. Thanks Keralin",
+            "Update library LibItemInfo.",
+            "Update library LibObjectiveProgress."
         },
         ["koKR"] = {
-            "스페인어 번역이 업데이트되었습니다. Keralin님 감사합니다."
+            "스페인어 번역이 업데이트되었습니다. Keralin님 감사합니다.",
+            "라이브러리 LibItemInfo를 업데이트했습니다.",
+            "라이브러리 LibObjectiveProgress를 업데이트했습니다."
         },
         ["ruRU"] = {
-            "Обновлен перевод на испанский язык. Спасибо Keralin"
+            "Обновлен перевод на испанский язык. Спасибо Keralin",
+            "Обновлена библиотека LibItemInfo.",
+            "Обновлена библиотека LibObjectiveProgress."
         }
     },
     NEW = {
@@ -30,27 +40,27 @@ W.Changelog[327] = {
         ["zhCN"] = {
             "[额外物品条] 更新可开启物品列表.",
             "[聊天文字] 公会成员状态信息现在跟随系统频道设定.",
-            "[聊天文字] 合并后的成就信息现在跟随系统频道设定.",
+            "[聊天文字] 合并后的成就信息现在跟随系统频道设定."
         },
         ["zhTW"] = {
             "[額外物品條] 更新可開啟物品列表.",
             "[聊天文字] 公會成員狀態信息現在跟隨系統頻道設定.",
-            "[聊天文字] 合併後的成就信息現在跟隨系統頻道設定.",
+            "[聊天文字] 合併後的成就信息現在跟隨系統頻道設定."
         },
         ["enUS"] = {
             "[Extra Item Bar] Updated openable item list.",
             "[Chat Text] Guild member status message now follows system channel setting.",
-            "[Chat Text] Merged achievement message now follows system channel setting.",
+            "[Chat Text] Merged achievement message now follows system channel setting."
         },
         ["koKR"] = {
             "[아이템 바] 열 수 있는 아이템 목록이 업데이트되었습니다.",
             "[대화창 글자] 길드원 상태 메시지가 이제 시스템 채널 설정에 따라 표시됩니다.",
-            "[대화창 글자] 병합된 업적 메시지가 이제 시스템 채널 설정에 따라 표시됩니다.",
+            "[대화창 글자] 병합된 업적 메시지가 이제 시스템 채널 설정에 따라 표시됩니다."
         },
         ["ruRU"] = {
             "[Панель дополнительных предметов] Обновление списка открываемых предметов.",
             "[Текст чата] Сообщение о состоянии члена гильдии теперь следует настройкам системного канала.",
-            "[Текст чата] Сообщение о слиянии достижений теперь следует настройкам системного канала.",
+            "[Текст чата] Сообщение о слиянии достижений теперь следует настройкам системного канала."
         }
     }
 }

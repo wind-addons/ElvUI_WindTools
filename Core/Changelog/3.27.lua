90d7262a5b25b6a7113550a2e9bad758102422e4
Zhou Fang <fang2hou@outlook.com>
2023-03-24 23:21:12 +0900

    docs(changelog): start 3.27

---
+++ Core/Changelog/3.27.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[327] = {
+    RELEASE_DATE = "2023/03/24",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "西班牙语翻译大幅度更新. 感谢 Keralin"
+        },
+        ["zhTW"] = {
+            "西班牙語翻譯大幅度更新. 感謝 Keralin"
+        },
+        ["enUS"] = {
+            "Spanish translation has been updated. Thanks Keralin"
+        },
+        ["koKR"] = {
+            "스페인어 번역이 업데이트되었습니다. Keralin님 감사합니다."
+        },
+        ["ruRU"] = {
+            "Обновлен перевод на испанский язык. Спасибо Keralin"
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}

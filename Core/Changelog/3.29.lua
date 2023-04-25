4af393f67aa04eeb1e524756041c9908caf99bb9
Zhou Fang <fang2hou@outlook.com>
2023-04-26 01:25:47 +0900

    docs(changelog): add 3.29 changelog

---
+++ Core/Changelog/3.29.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[329] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[额外物品条] 更新专业物品列表.",
+            "[美化外观] 更新 TomCats 美化皮肤.",
+            "[小地图图标] 修复 TomCats 小地图图标材质错位的问题."
+        },
+        ["zhTW"] = {
+            "[額外物品條] 更新專業物品清單.",
+            "[美化外觀] 更新 TomCats 美化皮膚.",
+            "[小地圖圖標] 修復 TomCats 小地圖圖標材質錯位的問題."
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Update professional item list.",
+            "[Skins] Update TomCats skin.",
+            "[Minimap Buttons] Fix TomCats minimap button texture misalignment."
+        },
+        ["koKR"] = {
+            "[추가 아이템 바] 전문 아이템 목록 업데이트.",
+            "[스킨] TomCats 스킨 업데이트.",
+            "[미니맵 버튼] TomCats 미니맵 버튼 텍스처 정렬 문제 수정."
+        },
+        ["ruRU"] = {
+            "[Панель дополнительных предметов] Обновление списка профессиональных предметов.",
+            "[Скины] Обновление скинов TomCats.",
+            "[Кнопки миникарты] Исправление проблемы с выравниванием текстур кнопок миникарты TomCats."
+        }
+    }
+}

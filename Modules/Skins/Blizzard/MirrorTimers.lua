30c35dadb1c71c52916ae21a6bbd87395122ba7e
Zhou Fang <fang2hou@outlook.com>
2023-07-03 21:07:22 +0900

    fix: fix the skin of mirror timers in 10.1.5

--- Modules/Skins/Blizzard/MirrorTimers.lua
+++ Modules/Skins/Blizzard/MirrorTimers.lua
@@ -2,17 +2,18 @@ local W, F, E, L = unpack((select(2, ...)))
 local S = W.Modules.Skins
 
 local _G = _G
-local MIRRORTIMER_NUMTIMERS = MIRRORTIMER_NUMTIMERS
+local hooksecurefunc = hooksecurefunc
 
 function S:MirrorTimers()
     if not self:CheckDB("mirrorTimers") then
         return
     end
 
-    for i = 1, MIRRORTIMER_NUMTIMERS do
-        local statusBar = _G["MirrorTimer" .. i]
-        self:CreateShadow(statusBar)
-    end
+    hooksecurefunc(_G.MirrorTimerMixin, "Setup", function(timer)
+        if timer and timer.StatusBar then
+            self:CreateShadow(timer.StatusBar)
+        end
+    end)
 end
 
 S:AddCallback("MirrorTimers")

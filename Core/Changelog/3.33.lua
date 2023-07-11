ff3dc79099b690f6b3745094d478007e26376347
Zhou Fang <fang2hou@outlook.com>
2023-07-12 01:18:31 +0900

    docs: update changelog

--- Core/Changelog/3.33.lua
+++ Core/Changelog/3.33.lua
@@ -1,27 +1,27 @@
 local W = unpack((select(2, ...)))
 
 W.Changelog[333] = {
-    RELEASE_DATE = "2023/07/11",
+    RELEASE_DATE = "2023/07/12",
     IMPORTANT = {
         ["zhCN"] = {
             "适配魔兽世界 10.1.5 版本.",
-            "适配 ElvUI 13.35 版本."
+            "适配 ElvUI 13.36 版本."
         },
         ["zhTW"] = {
             "適配魔獸世界 10.1.5 版本.",
-            "適配 ElvUI 13.35 版本."
+            "適配 ElvUI 13.36 版本."
         },
         ["enUS"] = {
             "Support WoW 10.1.5.",
-            "Support ElvUI 13.35."
+            "Support ElvUI 13.36."
         },
         ["koKR"] = {
             "월드 오브 워크래프트 10.1.5 지원.",
-            "ElvUI 13.35 지원."
+            "ElvUI 13.36 지원."
         },
         ["ruRU"] = {
             "Поддержка WoW 10.1.5.",
-            "Поддержка ElvUI 13.35."
+            "Поддержка ElvUI 13.36."
         }
     },
     NEW = {

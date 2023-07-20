37f994f501968b3fdd5562a79ecc25d408ce11e5
Zhou Fang <fang2hou@outlook.com>
2023-07-20 22:59:48 +0900

    docs(changelog): 3.34

---
+++ Core/Changelog/3.34.lua
@@ -0,0 +1,51 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[334] = {
+    RELEASE_DATE = "2023/07/20",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[事件追踪器] 新增研究员遇袭追踪. 感谢 fubaWoW",
+            "[事件追踪器] 新增时光裂隙追踪. 感谢 fubaWoW"
+        },
+        ["zhTW"] = {
+            "[事件追蹤器] 新增研究員遇襲追蹤. 感謝 fubaWoW",
+            "[事件追蹤器] 新增時光的裂隙追蹤. 感謝 fubaWoW"
+        },
+        ["enUS"] = {
+            "[Event Tracker] Add Researchers under Fire tracker. Thanks fubaWoW",
+            "[Event Tracker] Add Time Rift Thaldraszus tracker. Thanks fubaWoW"
+        },
+        ["koKR"] = {
+            "[이벤트 추적기] 연구원의 위기 추적기 추가. fubaWoW 님께 감사드립니다.",
+            "[이벤트 추적기] 타르드라수스의 시간 균열 추적기 추가. fubaWoW 님께 감사드립니다."
+        },
+        ["ruRU"] = {
+            '[Отслеживание событий] Добавлено отслеживание "Исследователи под огнем". Спасибо fubaWoW',
+            '[Отслеживание событий] Добавлено отслеживание "Временной разлом Тал\'драззуса". Спасибо fubaWoW'
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[额外物品条] 更新物品列表."
+        },
+        ["zhTW"] = {
+            "[額外物品條] 更新物品清單."
+        },
+        ["enUS"] = {
+            "[Extra Item Bar] Update item list."
+        },
+        ["koKR"] = {
+            "[추가 아이템 바] 아이템 목록 업데이트."
+        },
+        ["ruRU"] = {
+            "[Панель дополнительных предметов] Обновление списка предметов."
+        }
+    }
+}

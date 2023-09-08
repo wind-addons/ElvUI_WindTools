91845554d8cf692c1ae9114143bea8858feb0e1f
Zhou Fang <fang2hou@outlook.com>
2023-09-08 10:13:30 +0900

    perf: use local variable

--- Modules/Skins/Blizzard/ExpansionLandingPage.lua
+++ Modules/Skins/Blizzard/ExpansionLandingPage.lua
@@ -2,6 +2,7 @@ local W, F, E, L = unpack((select(2, ...)))
 local S = W.Modules.Skins
 
 local _G = _G
+local next = next
 
 function S:Blizzard_ExpansionLandingPage()
     if not self:CheckDB("expansionLanding", "expansionLandingPage") then

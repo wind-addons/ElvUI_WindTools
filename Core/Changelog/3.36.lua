f9462dcd3df07a7dc1a9ddc69f2f2bc44bf8690e
Zhou Fang <fang2hou@outlook.com>
2023-09-10 05:00:33 +0900

    docs(changelog): 3.36-fix-2

--- Core/Changelog/3.36.lua
+++ Core/Changelog/3.36.lua
@@ -6,27 +6,32 @@ W.Changelog[336] = {
         ["zhCN"] = {
             "适配魔兽世界 10.1.7 版本.",
             "适配 ElvUI 13.40 版本.",
-            "更新韩语翻译. 感谢 Reim"
+            "更新韩语翻译. 感谢 Reim",
+            "LibOpenRaid 升級到 112 版本."
         },
         ["zhTW"] = {
             "適配魔獸世界 10.1.7 版本.",
             "適配 ElvUI 13.40 版本.",
-            "更新韓語翻譯. 感謝 Reim"
+            "更新韓語翻譯. 感謝 Reim",
+            "LibOpenRaid 升級到 112 版本."
         },
         ["enUS"] = {
             "Support WoW 10.1.7.",
             "Support ElvUI 13.40.",
-            "Update Korean translation. Thanks Reim"
+            "Update Korean translation. Thanks Reim",
+            "Update LibOpenRaid to version 112."
         },
         ["koKR"] = {
             "월드 오브 워크래프트 10.1.7 지원.",
             "ElvUI 13.40 지원.",
-            "한국어 번역 업데이트. Reim 님께 감사드립니다."
+            "한국어 번역 업데이트. Reim 님께 감사드립니다.",
+            "LibOpenRaid을 버전 112로 업데이트."
         },
         ["ruRU"] = {
             "Поддержка WoW 10.1.7.",
             "Поддержка ElvUI 13.40.",
-            "Обновлен перевод на корейский язык. Спасибо Reim"
+            "Обновлен перевод на корейский язык. Спасибо Reim",
+            "Обновление LibOpenRaid до версии 112."
         }
     },
     NEW = {

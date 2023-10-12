d924a51a8bb2c716229d618b32ce73a2035f5253
Zhou Fang <fang2hou@outlook.com>
2023-10-13 00:19:07 +0900

    docs(changelog): 3.40

---
+++ Core/Changelog/3.40.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[340] = {
+    RELEASE_DATE = "2023/10/13",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[美化外观] 修复 Auctionator 皮肤. 部分新界面将在之后添加支持."
+        },
+        ["zhTW"] = {
+            "[美化外觀] 修復 Auctionator 皮膚. 部分新界面將在之後添加支援."
+        },
+        ["enUS"] = {
+            "[Skins] Fix Auctionator skin. The support of new UI panels will be added later."
+        },
+        ["koKR"] = {
+            "[스킨] Auctionator 스킨을 수정했습니다. 새로운 UI 패널의 지원은 나중에 추가될 예정입니다."
+        },
+        ["ruRU"] = {
+            "[Скины] Исправлен скин Auctionator. Поддержка новых панелей интерфейса будет добавлена позже."
+        }
+    }
+}

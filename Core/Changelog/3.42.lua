71249d70335de9425d026a0dbc32d17374fe09d5
Zhou Fang <fang2hou@outlook.com>
2023-11-08 21:32:35 +0900

    docs(changelog) 3.42

---
+++ Core/Changelog/3.42.lua
@@ -0,0 +1,27 @@
+local W = unpack((select(2, ...)))
+W.Changelog[342] = {
+    RELEASE_DATE = "2023/11/08",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "WeakAuras 皮肤仅支持 WeakAurasPatched (一个我发布的WeakAuras改版, 你可以在设定中找到它)"
+        },
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}

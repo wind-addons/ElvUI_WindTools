e766591bb56c02863f38a9a6595f705c9716160d
Zhou Fang <fang2hou@outlook.com>
2023-11-13 21:28:04 +0900

    docs(changelog): update

--- Core/Changelog/3.44.lua
+++ Core/Changelog/3.44.lua
@@ -3,19 +3,29 @@ W.Changelog[344] = {
     RELEASE_DATE = "2023/11/12",
     IMPORTANT = {
         ["zhCN"] = {
-            "错误信息的字体可以通过 ElvUI 设定了, 故移除错误信息的字体设定功能."
+            "韩语翻译更新, 感谢 Sang Jeon@GitHub.",
+            "错误信息的字体可以通过 ElvUI 设定了, 故移除错误信息的字体设定功能.",
+            "由于还不清楚 10.2 的词缀循环规律, 故暂时移除预组队列表右侧面板中的词缀循环提示."
         },
         ["zhTW"] = {
-            "錯誤訊息的字體可以透過 ElvUI 設定了, 故移除錯誤訊息的字體設定功能."
+            "韓語翻譯更新, 感謝 Sang Jeon@GitHub.",
+            "錯誤訊息的字體可以透過 ElvUI 設定了, 故移除錯誤訊息的字體設定功能.",
+            "由於還不清楚 10.2 的詞綴循環規律, 故暫時移除預組列表右側面板中的詞綴循環提示."
         },
         ["enUS"] = {
-            "The font of error messages can be set through ElvUI, so remove the font setting function of error messages."
+            "Korean translation updated, thanks to Sang Jeon@GitHub.",
+            "The font of error messages can be set through ElvUI, so remove the font setting function of error messages.",
+            "Since the affix cycle rules of 10.2 are not clear yet, the affix cycle tooltip in the right panel of the LFG List is temporarily disabled."
         },
         ["koKR"] = {
-            "오류 메시지의 글꼴은 ElvUI를 통해 설정할 수 있으므로 오류 메시지의 글꼴 설정 기능을 제거합니다."
+            "한국어 번역이 업데이트되었습니다. Sang Jeon@GitHub에 감사드립니다.",
+            "오류 메시지의 글꼴은 ElvUI를 통해 설정할 수 있으므로 오류 메시지의 글꼴 설정 기능을 제거합니다.",
+            "10.2의 접두사 순환 규칙이 아직 명확하지 않으므로 파티 찾기 목록의 오른쪽 패널에 접두사 순환 툴팁을 일시적으로 비활성화했습니다."
         },
         ["ruRU"] = {
-            "Шрифт сообщений об ошибках можно настроить через ElvUI, поэтому удалите функцию настройки шрифта сообщений об ошибках."
+            "Обновлен перевод на корейский язык, спасибо Sang Jeon@GitHub.",
+            "Шрифт сообщений об ошибках можно настроить через ElvUI, поэтому удалите функцию настройки шрифта сообщений об ошибках.",
+            "Поскольку правила цикла приставок 10.2 еще не ясны, подсказка цикла приставок в правой панели списка LFG временно отключена."
         }
     },
     NEW = {
@@ -43,23 +53,38 @@ W.Changelog[344] = {
     IMPROVEMENT = {
         ["zhCN"] = {
             "[进度] 更新 10.2 数据.",
-            "[进度] 默认追踪 S3 成就, 默认停止追踪 S1 相关成就."
+            "[进度] 默认追踪 S3 成就, 默认停止追踪 S1 相关成就.",
+            "[预组队列表] 更新 10.2 数据.",
+            "[事件追踪器] 更新事件缩写.",
+            "[额外物品条] 新增 10.2 药水. 感谢 lakeland1990@GitHub"
         },
         ["zhTW"] = {
             "[進度] 更新 10.2 資料.",
-            "[進度] 預設追蹤 S3 成就, 預設停止追蹤 S1 相關成就."
+            "[進度] 預設追蹤 S3 成就, 預設停止追蹤 S1 相關成就.",
+            "[預組列表] 更新 10.2 資料.",
+            "[事件追蹤器] 更新事件縮寫.",
+            "[額外物品條] 新增 10.2 藥水. 感謝 lakeland1990@GitHub"
         },
         ["enUS"] = {
             "[Progression] Update 10.2 data.",
-            "[Progression] Track S3 achievements by default, and stop tracking S1 related achievements by default."
+            "[Progression] Track S3 achievements by default, and stop tracking S1 related achievements by default.",
+            "[LFG List] Update 10.2 data.",
+            "[Event Tracker] Update event abbreviations.",
+            "[Extra Item Bar] Added 10.2 potions. Thanks to lakeland1990@GitHub"
         },
         ["koKR"] = {
             "[진행] 10.2 데이터를 업데이트했습니다.",
-            "[진행] 기본적으로 S3 업적을 추적하고, 기본적으로 S1 관련 업적 추적을 중지합니다."
+            "[진행] 기본적으로 S3 업적을 추적하고, 기본적으로 S1 관련 업적 추적을 중지합니다.",
+            "[파티 찾기 목록] 10.2 데이터를 업데이트했습니다.",
+            "[이벤트 추적기] 이벤트 약어를 업데이트했습니다.",
+            "[추가 아이템 바] 10.2 물약을 추가했습니다. lakeland1990@GitHub에 감사드립니다."
         },
         ["ruRU"] = {
             "[Прогресс] Обновлены данные 10.2.",
-            "[Прогресс] По умолчанию отслеживаются достижения S3, а по умолчанию прекращается отслеживание связанных с S1 достижений."
+            "[Прогресс] По умолчанию отслеживаются достижения S3, а по умолчанию прекращается отслеживание связанных с S1 достижений.",
+            "[Список поиска группы] Обновлены данные 10.2.",
+            "[Трекер событий] Обновлены сокращения событий.",
+            "[Дополнительная панель предметов] Добавлены зелья 10.2. Спасибо lakeland1990@GitHub"
         }
     }
 }

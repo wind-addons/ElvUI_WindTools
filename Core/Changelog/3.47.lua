dd3b700c4edb925b64c77bcd9d4c4c5955a6bc35
Zhou Fang <fang2hou@outlook.com>
2024-01-11 22:37:25 +0900

    docs(changelog): 3.47

---
+++ Core/Changelog/3.47.lua
@@ -0,0 +1,56 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[347] = {
+    RELEASE_DATE = "2024/01/11",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "更新 OpenRaid 库. 感谢 Valdemar@GitHub",
+            "法语翻译更新. 感谢 ckeurk@GitHub"
+        },
+        ["zhTW"] = {
+            "更新 OpenRaid 庫. 感謝 Valdemar@GitHub",
+            "法語翻譯更新. 感謝 ckeurk@GitHub"
+        },
+        ["enUS"] = {
+            "Update OpenRaid library. Thanks Valdemar@GitHub",
+            "Update French translation. Thanks ckeurk@GitHub"
+        },
+        ["koKR"] = {
+            "OpenRaid 라이브러리 업데이트. 감사합니다 Valdemar@GitHub",
+            "프랑스어 번역 업데이트. 감사합니다 ckeurk@GitHub"
+        },
+        ["ruRU"] = {
+            "Обновлена библиотека OpenRaid. Благодарим Valdemar@GitHub",
+            "Обновлен перевод на французский язык. Благодарим ckeurk@GitHub"
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[进度] 修复新 Raid 进度无法正确显示的问题. 感谢 Nujitsu@GitHub",
+            "[预组建队伍] 新增有T, 有奶筛选按钮. 感谢 mattiagraziani-it@GitHub"
+        },
+        ["zhTW"] = {
+            "[進度] 修復新 Raid 進度無法正確顯示的問題. 感謝 Nujitsu@GitHub",
+            "[預組建隊伍] 新增有T, 有奶篩選按鈕. 感謝 mattiagraziani-it@GitHub"
+        },
+        ["enUS"] = {
+            "[Progression] Fix the issue that new raid progress cannot be displayed correctly. Thanks Nujitsu@GitHub",
+            "[LFG List] Add Tank and Healer filter buttons. Thanks mattiagraziani-it@GitHub"
+        },
+        ["koKR"] = {
+            "[진행률] 새로운 던전 진행률이 정확히 표시되지 않는 문제를 수정했습니다. 감사합니다 Nujitsu@GitHub",
+            "[파티 찾기] 탱커와 힐러 필터 버튼을 추가했습니다. 감사합니다 mattiagraziani-it@GitHub"
+        },
+        ["ruRU"] = {
+            "[Прогресс] Исправлена проблема с отображением прогресса нового рейда. Благодарим Nujitsu@GitHub",
+            "[Поиск группы] Добавлены кнопки фильтрации танков и лекарей. Благодарим mattiagraziani-it@GitHub"
+        }
+    }
+}

5e34a39250d87a19726d0abd7ab957736530fc54
Zhou Fang <fang2hou@outlook.com>
2024-02-11 23:29:41 +0900

    docs: add changelog of 3.50

---
+++ Core/Changelog/3.50.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[350] = {
+    RELEASE_DATE = "2024/02/11",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "更新 OpenRaid 库."
+        },
+        ["zhTW"] = {
+            "更新 OpenRaid 庫."
+        },
+        ["enUS"] = {
+            "Update OpenRaid library."
+        },
+        ["koKR"] = {
+            "OpenRaid 라이브러리 업데이트."
+        },
+        ["ruRU"] = {
+            "Обновлена библиотека OpenRaid."
+        }
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[游戏条] 新增开悟者炉石到炉石列表."
+        },
+        ["zhTW"] = {
+            "[遊戲條] 新增受啟迪的爐石到爐石列表."
+        },
+        ["enUS"] = {
+            "[Game Bar] Add Enlightened Hearthstone to the Hearthstone list."
+        },
+        ["koKR"] = {
+            "[게임 바] 깨달음의 귀환석을 귀환석 목록에 추가했습니다."
+        },
+        ["ruRU"] = {
+            "[Игровая панель] Добавлен Камень возвращения Просветленных в список камней возвращения."
+        }
+    }
+}

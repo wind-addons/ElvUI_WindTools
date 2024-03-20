2eb09d6177a344bb082544fbcf8def8396a4645f
Zhou Fang <fang2hou@outlook.com>
2024-03-20 10:28:37 +0900

    docs(changelog): 3.52

---
+++ Core/Changelog/3.52.lua
@@ -0,0 +1,35 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[348] = {
+	RELEASE_DATE = "2024/03/20",
+	IMPORTANT = {
+		["zhCN"] = {
+			"适配魔兽世界 10.2.6 版本.",
+			"适配 ElvUI 13.60 版本.",
+		},
+		["zhTW"] = {
+			"適配魔獸世界 10.2.6 版本.",
+			"適配 ElvUI 13.60 版本.",
+		},
+		["enUS"] = {
+			"Support WoW 10.2.6.",
+			"Support ElvUI 13.60.",
+		},
+		["koKR"] = {
+			"월드 오브 워크래프트 10.2.6 지원.",
+			"ElvUI 13.60 지원.",
+		},
+		["ruRU"] = {
+			"Поддержка WoW 10.2.6.",
+			"Поддержка ElvUI 13.60.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {},
+}

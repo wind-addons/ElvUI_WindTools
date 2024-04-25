c8bea5a86f3b43d1756bf8c8c9a0d2c010c82080
Zhou Fang <fang2hou@outlook.com>
2024-04-25 20:42:32 +0900

    docs: add changelog for 3.54

---
+++ Core/Changelog/3.54.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[354] = {
+    RELEASE_DATE = "2024/04/25",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[美化皮肤] 更新 RaiderIO 皮肤.",
+            "[进度] 更新第四赛季数据.",
+            "[预组建列表] 更新第四赛季数据.",
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 更新 RaiderIO 皮膚.",
+            "[進度] 更新第四賽季數據.",
+            "[預組列表] 更新第四賽季數據.",
+        },
+        ["enUS"] = {
+            "[Skin] Update RaiderIO skin.",
+            "[Progression] Update data for Season 4.",
+            "[LFG List] Update data for Season 4.",
+        },
+        ["koKR"] = {
+            "[스킨] RaiderIO 스킨을 업데이트했습니다.",
+            "[진행] 시즌 4 데이터를 업데이트했습니다.",
+            "[미리 구성된 그룹] 시즌 4 데이터를 업데이트했습니다.",
+        },
+        ["ruRU"] = {
+            "[Скин] Обновлен скин RaiderIO.",
+            "[Прогресс] Обновлены данные для 4 сезона.",
+            "[Список LFG] Обновлены данные для 4 сезона.",
+        },
+    }
+}

51db82ed92b47eccb632490bb0c92058a7f4480c
Zhou Fang <fang2hou@outlook.com>
2024-05-28 10:21:57 +0900

    docs: changelog

---
+++ Core/Changelog/3.58.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[358] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {
+            "[好友名单] 为好友名添加新的 Remix 小图标."
+        },
+        ["zhTW"] = {
+            "[好友名單] 為好友名添加新的 Remix 小圖示."
+        },
+        ["enUS"] = {
+            "[Friends List] Added new Remix icon for friend name."
+        },
+        ["koKR"] = {
+            "[친구 목록] 친구 이름에 새로운 Remix 아이콘 추가."
+        },
+        ["ruRU"] = {
+            "[Список друзей] Добавлен новый значок Remix для имени друга."
+        }
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}

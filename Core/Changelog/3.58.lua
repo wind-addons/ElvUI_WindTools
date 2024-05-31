913c763ab074a0b4fe12681088c1f48b20afedb6
Zhou Fang <fang2hou@outlook.com>
2024-06-01 00:17:50 +0900

    docs: changelog

--- Core/Changelog/3.58.lua
+++ Core/Changelog/3.58.lua
@@ -1,13 +1,23 @@
 local W = unpack((select(2, ...)))
 
 W.Changelog[358] = {
-    RELEASE_DATE = "TBD",
+    RELEASE_DATE = "2024/06/01",
     IMPORTANT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {},
-        ["ruRU"] = {}
+        ["zhCN"] = {
+            "适配 ElvUI 版本最低为 13.66."
+        },
+        ["zhTW"] = {
+            "適配 ElvUI 版本最低為 13.66."
+        },
+        ["enUS"] = {
+            "Adapt to ElvUI version minimum 13.66."
+        },
+        ["koKR"] = {
+            "ElvUI 버전 최소 13.66에 맞춰집니다."
+        },
+        ["ruRU"] = {
+            "Адаптация к версии ElvUI не ниже 13.66."
+        }
     },
     NEW = {
         ["zhCN"] = {
@@ -27,10 +37,25 @@ W.Changelog[358] = {
         }
     },
     IMPROVEMENT = {
-        ["zhCN"] = {},
-        ["zhTW"] = {},
-        ["enUS"] = {},
-        ["koKR"] = {},
-        ["ruRU"] = {}
+        ["zhCN"] = {
+            "[聊天文字] 同步最新的 ElvUI 代码, 修正作者名字显示样式.",
+            "[预组队列表] 优化代码, 提升性能."
+        },
+        ["zhTW"] = {
+            "[聊天文字] 同步最新的 ElvUI 代码, 修正作者名字顯示樣式.",
+            "[預組隊列表] 優化代碼, 提升性能."
+        },
+        ["enUS"] = {
+            "[Chat Text] Synchronize the latest ElvUI code and correct the author name display style.",
+            "[Premade Group List] Optimize the code and improve performance."
+        },
+        ["koKR"] = {
+            "[채팅 텍스트] 최신 ElvUI 코드를 동기화하고 작성자 이름 표시 스타일을 수정하였습니다.",
+            "[프리메이드 그룹 목록] 코드를 최적화하고 성능을 향상하였습니다."
+        },
+        ["ruRU"] = {
+            "[Текст чата] Синхронизация с последним кодом ElvUI и коррекция стиля отображения имени автора.",
+            "[Список групп по предварительной настройке] Оптимизация кода и улучшение производительности."
+        }
     }
 }

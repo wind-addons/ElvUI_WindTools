ba0ec357440f7c975a896155e7e671fc372b7cd4
Zhou Fang <fang2hou@outlook.com>
2024-06-29 22:52:35 +0900

    fix: stop reposition the close button in raid utility

--- Modules/Skins/ElvUI/RaidUtility.lua
+++ Modules/Skins/ElvUI/RaidUtility.lua
@@ -1,24 +1,8 @@
 local W, F, E, L = unpack((select(2, ...)))
 local S = W.Modules.Skins
-local RU = E:GetModule("RaidUtility")
 
 local _G = _G
 local pairs = pairs
-local InCombatLockdown = InCombatLockdown
-
-function S:RaidUtility_ShowButton_OnClick()
-    if InCombatLockdown() then
-        return
-    end
-
-    _G.RaidUtility_CloseButton:ClearAllPoints()
-    local anchor = _G.RaidUtilityPanel:GetPoint()
-    if anchor == "TOP" then
-        _G.RaidUtility_CloseButton:Point("TOP", _G.RaidUtilityPanel, "BOTTOM", 0, -4)
-    else
-        _G.RaidUtility_CloseButton:Point("BOTTOM", _G.RaidUtilityPanel, "TOP", 0, 4)
-    end
-end
 
 function S:RaidUtility()
     if not E.private.WT.skins.elvui.enable or not E.private.WT.skins.elvui.raidUtility then
@@ -33,15 +17,12 @@ function S:RaidUtility()
         _G.RaidUtilityPanel,
         _G.RaidUtility_ShowButton,
         _G.RaidUtility_CloseButton,
-        _G.RaidUtilityRoleIcons
+        _G.RaidUtilityRoleIcons,
+        _G.RaidUtilityTargetIcons
     }
 
     for _, frame in pairs(frames) do
         self:CreateShadow(frame)
-    end
-
-    if _G.RaidUtility_ShowButton then
-        self:SecureHookScript(_G.RaidUtility_ShowButton, "OnClick", "RaidUtility_ShowButton_OnClick")
     end
 end
 

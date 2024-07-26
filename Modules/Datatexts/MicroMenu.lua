c33f21a832ae0bb5b3b3bcc4c3cf3d8072256abc
Zhou Fang <fang2hou@outlook.com>
2024-07-26 09:54:48 +0900

    fix: micro menu datatext

--- Modules/Datatexts/MicroMenu.lua
+++ Modules/Datatexts/MicroMenu.lua
@@ -9,141 +9,115 @@ local CreateFrame = CreateFrame
 local EasyMenu = EasyMenu
 local GameMenuFrame = _G.GameMenuFrame
 local IsInGuild = IsInGuild
-local PVEFrame_ToggleFrame = PVEFrame_ToggleFrame
-local ToggleAchievementFrame = ToggleAchievementFrame
-local ToggleCharacter = ToggleCharacter
-local ToggleCollectionsJournal = ToggleCollectionsJournal
-local ToggleEncounterJournal = ToggleEncounterJournal
-local ToggleFrame = ToggleFrame
-local ToggleFriendsFrame = ToggleFriendsFrame
-local ToggleGuildFinder = ToggleGuildFinder
-local ToggleGuildFrame = ToggleGuildFrame
-local ToggleHelpFrame = ToggleHelpFrame
-local ToggleTalentFrame = ToggleTalentFrame
 
-local MAINMENU_BUTTON = MAINMENU_BUTTON
+local dropdown =
+	CreateFrame("DropdownButton", "WTMicroMenuDatatextMenuDropDown", E.UIParent, "WowStyle1DropdownTemplate")
 
-local menuFrame = CreateFrame("Frame", "WTMicroMenuDatatextMenu", E.UIParent, "UIDropDownMenuTemplate")
+local function GenerateDayContextMenu(owner, rootDescription)
+	rootDescription:SetTag("WT_MICRO_MENU")
 
-local microMenu = {
-	{text = MAINMENU_BUTTON, isTitle = true, notCheckable = true},
-	{
-		text = CHARACTER_BUTTON,
-		func = function()
-			ToggleCharacter("PaperDollFrame")
-		end,
-		notCheckable = true
-	},
-	{
-		text = SPELLBOOK_ABILITIES_BUTTON,
-		func = function()
-			ToggleFrame(_G.SpellBookFrame)
-		end,
-		notCheckable = true
-	},
-	{
-		text = TALENTS_BUTTON,
-		func = function()
-			ToggleTalentFrame()
-		end,
-		notCheckable = true
-	},
-	{
-		text = ACHIEVEMENT_BUTTON,
-		func = function()
-			ToggleAchievementFrame()
-		end,
-		notCheckable = true
-	},
-	{
-		text = MOUNTS,
-		func = function()
-			ToggleCollectionsJournal(1)
-		end,
-		notCheckable = true
-	},
-	{
-		text = PETS,
-		func = function()
-			ToggleCollectionsJournal(2)
-		end,
-		notCheckable = true
-	},
-	{
-		text = TOY_BOX,
-		func = function()
-			ToggleCollectionsJournal(3)
-		end,
-		notCheckable = true
-	},
-	{
-		text = HEIRLOOMS,
-		func = function()
-			ToggleCollectionsJournal(4)
-		end,
-		notCheckable = true
-	},
-	{
-		text = SOCIAL_BUTTON,
-		func = function()
-			ToggleFriendsFrame(1)
-		end,
-		notCheckable = true
-	},
-	{
-		text = ACHIEVEMENTS_GUILD_TAB,
-		func = function()
+	rootDescription:CreateTitle(MAINMENU_BUTTON)
+	rootDescription:CreateButton(
+		CHARACTER_BUTTON,
+		function()
+			_G.ToggleCharacter("PaperDollFrame")
+		end
+	)
+	rootDescription:CreateButton(
+		SPELLBOOK,
+		function()
+			_G.PlayerSpellsUtil.ToggleSpellBookFrame()
+		end
+	)
+	rootDescription:CreateButton(
+		TALENTS_BUTTON,
+		function()
+			_G.PlayerSpellsUtil.ToggleClassTalentFrame()
+		end
+	)
+	rootDescription:CreateButton(
+		ACHIEVEMENT_BUTTON,
+		function()
+			_G.ToggleAchievementFrame()
+		end
+	)
+	rootDescription:CreateButton(
+		MOUNTS,
+		function()
+			_G.ToggleCollectionsJournal(1)
+		end
+	)
+	rootDescription:CreateButton(
+		PETS,
+		function()
+			_G.ToggleCollectionsJournal(2)
+		end
+	)
+	rootDescription:CreateButton(
+		TOY_BOX,
+		function()
+			_G.ToggleCollectionsJournal(3)
+		end
+	)
+	rootDescription:CreateButton(
+		HEIRLOOMS,
+		function()
+			_G.ToggleCollectionsJournal(4)
+		end
+	)
+	rootDescription:CreateButton(
+		SOCIAL_BUTTON,
+		function()
+			_G.ToggleFriendsFrame(1)
+		end
+	)
+	rootDescription:CreateButton(
+		ACHIEVEMENTS_GUILD_TAB,
+		function()
 			if IsInGuild() then
-				ToggleGuildFrame()
+				_G.ToggleGuildFrame()
 			else
-				ToggleGuildFinder()
+				_G.ToggleGuildFinder()
 			end
-		end,
-		notCheckable = true
-	},
-	{
-		text = COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE .. " / " .. COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVP,
-		func = function()
-			PVEFrame_ToggleFrame()
-		end,
-		notCheckable = true
-	},
-	{
-		text = RAID,
-		func = function()
-			ToggleFriendsFrame(3)
-		end,
-		notCheckable = true
-	},
-	{
-		text = ENCOUNTER_JOURNAL,
-		func = function()
-			ToggleEncounterJournal()
-		end,
-		notCheckable = true
-	},
-	{
-		text = HELP_BUTTON,
-		func = function()
-			ToggleHelpFrame()
-		end,
-		notCheckable = true
-	},
-	{
-		text = "|cfffe7b2cElvUI|r",
-		func = function()
+		end
+	)
+	rootDescription:CreateButton(
+		COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE .. " / " .. COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVP,
+		function()
+			_G.PVEFrame_ToggleFrame()
+		end
+	)
+	rootDescription:CreateButton(
+		RAID,
+		function()
+			_G.ToggleFriendsFrame(3)
+		end
+	)
+	rootDescription:CreateButton(
+		ENCOUNTER_JOURNAL,
+		function()
+			_G.ToggleEncounterJournal()
+		end
+	)
+	rootDescription:CreateButton(
+		HELP_BUTTON,
+		function()
+			_G.ToggleHelpFrame()
+		end
+	)
+	rootDescription:CreateButton(
+		"|cfffe7b2cElvUI|r",
+		function()
 			E:ToggleOptions()
-		end,
-		notCheckable = true
-	},
-	{
-		text = W.Title,
-		func = function()
+		end
+	)
+	rootDescription:CreateButton(
+		W.Title,
+		function()
 			E:ToggleOptions("WindTools")
-		end,
-		notCheckable = true
-	}
-}
+		end
+	)
+end
 
 local function OnEvent(self, event, unit)
 	self.text:SetFormattedText("%s", MAINMENU_BUTTON)
@@ -151,7 +125,7 @@ end
 
 local function OnClick(self, button)
 	if button == "LeftButton" then
-		EasyMenu(microMenu, menuFrame, "cursor", -50, 220, "MENU")
+		MenuUtil.CreateContextMenu(self, GenerateDayContextMenu)
 	end
 
 	if button == "RightButton" then

6ff9d182db48ddf27a96fb35ddf3b2e753f25ff4
Zhou Fang <fang2hou@outlook.com>
2024-08-08 06:37:16 +0900

    fix: update copy chat frame with latest elvui change

--- Modules/Skins/ElvUI/CopyChatFrame.lua
+++ Modules/Skins/ElvUI/CopyChatFrame.lua
@@ -1,5 +1,6 @@
 local W, F, E, L = unpack((select(2, ...)))
 local S = W.Modules.Skins
+local CH = E:GetModule("Chat")
 
 local _G = _G
 
@@ -8,8 +9,13 @@ function S:ElvUICopyChatFrame()
         return
     end
 
-    self:CreateShadow(_G.CopyChatFrame)
-    F.SetFontOutline(_G.CopyChatFrameEditBox)
+    if CH and CH.CopyChatFrame then
+        self:CreateShadow(CH.CopyChatFrame)
+    end
+
+    if CH and CH.CopyChatFrameEditBox then
+        F.SetFontOutline(CH.CopyChatFrameEditBox)
+    end
 end
 
 S:AddCallback("ElvUICopyChatFrame")

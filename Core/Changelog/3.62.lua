78550be555af40d807ddeccf0529d36ce176eb37
Zhou Fang <fang2hou@outlook.com>
2024-08-10 17:46:59 +0900

    docs: add 3.62 changelog

---
+++ Core/Changelog/3.62.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[362] = {
+    RELEASE_DATE = "TBD",
+    IMPORTANT = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[美化皮肤] 修复 Myslot 插件美化皮肤。"
+        },
+        ["zhTW"] = {
+            "[美化皮膚] 修復 Myslot 插件美化皮膚。"
+        },
+        ["enUS"] = {
+            "[Skins] Fixed Myslot skin."
+        },
+        ["koKR"] = {
+            "[스킨] Myslot 스킨이 수정되었습니다."
+        },
+        ["ruRU"] = {
+            "[Скины] Исправлен скин Myslot."
+        }
+    }
+}

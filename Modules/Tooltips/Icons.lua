07bca512d39d5670944c7dbe8e4be93532f29b1e
geeet1 <120192641+geeet1@users.noreply.github.com>
2024-08-15 19:56:40 +0200

    Update Icons.lua for API changes
    Fix deprecated GetItemIcon function

--- Modules/Tooltips/Icons.lua
+++ Modules/Tooltips/Icons.lua
@@ -10,7 +10,7 @@ local tonumber = tonumber
 local unpack = unpack
 
 local GetAchievementInfo = GetAchievementInfo
-local GetItemIcon = GetItemIcon
+local GetItemIcon = C_Item.GetItemIconByID
 local C_Spell_GetSpellTexture = C_Spell.GetSpellTexture
 local UnitBattlePetSpeciesID = UnitBattlePetSpeciesID
 local UnitBattlePetType = UnitBattlePetType

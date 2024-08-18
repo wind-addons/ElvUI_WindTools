108bdc49e701ff967af2e244a1318dcff7818f3a
Zhou Fang <fang2hou@outlook.com>
2024-08-18 17:21:33 +0900

    feat: add more checks

--- Modules/Misc/CustomHotKeyAlias.lua
+++ Modules/Misc/CustomHotKeyAlias.lua
@@ -8,18 +8,24 @@ local buttons = {}
 
 local function SetHotKeyText(_, button)
     if not button or not button.HotKey or not button.HotKey.GetText then
-        return
+        return false
     end
 
     -- cache the original hotkey text
     if not buttons[button] then
-        buttons[button] = button.HotKey:GetText()
+        local text = button.HotKey:GetText()
+        if text and text ~= _G.RANGE_INDICATOR then
+            buttons[button] = text
+        end
     end
 
     local bind = buttons[button]
+    if not bind then
+        return false
+    end
 
     if E.db and E.db.WT and E.db.WT.misc and E.db.WT.misc.customHotKeyAlias and E.db.WT.misc.customHotKeyAlias.enable then
-        if E.db.WT.misc.customHotKeyAlias.list[bind] then
+        if E.db.WT.misc.customHotKeyAlias.list and E.db.WT.misc.customHotKeyAlias.list[bind] then
             button.HotKey:SetText(E.db.WT.misc.customHotKeyAlias.list[bind])
             return true
         end
@@ -28,6 +34,7 @@ local function SetHotKeyText(_, button)
     button.HotKey:SetText(bind)
 end
 
+-- hijack the AB.FixKeybindText function
 local AB_FixKeybindText = AB.FixKeybindText
 AB.FixKeybindText = function(...)
     if not SetHotKeyText(...) then

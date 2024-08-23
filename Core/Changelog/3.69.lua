82604769b2a418d23d938e93b4ffb256b98e015c
Zhou Fang <fang2hou@outlook.com>
2024-08-24 06:27:52 +0900

    docs: changelog

---
+++ Core/Changelog/3.69.lua
@@ -0,0 +1,68 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[369] = {
+	RELEASE_DATE = "Alpha",
+	IMPORTANT = {
+		["zhCN"] = {
+			"联动 ElvUI 进行了性能优化，该改动已经包含在 ElvUI 13.75 版本中，请尽快更新. 感谢 Simpy",
+			"大幅优化了游戏条的动画表现，可提升约 6% 左右的帧数。",
+			"使用 ElvUI 内的 LibAnim 重构了组件动画系统，动画时的帧数提升约 5%。",
+		},
+		["zhTW"] = {
+			"聯動 ElvUI 進行了性能優化，該改動已經包含在 ElvUI 13.75 版本中，请盡快更新。 感謝 Simpy",
+			"大幅改善了遊戲條的動畫表現，可提升約 6% 左右的幀數。",
+			"使用 ElvUI 內的 LibAnim 重構了組件動畫系統，動畫時的幀數提升約 5%。",
+		},
+		["enUS"] = {
+			"Performance optimizations have been made in the ElvUI. The change has been included in ElvUI 13.75. Please update. Thank you Simpy",
+			"Large performance improvements have been made to the game bar. The frame rate increase is about 6%.",
+			"The widget animation system has been reworked. The frame rate increase expected is about 5%.",
+		},
+		["koKR"] = {
+			"ElvUI에서 성능 최적화가 이루어졌습니다. 이 변경 사항은 ElvUI 13.75에 포함되어 있습니다. 업데이트 해주세요. 감사합니다 Simpy",
+			"게임 바에 대한 성능 개선이 크게 이루어졌습니다. 프레임 속도가 약 6% 증가합니다.",
+			"이동화 실제 시스템 플러그를 재사용해야 합니다. 프레임 속도가 약 5% 증가합니다.",
+		},
+		["ruRU"] = {
+			"В ElvUI была проведена оптимизация производительности. Изменение было включено в ElvUI 13.75. Пожалуйста, обновитесь. Спасибо Simpy",
+			"Были сделаны значительные улучшения производительности панели игры. Увеличение частоты кадров составляет около 6%.",
+			"Система анимации была переработана. Ожидаемое увеличение частоты кадров составляет около 5%.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 优化了成就界面的皮肤。",
+			"[美化皮肤] 修复了 RareScanner 皮肤。感谢 DakJaniels",
+			"[小地图按钮] 优化了小地图按钮的背景移除逻辑，支持管理更多插件的小地图图标。",
+			"[事件追踪器] 默认关闭部分事件的追踪。",
+		},
+		["zhTW"] = {
+			"[美化皮肤] 修復了成就界面的皮膚。",
+			"[美化皮肤] 修復了 RareScanner 皮肤。感謝 DakJaniels",
+			"[小地圖按鍵] 修復了小地圖按鈕的背景移除邏輯，支持管理更多插件的小地圖圖標。",
+			"[事件追蹤器] 預設關閉部分事件的追蹤。",
+		},
+		["enUS"] = {
+			"[Skins] Optimized Achievement skin.",
+			"[Skins] Fixed RareScanner skin. Thank you, DakJaniels",
+			"[Minimap Buttons] Optimized Minimap Button background removal logic, support for managing more addons' minimap icons.",
+		},
+		["koKR"] = {
+			"[스킨] 업적 스킨을 최적화했습니다.",
+			"[스킨] RareScanner 스킨이 수정되었습니다. 감사합니다, DakJaniels",
+			"[미니맵 버튼] 미니맵 버튼 배경 제거 로직을 최적화했습니다. 더 많은 애드온의 미니맵 아이콘 관리를 지원합니다.",
+		},
+		["ruRU"] = {
+			"[Скины] Оптимизирован скин достижений.",
+			"[Скины] Исправлен скин RareScanner. Спасибо, DakJaniels",
+			"[Кнопки миникарты] Оптимизирована логика удаления фона кнопки миникарты, поддержка управления значками миникарты большего количества аддонов.",
+		},
+	},
+}

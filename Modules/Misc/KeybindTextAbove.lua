06cd66a841b40e16fdddb2b150a04eeea4f772fb
Zhou Fang <fang2hou@outlook.com>
2024-08-24 12:32:55 +0900

    feat: HotKeyAboveCD -> KeybindTextAbove

--- Modules/Misc/KeybindTextAbove.lua
+++ Modules/Misc/KeybindTextAbove.lua
@@ -7,30 +7,31 @@ local pairs = pairs
 
 local CreateFrame = CreateFrame
 
-local function HotKeyTweak(button)
+local function setParent(button)
 	if button.windHotKeyFrame or not button.cooldown then
 		return
 	end
 	button.windHotKeyFrame = CreateFrame("Frame", nil, button)
 	button.windHotKeyFrame:SetAllPoints(button)
 	button.windHotKeyFrame:SetFrameStrata("LOW")
-	button.windHotKeyFrame:SetFrameLevel(button.cooldown:GetFrameLevel() + 2)
+	button.windHotKeyFrame:SetFrameLevel(button.cooldown:GetFrameLevel() + 11) -- The glow is about 10
 	button.HotKey:SetParent(button.windHotKeyFrame)
 end
 
-function M:HotKeyAboveCD()
-	if not E.private.actionbar.enable or not E.db.cooldown.enable or not E.private.WT.misc.hotKeyAboveCD then
+function M:KeybindTextAbove()
+	if not E.private.actionbar.enable or not E.db.cooldown.enable or not E.private.WT.misc.keybindTextAbove then
 		return
 	end
 
 	hooksecurefunc(E, "CreateCooldownTimer", function(_, button)
 		if button and button.cooldown and AB.handledbuttons[button] then
-			HotKeyTweak(button)
+			setParent(button)
 		end
 	end)
+
 	for button in pairs(AB.handledbuttons) do
-		HotKeyTweak(button)
+		setParent(button)
 	end
 end
 
-M:AddCallback("HotKeyAboveCD")
+M:AddCallback("KeybindTextAbove")

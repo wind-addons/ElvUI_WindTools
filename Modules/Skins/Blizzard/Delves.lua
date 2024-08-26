3494304de46424c427e07319f92ded4363669785
Zhou Fang <fang2hou@outlook.com>
2024-08-27 08:55:11 +0900

    feat: move a little

--- Modules/Skins/Blizzard/Delves.lua
+++ Modules/Skins/Blizzard/Delves.lua
@@ -9,14 +9,16 @@ function S:Blizzard_DelvesCompanionConfiguration()
 	end
 
 	local CompanionConfigurationFrame = _G.DelvesCompanionConfigurationFrame
-    if CompanionConfigurationFrame then
-        self:CreateShadow(CompanionConfigurationFrame)
-    end
+	if CompanionConfigurationFrame then
+		self:CreateShadow(CompanionConfigurationFrame)
+		CompanionConfigurationFrame.CloseButton:ClearAllPoints()
+		CompanionConfigurationFrame.CloseButton:SetPoint("TOPRIGHT", CompanionConfigurationFrame, "TOPRIGHT", -3, -3)
+	end
 
-    local CompanionAbilityListFrame = _G.DelvesCompanionAbilityListFrame
-    if CompanionAbilityListFrame then
-        self:CreateShadow(CompanionAbilityListFrame)
-    end
+	local CompanionAbilityListFrame = _G.DelvesCompanionAbilityListFrame
+	if CompanionAbilityListFrame then
+		self:CreateShadow(CompanionAbilityListFrame)
+	end
 end
 
 S:AddCallbackForAddon("Blizzard_DelvesCompanionConfiguration")

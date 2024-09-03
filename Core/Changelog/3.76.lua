eab6e04cb1ac2f3cbf6a88d973bd2af47384b66c
Zhou Fang <fang2hou@outlook.com>
2024-09-04 08:39:03 +0900

    docs: changelog

---
+++ Core/Changelog/3.76.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[376] = {
+	RELEASE_DATE = "Alpha",
+	IMPORTANT = {
+		["zhCN"] = {
+			"从此版本开始，如果你没有在最新版本的更新记录中点击确认按钮，那么更新记录的阅读提示将在你每次进入游戏时出现。",
+			"[高级] 新增了切换更新记录为弹窗的选项. 默认为聊天文本模式",
+			"[高级] 新增了切换 ElvUI 版本过期提示为弹窗的选项. 默认为弹窗模式",
+		},
+		["zhTW"] = {
+			"從此版本開始，如果你沒有在最新版本的更新記錄中點擊確認按鈕，那麼更新記錄的閱讀提示將在你每次進入遊戲時出現。",
+			"[進階] 新增了切換更新記錄為彈窗的選項. 默認為聊天文本模式",
+			"[進階] 新增了切換 ElvUI 版本過期提示為彈窗的選項. 默認為彈窗模式",
+		},
+		["enUS"] = {
+			"Starting from this version, if you haven't clicked the confirm button in the latest change log, the change log reading reminder will appear every time you enter the game.",
+			"[Advanced] Add an option to switch the change log to a popup. Default to chat text mode",
+			"[Advanced] Add an option to switch the ElvUI version expiration reminder to a popup. Default to popup mode",
+		},
+		["koKR"] = {
+			"이 버전부터 최신 변경 로그에서 확인 버튼을 클릭하지 않은 경우, 변경 로그 읽기 알림이 게임에 들어갈 때마다 표시됩니다.",
+			"[고급] 업데이트 로그를 팝업으로 전환하는 옵션을 추가합니다. 기본값은 채팅 텍스트 모드입니다.",
+			"[고급] ElvUI 버전 만료 알림을 팝업으로 전환하는 옵션을 추가합니다. 기본값은 팝업 모드입니다.",
+		},
+		["ruRU"] = {
+			"Начиная с этой версии, если вы не нажали кнопку подтверждения в последнем журнале изменений, напоминание о прочтении журнала изменений будет появляться каждый раз при входе в игру.",
+			"[Advanced] Add an option to switch the change log to a popup. Default to chat text mode",
+			"[Advanced] Add an option to switch the ElvUI version expiration reminder to a popup. Default to popup mode",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+}

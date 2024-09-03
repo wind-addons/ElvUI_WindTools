3abc3ae08c8f86d75099192c0e0c3a8c23c5f630
Zhou Fang <fang2hou@outlook.com>
2024-09-04 00:30:13 +0900

    docs: changelog

---
+++ Core/Changelog/3.75.lua
@@ -0,0 +1,51 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[375] = {
+	RELEASE_DATE = "2024/09/03",
+	IMPORTANT = {
+		["zhCN"] = {
+			"新发布一个临时插件用来解决跨阵营公会的成就系统，Curse 搜索 [Guild Achievements Fix] 下载！",
+			"[高级] 由于暴雪已经修复 Bug，移除了 PlayStyleString 修复功能。",
+		},
+		["zhTW"] = {
+			"新發布一個臨時插件用來解決跨陣營公會的成就系統，Curse 搜索 [Guild Achievements Fix] 下載！",
+			"[進階] 由於暴雪已經修復 Bug，移除了 PlayStyleString 修復功能。",
+		},
+		["enUS"] = {
+			"Newly released a temporarily addon to fix the cross-faction guild achievement system, search [Guild Achievements Fix] on Curse to download!",
+			"[Advanced] Removed the PlayStyleString fix feature as Blizzard has fixed the bug.",
+		},
+		["koKR"] = {
+			"신규로 출시된 애드온으로, 교전 진영 길드 업적 시스템을 일시적으로 수정합니다. Curse 에서 [Guild Achievements Fix] 를 검색하여 다운로드하세요!",
+			"[고급] 블리자드가 버그를 수정했기 때문에 PlayStyleString 수정 기능을 제거했습니다.",
+		},
+		["ruRU"] = {
+			"Недавно выпущен аддон для временного исправления системы достижений гильдий между фракциями, поиск [Guild Achievements Fix] на Curse для загрузки!",
+			"[Расширенный] Удалена функция исправления PlayStyleString, так как Blizzard исправила ошибку.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[其他] - [自动截图] 修复截图过快可能导致的闪光动画被截图的问题。",
+		},
+		["zhTW"] = {
+			"[其他] - [自動截圖] 修復截圖過快可能導致的閃光動畫被截圖的問題。",
+		},
+		["enUS"] = {
+			"[Misc] - [Auto Screenshot] Fixed the issue that the flash animation was captured by the screenshot due to the screenshot being too fast.",
+		},
+		["koKR"] = {
+			"[기타] - [자동 스크린샷] 스크린샷이 너무 빨라서 플래시 애니메이션이 스크린샷에 포함되는 문제를 수정했습니다.",
+		},
+		["ruRU"] = {
+			"[Разное] - [Автоматический скриншот] Исправлена проблема, когда вспышка анимации была захвачена скриншотом из-за слишком быстрого скриншота.",
+		},
+	},
+}

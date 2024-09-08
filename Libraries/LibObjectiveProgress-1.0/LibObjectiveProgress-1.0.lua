35cee738836563d426cdb5c7b09a9e3d4af892ee
Zhou Fang <fang2hou@outlook.com>
2024-09-08 23:07:27 +0900

    feat: update LibObjectiveProgress

--- Libraries/LibObjectiveProgress-1.0/LibObjectiveProgress-1.0.lua
+++ Libraries/LibObjectiveProgress-1.0/LibObjectiveProgress-1.0.lua
@@ -4,7 +4,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 ]]--
 
 -- Set major/minor version
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 67
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", 72
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library

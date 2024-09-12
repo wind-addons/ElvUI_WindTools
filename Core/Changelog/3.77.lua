45747e4f5a60e154606db6554c5d05ab0a9565d2
Zhou Fang <fang2hou@outlook.com>
2024-09-12 09:22:59 +0900

    docs: changelog

---
+++ Core/Changelog/3.77.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[377] = {
+	RELEASE_DATE = "Alpha",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[额外物品条] 新增 FISHING, FISHINGTWW 钓鱼物品分组。感谢 LvWind",
+		},
+		["zhTW"] = {
+			"[額外物品條] 新增 FISHING, FISHINGTWW 釣魚物品分組。感謝 LvWind",
+		},
+		["enUS"] = {
+			"[Extra Item Bar] Added FISHING, FISHINGTWW fishing item groups. Thanks LvWind",
+		},
+		["koKR"] = {
+			"[추가 아이템 바] FISHING, FISHINGTWW 낚시 아이템 그룹 추가. 감사합니다 LvWind",
+		},
+		["ruRU"] = {
+			"[Дополнительная панель предметов] Добавлены группы предметов для рыбалки FISHING, FISHINGTWW. Спасибо LvWind",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[职业助手] 灵界打击预测现在可以用在全部专精中。感谢 AngelosNaoumis",
+		},
+		["zhTW"] = {
+			"[職業助手] 死亡打擊預測現在可以用在 DK 全部專精中。感謝 AngelosNaoumis",
+		},
+		["enUS"] = {
+			"[Class Helper]  Death Strike prediction can now be used in all DK specializations. Thanks AngelosNaoumis",
+		},
+		["koKR"] = {
+			"[직업 도우미] 죽음의 일격 예측이 이제 모든 DK 전문화에서 사용할 수 있습니다. 감사합니다 AngelosNaoumis",
+		},
+		["ruRU"] = {
+			"[Помощник по классу] Прогнозирование удара смерти теперь можно использовать во всех специализациях DK. Спасибо AngelosNaoumis",
+		},
+	},
+}

05e9bb4b3bfb1ca0d33f059103104707b0e8bac1
Zhou Fang <fang2hou@outlook.com>
2024-09-15 11:06:54 +0900

    feat: use mdt to fetch progress in dungeon

--- Modules/Tooltips/ObjectiveProgress.lua
+++ Modules/Tooltips/ObjectiveProgress.lua
@@ -3,18 +3,17 @@ local T = W.Modules.Tooltips
 local LOP = E.Libs.LOP
 
 local _G = _G
-local floor = floor
 local format = format
 local next = next
 local select = select
 local strsplit = strsplit
 local tonumber = tonumber
-local tostring = tostring
 
-local UnitGUID = UnitGUID
 local C_QuestLog_GetInfo = C_QuestLog.GetInfo
 local C_QuestLog_GetLogIndexForQuestID = C_QuestLog.GetLogIndexForQuestID
 local TooltipDataProcessor_AddTooltipPostCall = TooltipDataProcessor.AddTooltipPostCall
+
+local GameTooltip = _G.GameTooltip
 
 local Enum_TooltipDataType_Unit = Enum.TooltipDataType.Unit
 
@@ -31,18 +30,34 @@ local function addObjectiveProgress(tt, data)
 		return
 	end
 
-	local weightsTable = LOP:GetNPCWeightByCurrentQuests(tonumber(npcID))
-	if not weightsTable then
-		return
+	npcID = tonumber(npcID)
+
+	local weightsTable = LOP:GetNPCWeightByCurrentQuests(npcID)
+	if weightsTable then
+		for questID, npcWeight in next, weightsTable do
+			local info = C_QuestLog_GetInfo(C_QuestLog_GetLogIndexForQuestID(questID))
+			for i = 1, tt:NumLines() do
+				local text = _G["GameTooltipTextLeft" .. i]
+				if text and text:GetText() == info.title then
+					text:SetText(text:GetText() .. format(" + %s%%", F.Round(npcWeight, accuracy)))
+				end
+			end
+		end
 	end
 
-	for questID, npcWeight in next, weightsTable do
-		local info = C_QuestLog_GetInfo(C_QuestLog_GetLogIndexForQuestID(questID))
-		for i = 1, tt:NumLines() do
-			local text = _G["GameTooltipTextLeft" .. i]
-			if text and text:GetText() == info.title then
-				text:SetText(text:GetText() .. format(" + %s%%", F.Round(npcWeight, accuracy)))
-			end
+	if _G.MDT and _G.MDT.GetEnemyForces then
+		local count, max = _G.MDT:GetEnemyForces(npcID)
+
+		if count and max and count > 0 and max > 0 then
+			local left =
+				format("%s |cff00d1b2%s|r |cffffffff-|r |cffffdd57%s|r", F.GetIconString(132147, 14), count, max)
+			local right = format(
+				"%s |cff209cee%s|r|cffffffff%%|r",
+				F.GetIconString(5926319, 14),
+				F.Round(100 * count / max, accuracy)
+			)
+			GameTooltip:AddDoubleLine(left, right)
+			GameTooltip:Show()
 		end
 	end
 end

29c6a9ee87bba688274b4017ffd873f279ced2d1
Zhou Fang <fang2hou@outlook.com>
2024-10-05 07:26:09 +0900

    docs: changelog

---
+++ Core/Changelog/3.82.lua
@@ -0,0 +1,51 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[382] = {
+	RELEASE_DATE = "2024/10/05",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[世界地图] 缩放现在不会作用在全屏模式了。",
+			"[移动框体] 优化了框体位置更新的稳定性。",
+			"[额外物品条] 更新了大餐列表。感谢 mcc1",
+			"[额外物品条] 更新了传送门列表。感谢 mcc1",
+		},
+		["zhTW"] = {
+			"[世界地圖] 縮放現在不會作用在全屏模式了。",
+			"[移動框架] 優化了框架位置更新的穩定性。",
+			"[額外物品條] 更新了大餐列表。感謝 mcc1",
+			"[額外物品條] 更新了傳送門列表。感謝 mcc1",
+		},
+		["enUS"] = {
+			"[World Map] World Map Scale will no longer affect full screen mode.",
+			"[Move Frames] Optimize the stability of frame position updates.",
+			"[Extra Item Bar] Update the feast list. Thanks mcc1",
+			"[Extra Item Bar] Update the portal list. Thanks mcc1",
+		},
+		["koKR"] = {
+			"[세계 지도] 세계 지도 크기는 더 이상 전체 화면 모드에 영향을 미치지 않습니다.",
+			"[프레임 이동] 프레임 위치 업데이트의 안정성을 최적화했습니다.",
+			"[추가 아이템 바] 연회 목록을 업데이트했습니다. 감사합니다 mcc1",
+			"[추가 아이템 바] 포탈 목록을 업데이트했습니다. 감사합니다 mcc1",
+		},
+		["ruRU"] = {
+			"[World Map] World Map Scale will no longer affect full screen mode.",
+			"[Move Frames] Optimize the stability of frame position updates.",
+			"[Extra Item Bar] Update the feast list. Thanks mcc1",
+			"[Extra Item Bar] Update the portal list. Thanks mcc1",
+		},
+	},
+}

40e0da5b96f125184524764175b793dbb0966735
Zhou Fang <fang2hou@outlook.com>
2024-11-02 08:13:23 +0900

    docs: changelog

---
+++ Core/Changelog/3.83.lua
@@ -0,0 +1,56 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[383] = {
+    RELEASE_DATE = "2025/11/02",
+    IMPORTANT = {
+        ["zhCN"] = {
+            "更新 LibOpenLib 到最新版本。"
+        },
+        ["zhTW"] = {
+            "更新 LibOpenLib 到最新版本。"
+        },
+        ["enUS"] = {
+            "Updated LibOpenLib to the latest version."
+        },
+        ["koKR"] = {
+            "LibOpenLib 최신 버전으로 업데이트했습니다."
+        },
+        ["ruRU"] = {
+            "Обновлена LibOpenLib до последней версии."
+        },
+    },
+    NEW = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+    },
+    IMPROVEMENT = {
+        ["zhCN"] = {
+            "[预组列表] 修复了词缀显示不正确的问题。",
+            "[观察] 修复了可能导致 Lua 错误的问题。",
+            "[额外物品条] 更新了物品列表。感谢 mcc1",
+        },
+        ["zhTW"] = {
+            "[預組隊列表] 修復了詞綴顯示不正確的問題。",
+            "[觀察] 修復了可能導致 Lua 錯誤的問題。",
+            "[額外物品條] 更新了物品列表。感謝 mcc1",
+        },
+        ["enUS"] = {
+            "[LFG List] Fixed an issue with incorrect affix display.",
+            "[Inspect] Fixed a potential Lua error.",
+            "[Extra Item Bar] Updated item list. Thanks to mcc1",
+        },
+        ["koKR"] = {
+            "[파티 찾기 목록] 잘못된 속성 표시 문제를 수정했습니다.",
+            "[검사] 잠재적인 Lua 오류를 수정했습니다.",
+            "[추가 아이템 바] 아이템 목록을 업데이트했습니다. mcc1에게 감사드립니다.",
+        },
+        ["ruRU"] = {
+            "[Список LFG] Исправлена проблема с неправильным отображением аффиксов.",
+            "[Осмотр] Исправлена потенциальная ошибка Lua.",
+            "[Дополнительная панель предметов] Обновлен список предметов. Спасибо mcc1.",
+        },
+    },
+}

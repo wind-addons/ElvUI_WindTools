5b95873daf3d308a8d9eac93621ef5f7af447bf4
Zhou Fang <fang2hou@outlook.com>
2024-11-07 21:59:26 +0900

    docs: 3.84

---
+++ Core/Changelog/3.84.lua
@@ -0,0 +1,61 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[384] = {
+	RELEASE_DATE = "2024/11/07",
+	IMPORTANT = {
+		["zhCN"] = {
+			"ElvUI 最低支持版本更新到 13.80。",
+		},
+		["zhTW"] = {
+			"ElvUI 最低支援版本更新到 13.80。",
+		},
+		["enUS"] = {
+			"ElvUI minimum supported version updated to 13.80.",
+		},
+		["koKR"] = {
+			"ElvUI 최소 지원 버전이 13.80으로 업데이트되었습니다.",
+		},
+		["ruRU"] = {
+			"Минимальная поддерживаемая версия ElvUI обновлена до 13.80.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[美化皮肤] 新增对日历事件创建界面美化。",
+		},
+		["zhTW"] = {
+			"[美化皮膚] 新增對行事曆事件創建介面美化。",
+		},
+		["enUS"] = {
+			"[Skin] Add skin for the calendar event creation window.",
+		},
+		["koKR"] = {
+			"[스킨] 캘린더 이벤트 생성 창에 스킨 추가.",
+		},
+		["ruRU"] = {
+			"[Скин] Добавлен скин для окна создания события календаря.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[鼠标提示] 更新部分大秘境计算数据。",
+			"[美化皮肤] WarpDeplete 美化皮肤支持 v4 版本。",
+		},
+		["zhTW"] = {
+			"[滑鼠提示] 更新部分大秘境計算數據。",
+			"[美化皮膚] WarpDeplete 美化皮膚支援 v4 版本。",
+		},
+		["enUS"] = {
+			"[Tooltip] Update some Mythic+ calculation data.",
+			"[Skin] WarpDeplete skin now supports v4.",
+		},
+		["koKR"] = {
+			"[툴팁] 일부 던전+ 계산 데이터를 업데이트했습니다.",
+			"[스킨] WarpDeplete 스킨이 이제 v4를 지원합니다.",
+		},
+		["ruRU"] = {
+			"[Подсказка] Обновлены некоторые данные расчета Мифик+.",
+			"[Скин] Скин WarpDeplete теперь поддерживает v4.",
+		},
+	},
+}

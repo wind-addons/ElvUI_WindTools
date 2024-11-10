d8838a441fc5e7cc8bb7349310ff1dd494407dee
Zhou Fang <fang2hou@outlook.com>
2024-11-10 12:54:24 +0900

    docs: changelog

--- Core/Changelog/3.85.lua
+++ Core/Changelog/3.85.lua
@@ -4,19 +4,19 @@ W.Changelog[385] = {
 	RELEASE_DATE = "2024/11/10",
 	IMPORTANT = {
 		["zhCN"] = {
-			"更新 OpenRaid 库。"
+			"更新 OpenRaid 库。",
 		},
 		["zhTW"] = {
-			"更新 OpenRaid 庫。"
+			"更新 OpenRaid 庫。",
 		},
 		["enUS"] = {
-			"Update OpenRaid library."
+			"Update OpenRaid library.",
 		},
 		["koKR"] = {
-			"OpenRaid 라이브러리 업데이트."
+			"OpenRaid 라이브러리 업데이트.",
 		},
 		["ruRU"] = {
-			"Обновление библиотеки OpenRaid."
+			"Обновление библиотеки OpenRaid.",
 		},
 	},
 	NEW = {
@@ -29,23 +29,28 @@ W.Changelog[385] = {
 	IMPROVEMENT = {
 		["zhCN"] = {
 			"[美化皮肤] 更新 OmniCD 美化皮肤。",
-			"[额外物品条] 更新种子 SEEDS 分组。"
+			"[额外物品条] 更新种子 SEEDS 分组。",
+			"[观察] 修复可能产生的错误。",
 		},
 		["zhTW"] = {
 			"[美化皮膚] 更新 OmniCD 美化皮膚。",
-			"[額外物品條] 更新種子 SEEDS 分組。"
+			"[額外物品條] 更新種子 SEEDS 分組。",
+			"[觀察] 修復可能產生的錯誤。",
 		},
 		["enUS"] = {
 			"[Skin] Update OmniCD skin.",
-			"[Extra Items Bar] Update SEEDS group."
+			"[Extra Items Bar] Update SEEDS group.",
+			"[Inspect] Fix errors that may occur.",
 		},
 		["koKR"] = {
 			"[스킨] OmniCD 스킨 업데이트.",
-			"[추가 아이템 바] SEEDS 그룹 업데이트."
+			"[추가 아이템 바] SEEDS 그룹 업데이트.",
+			"[검사] 발생할 수 있는 오류 수정.",
 		},
 		["ruRU"] = {
 			"[Скин] Обновление скина OmniCD.",
-			"[Дополнительная панель предметов] Обновление группы SEEDS."
+			"[Дополнительная панель предметов] Обновление группы SEEDS.",
+			"[Инспекция] Исправление ошибок, которые могут возникнуть.",
 		},
 	},
 }

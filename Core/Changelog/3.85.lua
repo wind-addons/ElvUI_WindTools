22c226084a3a7c346470d505940b1f3a3c79ec7c
Zhou Fang <fang2hou@outlook.com>
2024-11-10 12:37:13 +0900

    docs: update changelog

---
+++ Core/Changelog/3.85.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[385] = {
+	RELEASE_DATE = "2024/11/10",
+	IMPORTANT = {
+		["zhCN"] = {
+			"更新 OpenRaid 库。"
+		},
+		["zhTW"] = {
+			"更新 OpenRaid 庫。"
+		},
+		["enUS"] = {
+			"Update OpenRaid library."
+		},
+		["koKR"] = {
+			"OpenRaid 라이브러리 업데이트."
+		},
+		["ruRU"] = {
+			"Обновление библиотеки OpenRaid."
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 更新 OmniCD 美化皮肤。",
+		},
+		["zhTW"] = {
+			"[美化皮膚] 更新 OmniCD 美化皮膚。",
+		},
+		["enUS"] = {
+			"[Skin] Update OmniCD skin.",
+		},
+		["koKR"] = {
+			"[스킨] OmniCD 스킨 업데이트.",
+		},
+		["ruRU"] = {
+			"[Скин] Обновление скина OmniCD.",
+		},
+	},
+}

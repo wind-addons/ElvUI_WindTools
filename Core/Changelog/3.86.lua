f0614a98797a493ca25699cd46150c58ef1ef34d
Zhou Fang <fang2hou@outlook.com>
2024-11-17 00:03:49 +0900

    docs: changelog

--- Core/Changelog/3.86.lua
+++ Core/Changelog/3.86.lua
@@ -5,23 +5,23 @@ W.Changelog[386] = {
 	IMPORTANT = {
 		["zhCN"] = {
 			"在 [高级] 中新增了 [公会新闻更新修复]，默认开启，预计能大幅提升在公会人数非常多的角色的游戏帧数。感谢 老李三鹿李@NGA",
-			"部分代码清理。"
+			"部分代码清理。",
 		},
 		["zhTW"] = {
 			"在 [進階] 中新增了 [公會新聞更新修復]，默認開啓，預計能大幅提升在公會人數非常多的角色的遊戲幀數。感謝 老李三鹿李@NGA",
-			"部分代碼清理。"
+			"部分代碼清理。",
 		},
 		["enUS"] = {
 			"Added [Guild News Update Fix] in [Advanced], enabled by default, expected to significantly increase the game frame rate of characters with a large number of guild members. Thanks to 老李三鹿李@NGA",
-			"Partial code cleanup."
+			"Partial code cleanup.",
 		},
 		["koKR"] = {
 			"[고급]에 [Guild News Update Fix]을 추가했습니다. 기본적으로 활성화되며, 길드 멤버가 많은 캐릭터의 게임 프레임 속도를 크게 향상시킬 것으로 예상됩니다. 감사합니다. 老李三鹿李@NGA",
-			"일부 코드 정리."
+			"일부 코드 정리.",
 		},
 		["ruRU"] = {
 			"Добавлено [Guild News Update Fix] в [Расширенные], включено по умолчанию, ожидается значительное увеличение частоты кадров игры у персонажей с большим количеством членов гильдии. Спасибо 老李三鹿李@NGA",
-			"Частичная очистка кода."
+			"Частичная очистка кода.",
 		},
 	},
 	NEW = {
@@ -34,18 +34,28 @@ W.Changelog[386] = {
 	IMPROVEMENT = {
 		["zhCN"] = {
 			"[额外物品条] 更新了物品列表。感谢 mcc1",
+			"[美化皮肤] 在启用超级追踪时，RareScanner的搜索框将会和超级追踪的搜索框排列在一起并获得全新的风格美化。",
+			"[美化皮肤] OmniCD 美化皮肤的条背景现在会跟随 OmniCD 的隐藏边框选项自动隐藏了。",
 		},
 		["zhTW"] = {
 			"[額外物品條] 更新了物品列表。感謝 mcc1",
+			"[美化皮膚] 在啟用超級追踪時，RareScanner的搜索框將會和超級追踪的搜索框排列在一起並獲得全新的風格美化。",
+			"[美化皮膚] OmniCD 美化皮膚的條背景現在會跟隨 OmniCD 的隱藏邊框選項自動隱藏了。",
 		},
 		["enUS"] = {
 			"[Extra Item Bar] Updated item list. Thanks to mcc1",
+			"[Skins] When SuperTracker is enabled, the search box of RareScanner will be arranged together with the waypoint input box of SuperTracker and get a brand new style.",
+			"[Skins] The bar background of OmniCD skin will now automatically hide with OmniCD's hide border option.",
 		},
 		["koKR"] = {
 			"[추가 아이템 바] 아이템 목록을 업데이트했습니다. mcc1에게 감사드립니다.",
+			"[스킨] SuperTracker가 활성화되어 있을 때 RareScanner의 검색 상자가 SuperTracker의 웨이포인트 입력 상자와 함께 배열되고 새로운 스타일을 얻습니다.",
+			"[스킨] OmniCD 스킨의 바 배경은 이제 OmniCD의 테두리 숨김 옵션과 함께 자동으로 숨겨집니다.",
 		},
 		["ruRU"] = {
 			"[Дополнительная панель предметов] Обновлен список предметов. Спасибо mcc1.",
+			"[Скин] Когда включен SuperTracker, поле поиска RareScanner будет расположено вместе с полем ввода путевой точки SuperTracker и получит совершенно новый стиль.",
+			"[Скин] Фон панели скина OmniCD теперь автоматически скрывается с опцией скрытия границ OmniCD.",
 		},
 	},
 }

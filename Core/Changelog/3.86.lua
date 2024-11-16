e27acb3fd7d38545ea49fd5609f0c6b2bb60b2ed
Zhou Fang <fang2hou@outlook.com>
2024-11-16 23:37:45 +0900

    docs: update changelog

---
+++ Core/Changelog/3.86.lua
@@ -0,0 +1,51 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[386] = {
+	RELEASE_DATE = "2024/11/16",
+	IMPORTANT = {
+		["zhCN"] = {
+			"在 [高级] 中新增了 [公会新闻更新修复]，默认开启，预计能大幅提升在公会人数非常多的角色的游戏帧数。感谢 老李三鹿李@NGA",
+			"部分代码清理。"
+		},
+		["zhTW"] = {
+			"在 [進階] 中新增了 [公會新聞更新修復]，默認開啓，預計能大幅提升在公會人數非常多的角色的遊戲幀數。感謝 老李三鹿李@NGA",
+			"部分代碼清理。"
+		},
+		["enUS"] = {
+			"Added [Guild News Update Fix] in [Advanced], enabled by default, expected to significantly increase the game frame rate of characters with a large number of guild members. Thanks to 老李三鹿李@NGA",
+			"Partial code cleanup."
+		},
+		["koKR"] = {
+			"[고급]에 [Guild News Update Fix]을 추가했습니다. 기본적으로 활성화되며, 길드 멤버가 많은 캐릭터의 게임 프레임 속도를 크게 향상시킬 것으로 예상됩니다. 감사합니다. 老李三鹿李@NGA",
+			"일부 코드 정리."
+		},
+		["ruRU"] = {
+			"Добавлено [Guild News Update Fix] в [Расширенные], включено по умолчанию, ожидается значительное увеличение частоты кадров игры у персонажей с большим количеством членов гильдии. Спасибо 老李三鹿李@NGA",
+			"Частичная очистка кода."
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[额外物品条] 更新了物品列表。感谢 mcc1",
+		},
+		["zhTW"] = {
+			"[額外物品條] 更新了物品列表。感謝 mcc1",
+		},
+		["enUS"] = {
+			"[Extra Item Bar] Updated item list. Thanks to mcc1",
+		},
+		["koKR"] = {
+			"[추가 아이템 바] 아이템 목록을 업데이트했습니다. mcc1에게 감사드립니다.",
+		},
+		["ruRU"] = {
+			"[Дополнительная панель предметов] Обновлен список предметов. Спасибо mcc1.",
+		},
+	},
+}

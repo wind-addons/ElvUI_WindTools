3eb318d6c7f575eee77779d1aaaafd792e357910
Zhou Fang <fang2hou@outlook.com>
2024-11-22 14:21:54 +0900

    docs: changelog

---
+++ Core/Changelog/3.87.lua
@@ -0,0 +1,66 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[387] = {
+	RELEASE_DATE = "2024/11/22",
+	IMPORTANT = {
+		["zhCN"] = {
+			"在 [信息] 中，现在会展示大于 30 CNY (5 USD) 捐助者的姓名。",
+			"代码整理。",
+			"本地化文本更新。",
+		},
+		["zhTW"] = {
+			"在 [信息] 中，現在會展示大於 30 CNY (5 USD) 捐助者的姓名。",
+			"代碼整理。",
+			"本地化文本更新。",
+		},
+		["enUS"] = {
+			"In [Information], now will show the name of donors who donate more than 30 CNY (5 USD).",
+			"Code cleanup.",
+			"Update localizations.",
+		},
+		["koKR"] = {
+			"[정보]에서 30 CNY (5 USD) 이상 기부한 기부자의 이름이 표시됩니다.",
+			"코드 정리.",
+			"로컬라이제이션 업데이트.",
+		},
+		["ruRU"] = {
+			"В [Информации] теперь будет отображаться имя доноров, которые пожертвовали более 30 CNY (5 USD).",
+			"Очистка кода.",
+			"Обновление локализации.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[高级] 新增一个高级战斗日志事件增强功能，可在使用 /etrace 时获取更加详细的信息。感谢 DakJaniels",
+		},
+		["zhTW"] = {
+			"[進階] 新增一個進階戰鬥日誌事件增強功能，可在使用 /etrace 時獲取更加詳細的訊息。感謝 DakJaniels",
+		},
+		["enUS"] = {
+			"[Advanced] Add a new CLEU event enhancement feature, which can get more detailed information when using /etrace. Thanks DakJaniels",
+		},
+		["koKR"] = {
+			"[고급] /etrace 사용 시 더 자세한 정보를 얻을 수 있는 새로운 CLEU 이벤트 향상 기능 추가. 감사합니다 DakJaniels",
+		},
+		["ruRU"] = {
+			"[Продвинутый] Добавлена новая функция улучшения событий CLEU, которая позволяет получать более подробную информацию при использовании /etrace. Спасибо DakJaniels",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 优化了 OmniCD 皮肤在旧版本上的稳定性。",
+		},
+		["zhTW"] = {
+			"[美化皮膚] 優化了 OmniCD 皮膚在舊版本上的穩定性。",
+		},
+		["enUS"] = {
+			"[Skin] Optimized the stability of the OmniCD skin on old versions.",
+		},
+		["koKR"] = {
+			"[스킨] 이전 버전에서 OmniCD 스킨의 안정성을 최적화했습니다.",
+		},
+		["ruRU"] = {
+			"[Скин] Оптимизирована стабильность скина OmniCD на старых версиях.",
+		},
+	},
+}

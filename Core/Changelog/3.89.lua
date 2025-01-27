e1c98892e737be76ee06e94d052a02e7d50eeb83
Zhou Fang <fang2hou@outlook.com>
2025-01-27 23:06:04 +0900

    docs: add changelog for 3.89

---
+++ Core/Changelog/3.89.lua
@@ -0,0 +1,71 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[389] = {
+	RELEASE_DATE = "2024/01/27",
+	IMPORTANT = {
+		["zhCN"] = {
+			"蛇年新年快乐！",
+			"LibOpenRaid 更新。",
+			"最低支持版本更新为 ElvUI 13.82。",
+			"暂时增加了 LibRangeCheck-3.0，预计在一个月后移除，请在这个期间调整您的配置移除距离相关功能的使用。",
+		},
+		["zhTW"] = {
+			"蛇年新年快樂！",
+			"LibOpenRaid 更新。",
+			"最低支持版本更新為 ElvUI 13.82。",
+			"暫時增加了 LibRangeCheck-3.0，預計在一個月後移除，請在這個期間調整您的配置移除距離相關功能的使用。",
+		},
+		["enUS"] = {
+			"Happy Year of the Snake!",
+			"LibOpenRaid updated.",
+			"Minimum supported version updated to ElvUI 13.82.",
+			"Temporarily added LibRangeCheck-3.0, it will be removed in a month, please adjust your configuration to remove the use of range-related features during this period.",
+		},
+		["koKR"] = {
+			"뱀의 해 새해 복 많이 받으세요!",
+			"LibOpenRaid 업데이트.",
+			"최소 지원 버전이 ElvUI 13.82로 업데이트되었습니다.",
+			"일시적으로 LibRangeCheck-3.0이 추가되었습니다. 한 달 후에 제거될 예정이니 이 기간 동안 거리 관련 기능 사용을 제거하도록 구성을 조정해 주십시오.",
+		},
+		["ruRU"] = {
+			"С Новым Годом Змеи!",
+			"Обновление LibOpenRaid.",
+			"Минимальная поддерживаемая версия обновлена до ElvUI 13.82.",
+			"Временно добавлен LibRangeCheck-3.0, он будет удален через месяц, пожалуйста, настройте вашу конфигурацию для удаления использования функций, связанных с диапазоном, в течение этого периода.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[标签] 新增了距离范围标签 [range]。（例：10-40）。但是暴雪限制下几乎不可用，只是为了防止部分框架显示出错而暂时添加。",
+		},
+		["zhTW"] = {
+			"[標籤] 新增了距離範圍標籤 [range]。（例：10-40）。但是暴雪限制下幾乎不可用，只是為了防止部分框架顯示出錯而暫時添加。",
+		},
+		["enUS"] = {
+			"[Tags] Added a new tag [range] for distance range. (e.g. 10-40). However, it is almost unusable under Blizzard restrictions, it is temporarily added just to prevent some frames from displaying incorrectly.",
+		},
+		["koKR"] = {
+			"[태그] 거리 범위를 위한 새로운 태그 [range]를 추가했습니다. (예: 10-40). 그러나 블리자드 제한으로 거의 사용할 수 없으며, 일부 프레임이 잘못 표시되는 것을 방지하기 위해 일시적으로 추가되었습니다.",
+		},
+		["ruRU"] = {
+			"[Теги] Добавлен новый тег [range] для диапазона дистанции. (например: 10-40). Однако, он практически не используется из-за ограничений Blizzard, он временно добавлен только для предотвращения неправильного отображения некоторых фреймов.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 修复 Auctionator 美化皮肤。",
+		},
+		["zhTW"] = {
+			"[美化皮膚] 修復 Auctionator 美化皮膚。",
+		},
+		["enUS"] = {
+			"[Skin] Fixed Auctionator skin.",
+		},
+		["koKR"] = {
+			"[스킨] Auctionator 스킨이 수정되었습니다.",
+		},
+		["ruRU"] = {
+			"[Скин] Исправлен скин Auctionator.",
+		},
+	},
+}

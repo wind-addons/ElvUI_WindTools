332a8d29281c0827dd0e8c96f02af9dfc8c01be0
Zhou Fang <fang2hou@outlook.com>
2025-04-05 03:12:41 +0900

    docs: 3.91

---
+++ Core/Changelog/3.91.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[391] = {
+	RELEASE_DATE = "2025/04/05",
+	IMPORTANT = {
+		["zhCN"] = {
+			"更新 OpenRaid 库。",
+		},
+		["zhTW"] = {
+			"更新 OpenRaid 庫。",
+		},
+		["enUS"] = {
+			"Update OpenRaid library.",
+		},
+		["koKR"] = {
+			"OpenRaid 라이브러리 업데이트.",
+		},
+		["ruRU"] = {
+			"Обновление библиотеки OpenRaid.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[交接] 自动对话现在支持地下堡。",
+		},
+		["zhTW"] = {
+			"[交接] 自動對話現在支援探究。",
+		},
+		["enUS"] = {
+			"[Turn In] Smart chat now supports Delves.",
+		},
+		["koKR"] = {
+			"[자동 수락] 자동 대화가 지하구를 지원합니다.",
+		},
+		["ruRU"] = {
+			"[Сдать квест] Умный чат теперь поддерживает Раскопки.",
+		},
+	},
+}

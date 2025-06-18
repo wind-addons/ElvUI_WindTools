f2d4d88a947f923e3270ec7beccf6f0fbd2450fd
Zhou Fang <fang2hou@outlook.com>
2025-06-18 11:16:33 +0900

    docs: add changelog

---
+++ Core/Changelog/3.92.lua
@@ -0,0 +1,72 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[392] = {
+	RELEASE_DATE = "2025/06/18",
+	IMPORTANT = {
+		["zhCN"] = {
+			"支持魔兽世界 11.1.7 版本。",
+			"最低支持的 ElvUI 版本提升到 13.92。",
+			"更新了一些依赖库。",
+			"[皮肤] 由于原插件对外部改动的屏蔽，移除 TLDRMissions 皮肤。",
+
+		},
+		["zhTW"] = {
+			"支援魔獸世界 11.1.7 版本。",
+			"最低支援的 ElvUI 版本提升到 13.92。",
+			"更新了一些依賴庫。",
+			"[皮膚] 由於原插件對外部改動的屏蔽，移除 TLDRMissions 皮膚。",
+		},
+		["enUS"] = {
+			"Support for WoW 11.1.7.",
+			"Minimum supported ElvUI version is now 13.92.",
+			"Update some dependencies.",
+			"[Skins] Remove TLDRMissions skin due to the addon blocking external changes.",
+		},
+		["koKR"] = {
+			"WoW 11.1.7 버전을 지원합니다.",
+			"최소 지원 ElvUI 버전이 13.92로 업데이트되었습니다.",
+			"일부 종속성을 업데이트했습니다.",
+			"[스킨] TLDRMissions 스킨을 외부 변경을 차단하는 애드온으로 인해 제거했습니다.",
+		},
+		["ruRU"] = {
+			"Поддержка WoW 11.1.7.",
+			"Минимальная поддерживаемая версия ElvUI теперь 13.92.",
+			"Обновлены некоторые зависимости.",
+			"[Скин] Удалена TLDRMissions skin из-за блокировки внешних изменений аддоном.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[额外物品条] 增补了一些新物品ID。",
+			"[游戏条] 更新炉石ID列表。",
+			"[通告] 更新了大餐技能ID列表。",
+		},
+		["zhTW"] = {
+			"[額外物品條] 增補了一些新物品ID。",
+			"[遊戲條] 更新爐石ID列表。",
+			"[通告] 更新了大餐技能ID列表。",
+		},
+		["enUS"] = {
+			"[Extra Item Bar] Added some new item IDs.",
+			"[Game Bar] Updated Hearthstone ID list.",
+			"[Announcements] Updated Feasts skill ID list.",
+		},
+		["koKR"] = {
+			"[추가 아이템 바] 일부 새로운 아이템 ID를 추가했습니다.",
+			"[게임 바] 하스스톤 ID 목록을 업데이트했습니다.",
+			"[공지사항] 잔치 기술 ID 목록을 업데이트했습니다.",
+		},
+		["ruRU"] = {
+			"[Дополнительная панель предметов] Добавлены новые идентификаторы предметов.",
+			"[Игровая панель] Обновлен список идентификаторов Hearthstone.",
+			"[Объявления] Обновлен список идентификаторов навыков пиршества.",
+		},
+	},
+}

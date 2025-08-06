720c5d132891430bc6ba16b4f981ac43d426ed17
Zhou Fang <fang2hou@outlook.com>
2025-08-07 02:40:47 +0900

    feat: update blizzard bags skin

--- Modules/Skins/Blizzard/Bags.lua
+++ Modules/Skins/Blizzard/Bags.lua
@@ -3,44 +3,55 @@ local S = W.Modules.Skins
 
 local _G = _G
 
+local hooksecurefunc = hooksecurefunc
 local pairs = pairs
 
 local NUM_CONTAINER_FRAMES = NUM_CONTAINER_FRAMES
 
 function S:ContainerFrame()
-	if E.private.bags.enable or not self:CheckDB("bags") then
-		return
-	end
-
-	for bagID = 1, NUM_CONTAINER_FRAMES do
-		local container = _G["ContainerFrame" .. bagID]
-		if container and container.template then
-			self:CreateShadow(container)
+	E:Delay(1, function()
+		if E.private.bags.enable or not self:CheckDB("bags") then
+			return
 		end
-	end
 
-	self:CreateShadow(_G.ContainerFrameCombinedBags)
-
-	-- Bank
-	self:CreateShadow(_G.BankFrame)
-
-	for _, frame in pairs({ _G.BankSlotsFrame, _G.ReagentBankFrame, _G.AccountBankPanel }) do
-		if frame and frame.EdgeShadows then
-			frame.EdgeShadows:SetAlpha(0)
+		for bagID = 1, NUM_CONTAINER_FRAMES do
+			local container = _G["ContainerFrame" .. bagID]
+			if container and container.template then
+				self:CreateShadow(container)
+				self:CreateShadow(container.PortraitButton)
+			end
 		end
-	end
 
-	if _G.BankFrame.TabSystem then
-		local tabSet = _G.BankFrame:GetTabSet()
-		if tabSet then
-			for _, tabID in ipairs(tabSet) do
-				local tabButton = _G.BankFrame:GetTabButton(tabID)
-				if tabButton then
-					self:ReskinTab(tabButton)
+		self:CreateShadow(_G.ContainerFrameCombinedBags)
+
+		-- Bank
+		self:CreateBackdropShadow(_G.BankFrame)
+
+		for _, frame in pairs({ _G.BankSlotsFrame, _G.ReagentBankFrame, _G.AccountBankPanel }) do
+			if frame and frame.EdgeShadows then
+				frame.EdgeShadows:SetAlpha(0)
+			end
+		end
+
+		if _G.BankFrame.TabSystem then
+			for _, tab in pairs(_G.BankFrame.TabSystem.tabs) do
+				self:ReskinTab(tab)
+			end
+		end
+
+		local function ReskinPanelTabs(frame)
+			if frame.bankTabPool then
+				for tab in frame.bankTabPool:EnumerateActive() do
+					self:ReskinTab(tab.Icon)
 				end
 			end
 		end
-	end
+
+		if _G.BankPanel then
+			self:ReskinTab(_G.BankPanel.PurchaseTab.Icon)
+			hooksecurefunc(_G.BankPanel, "RefreshBankTabs", ReskinPanelTabs)
+		end
+	end)
 end
 
 S:AddCallback("ContainerFrame")

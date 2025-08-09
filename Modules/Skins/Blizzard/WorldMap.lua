57a647b37f448f5f9eabebc565bb0eb5b63a1966
Zhou Fang <fang2hou@outlook.com>
2025-08-09 12:55:14 +0900

    chore: remove debug code

--- Modules/Skins/Blizzard/WorldMap.lua
+++ Modules/Skins/Blizzard/WorldMap.lua
@@ -44,31 +44,29 @@ function S:WorldMapFrame()
 		self:CreateBackdropShadow(dialog)
 	end)
 
-	E:Delay(2, function()
-		local tabs = {
-			QuestMapFrame.QuestsTab,
-			QuestMapFrame.EventsTab,
-			QuestMapFrame.MapLegendTab,
-		}
-		for i, tab in pairs(tabs) do
-			if tab.backdrop then
-				self:CreateBackdropShadow(tab)
-				tab.backdrop:SetTemplate("Transparent")
-			end
-
-			if i > 1 then
-				tab:ClearAllPoints()
-				tab:SetPoint("TOP", tabs[i-1], "BOTTOM", 0, -5)
-			end
+	local tabs = {
+		QuestMapFrame.QuestsTab,
+		QuestMapFrame.EventsTab,
+		QuestMapFrame.MapLegendTab,
+	}
+	for i, tab in pairs(tabs) do
+		if tab.backdrop then
+			self:CreateBackdropShadow(tab)
+			tab.backdrop:SetTemplate("Transparent")
 		end
 
-		if QuestMapFrame.QuestsTab then
-			QuestMapFrame.QuestsTab:ClearAllPoints()
-			QuestMapFrame.QuestsTab.__SetPoint = QuestMapFrame.QuestsTab.SetPoint
-			QuestMapFrame.QuestsTab.SetPoint = E.noop
-			QuestMapFrame.QuestsTab:__SetPoint("TOPLEFT", QuestMapFrame, "TOPRIGHT", 13, -30)
+		if i > 1 then
+			tab:ClearAllPoints()
+			tab:SetPoint("TOP", tabs[i - 1], "BOTTOM", 0, -5)
 		end
-	end)
+	end
+
+	if QuestMapFrame.QuestsTab then
+		QuestMapFrame.QuestsTab:ClearAllPoints()
+		QuestMapFrame.QuestsTab.__SetPoint = QuestMapFrame.QuestsTab.SetPoint
+		QuestMapFrame.QuestsTab.SetPoint = E.noop
+		QuestMapFrame.QuestsTab:__SetPoint("TOPLEFT", QuestMapFrame, "TOPRIGHT", 13, -30)
+	end
 end
 
 S:AddCallback("WorldMapFrame")

441adbff6687587c2e6bc4ee1e8392020644045b
Zhou Fang <fang2hou@outlook.com>
2025-08-09 12:51:17 +0900

    feat: add skin for new tabs

--- Modules/Skins/Blizzard/WorldMap.lua
+++ Modules/Skins/Blizzard/WorldMap.lua
@@ -43,6 +43,32 @@ function S:WorldMapFrame()
 	hooksecurefunc(_G.QuestSessionManager, "NotifyDialogShow", function(_, dialog)
 		self:CreateBackdropShadow(dialog)
 	end)
+
+	E:Delay(2, function()
+		local tabs = {
+			QuestMapFrame.QuestsTab,
+			QuestMapFrame.EventsTab,
+			QuestMapFrame.MapLegendTab,
+		}
+		for i, tab in pairs(tabs) do
+			if tab.backdrop then
+				self:CreateBackdropShadow(tab)
+				tab.backdrop:SetTemplate("Transparent")
+			end
+
+			if i > 1 then
+				tab:ClearAllPoints()
+				tab:SetPoint("TOP", tabs[i-1], "BOTTOM", 0, -5)
+			end
+		end
+
+		if QuestMapFrame.QuestsTab then
+			QuestMapFrame.QuestsTab:ClearAllPoints()
+			QuestMapFrame.QuestsTab.__SetPoint = QuestMapFrame.QuestsTab.SetPoint
+			QuestMapFrame.QuestsTab.SetPoint = E.noop
+			QuestMapFrame.QuestsTab:__SetPoint("TOPLEFT", QuestMapFrame, "TOPRIGHT", 13, -30)
+		end
+	end)
 end
 
 S:AddCallback("WorldMapFrame")

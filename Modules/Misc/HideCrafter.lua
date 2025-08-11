7f40ec665a144978ed1d4dc88a2534cc601db117
Zhou Fang <fang2hou@outlook.com>
2025-08-11 23:56:43 +0900

    fix: hide crafter

--- Modules/Misc/HideCrafter.lua
+++ Modules/Misc/HideCrafter.lua
@@ -23,9 +23,9 @@ local function removeCraftInformation(tooltip, data)
 
 	local tooltipName = tooltip:GetName()
 	if F.In(tooltipName, tooltips) then
-		for dataIndex = #data.lines, 10, -1 do
+		for dataIndex = #data.lines, (10 < #data.lines and 10 or 0), -1 do
 			local line = data.lines[dataIndex] and data.lines[dataIndex].leftText
-			if line and strmatch(line, "<(.+)>|r$") then
+			if line and strmatch(line, "^|cff00ff00<(.+)>|r$") then
 				for tooltipLineIndex = dataIndex, dataIndex + 2 do
 					local realLine = _G[tooltipName .. "TextLeft" .. tooltipLineIndex]
 					if realLine and realLine:GetText() == line then

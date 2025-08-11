106a4ac0e76645cf4ea5168413317d4c1860200d
Zhou Fang <fang2hou@outlook.com>
2025-08-11 15:59:52 +0900

    docs: changelog

---
+++ Core/Changelog/3.96.lua
@@ -0,0 +1,51 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[396] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[游戏条] 更新可用炉石列表",
+			"[游戏条] 随机炉石功能不再会尝试使用盟约炉石, 防止失败.",
+			"[美化皮肤] 优化 Auctionator 皮肤.",
+			"[美化皮肤] 优化专业技能界面皮肤.",
+		},
+		["zhTW"] = {
+			"[游戲條] 更新可用爐石列表",
+			"[游戲條] 隨機爐石功能不再會嘗試使用盟約爐石, 防止失敗.",
+			"[美化外觀] 優化 Auctionator 皮膚.",
+			"[美化外觀] 優化專業技能界面皮膚.",
+		},
+		["enUS"] = {
+			"[Game Bar] Updated available hearthstone list",
+			"[Game Bar] Random hearthstone feature will no longer attempt to use covenant hearthstones, preventing failure.",
+			"[Skins] Optimized Auctionator skin.",
+			"[Skins] Optimized profession skill interface skin.",
+		},
+		["koKR"] = {
+			"[게임 바] 사용 가능한 하스스톤 목록 업데이트",
+			"[게임 바] 무작위 하스스톤 기능이 더 이상 계약 하스스톤을 사용하려고 시도하지 않음으로써 실패를 방지합니다.",
+			"[스킨] 경매장 스킨 최적화.",
+			"[스킨] 전문 기술 인터페이스 스킨 최적화.",
+		},
+		["ruRU"] = {
+			"[Игровая панель] Обновлен список доступных камней возвращения",
+			"[Игровая панель] Функция случайного камня возвращения больше не будет пытаться использовать камни возвращения covenant, предотвращая сбои.",
+			"[Скины] Оптимизирован скин Auctionator.",
+			"[Скины] Оптимизирован скин интерфейса профессий.",
+		},
+	},
+}

6acfdbd89b415a8b661694ab15e8fb1877536649
Cullen Daugherty <dakjaniels@outlook.com>
2025-08-11 06:20:22 -0500

    depreciated api updates

--- Modules/Social/ContextMenu.lua
+++ Modules/Social/ContextMenu.lua
@@ -26,11 +26,11 @@ local GetHaste = GetHaste
 local GetLifesteal = GetLifesteal
 local GetMasteryEffect = GetMasteryEffect
 local GetRangedCritChance = GetRangedCritChance
-local GetSpecialization = GetSpecialization
-local GetSpecializationInfo = GetSpecializationInfo
+local C_SpecializationInfo_GetSpecialization = C_SpecializationInfo.GetSpecialization
+local C_SpecializationInfo_GetSpecializationInfo = C_SpecializationInfo.GetSpecializationInfo
 local GetSpellCritChance = GetSpellCritChance
 local GetVersatilityBonus = GetVersatilityBonus
-local SendChatMessage = SendChatMessage
+local C_ChatInfo_SendChatMessage = C_ChatInfo.SendChatMessage
 local UnitClass = UnitClass
 local UnitHealthMax = UnitHealthMax
 local UnitPlayerControlled = UnitPlayerControlled
@@ -323,10 +323,10 @@ CM.Features = {
 		},
 		func = function(contextData)
 			local name
-			local _SendChatMessage = SendChatMessage
+			local _SendChatMessage = C_ChatInfo_SendChatMessage
 
 			if contextData.bnetIDAccount then
-				_SendChatMessage = function(message)
+				_SendChatMessage = function(message, chatType, languageID, target)
 					BNSendWhisper(contextData.bnetIDAccount, message)
 				end
 				name = "BN"
@@ -349,13 +349,14 @@ CM.Features = {
 			for i, message in ipairs({
 				format(
 					"(%s) %s: %.1f %s: %s",
-					select(2, GetSpecializationInfo(GetSpecialization())) .. select(1, UnitClass("player")),
+					select(2, C_SpecializationInfo_GetSpecializationInfo(C_SpecializationInfo_GetSpecialization()))
+						.. select(1, UnitClass("player")),
 					ITEM_LEVEL_ABBR,
 					select(2, GetAverageItemLevel()),
 					HP,
 					AbbreviateNumbers(UnitHealthMax("player"))
 				),
-				format(" * %s: %.2f%%", CRITICAL, max(GetRangedCritChance(), GetCritChance(), GetSpellCritChance(2))),
+				format(" * %s: %.2f%%", CRITICAL, max(GetRangedCritChance(), GetCritChance(), GetSpellCritChance())),
 				format(" * %s: %.2f%%", STAT_HASTE, GetHaste()),
 				format(" * %s: %.2f%%", STAT_MASTERY, GetMasteryEffect()),
 				format(

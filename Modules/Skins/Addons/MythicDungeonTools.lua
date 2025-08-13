2fb778759bb681788bad04995f21814a9f997839
Zhou Fang <fang2hou@outlook.com>
2025-08-13 20:14:59 +0900

    feat: add reskin for mdt buttons

--- Modules/Skins/Addons/MythicDungeonTools.lua
+++ Modules/Skins/Addons/MythicDungeonTools.lua
@@ -20,6 +20,59 @@ local function reskinTooltip(tt)
 	S:CreateShadow(tt)
 end
 
+local function reskinDungeonButton(MDT)
+	local db = MDT:GetDB()
+	local dungeonList = db and db.selectedDungeonList
+	local currentList = MDT.dungeonSelectionToIndex and dungeonList and MDT.dungeonSelectionToIndex[dungeonList]
+	if not currentList then
+		return
+	end
+
+	for idx = 1, #currentList do
+		if _G["MDTDungeonButton" .. idx] and not _G["MDTDungeonButton" .. idx].template then
+			local button = _G["MDTDungeonButton" .. idx]
+
+			if button.texture then
+				button.texture:SetTexCoord(unpack(E.TexCoords))
+				button.texture:SetInside()
+			end
+
+			if button.highlightTexture then
+				button.highlightTexture:SetTexture(E.media.blankTex)
+				button.highlightTexture:SetVertexColor(1, 1, 1, 0.2)
+				button.highlightTexture:SetInside()
+			end
+
+			if button.selectedTexture then
+				button.selectedTexture:SetTexture(E.media.blankTex)
+				button.selectedTexture:SetVertexColor(1, 0.85, 0, 0.4)
+				button.selectedTexture:SetInside()
+			end
+
+			if not button.template then
+				button:SetTemplate()
+				S:CreateShadow(button)
+			end
+
+			local BUTTON_SIZE = 40
+
+			button:ClearAllPoints()
+			button:SetPoint("TOPLEFT", MDT.main_frame, "TOPLEFT", (idx - 1) * (BUTTON_SIZE + 2) + 2, -2)
+		end
+	end
+end
+
+local function reskinProgressBar(_, progressBar)
+	local bar = progressBar and progressBar.Bar
+	bar:StripTextures()
+	bar:CreateBackdrop(nil, nil, nil, nil, nil, nil, nil, nil, true)
+	bar:SetStatusBarTexture(E.media.normTex)
+
+	bar.Label:ClearAllPoints()
+	bar.Label:Point("CENTER", bar, 0, 0)
+	F.SetFontOutline(bar.Label)
+end
+
 function S:MythicDungeonTools()
 	if not E.private.WT.skins.enable or not E.private.WT.skins.addons.mythicDungeonTools then
 		return
@@ -61,47 +114,65 @@ function S:MythicDungeonTools()
 		end
 	end)
 
-	self:SecureHook(_G.MDT, "UpdateDungeonDropDown", function()
-		local db = _G.MDT:GetDB()
-		local dungeonList = db and db.selectedDungeonList
-		local currentList = MDT.dungeonSelectionToIndex and dungeonList and MDT.dungeonSelectionToIndex[dungeonList]
-		if not currentList then
-			return
-		end
+	self:SecureHook(_G.MDT, "UpdateDungeonDropDown", reskinDungeonButton)
+	self:SecureHook(_G.MDT, "SkinProgressBar", reskinProgressBar)
+end
 
-		for idx = 1, #currentList do
-			if _G["MDTDungeonButton" .. idx] and not _G["MDTDungeonButton" .. idx].template then
-				local button = _G["MDTDungeonButton" .. idx]
+local function reskinButtonTexture(texture, alphaTimes)
+	if not texture then
+		return
+	end
 
-				if button.texture then
-					button.texture:SetTexCoord(unpack(E.TexCoords))
-					button.texture:SetInside()
-				end
+	texture:SetTexCoord(unpack(E.TexCoords))
+	texture:SetInside()
 
-				if button.highlightTexture then
-					button.highlightTexture:SetTexture(E.media.blankTex)
-					button.highlightTexture:SetVertexColor(1, 1, 1, 0.2)
-					button.highlightTexture:SetInside()
-				end
-
-				if button.selectedTexture then
-					button.selectedTexture:SetTexture(E.media.blankTex)
-					button.selectedTexture:SetVertexColor(1, 0.85, 0, 0.4)
-					button.selectedTexture:SetInside()
-				end
-
-				if not button.template then
-					button:SetTemplate()
-					self:CreateShadow(button)
-				end
-
-				local BUTTON_SIZE = 40
-
-				button:ClearAllPoints()
-				button:SetPoint("TOPLEFT", MDT.main_frame, "TOPLEFT", (idx - 1) * (BUTTON_SIZE + 2) + 2, -2)
-			end
-		end
+	texture:SetTexture(E.media.blankTex)
+	texture.SetVertexColor_ = texture.SetVertexColor
+	hooksecurefunc(texture, "SetVertexColor", function(self, r, g, b, a)
+		self:SetVertexColor_(r, g, b, a * alphaTimes)
 	end)
+	texture:SetVertexColor(texture:GetVertexColor())
+end
+
+function S:MDTPullButton(Constructor)
+	if not E.private.WT.skins.enable or not E.private.WT.skins.addons.mythicDungeonTools then
+		return Constructor
+	end
+
+	local function SkinnedConstructor()
+		local widget = Constructor()
+
+		reskinButtonTexture(widget.frame.pickedGlow, 0.6)
+		reskinButtonTexture(widget.frame.highlight, 0.2)
+		reskinButtonTexture(widget.background, 0.3)
+
+		widget.pullNumber:ClearAllPoints()
+		widget.pullNumber:SetPoint("CENTER", widget.frame, "LEFT", 12, -1)
+
+		widget.pullNumberSelected = widget.frame:CreateFontString(nil, "OVERLAY")
+		widget.pullNumberSelected:FontTemplate(nil, 18, "OUTLINE")
+		widget.pullNumberSelected:SetPoint("CENTER", widget.pullNumber, "CENTER", 1, -3)
+		widget.pullNumberSelected:SetTextColor(1, 1, 1, 1)
+		widget.pullNumberSelected:SetText("__")
+		widget.pullNumberSelected:SetJustifyH("CENTER")
+
+		hooksecurefunc(widget.frame.pickedGlow, "Show", function()
+			widget.pullNumber:FontTemplate(nil, 18, "OUTLINE")
+			widget.pullNumber:SetTextColor(1, 1, 1, 1)
+			widget.pullNumberSelected:Show()
+		end)
+
+		hooksecurefunc(widget.frame.pickedGlow, "Hide", function()
+			widget.pullNumber:FontTemplate(nil, 14, "OUTLINE")
+			widget.pullNumber:SetTextColor(1, 0.93, 0.76, 0.8)
+			widget.pullNumberSelected:Hide()
+		end)
+
+		return widget
+	end
+
+	return SkinnedConstructor
 end
 
 S:AddCallbackForAddon("MythicDungeonTools")
+S:AddCallbackForAceGUIWidget("MDTPullButton")

34a5657eff91fc092fbc517843a3e051b8f5a352
Zhou Fang <fang2hou@outlook.com>
2025-08-13 09:41:22 +0900

    feat: update mdt skin

--- Modules/Skins/Addons/MythicDungeonTools.lua
+++ Modules/Skins/Addons/MythicDungeonTools.lua
@@ -60,6 +60,48 @@ function S:MythicDungeonTools()
 			self:CreateShadow(_G.MDTToolbarFrame)
 		end
 	end)
+
+	self:SecureHook(_G.MDT, "UpdateDungeonDropDown", function()
+		local db = _G.MDT:GetDB()
+		local dungeonList = db and db.selectedDungeonList
+		local currentList = MDT.dungeonSelectionToIndex and dungeonList and MDT.dungeonSelectionToIndex[dungeonList]
+		if not currentList then
+			return
+		end
+
+		for idx = 1, #currentList do
+			if _G["MDTDungeonButton" .. idx] and not _G["MDTDungeonButton" .. idx].template then
+				local button = _G["MDTDungeonButton" .. idx]
+
+				if button.texture then
+					button.texture:SetTexCoord(unpack(E.TexCoords))
+					button.texture:SetInside()
+				end
+
+				if button.highlightTexture then
+					button.highlightTexture:SetTexture(E.media.blankTex)
+					button.highlightTexture:SetVertexColor(1, 1, 1, 0.2)
+					button.highlightTexture:SetInside()
+				end
+
+				if button.selectedTexture then
+					button.selectedTexture:SetTexture(E.media.blankTex)
+					button.selectedTexture:SetVertexColor(1, 0.85, 0, 0.4)
+					button.selectedTexture:SetInside()
+				end
+
+				if not button.template then
+					button:SetTemplate()
+					self:CreateShadow(button)
+				end
+
+				local BUTTON_SIZE = 40
+
+				button:ClearAllPoints()
+				button:SetPoint("TOPLEFT", MDT.main_frame, "TOPLEFT", (idx - 1) * (BUTTON_SIZE + 2) + 2, -2)
+			end
+		end
+	end)
 end
 
 S:AddCallbackForAddon("MythicDungeonTools")

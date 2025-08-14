08ff16c3eebbf49a39390a225cd9e05512e07d23
Zhou Fang <fang2hou@outlook.com>
2025-08-15 02:10:31 +0900

    docs: changelog

---
+++ Core/Changelog/3.97.lua
@@ -0,0 +1,61 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[397] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {
+			"更新一些库文件.",
+		},
+		["zhTW"] = {
+			"更新一些庫文件.",
+		},
+		["enUS"] = {
+			"Updated some libraries.",
+		},
+		["koKR"] = {
+			"일부 라이브러리를 업데이트했습니다.",
+		},
+		["ruRU"] = {
+			"Обновлены некоторые библиотеки.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[其他] 新增鼠标中键点击披风打开升级菜单功能.",
+		},
+		["zhTW"] = {
+			"[其他] 新增滑鼠中鍵點擊披風打開升級選單功能.",
+		},
+		["enUS"] = {
+			"[Misc] Added a new feature to open upgrade menu on middle mouse button click on back slot.",
+		},
+		["koKR"] = {
+			"[기타] 망토에 마우스 중간 버튼 클릭 시 업그레이드 메뉴 열기 기능 추가.",
+		},
+		["ruRU"] = {
+			"[Другое] Добавлена функция открытия меню улучшения при нажатии средней кнопки мыши на плаще.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[通告] 修正了新版大锅通告失效的问题.",
+			"[美化皮肤] BigWigs 皮肤现在支持全新的钥石界面.",
+		},
+		["zhTW"] = {
+			"[通告] 修正了新版大鍋通告失效的問題.",
+			"[美化外觀] BigWigs 外觀現在支持全新的鑰石介面.",
+		},
+		["enUS"] = {
+			"[Announcement] Fix the issue with the new cauldrons announce not working.",
+			"[Skins] BigWigs skin now supports the new keystone frame.",
+		},
+		["koKR"] = {
+			"[알림] 새로운 대형 냄비 알림이 작동하지 않는 문제 수정.",
+			"[스킨] BigWigs 스킨이 새로운 열쇠석 프레임을 지원합니다.",
+		},
+		["ruRU"] = {
+			"[Объявления] Исправлена проблема с неработающим новым уведомлением о большом горшке.",
+			"[Скины] Скин BigWigs теперь поддерживает новую рамку ключевого камня.",
+		},
+	},
+}

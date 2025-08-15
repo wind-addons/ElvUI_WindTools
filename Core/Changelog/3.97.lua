58fb7336f2afd38a35d3c8e10b9574f83db4ad8f
Zhou Fang <fang2hou@outlook.com>
2025-08-16 01:13:47 +0900

    docs: changelog

--- Core/Changelog/3.97.lua
+++ Core/Changelog/3.97.lua
@@ -5,57 +5,92 @@ W.Changelog[397] = {
 	IMPORTANT = {
 		["zhCN"] = {
 			"更新一些库文件.",
+			"代码整理和过期代码清理.",
+			"重新设计了鼠标提示的设定.",
+			"更新重置模块.",
 		},
 		["zhTW"] = {
 			"更新一些庫文件.",
+			"代碼整理和過期代碼清理.",
+			"重新設計了浮動提示的設定.",
+			"更新重置模組.",
 		},
 		["enUS"] = {
-			"Updated some libraries.",
+			"Update some libraries.",
+			"Code cleanup and deprecated code removal.",
+			"Redesign tooltip settings.",
+			"Update reset module.",
 		},
 		["koKR"] = {
 			"일부 라이브러리를 업데이트했습니다.",
+			"코드 정리 및 더 이상 사용되지 않는 코드 제거.",
+			"툴팁 설정을 재설계했습니다.",
+			"리셋 모듈을 업데이트했습니다.",
 		},
 		["ruRU"] = {
 			"Обновлены некоторые библиотеки.",
+			"Очистка кода и удаление устаревшего кода.",
+			"Переработаны настройки подсказок.",
+			"Обновлен модуль сброса.",
 		},
 	},
 	NEW = {
 		["zhCN"] = {
 			"[其他] 新增鼠标中键点击披风打开升级菜单功能.",
+			"[美化皮肤] 新增 BtWQuests 皮肤.",
 		},
 		["zhTW"] = {
 			"[其他] 新增滑鼠中鍵點擊披風打開升級選單功能.",
+			"[美化外觀] 新增 BtWQuests 外觀.",
 		},
 		["enUS"] = {
 			"[Misc] Added a new feature to open upgrade menu on middle mouse button click on back slot.",
+			"[Skins] Added BtWQuests skin.",
 		},
 		["koKR"] = {
 			"[기타] 망토에 마우스 중간 버튼 클릭 시 업그레이드 메뉴 열기 기능 추가.",
+			"[스킨] BtWQuests 스킨 추가.",
 		},
 		["ruRU"] = {
 			"[Другое] Добавлена функция открытия меню улучшения при нажатии средней кнопки мыши на плаще.",
+			"[Скины] Добавлен скин BtWQuests.",
 		},
 	},
 	IMPROVEMENT = {
 		["zhCN"] = {
 			"[通告] 修正了新版大锅通告失效的问题.",
 			"[美化皮肤] BigWigs 皮肤现在支持全新的钥石界面.",
+			"[美化皮肤] 优化 RareScanner 鼠标提示外观.",
+			"[鼠标提示] 标题图标支持自定义大小了.",
+			"[鼠标提示] 标题图标功能支持宏.",
 		},
 		["zhTW"] = {
 			"[通告] 修正了新版大鍋通告失效的問題.",
 			"[美化外觀] BigWigs 外觀現在支援全新的鑰石介面.",
+			"[美化外觀] 優化 RareScanner 滑鼠提示外觀.",
+			"[滑鼠提示] 標題圖示支援自訂大小了.",
+			"[滑鼠提示] 標題圖示功能支援巨集.",
 		},
 		["enUS"] = {
 			"[Announcement] Fix the issue with the new cauldrons announce not working.",
 			"[Skins] BigWigs skin now supports the new keystone frame.",
+			"[Skins] Optimize RareScanner tooltip appearance.",
+			"[Tooltips] Title icons now support custom sizes.",
+			"[Tooltips] Title icons now support macros.",
 		},
 		["koKR"] = {
 			"[알림] 새로운 대형 냄비 알림이 작동하지 않는 문제 수정.",
 			"[스킨] BigWigs 스킨이 새로운 열쇠석 프레임을 지원합니다.",
+			"[스킨] RareScanner 툴팁 외관 최적화.",
+			"[툴팁] 제목 아이콘이 이제 사용자 정의 크기를 지원합니다.",
+			"[툴팁] 제목 아이콘이 이제 매크로를 지원합니다.",
 		},
 		["ruRU"] = {
 			"[Объявления] Исправлена проблема с неработающим новым уведомлением о большом горшке.",
 			"[Скины] Скин BigWigs теперь поддерживает новую рамку ключевого камня.",
+			"[Скины] Оптимизирован внешний вид подсказки RareScanner.",
+			"[Подсказки] Заголовочные значки теперь поддерживают пользовательские размеры.",
+			"[Подсказки] Заголовочные значки теперь поддерживают макросы.",
 		},
 	},
 }

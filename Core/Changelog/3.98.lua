a2d1e25010d5feef96a10015b92de2b9847e9c56
Zhou Fang <fang2hou@outlook.com>
2025-08-20 11:33:40 +0900

    docs: 3.98

---
+++ Core/Changelog/3.98.lua
@@ -0,0 +1,56 @@
+local W = unpack((select(2, ...)))
+
+W.Changelog[398] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[事件追踪器] 新增了夜幕激斗事件.",
+			"[事件追踪器] 新增了生态演替周常.",
+		},
+		["zhTW"] = {
+			"[事件追蹤器] 新增了夜幕激戰事件.",
+			"[事件追蹤器] 新增了生態重構週常.",
+		},
+		["enUS"] = {
+			"[Event Tracker] Add the Nightfall event.",
+			"[Event Tracker] Add the Ecological Succession weekly event.",
+		},
+		["koKR"] = {
+			"[이벤트 추적] 새로운 야경 전투 이벤트 추가.",
+			"[이벤트 추적] 새로운 생태계 계승 주간 이벤트 추가.",
+		},
+		["ruRU"] = {
+			"[События] Добавлено событие 'Ночной бой'.",
+			"[События] Добавлено еженедельное событие 'Экологическая преемственность'.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 优化 OmniCD 外观.",
+			"[美化皮肤] 优化 BtWQuests 外观.",
+		},
+		["zhTW"] = {
+			"[美化外觀] 優化 OmniCD 外觀.",
+			"[美化外觀] 優化 BtWQuests 外觀.",
+		},
+		["enUS"] = {
+			"[Skins] Optimize OmniCD skin.",
+			"[Skins] Optimize BtWQuests skin.",
+		},
+		["koKR"] = {
+			"[스킨] OmniCD 외관 최적화.",
+			"[스킨] BtWQuests 외관 최적화.",
+		},
+		["ruRU"] = {
+			"[Скины] Оптимизирован внешний вид OmniCD.",
+			"[Скины] Оптимизирован внешний вид BtWQuests.",
+		},
+	},
+}

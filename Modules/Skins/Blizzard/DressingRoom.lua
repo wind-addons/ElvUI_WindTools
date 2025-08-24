eb04ca81bfe72170f23a918552f719b4e8006c5b
Zhou Fang <fang2hou@outlook.com>
2025-08-24 19:23:16 +0900

    feat: update dressing room skin

--- Modules/Skins/Blizzard/DressingRoom.lua
+++ Modules/Skins/Blizzard/DressingRoom.lua
@@ -2,6 +2,7 @@ local W, F, E, L = unpack((select(2, ...)))
 local S = W.Modules.Skins
 
 local _G = _G
+local hooksecurefunc = hooksecurefunc
 
 function S:DressUpFrame()
 	if not self:CheckDB("dressingroom", "dressingRoom") then
@@ -9,8 +10,22 @@ function S:DressUpFrame()
 	end
 
 	self:CreateShadow(_G.DressUpFrame)
+	self:CreateShadow(_G.DressUpFrame.SetSelectionPanel)
 	self:CreateBackdropShadow(_G.DressUpFrame.OutfitDetailsPanel)
-	self:CreateBackdropShadow(_G.DressUpFrame.SetSelectionPanel) -- MOP Remix
+
+	hooksecurefunc(_G.DressUpFrame.SetSelectionPanel.ScrollBox, "Update", function(box)
+		box:ForEachFrame(function(frame)
+			if frame.__windSkin then
+				return
+			end
+			F.SetFontOutline(frame.ItemName)
+			local width = frame.ItemSlot:GetWidth()
+			F.SetFontOutline(frame.ItemSlot)
+			frame.ItemSlot:SetWidth(width + 4)
+
+			frame.__windSkin = true
+		end)
+	end)
 end
 
 S:AddCallback("DressUpFrame")

d885d49bb389c3c52eb06e3a96a11aec22ce7e7c
Zhou Fang <fang2hou@outlook.com>
2025-08-26 22:11:21 +0900

    refactor: use elvui api to fetch aura

--- Modules/Misc/ExitPhaseDiving.lua
+++ Modules/Misc/ExitPhaseDiving.lua
@@ -7,18 +7,11 @@ local _G = _G
 local select = select
 
 local CreateFrame = CreateFrame
-local AuraUtil_FindAura = AuraUtil.FindAura
 
 local tooltipTitle = "Exit Phase Diving"
 async.WithSpellID(1250255, function(spell)
 	tooltipTitle = F.GetWindStyleText(spell:GetSpellName())
 end)
-
-local function hasBuff(id)
-	return AuraUtil_FindAura(function(...)
-		return id == select(13, ...)
-	end, "player", "HELPFUL")
-end
 
 local function visuallyHide(button)
 	button.backdrop:SetAlpha(0)
@@ -33,7 +26,7 @@ local function visuallyShow(button)
 end
 
 local function updateVisual(button)
-	if hasBuff(1214374) then
+	if E:GetAuraByID("player", 1214374, "HELPFUL") then
 		visuallyShow(button)
 	else
 		visuallyHide(button)
@@ -75,7 +68,7 @@ local function createButton()
 	button.Highlight:Hide()
 
 	button:SetScript("OnEnter", function()
-		if hasBuff(1214374) then
+		if E:GetAuraByID("player", 1214374, "HELPFUL") then
 			button.Highlight:Show()
 			_G.GameTooltip:SetOwner(button, "ANCHOR_BOTTOM", 0, -5)
 			_G.GameTooltip:SetText(tooltipTitle, 1, 1, 1)

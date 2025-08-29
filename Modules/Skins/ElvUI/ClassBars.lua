e6302587f04a57871a0b0a449b124243182ad5f4
Zhou Fang <fang2hou@outlook.com>
2025-08-29 18:55:32 +0900

    feat: add shadow to additional power

--- Modules/Skins/ElvUI/ClassBars.lua
+++ Modules/Skins/ElvUI/ClassBars.lua
@@ -4,20 +4,28 @@ local UF = E:GetModule("UnitFrames")
 
 local _G = _G
 
-function S:ElvUI_UnitFrames_SkinClassBar(_, frame)
-	local bar = frame[frame.ClassBar]
-	self:CreateBackdropShadow(bar)
+local function reskinClassBar(_, frame)
+	local classBar = frame[frame.ClassBar]
+	if classBar then
+		S:CreateBackdropShadow(classBar)
+	end
+
+	local additionalPowerBar = frame.AdditionalPower
+	if additionalPowerBar then
+		S:CreateBackdropShadow(additionalPowerBar)
+	end
 end
 
 function S:ElvUI_ClassBars()
 	if not E.private.unitframe.enable then
 		return
 	end
+
 	if not (E.private.WT.skins.elvui.enable and E.private.WT.skins.elvui.classBars) then
 		return
 	end
 
-	self:SecureHook(UF, "Configure_ClassBar", "ElvUI_UnitFrames_SkinClassBar")
+	self:SecureHook(UF, "Configure_ClassBar", reskinClassBar)
 end
 
 S:AddCallback("ElvUI_ClassBars")

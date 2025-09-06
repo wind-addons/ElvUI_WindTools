d1fcdf0520be13a15622fdf2a4c3d82f671c5eeb
Zhou Fang <fang2hou@outlook.com>
2025-09-06 19:10:38 +0900

    feat: update the libqtip skin

--- Modules/Skins/Libraries/LibQTip.lua
+++ Modules/Skins/Libraries/LibQTip.lua
@@ -5,20 +5,45 @@ local TT = E:GetModule("Tooltip")
 local type = type
 local select = select
 
+function S:LibQTip_UpdateScrolling(tooltip)
+	local slider = tooltip and tooltip.slider
+	if slider and not slider.__windSkin then
+		self:Proxy("HandleSliderFrame", slider)
+	end
+end
+
 function S:LibQTip_SetCell(tooltip, ...)
 	local setCell = self.hooks[tooltip] and self.hooks[tooltip].SetCell
 	if not setCell then
 		return
 	end
 
-	local lineNum, colNum, value = select(1, ...)
+	local lineNum, colNum, value, arg = select(1, ...)
 
 	-- Only style if we have valid parameters and string value
-	if type(lineNum) == "number" and type(colNum) == "number" and type(value) == "string" then
-		local styledValue = self:StyleTextureString(value)
-		if styledValue ~= value then
-			-- Replace the value in the argument list
-			return setCell(tooltip, lineNum, colNum, styledValue, select(4, ...))
+	if type(lineNum) == "number" and type(colNum) == "number" then
+		if type(value) == "string" then
+			local styledValue = self:StyleTextureString(value)
+			if styledValue ~= value then
+				-- Replace the value in the argument list
+				return setCell(tooltip, lineNum, colNum, styledValue, select(4, ...))
+			end
+		elseif arg and type(arg) == "table" and type(arg.AcquireCell) == "function" then
+			if not arg.__windSkin then
+				local AcquireCell = arg.AcquireCell
+				arg.AcquireCell = function(prototype, ...)
+					local cell = AcquireCell(prototype, ...)
+					if cell and cell.texture and not cell.__windSkin then
+						self:TryCropTexture(cell.texture)
+						cell.__windSkin = true
+					end
+					return cell
+				end
+
+				arg.__windSkin = true
+			end
+
+			return setCell(tooltip, lineNum, colNum, value, arg, select(5, ...))
 		end
 	end
 
@@ -36,6 +61,11 @@ function S:ReskinLibQTip(lib)
 			end
 
 			TT:SetStyle(tt)
+
+			if tt.UpdateScrolling and not self:IsHooked(tt, "UpdateScrolling") then
+				self:RawHook(tt, "UpdateScrolling", "LibQTip_UpdateScrolling")
+			end
+
 			if tt.SetCell and not self:IsHooked(tt, "SetCell") then
 				self:RawHook(tt, "SetCell", "LibQTip_SetCell")
 			end

13e2a7e7f577d48c79a71bc357fa0a6130b2ef7c
Zhou Fang <fang2hou@outlook.com>
2025-09-09 01:17:15 +0900

    feat: disable the minimap button for BasicMinimap

--- Modules/Maps/MinimapButtons.lua
+++ Modules/Maps/MinimapButtons.lua
@@ -328,8 +328,8 @@ function MB:SkinButton(button, force)
 		button.GRM_MinimapButtonBorder:Hide()
 		button:SetPushedTexture("")
 		button:SetHighlightTexture("")
-		button.SetPushedTexture = E.noop
-		button.SetHighlightTexture = E.noop
+		F.InternalizeMethod(button, "SetPushedTexture", true)
+		F.InternalizeMethod(button, "SetHighlightTexture", true)
 		if button:HasScript("OnEnter") then
 			self:SetButtonMouseOver(button, button, true)
 			button.OldSetScript = button.SetScript
@@ -356,7 +356,7 @@ function MB:SkinButton(button, force)
 		button.icon.Hide = E.noop
 		button.icon2:Kill()
 		button.timer:SetScript("OnUpdate", nil)
-		button.timer.SetScript = E.noop
+		F.InternalizeMethod(button.timer, "SetScript", true)
 	elseif name == "MRPMinimapButton" then
 		for _, region in pairs({ button:GetRegions() }) do
 			if region:GetTexture() > 0 then
@@ -720,6 +720,11 @@ function MB:PLAYER_ENTERING_WORLD()
 end
 
 function MB:Initialize()
+	if C_AddOns_IsAddOnLoaded("BasicMinimap") then
+		self.StopRunning = "BasicMinimap"
+		return
+	end
+
 	self.db = E.private.WT.maps.minimapButtons
 	if not self.db.enable then
 		return

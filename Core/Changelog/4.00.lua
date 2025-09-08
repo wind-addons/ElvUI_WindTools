5b00fad9144121c81f721ce44f5e0b5c88d07b78
Zhou Fang <fang2hou@outlook.com>
2025-09-09 00:47:59 +0900

    docs: changelog

---
+++ Core/Changelog/4.00.lua
@@ -0,0 +1,61 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[400] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {
+			"[装备观察] 完全重写了模块, 由于配置项目进行了改动, 该模块相关的配置将被重置.",
+		},
+		["zhTW"] = {
+			"[裝備觀察] 完全重寫了模組, 由於配置項目進行了改動, 該模組相關的配置將被重置.",
+		},
+		["enUS"] = {
+			"[Inspect] The module has been completely rewritten. Due to changes in options, the saved options related to this module will be reset.",
+		},
+		["koKR"] = {
+			"[장비 살펴보기] 모듈이 완전히 재작성되었습니다. 옵션이 변경되어 이 모듈과 관련된 저장된 옵션이 재설정됩니다.",
+		},
+		["ruRU"] = {
+			"[Осмотр] Модуль был полностью переписан. Из-за изменений в параметрах сохраненные параметры, связанные с этим модулем, будут сброшены.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[美化外观] 新增了暴雪游戏选项 (Esc键菜单) 的皮肤.",
+		},
+		["zhTW"] = {
+			"[美化外觀] 新增了暴雪遊戲選項 (Esc鍵選單) 的皮膚.",
+		},
+		["enUS"] = {
+			"[Skins] Added skin for the Blizzard Game Menu (Esc key menu).",
+		},
+		["koKR"] = {
+			"[스킨] 블리자드 게임 메뉴(ESC 키 메뉴)에 스킨이 추가되었습니다.",
+		},
+		["ruRU"] = {
+			"[Скины] Добавлен скин для игрового меню Blizzard (меню клавиши Esc).",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[鼠标提示] 修复了非亚洲语言环境下, 有时图标无法显示的问题.",
+			"[美化外观] 优化了组件皮肤的性能.",
+		},
+		["zhTW"] = {
+			"[浮動提示] 修復了非亞洲語言環境下, 有時圖示無法顯示的問題.",
+			"[美化外觀] 優化了組件皮膚外觀的性能.",
+		},
+		["enUS"] = {
+			"[Tooltip] Fix the problem that the icon could not be displayed in some non-Asian languages.",
+			"[Skins] Optimize the performance of the module skins.",
+		},
+		["koKR"] = {
+			"[툴팁] 일부 비아시아 언어에서 아이콘이 표시되지 않는 문제를 수정했습니다.",
+			"[스킨] 모듈 스킨의 성능을 최적화했습니다.",
+		},
+		["ruRU"] = {
+			"[Подсказки] Исправлена проблема, из-за которой значок не отображался в некоторых неазиатских языках.",
+			"[Скины] Оптимизирована производительность скинов модулей.",
+		},
+	},
+}

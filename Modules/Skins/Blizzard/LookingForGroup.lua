11465902328ad49d684b459009adc2f80ab9ef19
Zhou Fang <fang2hou@outlook.com>
2025-09-09 10:29:51 +0900

    Revert "fix: avoid taint"
    This reverts commit d3b29f8e9d89db9213bc15de1914ca748c7a9756.

--- Modules/Skins/Blizzard/LookingForGroup.lua
+++ Modules/Skins/Blizzard/LookingForGroup.lua
@@ -37,6 +37,9 @@ function S:LookingForGroupFrames()
 		F.SetFontOutline(frame.rewardsDescription, E.db.general.font)
 	end
 
+	-- if no party found, the button also need skin
+	S:Proxy("HandleButton", _G.LFGListFrame.SearchPanel.ScrollBox.StartGroupButton)
+
 	_G.LFGListFrame.SearchPanel.FilterButton:SetWidth(93)
 	_G.LFGListFrame.SearchPanel.FilterButton.SetWidth = E.noop
 end

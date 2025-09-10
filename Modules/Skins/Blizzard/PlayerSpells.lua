d8df4dccdc0615ce8f8429e2fefa751168f5a9a6
Cullen Daugherty <dakjaniels@outlook.com>
2025-09-10 09:31:48 -0500

    Revert "maybe this will work, no taint now."
    This reverts commit ff5d42a8d7b9f31a3d610e85fb7cf7e0dbcb9e93.

--- Modules/Skins/Blizzard/PlayerSpells.lua
+++ Modules/Skins/Blizzard/PlayerSpells.lua
@@ -4,7 +4,6 @@ local S = W.Modules.Skins ---@type Skins
 local _G = _G
 local hooksecurefunc = hooksecurefunc
 local next = next
-local C_Timer_After = C_Timer.After
 
 function S:Blizzard_PlayerSpells()
 	if not self:CheckDB("talent", "playerSpells") then
@@ -23,21 +22,8 @@ function S:Blizzard_PlayerSpells()
 
 	local SpellBookFrame = _G.PlayerSpellsFrame.SpellBookFrame
 	if SpellBookFrame then
-		local function CenterTabText(tab)
-			if tab.Text and not tab.__windTextCentered then
-				C_Timer_After(0.05, function()
-					if tab.Text and not tab.__windTextCentered then
-						tab.Text:ClearAllPoints()
-						tab.Text:SetPoint("CENTER", tab, "CENTER", 0, 0)
-						tab.__windTextCentered = true
-					end
-				end)
-			end
-		end
-
 		for _, tab in next, { SpellBookFrame.CategoryTabSystem:GetChildren() } do
 			tab:SetPushedTextOffset(0, 0)
-			CenterTabText(tab)
 		end
 	end
 

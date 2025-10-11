3abf12f573a0d8fba4a4fb43a11fc6cef1c42eef
Zhou Fang <fang2hou@outlook.com>
2025-10-11 14:59:47 +0900

    feat: update recent allies skin

--- Modules/Skins/Blizzard/Friends.lua
+++ Modules/Skins/Blizzard/Friends.lua
@@ -13,9 +13,9 @@ local function ReskinFriendButton(button)
 	end
 
 	button.right = button:CreateTexture(nil, "BACKGROUND")
-	button.right:SetWidth(button:GetWidth() / 2)
-	button.right:SetHeight(32)
-	button.right:SetPoint("LEFT", button, "CENTER", 0)
+	button.right:Width(button:GetWidth() / 2)
+	button.right:Height(32)
+	button.right:Point("LEFT", button, "CENTER", 0)
 	button.right:SetTexture(E.Media.Textures.White8x8)
 	button.right:SetGradient("HORIZONTAL", CreateColor(0.243, 0.57, 1, 0), CreateColor(0.243, 0.57, 1, 0.25))
 
@@ -36,6 +36,67 @@ local function ReskinFriendButton(button)
 	end
 end
 
+local function ReskinPartyButton(button)
+	button:Width(button:GetWidth() - 4)
+
+	S:Proxy("HandleButton", button)
+	local normal = button:GetNormalTexture()
+	normal:SetTexture(W.Media.Icons.buttonPlus)
+	normal:SetTexCoord(0, 1, 0, 1)
+	normal:Size(14)
+	normal:ClearAllPoints()
+	normal:Point("CENTER")
+	normal:SetVertexColor(1, 1, 1, 1)
+	normal:Show()
+
+	local highlight = button:GetHighlightTexture()
+	highlight:SetTexture(E.media.blankTex)
+	highlight:SetColorTexture(1, 1, 1, 0.2)
+	highlight:SetInside(button)
+	highlight:SetDrawLayer("OVERLAY")
+	highlight:Hide()
+
+	button:HookScript("OnEnter", function()
+		highlight:Show()
+	end)
+
+	button:HookScript("OnLeave", function()
+		highlight:Hide()
+	end)
+
+	F.Move(button, -3, 0)
+end
+
+local function ReskinRecentAllyButton(button)
+	if not button or button.__windSkin then
+		return
+	end
+
+	local normal = button:GetNormalTexture()
+	normal:SetTexture(E.media.blankTex)
+	normal:Width(button:GetWidth() / 2)
+	normal:Height(button:GetHeight() - 2)
+	normal:Point("LEFT", button, "CENTER", 0)
+	normal:SetGradient("HORIZONTAL", CreateColor(0.243, 0.57, 1, 0), CreateColor(0.243, 0.57, 1, 0.25))
+
+	local highlight = button:GetHighlightTexture()
+	highlight:SetTexture(E.media.blankTex)
+	highlight:SetVertexColor(0.243, 0.57, 1, 0.2)
+	highlight:SetInside(button)
+
+	button:HookScript("OnEnter", function()
+		highlight:Show()
+	end)
+
+	button:HookScript("OnLeave", function()
+		highlight:Hide()
+	end)
+
+	ReskinPartyButton(button.PartyButton)
+
+	button.__windSkin = true
+end
+
 local function UpdateRewards()
 	for tab in _G.RecruitAFriendRewardsFrame.rewardTabPool:EnumerateActive() do
 		if not tab.__windSkin then
@@ -47,44 +108,6 @@ local function UpdateRewards()
 			tab.__windSkin = true
 		end
 	end
-end
-
-local function ReskinRecentAllyButton(button)
-	if not button or button.__windSkin then
-		return
-	end
-
-	local PartyButton = button.PartyButton
-	PartyButton:Width(PartyButton:GetWidth() - 4)
-
-	S:Proxy("HandleButton", PartyButton)
-	local normalTex = PartyButton:GetNormalTexture()
-	normalTex:SetTexture(W.Media.Icons.buttonPlus)
-	normalTex:SetTexCoord(0, 1, 0, 1)
-	normalTex:Size(14)
-	normalTex:ClearAllPoints()
-	normalTex:Point("CENTER")
-	normalTex:SetVertexColor(1, 1, 1, 1)
-	normalTex:Show()
-
-	local highlightTex = PartyButton:GetHighlightTexture()
-	highlightTex:SetTexture(E.media.blankTex)
-	highlightTex:SetColorTexture(1, 1, 1, 0.2)
-	highlightTex:SetInside(PartyButton)
-	highlightTex:SetDrawLayer("OVERLAY")
-	highlightTex:Hide()
-
-	PartyButton:HookScript("OnEnter", function()
-		highlightTex:Show()
-	end)
-
-	PartyButton:HookScript("OnLeave", function()
-		highlightTex:Hide()
-	end)
-
-	F.Move(PartyButton, -3, 0)
-
-	button.__windSkin = true
 end
 
 function S:FriendsFrame()
@@ -118,15 +141,16 @@ function S:FriendsFrame()
 	end
 
 	self:SecureHook("FriendsFrame_UpdateFriendButton", ReskinFriendButton)
-	self:SecureHook(_G.RecruitAFriendRewardsFrame, "UpdateRewards", UpdateRewards)
-	UpdateRewards()
 
 	local RecentAlliesFrame = _G.RecentAlliesFrame
 	if RecentAlliesFrame and RecentAlliesFrame.List and RecentAlliesFrame.List.ScrollBox then
-		hooksecurefunc(RecentAlliesFrame.List.ScrollBox, "Update", function(scrollBox)
+		self:SecureHook(RecentAlliesFrame.List.ScrollBox, "Update", function(scrollBox)
 			scrollBox:ForEachFrame(ReskinRecentAllyButton)
 		end)
 	end
+
+	self:SecureHook(_G.RecruitAFriendRewardsFrame, "UpdateRewards", UpdateRewards)
+	UpdateRewards()
 end
 
 S:AddCallback("FriendsFrame")

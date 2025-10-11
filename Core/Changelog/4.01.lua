e2a323704d660b58208b4431ba5522dfc745f9d9
Zhou Fang <fang2hou@outlook.com>
2025-10-11 14:26:08 +0900

    docs: update changelog

---
+++ Core/Changelog/4.01.lua
@@ -0,0 +1,82 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[401] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[美化外观] 新增了 Collectionator 的皮肤.",
+			"[美化外观] 新增了 Plumber 的皮肤.",
+			"[小地图按钮] 新增了根据按钮名优先度排序功能.",
+		},
+		["zhTW"] = {
+			"[美化外觀] 新增了 Collectionator 的皮膚.",
+			"[美化外觀] 新增了 Plumber 的皮膚.",
+			"[小地圖按鈕] 新增了根據按鍵名優先度排序功能.",
+		},
+		["enUS"] = {
+			"[Skins] Add skin for Collectionator.",
+			"[Skins] Add skin for Plumber.",
+			"[Minimap Buttons] Add function to sort by button name priority.",
+		},
+		["koKR"] = {
+			"[스킨] Collectionator에 스킨이 추가되었습니다.",
+			"[스킨] Plumber에 스킨이 추가되었습니다.",
+			"[미니맵 버튼] 버튼 이름 우선순위로 정렬하는 기능이 추가되었습니다.",
+		},
+		["ruRU"] = {
+			"[Скины] Добавлен скин для Collectionator.",
+			"[Скины] Добавлен скин для Plumber.",
+			"[Кнопки миникарты] Добавлена функция сортировки по приоритету имени кнопки.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化外观] 优化了 Auctionator 的皮肤.",
+			"[美化外观] 优化了冒险地图皮肤.",
+			"[美化外观] 优化了好友界面皮肤.",
+			"[美化外观] 优化了一些UI组件中的文字显示.",
+			"[小地图按钮] 增强了对部分按钮的支持.",
+			"[战斗提示] 重构了模组，提升性能.",
+			"[预组建队伍] 新增了军团Remix的史诗钥石支持.",
+		},
+		["zhTW"] = {
+			"[美化外觀] 優化了 Auctionator 的皮膚.",
+			"[美化外觀] 優化了冒險地圖皮膚.",
+			"[美化外觀] 優化了好友介面皮膚.",
+			"[戰鬥提示] 重構了模組，提升效能.",
+			"[小地圖按鈕] 增強了對部分按鈕的支援.",
+			"[預組隊伍] 新增了軍團Remix的史詩鑰石支援.",
+		},
+		["enUS"] = {
+			"[Skins] Updated Auctionator skin.",
+			"[Skins] Updated Adventure Map skin.",
+			"[Skins] Updated Friend List skin.",
+			"[Combat Alert] Refactored the module for better performance.",
+			"[Minimap Buttons] Improved support for some buttons.",
+			"[LFG List] Added support for Legion Remix Mythic+.",
+		},
+		["koKR"] = {
+			"[스킨] Auctionator 스킨이 업데이트되었습니다.",
+			"[스킨] 어드벤처 맵 스킨이 업데이트되었습니다.",
+			"[스킨] 친구 목록 스킨이 업데이트되었습니다.",
+			"[전투 알림] 더 나은 성능을 위해 모듈이 리팩토링되었습니다.",
+			"[미니맵 버튼] 일부 버튼에 대한 지원이 개선되었습니다.",
+			"[파티 찾기 목록] 군단 리믹스 신화+ 지원이 추가되었습니다.",
+		},
+		["ruRU"] = {
+			"[Скины] Обновлен скин Auctionator.",
+			"[Скины] Обновлен скин карты приключений.",
+			"[Скины] Обновлен скин списка друзей.",
+			"[Боевые оповещения] Модуль переработан для повышения производительности.",
+			"[Кнопки миникарты] Улучшена поддержка некоторых кнопок.",
+			"[Список поиска групп] Добавлена поддержка Легиона Remix Mythic+.",
+		},
+	},
+}

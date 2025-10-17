ad92a4fed412ee96036ae31d0f4b93924b8ab498
Zhou Fang <fang2hou@outlook.com>
2025-10-17 23:36:32 +0900

    docs: add 4.02 changelog

---
+++ Core/Changelog/4.02.lua
@@ -0,0 +1,61 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[402] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {
+			"优化了韩语翻译。感谢 Crazyyoungs",
+		},
+		["zhTW"] = {
+			"優化了韓語翻譯。感謝 Crazyyoungs",
+		},
+		["enUS"] = {
+			"Optimize Korean translation. Thanks to Crazyyoungs",
+		},
+		["koKR"] = {
+			"한국어 번역을 최적화했습니다. Crazyyoungs에게 감사합니다",
+		},
+		["ruRU"] = {
+			"Оптимизирован перевод на корейский язык. Спасибо Crazyyoungs",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化外观] 修复了 Rematch 皮肤在独立模式下可能的报错问题。",
+			"[美化外观] 优化了 Legion Remix Helper 收藏页面美化的稳定性。",
+			"[小地图按钮] 优化了 Rematch 和 WIM 小地图按钮的材质清理。",
+			"[额外物品条] 新增了一些可开启物品。",
+		},
+		["zhTW"] = {
+			"[美化外觀] 修復了 Rematch 皮膚在獨立模式下可能的報錯問題。",
+			"[美化外觀] 優化了 Legion Remix Helper 收藏頁面美化的穩定性。",
+			"[小地圖按鍵] 優化了 Rematch 和 WIM 小地圖按鍵的材質清理。",
+			"[額外物品條] 新增了一些可開啟物品。",
+		},
+		["enUS"] = {
+			"[Skins] Fix possible error with Rematch skin in standalone mode.",
+			"[Skins] Improve stability of Legion Remix Helper collection page styling.",
+			"[Minimap Buttons] Optimize texture cleanup for Rematch and WIM minimap buttons.",
+			"[Extra Item Bar] Add some activatable items.",
+		},
+		["koKR"] = {
+			"[스킨] 독립 모드에서 Rematch 스킨의 가능한 오류를 수정했습니다.",
+			"[스킨] Legion Remix Helper 컬렉션 페이지 스킬링의 안정성을 개선했습니다.",
+			"[미니맵 버튼] Rematch 및 WIM 미니맵 버튼의 텍스처 정리를 최적화했습니다.",
+			"[추가 아이템 바] 일부 활성화 가능한 아이템을 추가했습니다.",
+		},
+		["ruRU"] = {
+			"[Интерфейс] Исправлена возможная ошибка скина Rematch в автономном режиме.",
+			"[Интерфейс] Улучшена стабильность стилизации страницы коллекций Legion Remix Helper.",
+			"[Кнопки миникарты] Оптимизирована очистка текстур для кнопок миникарты Rematch и WIM.",
+			"[Дополнительная панель предметов] Добавлены некоторые активируемые предметы.",
+		},
+	},
+}

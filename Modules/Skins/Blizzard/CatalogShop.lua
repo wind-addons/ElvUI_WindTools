b344a0311b4d9584ac405676d2bee7a461fbd6de
Zhou Fang <fang2hou@outlook.com>
2025-10-21 04:37:14 +0900

    feat: add some shadow & fix the key

--- Modules/Skins/Blizzard/CatalogShop.lua
+++ Modules/Skins/Blizzard/CatalogShop.lua
@@ -5,11 +5,21 @@ local ES = E:GetModule("Skins")
 local _G = _G
 
 function S:Blizzard_CatalogShop()
-	if not self:CheckDB("catalog") then
+	if not self:CheckDB("catalogShop") then
 		return
 	end
 
-	self:CreateShadow(_G.CatalogShopFrame)
+	local CatalogShopFrame = _G.CatalogShopFrame
+	if not CatalogShopFrame then
+		return
+	end
+
+	self:CreateShadow(CatalogShopFrame)
+
+	local CatalogShopDetailsFrame = CatalogShopFrame.CatalogShopDetailsFrame
+	if CatalogShopDetailsFrame then
+		self:CreateShadow(CatalogShopDetailsFrame)
+	end
 end
 
 S:AddCallback("Blizzard_CatalogShop")

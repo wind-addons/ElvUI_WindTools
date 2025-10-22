1a321e08b399e97423c5eeab67df4a3714861d9c
Zhou Fang <fang2hou@outlook.com>
2025-10-22 10:29:25 +0900

    feat: add new character

--- Modules/Social/ChatText.lua
+++ Modules/Social/ChatText.lua
@@ -694,6 +694,7 @@ local authorIcons = {
 	["Player-2133-0EDB2716"] = windIcon, -- Tabidemon-影之哀伤
 	["Player-858-0390AAA9"] = windIcon, -- Tabiwarrior-寒冰皇冠
 	["Player-858-03954572"] = windIcon, -- Tabihunter-寒冰皇冠
+	["Player-858-05BB0270"] = windIcon, -- Tabipaladin-寒冰皇冠
 	-- zhTW Server
 	["Player-963-04504559"] = windIcon, -- Tabidruid-暗影之月
 	["Player-963-0450457B"] = windIcon, -- Tabilock-暗影之月

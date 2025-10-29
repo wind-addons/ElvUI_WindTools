702afb35fe729622a7338605298f5deb8166b0e5
Zhou Fang <fang2hou@outlook.com>
2025-10-30 00:04:51 +0900

    docs: 4.03 changelog

---
+++ Core/Changelog/4.03.lua
@@ -0,0 +1,61 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[403] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {
+			"一些代码清理.",
+		},
+		["zhTW"] = {
+			"一些代碼清理.",
+		},
+		["enUS"] = {
+			"Some code cleanup.",
+		},
+		["koKR"] = {
+			"일부 코드 정리.",
+		},
+		["ruRU"] = {
+			"Некоторая очистка кода.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[矩形小地图] 新增了对 HereBeDragons-Pins 插件图标的支持, 默认开启.",
+		},
+		["zhTW"] = {
+			"[矩形小地圖] 新增了對 HereBeDragons-Pins 插件圖示的支持, 預設開啟.",
+		},
+		["enUS"] = {
+			"[Rectangular Minimap] Add support for pins from HereBeDragons-Pins library, enabled by default.",
+		},
+		["koKR"] = {
+			"[사각형 미니맵] HereBeDragons-Pins 플러그인 핀 지원 추가, 기본값으로 활성화.",
+		},
+		["ruRU"] = {
+			"[Прямоугольная миникарта] Добавлена поддержка иконок из плагина HereBeDragons-Pins, включено по умолчанию.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 优化了社区窗口的皮肤.",
+			"[交接] 提升了多任务时的稳定性.",
+		},
+		["zhTW"] = {
+			"[美化皮膚] 優化了社區視窗的皮膚.",
+			"[交接] 提升了多任務時的穩定性.",
+		},
+		["enUS"] = {
+			"[Skins] Optimize the skin for the Communities frame.",
+			"[Turn In] Improve stability when there are multiple quests.",
+		},
+		["koKR"] = {
+			"[스킨] 커뮤니티 창의 스킨을 최적화했습니다.",
+			"[퀘스트 완료] 여러 퀘스트가 있을 때 안정성을 향상시켰습니다.",
+		},
+		["ruRU"] = {
+			"[Оформление] Оптимизация оформления для окна Сообщества.",
+			"[Сдача заданий] Повышена стабильность при наличии нескольких заданий.",
+		},
+	},
+}

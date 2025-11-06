17e9190298e5271e4388b1d6f507f71b2ff63fc3
Merathilis <neo0608@gmail.com>
2025-11-06 18:31:30 +0100

    feat: skin the TomTom Paste Frame

--- Modules/Skins/Addons/TomTom.lua
+++ Modules/Skins/Addons/TomTom.lua
@@ -2,6 +2,7 @@ local W, F, E, L = unpack((select(2, ...))) ---@type WindTools, Functions, ElvUI
 local S = W.Modules.Skins ---@type Skins
 
 local _G = _G
+local pairs = pairs
 
 function S:TomTom()
 	if not E.private.WT.skins.enable or not E.private.WT.skins.addons.tomTom then
@@ -19,6 +20,20 @@ function S:TomTom()
 
 		if _G.TomTomBlock.Text then
 			F.SetFont(_G.TomTomBlock.Text, E.db.general.font)
+		end
+	end
+
+	-- /ttpaste or /tomtompaste
+	if _G.TomTomPaste then
+		self:Proxy("HandleFrame", _G.TomTomPaste)
+		self:CreateShadow(_G.TomTomPaste)
+		self:BindShadowColorWithBorder(_G.TomTomPaste)
+
+		for _, child in pairs({ _G.TomTomPaste:GetChildren() }) do
+			local objectType = child.GetObjectType and child:GetObjectType()
+			if objectType == "Button" then
+				self:Proxy("HandleButton", child)
+			end
 		end
 	end
 

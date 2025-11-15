8a2ca6936d9cd9ff0eca420cde6cf3a4cc1d0764
Zhou Fang <fang2hou@outlook.com>
2025-11-15 13:52:50 +0900

    docs: 4.04

---
+++ Core/Changelog/4.04.lua
@@ -0,0 +1,41 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[404] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[交接] 优化部分任务自动交接逻辑，特别是军团幻境每日任务的处理.",
+			"[矩形小地图] 进一步提升和使用HereBeDragons-Pins库的插件的兼容性, 能够更加严格的隐藏超出矩形小地图边界的图标.",
+		},
+		["zhTW"] = {
+			"[交接] 優化部分任務自動交接邏輯，特別是軍團混搭每日任務的處理.",
+			"[矩形小地圖] 進一步提升和使用HereBeDragons-Pins庫的插件的兼容性, 能夠更加嚴格的隱藏超出矩形小地圖邊界的圖標.",
+		},
+		["enUS"] = {
+			"[Turn In] Optimized the auto turn-in logic for certain quests, especially for the handling of Legion Remix daily quests.",
+			"[Rectangle Minimap] Further improved compatibility with addons using the HereBeDragons-Pins library, allowing for stricter hiding of icons that exceed the boundaries of the rectangle minimap.",
+		},
+		["koKR"] = {
+			"[퀘스트 교환] 특정 퀘스트, 특히 군단 리믹스 일일 퀘스트 처리에 대한 자동 교환 로직이 최적화되었습니다.",
+			"[직사각형 미니맵] HereBeDragons-Pins 라이브러리를 사용하는 애드온과의 호환성이 더욱 향상되어 직사각형 미니맵 경계를 초과하는 아이콘을 더 엄격하게 숨길 수 있습니다.",
+		},
+		["ruRU"] = {
+			"[Сдача заданий] Оптимизирована логика автоматической сдачи для определенных заданий, особенно для обработки ежедневных заданий Легиона Ремикс.",
+			"[Прямоугольная мини-карта] Дополнительно улучшена совместимость с аддонами, использующими библиотеку HereBeDragons-Pins, что позволяет более строго скрывать значки, выходящие за пределы прямоугольной мини-карты.",
+		},
+	},
+}

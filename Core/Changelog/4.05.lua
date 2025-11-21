bc80e82699e9435b91fddea9257ad3e1dfcc3e2c
Zhou Fang <fang2hou@outlook.com>
2025-11-21 19:38:29 +0900

    docs: 4.05

---
+++ Core/Changelog/4.05.lua
@@ -0,0 +1,38 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[405] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"进一步优化 SetPassThroughButtons 修复功能.",
+			"[右键菜单] 添加了对中国大陆服务器玩家的英雄榜支持.",
+			"[美化外观] 优化了地下城手册中首领按钮的高亮效果.",
+		},
+		["zhTW"] = {
+			"進一步優化 SetPassThroughButtons 修復功能.",
+			"[右鍵選單] 添加了對中國大陸伺服器玩家的英雄榜支持.",
+			"[美化外觀] 優化了地下城手冊中首領按鈕的高亮效果.",
+		},
+		["enUS"] = {
+			"Further optimiz the SetPassThroughButtons fix feature.",
+			"[Context Menu] Add armory support for players on China mainland servers.",
+			"[Skins] Optimize the highlight effect of Boss buttons in the Encounter Journal.",
+		},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+}

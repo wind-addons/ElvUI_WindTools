ce435cb5fc0ba78999540e4f47c0fc63256e2f84
Merathilis <neo0608@gmail.com>
2025-12-04 14:24:00 +0100

    copy & paste skills -100

--- Modules/Social/ChatText.lua
+++ Modules/Social/ChatText.lua
@@ -1975,7 +1975,7 @@ function CT:MessageFormatter(
 				and gsub(message, arg2, senderLink, 1)
 			or message
 	elseif strsub(chatType, -5) == 'EMOTE' then
-		body = format(_G['CHAT_' .. chatType .. '_GET'] .. message, pflag .. senderLink)
+		body = format(_G["CHAT_" .. chatType .. "_GET"] .. message, pflag .. senderLink)
 	else
 		body = format(_G["CHAT_" .. chatType .. "_GET"] .. "%s", pflag .. senderLink, message)
 	end

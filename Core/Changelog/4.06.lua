cd4e209dc068351a5413a2fade0b9204287afbd2
Zhou Fang <fang2hou@outlook.com>
2025-12-09 02:13:25 +0900

    docs: update changelog

---
+++ Core/Changelog/4.06.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[406] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[游戏条] 新增住宅按钮.",
+		},
+		["zhTW"] = {
+			"[遊戲條] 新增住宅按鈕.",
+		},
+		["enUS"] = {
+			"[Game Bar] Add Home button.",
+		},
+		["koKR"] = {
+			"[게임 바] 홈 버튼이 추가되었습니다.",
+		},
+		["ruRU"] = {
+			"[Игровая панель] Добавлена кнопка 'Домой'.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[游戏条] 将之前的家按钮改动为炉石按钮.",
+		},
+		["zhTW"] = {
+			"[遊戲條] 將之前的家按鈕改動為爐石按鈕.",
+		},
+		["enUS"] = {
+			"[Game Bar] Change the previous Home button to Hearthstone button.",
+		},
+		["koKR"] = {
+			"[게임 바] 이전 홈 버튼이 하스스톤 버튼으로 변경되었습니다.",
+		},
+		["ruRU"] = {
+			"[Игровая панель] Кнопка 'Домой' изменена на кнопку 'Камень возвращения'.",
+		},
+	},
+}

2c99b6de02a74302c83077ae07a92a76ba1a8f0b
Zhou Fang <fang2hou@outlook.com>
2026-01-02 23:39:25 +0900

    refactor: update inspect skin

--- Modules/Skins/Blizzard/Inspect.lua
+++ Modules/Skins/Blizzard/Inspect.lua
@@ -3,28 +3,11 @@ local S = W.Modules.Skins ---@type Skins
 
 local _G = _G
 
-local function DeleteBackdrop(count)
-	count = count or 0
-
-	if count > 20 then
-		return
-	end
-
-	if _G.InspectModelFrame.backdrop then
-		_G.InspectModelFrame.backdrop:Kill()
-	else
-		E:Delay(0.05, function()
-			DeleteBackdrop(count + 1)
-		end)
-	end
-end
-
 function S:Blizzard_InspectUI()
 	if not self:CheckDB("inspect") then
 		return
 	end
 
-	-- 观察面板
 	self:CreateShadow(_G.InspectFrame)
 	for i = 1, 4 do
 		self:ReskinTab(_G["InspectFrameTab" .. i])
@@ -33,7 +16,7 @@ function S:Blizzard_InspectUI()
 	self:CreateShadow(_G.InspectPaperDollFrame.ViewButton)
 	self:CreateShadow(_G.InspectPaperDollItemsFrame.InspectTalents)
 
-	-- 去除人物模型背景
+	-- Remove character model background
 	local InspectModelFrame = _G.InspectModelFrame
 	if InspectModelFrame then
 		InspectModelFrame:DisableDrawLayer("BACKGROUND")
@@ -41,7 +24,11 @@ function S:Blizzard_InspectUI()
 		InspectModelFrame:DisableDrawLayer("OVERLAY")
 	end
 
-	DeleteBackdrop()
+	F.WaitFor(function()
+		return _G.InspectModelFrame and _G.InspectModelFrame.backdrop
+	end, function()
+		_G.InspectModelFrame.backdrop:Kill()
+	end)
 end
 
 S:AddCallbackForAddon("Blizzard_InspectUI")

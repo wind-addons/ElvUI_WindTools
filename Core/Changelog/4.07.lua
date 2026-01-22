3781e98f0378648cfef9ce0b2ac41a07574664ff
Zhou Fang <fang2hou@outlook.com>
2026-01-22 22:32:30 +0900

    docs: update changelog

---
+++ Core/Changelog/4.07.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[407] = {
+	RELEASE_DATE = "2026/01/22",
+	IMPORTANT = {
+		["zhCN"] = {
+			"由于暴雪 API 大幅改动, 部分功能暂时无法使用, 请耐心等待后续更新.",
+			"初步适配魔兽世界 12.0.0 版本.",
+			"适配 ElvUI git 版本.",
+		},
+		["zhTW"] = {
+			"由於暴雪 API 大幅改動, 部分功能暫時無法使用, 請耐心等待後續更新.",
+			"初步適配魔獸世界 12.0.0 版本.",
+			"適配 ElvUI git 版本.",
+		},
+		["enUS"] = {
+			"Due to significant changes in the Blizzard API, some features are temporarily unavailable. Please wait patiently for future updates.",
+			"Preliminary adaptation for World of Warcraft version 12.0.0.",
+			"Adapted for ElvUI git version.",
+		},
+		["koKR"] = {
+			"블리자드 API의 대대적인 변경으로 인해 일부 기능을 일시적으로 사용할 수 없습니다. 향후 업데이트를 기다려 주십시오.",
+			"월드 오브 워크래프트 버전 12.0.0에 대한 예비 적응.",
+			"ElvUI git 버전에 적응.",
+		},
+		["ruRU"] = {
+			"Из-за значительных изменений в API Blizzard некоторые функции временно недоступны. Пожалуйста, дождитесь будущих обновлений.",
+			"Предварительная адаптация для версии World of Warcraft 12.0.0.",
+			"Адаптировано для версии ElvUI git.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+}

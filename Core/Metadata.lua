2afd8b3fffa94ac7af84943aeb293f376bed5a89
Zhou Fang <fang2hou@outlook.com>
2026-01-22 22:34:01 +0900

    feat: bump up minimum elvui version

--- Core/Metadata.lua
+++ Core/Metadata.lua
@@ -29,7 +29,7 @@ W.PlainTitle = gsub(W.Title, "|c........([^|]+)|r", "%1")
 W.Locale = GetLocale()
 W.ChineseLocale = strsub(W.Locale, 0, 2) == "zh"
 W.AsianLocale = W.ChineseLocale or W.Locale == "koKR"
-W.SupportElvUIVersion = 14.05
+W.SupportElvUIVersion = 14.07
 W.UseKeyDown = C_CVar_GetCVarBool("ActionButtonUseKeyDown")
 
 -- Game

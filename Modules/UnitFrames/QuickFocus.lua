c09ab6e64a4bdf7a4be1aabaeaa5faa4f2e28eef
Zhou Fang <fang2hou@outlook.com>
2026-01-29 12:44:53 +0900

    fix: use config button

--- Modules/UnitFrames/QuickFocus.lua
+++ Modules/UnitFrames/QuickFocus.lua
@@ -117,7 +117,7 @@ function QF:Initialize()
 	end
 
 	local button = CreateFrame("Button", "WTQuickFocusButton", E.UIParent, "SecureActionButtonTemplate")
-	button:SetAttribute("type1", "macro")
+	button:SetAttribute("type" .. strsub(self.db.button, 7, 7), "macro")
 	button:SetAttribute("macrotext", self:GetMacroText())
 	button:RegisterForClicks(W.UseKeyDown and "AnyDown" or "AnyUp")
 	SetOverrideBindingClick(button, true, self.db.modifier .. "-" .. self.db.button, "WTQuickFocusButton")

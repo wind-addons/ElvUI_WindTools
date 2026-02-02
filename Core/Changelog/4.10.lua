0ab8a1d0b0288f20f4db1036d3ca0c1b1eaee269
Zhou Fang <fang2hou@outlook.com>
2026-02-02 13:04:49 +0900

    docs: 4.10 start

---
+++ Core/Changelog/4.10.lua
@@ -0,0 +1,56 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[410] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[额外物品条] 加回了旧版本中存在的 POSTIONSL, FLASKDF 等组别.",
+			"[额外物品条] 从 POTIONTWW, POTIONMN 中分离出了 POTIONGN 组别, 特别用来放置一些跨版本物品, 如治疗石.",
+		},
+		["zhTW"] = {
+			"[額外物品條] 加回了舊版本中存在的 POSTIONSL, FLASKDF 等組別.",
+			"[額外物品條] 從 POTIONTWW, POTIONMN 中分離出了 POTIONGN 組別, 特別用來放置一些跨版本物品, 如治療石.",
+		},
+		["enUS"] = {
+			"[Extra Item Bar] Re-added groups like POSTIONSL, FLASKDF that existed in the old version.",
+			"[Extra Item Bar] Separated POTIONGN from POTIONTWW and POTIONMN, specifically for placing some cross-version items, such as Healthstones.",
+		},
+		["koKR"] = {
+			"[추가 아이템 바] 이전 버전에 존재했던 POSTIONSL, FLASKDF 등의 그룹을 다시 추가했습니다.",
+			"[추가 아이템 바] POTIONTWW 및 POTIONMN에서 POTIONGN을 분리하여 건강석과 같은 일부 교차 버전 아이템을 배치하는 데 사용됩니다.",
+		},
+		["ruRU"] = {
+			"[Дополнительная панель предметов] Повторно добавлены такие группы, как POSTIONSL, FLASKDF, которые существовали в старой версии.",
+			"[Дополнительная панель предметов] POTIONGN была отделена от POTIONTWW и POTIONMN, специально для размещения некоторых кросс-версионных предметов, таких как Камни здоровья.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[标签] 现在可以在 ElvUI 的可用标签页面查看额外增加的标签了.",
+			"[美化皮肤] 优化了 Rematch 插件的皮肤适配.",
+		},
+		["zhTW"] = {
+			"[標籤] 現在可以在 ElvUI 的可用標籤頁面查看額外增加的標籤了.",
+			"[美化皮膚] 優化了 Rematch 的皮膚適配.",
+		},
+		["enUS"] = {
+			"[Tags] You can now view the additionally added tags on ElvUI's Available Tags page.",
+			"[Skins] Optimized Rematch skin.",
+		},
+		["koKR"] = {
+			"[태그] 이제 ElvUI의 사용 가능한 태그 페이지에서 추가된 태그를 볼 수 있습니다.",
+			"[스킨] Rematch 스킨을 최적화했습니다.",
+		},
+		["ruRU"] = {
+			"[Теги] Теперь вы можете просматривать дополнительно добавленные теги на странице доступных тегов ElvUI.",
+			"[Скины] Оптимизирован скин Rematch.",
+		},
+	},
+}

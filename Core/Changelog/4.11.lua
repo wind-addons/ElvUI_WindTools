8f3b9bbb153af7bea953dbf36f785d80e0ffccfb
Zhou Fang <fang2hou@outlook.com>
2026-02-06 23:31:36 +0900

    docs: 4.11

---
+++ Core/Changelog/4.11.lua
@@ -0,0 +1,41 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[411] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"优化了一个全局变量泄露问题.",
+			"[通告] 移除了通告模块中所有在战斗中受限的功能.",
+		},
+		["zhTW"] = {
+			"優化了一個全局變量洩露問題.",
+			"[通告] 移除了通告模塊中所有在戰鬥中受限的功能.",
+		},
+		["enUS"] = {
+			"Optimized a global variable leak issue.",
+			"[Announcement] Removed all in-combat restricted features in Announcement module.",
+		},
+		["koKR"] = {
+			"글로벌 변수 누수 문제를 최적화했습니다.",
+			"[공지] 공지 모듈에서 전투 중 제한된 모든 기능을 제거했습니다.",
+		},
+		["ruRU"] = {
+			"Оптимизирована проблема утечки глобальных переменных.",
+			"[Объявление] Удалены все функции, ограниченные в бою, в модуле Объявления.",
+		},
+	},
+}

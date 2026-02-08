5cacdeec389a52b6366be76cd1b64a2b3d9aaa2c
Zhou Fang <fang2hou@outlook.com>
2026-02-08 23:14:21 +0900

    fix: fix the no kanji math

--- Modules/Misc/Math.lua
+++ Modules/Misc/Math.lua
@@ -3,10 +3,57 @@ local M = W.Modules.Misc ---@class Misc
 
 function M:Math()
 	if E.private.WT.misc.noKanjiMath then
-		E.ShortPrefixStyles.CHINESE = { { 1e8, "Y" }, { 1e4, "W" } }
-		E.ShortPrefixStyles.TCHINESE = { { 1e8, "Y" }, { 1e4, "W" } }
+		hooksecurefunc(E, "BuildAbbreviateConfigs", function()
+			if not (E.db.general.numberPrefixStyle == "CHINESE" or E.db.general.numberPrefixStyle == "TCHINESE") then
+				return
+			end
 
-		E:BuildPrefixValues()
+			local asianUnits = {
+				CHINESE = { { 1e12, "Z" }, { 1e8, "Y" }, { 1e4, "W" } },
+				TCHINESE = { { 1e12, "Z" }, { 1e8, "Y" }, { 1e4, "W" } },
+				KOREAN = E.ShortPrefixStyles.KOREAN,
+			}
+
+			local asianDivisors = { 1e11, 1e7, 1e3 }
+
+			local short = { breakpoints = {} }
+			local long = { long = true }
+
+			E.Abbreviate.short = short
+			E.Abbreviate.long = long
+
+			local style = E.db.general.numberPrefixStyle
+			local asian = asianUnits[style]
+			local units = asian
+
+			long.isAsian = asian
+			short.isAsian = asian
+
+			local decimal = E.db.general.decimalLength or 1
+			if decimal > 3 then
+				decimal = 3
+			end
+
+			for i = 1, 3 do
+				local unit = units[i]
+
+				short.breakpoints[i] = {
+					breakpoint = unit[1],
+					abbreviation = unit[2],
+					significandDivisor = asianDivisors[i],
+					fractionDivisor = 10,
+					abbreviationIsGlobal = false,
+				}
+			end
+
+			if CreateAbbreviateConfig then
+				short.config = CreateAbbreviateConfig(short.breakpoints)
+
+				wipe(short.breakpoints)
+			end
+		end)
+
+		E:BuildAbbreviateConfigs()
 	end
 end
 

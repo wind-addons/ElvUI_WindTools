d1fee5e3d20b59fd97977b0b8cafa6918588b582
Zhou Fang <fang2hou@outlook.com>
2026-02-17 23:29:21 +0900

    docs: 4.12

---
+++ Core/Changelog/4.12.lua
@@ -0,0 +1,66 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[412] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[数据条] 在微型菜单中新增了任务和家宅选项.",
+			"[小地图按钮] 新增了隐藏和忽略正则表达式的选项. 部分代码来自 E11, 感谢贡献",
+		},
+		["zhTW"] = {
+			"[數據條] 在微型菜單中新增了任務和家宅選項.",
+			"[小地圖按鈕] 新增了隱藏和忽略正則表達式的選項. 部分代碼來自 E11, 感謝貢獻",
+		},
+		["enUS"] = {
+			"[Datatext] Added Quest and Garrison options to the micro menu.",
+			"[Minimap Button] Added options for hide and ignore regex. Part of the code is from E11, thanks for the contribution.",
+		},
+		["koKR"] = {
+			"[데이터바] 마이크로 메뉴에 퀘스트와 주둔지 옵션이 추가되었습니다.",
+			"[미니맵 버튼] 숨기기 및 무시 정규식 옵션이 추가되었습니다. 일부 코드는 E11에서 가져온 것으로, 기여에 감사드립니다.",
+		},
+		["ruRU"] = {
+			"[Дататекст] Добавлены опции Квест и Гарнизон в микроменю.",
+			"[Кнопка миникарты] Добавлены опции для скрытия и игнорирования регулярных выражений. Часть кода взята из E11, спасибо за вклад.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[美化皮肤] 修复了 Simple Addon Manager 皮肤.",
+			"[美化皮肤] 为 ElvUI 姓名板皮肤添加了光环图标的阴影边框.",
+			"[美化皮肤] 优化了公会社区界面皮肤, 降低了导致污染的可能性.",
+			"[鼠标提示] 修复了多个可能导致鼠标提示信息查询时可能出现的错误.",
+		},
+		["zhTW"] = {
+			"[美化皮肤] 修復了 Simple Addon Manager 皮膚.",
+			"[美化皮肤] 為 ElvUI 姓名板皮膚添加了光環圖標的陰影邊框.",
+			"[美化皮肤] 優化了公會社區界面皮膚, 降低了導致污染的可能性.",
+			"[浮動提示] 修復了多個可能導致鼠標提示信息查詢時可能出現的錯誤.",
+		},
+		["enUS"] = {
+			"[Skins] Fixed the skin for Simple Addon Manager.",
+			"[Skins] Added shadow borders to the aura icons for ElvUI Nameplate skin.",
+			"[Skins] Optimized the skin for Guild & Community, reducing the possibility of causing taint.",
+			"[Tooltips] Fixed multiple possible errors that may occur when querying tooltip information.",
+		},
+		["koKR"] = {
+			"[스킨] Simple Addon Manager 스킨이 수정되었습니다.",
+			"[스킨] ElvUI 이름표 스킨에 오라 아이콘의 그림자 테두리가 추가되었습니다.",
+			"[스킨] 길드 및 커뮤니티 스킨이 최적화되어 오염 가능성이 줄어들었습니다.",
+			"[툴팁] 툴팁 정보를 조회할 때 발생할 수 있는 여러 가능한 오류가 수정되었습니다.",
+		},
+		["ruRU"] = {
+			"[Скины] Исправлена тема для Simple Addon Manager.",
+			"[Скины] Добавлены теневые границы к иконкам аур для темы ElvUI Nameplate.",
+			"[Скины] Оптимизирована тема для Гильдии и Сообщества, что снижает вероятность возникновения загрязнения.",
+			"[Подсказки] Исправлено несколько возможных ошибок, которые могут возникать при запросе информации в подсказках.",
+		},
+	},
+}

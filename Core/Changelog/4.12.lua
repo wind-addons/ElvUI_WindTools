3ca3082f9e305d3a410d6e9c1ed30e44ae97ef40
Zhou Fang <fang2hou@outlook.com>
2026-02-23 14:08:43 +0900

    docs: update changelog

--- Core/Changelog/4.12.lua
+++ Core/Changelog/4.12.lua
@@ -4,12 +4,12 @@ W.Changelog[412] = {
 	RELEASE_DATE = "TBD",
 	IMPORTANT = {
 		["zhCN"] = {
-			"[美化皮肤] UIErrors 改为使用私有框体并同步原框体显隐逻辑, 以降低污染风险.",
-			"[美化皮肤] UIErrors 功能默认开启, 并新增高度选项.",
+			"[美化皮肤] 错误框体皮肤改为使用私有框体并同步原框体显隐逻辑, 以降低污染风险.",
+			"[美化皮肤] 错误框体功能默认开启, 并新增高度选项.",
 		},
 		["zhTW"] = {
-			"[美化皮肤] UIErrors 改為使用私有框架並同步原框架顯隱邏輯, 以降低污染風險.",
-			"[美化皮肤] UIErrors 功能預設開啟, 並新增高度選項.",
+			"[美化皮肤] 錯誤框架皮膚改為使用私有框架並同步原框架顯隱邏輯, 以降低污染風險.",
+			"[美化皮肤] 錯誤框架功能預設開啟, 並新增高度選項.",
 		},
 		["enUS"] = {
 			"[Skins] UIErrors now uses a private frame and synchronizes native frame visibility logic to reduce taint risk.",
@@ -28,59 +28,74 @@ W.Changelog[412] = {
 		["zhCN"] = {
 			"[数据条] 在微型菜单中新增了任务和家宅选项.",
 			"[小地图按钮] 新增了隐藏和忽略正则表达式的选项. 部分代码来自 E11, 感谢贡献",
+			"[美化皮肤] 新增了 HandyNotes MapNotes 的皮肤.",
 		},
 		["zhTW"] = {
 			"[數據條] 在微型菜單中新增了任務和家宅選項.",
 			"[小地圖按鈕] 新增了隱藏和忽略正則表達式的選項. 部分代碼來自 E11, 感謝貢獻",
+			"[美化皮肤] 新增了 HandyNotes MapNotes 的皮肤.",
 		},
 		["enUS"] = {
 			"[Datatext] Added Quest and Garrison options to the micro menu.",
 			"[Minimap Button] Added options for hide and ignore regex. Part of the code is from E11, thanks for the contribution.",
+			"[Skins] Added skin for HandyNotes MapNotes.",
 		},
 		["koKR"] = {
 			"[데이터바] 마이크로 메뉴에 퀘스트와 주둔지 옵션이 추가되었습니다.",
 			"[미니맵 버튼] 숨기기 및 무시 정규식 옵션이 추가되었습니다. 일부 코드는 E11에서 가져온 것으로, 기여에 감사드립니다.",
+			"[스킨] HandyNotes MapNotes의 스킨이 추가되었습니다.",
 		},
 		["ruRU"] = {
 			"[Текст данных] В микроменю добавлены опции 'Задания' и 'Гарнизон'.",
 			"[Кнопка миникарты] Добавлены опции для скрытия и игнорирования регулярных выражений. Часть кода взята из E11, спасибо за вклад.",
+			"[Скины] Добавлена тема для HandyNotes MapNotes.",
 		},
 	},
 	IMPROVEMENT = {
 		["zhCN"] = {
 			"[美化皮肤] 修复了 Simple Addon Manager 皮肤.",
+			"[美化皮肤] 优化了 Collectionator 皮肤, 支持其最新的家具页面.",
 			"[美化皮肤] 为 ElvUI 姓名板皮肤添加了光环图标的阴影边框.",
 			"[美化皮肤] 优化了公会社区界面皮肤, 降低了导致污染的可能性.",
 			"[美化皮肤] 优化了冷却管理器管理界面皮肤, 降低了导致污染的可能性.",
 			"[鼠标提示] 修复了多个可能导致鼠标提示信息查询时可能出现的错误.",
+			"[世界地图] 修复了去除迷雾在破碎群岛地图上可能无法正确显示阿古斯区域的问题.",
 		},
 		["zhTW"] = {
 			"[美化皮肤] 修復了 Simple Addon Manager 皮膚.",
+			"[美化皮肤] 優化了 Collectionator 皮膚, 支持其最新的家具頁面.",
 			"[美化皮肤] 為 ElvUI 姓名板皮膚添加了光環圖標的陰影邊框.",
 			"[美化皮肤] 優化了公會社區界面皮膚, 降低了導致污染的可能性.",
 			"[美化皮肤] 優化了冷卻管理器管理界面皮膚, 降低了導致污染的可能性.",
 			"[浮動提示] 修復了多個可能導致鼠標提示信息查詢時可能出現的錯誤.",
+			"[世界地圖] 修復了去除迷霧在破碎群島地圖上可能無法正確顯示阿古斯區域的問題.",
 		},
 		["enUS"] = {
 			"[Skins] Fixed the skin for Simple Addon Manager.",
+			"[Skins] Optimized the skin for Collectionator, supporting its decor page.",
 			"[Skins] Added shadow borders to the aura icons for ElvUI Nameplate skin.",
 			"[Skins] Optimized the skin for Guild & Community, reducing the possibility of causing taint.",
 			"[Skins] Optimized the skin for Cooldown Manager Setting, reducing the possibility of causing taint.",
 			"[Tooltips] Fixed multiple possible errors that may occur when querying tooltip information.",
+			"[World Map] Fixed an issue where removing fog of war may not correctly display the Argus area on the Broken Isles map.",
 		},
 		["koKR"] = {
 			"[스킨] Simple Addon Manager 스킨이 수정되었습니다.",
+			"[스킨] Collectionator 스킨이 최적화되어 최신 가구 페이지를 지원합니다.",
 			"[스킨] ElvUI 이름표 스킨에 오라 아이콘의 그림자 테두리가 추가되었습니다.",
 			"[스킨] 길드 및 커뮤니티 스킨이 최적화되어 오염 가능성이 줄어들었습니다.",
 			"[스킨] 쿨다운 관리자 설정 스킨이 최적화되어 오염 가능성이 줄어들었습니다.",
 			"[툴팁] 툴팁 정보를 조회할 때 발생할 수 있는 여러 가능한 오류가 수정되었습니다.",
+			"[세계 지도] 안개 제거가 부서진 섬 지도에서 아르거스 지역을 올바르게 표시하지 못할 수 있는 문제를 수정했습니다.",
 		},
 		["ruRU"] = {
 			"[Скины] Исправлена тема для Simple Addon Manager.",
+			"[Скины] Оптимизирована тема для Collectionator, поддерживающая его последнюю страницу декора.",
 			"[Скины] Добавлены теневые границы к иконкам аур для темы индикаторов здоровья ElvUI.",
 			"[Скины] Оптимизирована тема для Гильдии и Сообщества, что снижает вероятность возникновения ошибок Blizzard.",
 			"[Скины] Оптимизирована тема для настроек Менеджера перезарядки, что снижает вероятность возникновения ошибок Blizzard.",
 			"[Подсказки] Исправлено несколько возможных ошибок, которые могут возникать при запросе информации в подсказках.",
+			"[Карта мира] Исправлена проблема, из-за которой удаление тумана войны может некорректно отображать область Аргуса на карте Расколотых островов.",
 		},
 	},
 }

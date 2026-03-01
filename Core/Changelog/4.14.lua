57ae95a0ae7f3c1fb199077806443397937f3f55
Zhou Fang <fang2hou@outlook.com>
2026-03-01 21:35:55 +0900

    docs: 4.14 changelog

---
+++ Core/Changelog/4.14.lua
@@ -0,0 +1,46 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[414] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {
+			"更新了 LibRangeCheck-3.0 库, 能够更加准确地检测距离.",
+		},
+		["zhTW"] = {
+			"更新了 LibRangeCheck-3.0 函式庫, 能夠更加準確地檢測距離.",
+		},
+		["enUS"] = {
+			"Updated the LibRangeCheck-3.0 library, which can now detect distances more accurately.",
+		},
+		["koKR"] = {
+			"LibRangeCheck-3.0 라이브러리가 업데이트되어 거리를 보다 정확하게 감지할 수 있습니다.",
+		},
+		["ruRU"] = {
+			"Обновлена библиотека LibRangeCheck-3.0, которая теперь может более точно определять расстояния.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[小地图按钮] 优化了与 RestedXP Guides 插件的兼容性, 现在不会再出现一个空白区域了.",
+		},
+		["zhTW"] = {
+			"[小地圖按鍵] 優化了與 RestedXP Guides 插件的相容性, 現在不會再出現一個空白區域了.",
+		},
+		["enUS"] = {
+			"[Minimap Button] Improved compatibility with the RestedXP Guides addon, which should no longer cause a blank area to appear.",
+		},
+		["koKR"] = {
+			"[미니맵 버튼] RestedXP Guides 애드온과의 호환성이 향상되어 더 이상 빈 영역이 나타나지 않습니다.",
+		},
+		["ruRU"] = {
+			"[Кнопка миникарты] Улучшена совместимость с аддоном RestedXP Guides, который больше не должен вызывать появление пустой области.",
+		},
+	},
+}

8e85bd39b0549ec28b2dd8ee2ddd938ad20e554a
Zhou Fang <fang2hou@outlook.com>
2026-03-02 13:28:44 +0900

    docs: update changelog

--- Core/Changelog/4.14.lua
+++ Core/Changelog/4.14.lua
@@ -20,27 +20,47 @@ W.Changelog[414] = {
 		},
 	},
 	NEW = {
-		["zhCN"] = {},
-		["zhTW"] = {},
-		["enUS"] = {},
-		["koKR"] = {},
-		["ruRU"] = {},
+		["zhCN"] = {
+			"新增了 [战斗] - [取消图腾] 模块, 支持绑定按键或者通过宏命令来取消图腾.",
+			"[美化外观] 新增了 Angleur 皮肤.",
+		},
+		["zhTW"] = {
+			"新增了 [戰鬥] - [取消圖騰] 模組, 支持綁定按鍵或者通過宏命令來取消圖騰.",
+			"[美化外觀] 新增了 Angleur 皮膚.",
+		},
+		["enUS"] = {
+			"Added a new module [Combat] - [Destroy Totem], which supports binding keys or using macro commands to cancel totems.",
+			"[Skins] Added the Angleur skin.",
+		},
+		["koKR"] = {
+			"새로운 모듈 [전투] - [토템 파괴]가 추가되어 키 바인딩이나 매크로 명령을 사용하여 토템을 취소할 수 있습니다.",
+			"[스킨] Angleur 스킨이 추가되었습니다.",
+		},
+		["ruRU"] = {
+			"Добавлен новый модуль [Бой] - [Уничтожить тотем], который поддерживает привязку клавиш или использование макрокоманд для отмены тотемов.",
+			"[Скины] Добавлен скин Angleur.",
+		},
 	},
 	IMPROVEMENT = {
 		["zhCN"] = {
 			"[小地图按钮] 优化了与 RestedXP Guides 插件的兼容性, 现在不会再出现一个空白区域了.",
+			"[护盾] 优化了护盾材质的显示.",
 		},
 		["zhTW"] = {
 			"[小地圖按鍵] 優化了與 RestedXP Guides 插件的相容性, 現在不會再出現一個空白區域了.",
+			"[護盾] 優化了護盾材質的顯示.",
 		},
 		["enUS"] = {
 			"[Minimap Button] Improved compatibility with the RestedXP Guides addon, which should no longer cause a blank area to appear.",
+			"[Shield] Improved the display of shield textures.",
 		},
 		["koKR"] = {
 			"[미니맵 버튼] RestedXP Guides 애드온과의 호환성이 향상되어 더 이상 빈 영역이 나타나지 않습니다.",
+			"[방패] 방패 텍스처의 표시가 개선되었습니다.",
 		},
 		["ruRU"] = {
 			"[Кнопка миникарты] Улучшена совместимость с аддоном RestedXP Guides, который больше не должен вызывать появление пустой области.",
+			"[Щит] Улучшено отображение текстур щита.",
 		},
 	},
 }

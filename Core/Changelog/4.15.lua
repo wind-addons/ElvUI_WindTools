d50991a9518e933c2d9f928bc9c8345d587fd117
Zhou Fang <fang2hou@outlook.com>
2026-03-08 01:17:42 +0900

    docs: update changelog

--- Core/Changelog/4.15.lua
+++ Core/Changelog/4.15.lua
@@ -20,22 +20,27 @@ W.Changelog[415] = {
 		["zhCN"] = {
 			"[游戏条] 修复了点击好友列表图标有时候会变为添加好友的问题.",
 			"[装备观察] 更新了至暗之夜中额外宝石插槽的显示逻辑.",
+			"[右键菜单] 放宽污染防止策略, 现在只有在右键战网好友时才需要使用 Shift 来显示菜单, 其他情况下无需按住 Shift 键.",
 		},
 		["zhTW"] = {
 			"[遊戲條] 修復了點擊好友列表圖標有時候會變為添加好友的問題.",
 			"[裝備觀察] 更新了至暗之夜中額外寶石插槽的顯示邏輯.",
+			"[右鍵菜單] 放寬污染防止策略, 現在只有在右鍵戰網好友時才需要使用 Shift 來顯示菜單, 其他情況下無需按住 Shift 鍵.",
 		},
 		["enUS"] = {
 			"[Game Bar] Fixed an issue where clicking the friends list icon sometimes changed to add friend.",
 			"[Inspect] Updated the display logic for additional gem sockets in Shadowlands.",
+			"[Context Menu] Relaxed the anti-taint strategy, now only requiring Shift to show the menu when right-clicking a Battle.net friend, and no longer requiring Shift for other cases.",
 		},
 		["koKR"] = {
 			"[게임 바] 친구 목록 아이콘을 클릭할 때 가끔 친구 추가로 변경되는 문제를 수정했습니다.",
 			"[검사] 어둠땅에서 추가 보석 슬롯에 대한 표시 로직을 업데이트했습니다.",
+			"[컨텍스트 메뉴] 오염 방지 전략을 완화하여 이제 Battle.net 친구를 오른쪽 클릭할 때 Shift 키를 눌러야 메뉴가 표시되고, 다른 경우에는 Shift 키가 더 이상 필요하지 않습니다.",
 		},
 		["ruRU"] = {
 			"[Панель игры] Исправлена проблема, из-за которой при нажатии на значок списка друзей иногда происходило добавление друга.",
 			"[Осмотр] Обновлена логика отображения дополнительных слотов для самоцветов в Shadowlands.",
+			"[Контекстное меню] Ослаблена стратегия предотвращения загрязнения, теперь для отображения меню при правом клике на друга Battle.net требуется нажать Shift, а в других случаях Shift больше не требуется.",
 		},
 	},
 }

95898a2b51aa5da374388b5c06389dfca5605588
Zhou Fang <fang2hou@outlook.com>
2026-03-07 23:08:47 +0900

    docs: 4.15 changelog

---
+++ Core/Changelog/4.15.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[415] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {
+			"[游戏条] 修复了点击好友列表图标有时候会变为添加好友的问题.",
+		},
+		["zhTW"] = {
+			"[遊戲條] 修復了點擊好友列表圖標有時候會變為添加好友的問題.",
+		},
+		["enUS"] = {
+			"[Game Bar] Fixed an issue where clicking the friends list icon sometimes changed to add friend.",
+		},
+		["koKR"] = {
+			"[게임 바] 친구 목록 아이콘을 클릭할 때 가끔 친구 추가로 변경되는 문제를 수정했습니다.",
+		},
+		["ruRU"] = {
+			"[Панель игры] Исправлена проблема, из-за которой при нажатии на значок списка друзей иногда происходило добавление друга.",
+		},
+	},
+}

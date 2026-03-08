5ef5d9c0057f2703f31cf328caab3d9b40cc4ea0
Zhou Fang <fang2hou@outlook.com>
2026-03-08 15:03:34 +0900

    luals: fix the gametooltip api defination

--- .luals/General.lua
+++ .luals/General.lua
@@ -14,3 +14,13 @@
 ---@class LuaCurveObjectBase
 ---@class LuaCurveObject : LuaCurveObjectBase
 ---@class AbbreviateConfig
+
+
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GameTooltip_SetText)
+---@param text string
+---@param colorR? number
+---@param colorG? number
+---@param colorB? number
+---@param alpha? number
+---@param wrap? boolean
+function GameTooltip:SetText(text, colorR, colorG, colorB, alpha, wrap) end

0cfeff12f41cfbe85448fc3981db0633a338c04c
Zhou Fang <fang2hou@outlook.com>
2026-03-08 14:32:40 +0900

    docs: fix changelog

--- Core/Changelog/4.15.lua
+++ Core/Changelog/4.15.lua
@@ -31,19 +31,19 @@ W.Changelog[415] = {
 		},
 		["enUS"] = {
 			"[Game Bar] Fixed an issue where clicking the friends list icon sometimes changed to add friend.",
-			"[Inspect] Updated the display logic for additional gem sockets in Shadowlands.",
+			"[Inspect] Updated the display logic for additional gem sockets in Midnight.",
 			"[Context Menu] Relaxed the anti-taint strategy, now only requiring Shift to show the menu when right-clicking a Battle.net friend, and no longer requiring Shift for other cases.",
 			"[Skins] Optimized the skinning performance for Blizzard's bags.",
 		},
 		["koKR"] = {
 			"[게임 바] 친구 목록 아이콘을 클릭할 때 가끔 친구 추가로 변경되는 문제를 수정했습니다.",
-			"[검사] 어둠땅에서 추가 보석 슬롯에 대한 표시 로직을 업데이트했습니다.",
+			"[검사] 한밤중에서 추가 보석 슬롯에 대한 표시 로직을 업데이트했습니다.",
 			"[컨텍스트 메뉴] 오염 방지 전략을 완화하여 이제 Battle.net 친구를 오른쪽 클릭할 때 Shift 키를 눌러야 메뉴가 표시되고, 다른 경우에는 Shift 키가 더 이상 필요하지 않습니다.",
 			"[스킨] 블리자드 가방의 스킨 성능을 최적화했습니다.",
 		},
 		["ruRU"] = {
 			"[Панель игры] Исправлена проблема, из-за которой при нажатии на значок списка друзей иногда происходило добавление друга.",
-			"[Осмотр] Обновлена логика отображения дополнительных слотов для самоцветов в Темных Землях.",
+			"[Осмотр] Обновлена логика отображения дополнительных слотов для самоцветов в Полночь.",
 			"[Контекстное меню] Ослаблена стратегия предотвращения ошибок Blizzard. Теперь для отображения меню при ПКМ на друга в Battle.net требуется нажать Shift, а в других случаях Shift больше не требуется.",
 			"[Скины] Оптимизирована производительность скинов для сумок Blizzard.",
 		},

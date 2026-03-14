7944ec40a6d7e9be3bf401b28082489d22fc4fbc
Zhou Fang <fang2hou@outlook.com>
2026-03-15 02:26:05 +0900

    feat: add type

--- .luals/General.lua
+++ .luals/General.lua
@@ -24,3 +24,13 @@
 ---@param alpha? number
 ---@param wrap? boolean
 function GameTooltip:SetText(text, colorR, colorG, colorB, alpha, wrap) end
+
+---@class AceLocale-3.0
+local AceLocale = {}
+
+---@param application string Unique name of addon / module
+---@param locale? string|boolean Locale to get, defaults to the current locale
+---@param silent? boolean If true, the locale is optional, silently return nil if it's not found (defaults to false, optional)
+---@return table<string, string> locale The locale table for the current language.
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-locale-3-0#title-1)
+function AceLocale:GetLocale(application, locale, silent) end

e215a1190aafc6f269119590b8e00e0de9a87d7f
Zhou Fang <fang2hou@outlook.com>
2026-03-14 23:11:41 +0900

    feat: update color

--- Modules/Quest/PreyHunt.lua
+++ Modules/Quest/PreyHunt.lua
@@ -77,7 +77,7 @@ end
 
 local function NotifyStartTracking(questID)
 	local title = tostring(C_QuestLog_GetTitleForQuestID(questID) or questID)
-	title = C.StringByTemplate(title, "neutral-50")
+	title = C.StringByTemplate(title, "indigo-300")
 	local tag = C.StringByTemplate(format("[%s]", L["Prey Hunt"]), "amber-500")
 	F.Print(format("%s %s", tag, format(L["Start tracking %s."], title)))
 end

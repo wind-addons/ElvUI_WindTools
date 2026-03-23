cffd40e00fac3f4129a2d6bc730546ba2055955f
Zhou Fang <fang2hou@outlook.com>
2026-03-23 23:01:17 +0900

    docs: update changlog

--- Core/Changelog/4.16.lua
+++ Core/Changelog/4.16.lua
@@ -7,26 +7,31 @@ W.Changelog[416] = {
 			"继续优化插件的稳定性, 改善对秘密值的数据的处理模式. 感谢 Sargeraz00",
 			"本地化文件更新, 感谢 Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)",
 			"[鼠标提示] - [进度] 世界任务的怪物进度提示新增了许多至暗之夜数据, 更多数据将在未来的版本中陆续添加. 感谢 Dack",
+			"[鼠标提示] - [进度] 由于进度提示数据较多, 会明显提升插件的内存占用, 现在默认只会保留当前资料片的数据. 如果你有需要, 其他资料片的数据可以通过设定中的按钮来下载.",
 		},
 		["zhTW"] = {
 			"繼續優化插件的穩定性, 改善對秘密值的數據的處理模式. 感謝 Sargeraz00",
 			"本地化文件更新, 感謝 Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)",
 			"[滑鼠提示] - [進度] 世界任務的怪物進度提示新增了許多至暗之夜數據, 更多數據將在未來的版本中陸續添加. 感謝 Dack",
+			"[滑鼠提示] - [進度] 由於怪物進度提示數據較多, 會明顯提升插件的內存占用, 現在默認只會保留當前資料片的數據. 如果你有需要, 其他資料片的數據可以通過設定中的按鈕來下載.",
 		},
 		["enUS"] = {
 			"Continue to optimize the stability of the plugin and improve the processing of secret values. Thanks to Sargeraz00",
 			"Localization file updated, thanks to Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)",
-			"[Tooltip] - [Progress] The monster progress tips for world quests have added a lot of data for the Midnight Expansion, and more data will be added in future versions. Thanks to Dack",
+			"[Tooltip] - [Progress] The objective progress (e.g. +4%) for world quests have been updated for the Midnight Expansion, and more data will be added in future versions. Thanks to Dack",
+			"[Tooltip] - [Progress] Due to the large amount of data for the objective progress, it will significantly increase the memory usage of the addon. Now, by default, only the data for the current expansion will be retained. If you need data for other expansions, you can download it through the button in the settings.",
 		},
 		["koKR"] = {
 			"플러그인의 안정성을 개선하고 비밀 값의 데이터 처리를 개선합니다. Sargeraz00에게 감사드립니다.",
 			"현지화 파일이 업데이트되었습니다. Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)에게 감사드립니다.",
-			"[툴팁] - [진행 상황] 세계 퀘스트의 몬스터 진행 상황 팁에 가장 어두운 밤에 대한 많은 데이터가 추가되었으며 향후 버전에서 더 많은 데이터가 추가될 예정입니다. Dack에게 감사드립니다.",
+			"[툴팁] - [진행 상황] 월드 퀘스트의 목표 진행 상황(예: +4%)이 미드나이트 확장팩에 대해 업데이트되었으며, 향후 버전에 더 많은 데이터가 추가될 예정입니다. Dack에게 감사드립니다.",
+			"[툴팁] - [진행 상황] 목표 진행 상황에 대한 데이터가 많기 때문에 애드온의 메모리 사용량이 크게 증가합니다. 이제 기본적으로 현재 확장팩에 대한 데이터만 유지됩니다. 다른 확장팩에 대한 데이터가 필요한 경우 설정의 버튼을 통해 다운로드할 수 있습니다.",
 		},
 		["ruRU"] = {
 			"Повышена стабильность плагина и улучшена обработка секретных значений. Спасибо Sargeraz00",
 			"Файл локализации обновлен, спасибо Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)",
 			"[Подсказка] - [Прогресс] Подсказки по прогрессу монстров для мировых заданий добавили много данных для 'Полночи', и в будущих версиях будет добавлено ещё больше данных. Спасибо Dack",
+			"[Подсказка] - [Прогресс] Из-за большого объема данных для подсказок по прогрессу монстров, это значительно увеличивает использование памяти аддоном. Теперь по умолчанию сохраняются только данные для текущего дополнения. Если вам нужны данные для других дополнений, вы можете скачать их через кнопку в настройках.",
 		},
 	},
 	NEW = {

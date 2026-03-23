f81c633fe142d4a3c34742558e17649a7fa4b3fc
Zhou Fang <fang2hou@outlook.com>
2026-03-23 09:48:15 +0900

    docs: update changelog

--- Core/Changelog/4.16.lua
+++ Core/Changelog/4.16.lua
@@ -1,7 +1,7 @@
 local W = unpack((select(2, ...))) ---@type WindTools
 
 W.Changelog[416] = {
-	RELEASE_DATE = "2026/03/19",
+	RELEASE_DATE = "2026/03/20",
 	IMPORTANT = {
 		["zhCN"] = {
 			"继续优化插件的稳定性, 改善对秘密值的数据的处理模式. 感谢 Sargeraz00",
@@ -20,12 +20,12 @@ W.Changelog[416] = {
 		},
 		["koKR"] = {
 			"플러그인의 안정성을 개선하고 비밀 값의 데이터 처리를 개선합니다. Sargeraz00에게 감사드립니다.",
-			"현지화 파일이 업데이트되었습니다. Crazyyoungs (krKR), Dlarge (deDE)에게 감사드립니다.",
+			"현지화 파일이 업데이트되었습니다. Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)에게 감사드립니다.",
 			"[툴팁] - [진행 상황] 세계 퀘스트의 몬스터 진행 상황 팁에 가장 어두운 밤에 대한 많은 데이터가 추가되었으며 향후 버전에서 더 많은 데이터가 추가될 예정입니다. Dack에게 감사드립니다.",
 		},
 		["ruRU"] = {
 			"Повышена стабильность плагина и улучшена обработка секретных значений. Спасибо Sargeraz00",
-			"Файл локализации обновлен, спасибо Crazyyoungs (krKR), Dlarge (deDE)",
+			"Файл локализации обновлен, спасибо Crazyyoungs (krKR), Dlarge (deDE), Valdemar (ruRU)",
 			"[Подсказка] - [Прогресс] Подсказки по прогрессу монстров для мировых заданий добавили много данных для 'Полночи', и в будущих версиях будет добавлено ещё больше данных. Спасибо Dack",
 		},
 	},
@@ -48,6 +48,8 @@ W.Changelog[416] = {
 	},
 	IMPROVEMENT = {
 		["zhCN"] = {
+			"[美化外观] 适配更多的提醒框体.",
+			"[装备观察] 修复了一个秘密值导致的出错问题, 感谢 Merathilis",
 			"[聊天文字] 同步了最新版本的 ElvUI 聊天模块, 提升了稳定性.",
 			"[聊天文字] 优化了公会成员信息的获取逻辑, 大幅减少了获取公会成员信息时的性能消耗, 在人多的公会中效果更明显.",
 			"[聊天链接] 优化了装等获取逻辑, 现在会更加准确的显示装等压缩后的真实数值. 感谢 Dack",
@@ -55,6 +57,8 @@ W.Changelog[416] = {
 			"[预组建队伍] 修复了右侧面板的快速访问按钮显示不全的问题.",
 		},
 		["zhTW"] = {
+			"[美化外觀] 適配更多的提醒框體.",
+			"[裝備觀察] 修復了一個秘密值導致的出錯問題, 感謝 Merathilis",
 			"[聊天文字] 同步了最新版本的 ElvUI 聊天模块, 提升了穩定性.",
 			"[聊天文字] 優化了公會成員信息的獲取邏輯, 大幅減少了獲取公會成員信息時的性能消耗, 在人多的公會中效果更明顯.",
 			"[聊天連結] 優化了裝等獲取邏輯, 現在會更加準確的顯示裝等壓縮後的真實數值. 感謝 Dack",
@@ -62,6 +66,8 @@ W.Changelog[416] = {
 			"[預組隊伍] 修復了右側面板的快速訪問按鈕顯示不全的問題.",
 		},
 		["enUS"] = {
+			"[Skins] Adapted to more alert frames.",
+			"[Inspect] Fixed an error caused by secret values, thanks to Merathilis",
 			"[Chat Text] Synchronized with the latest version of the ElvUI chat module, improving stability.",
 			"[Chat Text] Optimized the logic for retrieving guild member information, significantly reducing the performance overhead when retrieving guild member information, which is more noticeable in larger guilds.",
 			"[Chat Link] Optimized the item level retrieval logic, now it will more accurately display the real value after item level compression. Thanks to Dack",
@@ -69,6 +75,8 @@ W.Changelog[416] = {
 			"[LFG List] Fixed an issue where the quick access buttons on the right panel were not fully displayed.",
 		},
 		["koKR"] = {
+			"[스킨] 더 많은 경고 프레임에 적응했습니다.",
+			"[장비 관찰] 비밀 값으로 인해 발생하는 오류를 수정했습니다. Merathilis에게 감사드립니다.",
 			"[채팅 텍스트] 최신 버전의 ElvUI 채팅 모듈과 동기화하여 안정성을 향상시켰습니다.",
 			"[채팅 텍스트] 길드원 정보를 가져오는 로직이 최적화되어 길드원 정보를 가져올 때의 성능 오버헤드가 크게 줄어들었으며, 대규모 길드에서 더욱 눈에 띄게 개선되었습니다.",
 			"[채팅 링크] 아이템 레벨 검색 로직이 최적화되어 이제 아이템 레벨 압축 후 실제 값을 보다 정확하게 표시합니다. Dack에게 감사드립니다.",
@@ -76,6 +84,8 @@ W.Changelog[416] = {
 			"[LFG 목록] 오른쪽 패널의 빠른 액세스 버튼이 완전히 표시되지 않는 문제를 수정했습니다.",
 		},
 		["ruRU"] = {
+			"[Скины] Адаптировано к большему количеству фреймов оповещений.",
+			"[Осмотр] Исправлена ошибка, вызванная секретными значениями, спасибо Merathilis",
 			"[Текст в чате] Синхронизировано с последней версией модуля чата ElvUI, улучшая стабильность.",
 			"[Текст в чате] Оптимизирована логика получения информации о членах гильдии, значительно снижая нагрузку на производительность при получении информации о членах гильдии, что более заметно в больших гильдиях.",
 			"[Ссылка в чате] Оптимизирована логика получения уровня предмета, теперь он будет более точно отображать реальное значение после сжатия уровня предмета. Спасибо Dack",

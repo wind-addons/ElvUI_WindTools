24bc82851c04b85c6ecc82f79a6cf06596e00869
Zhou Fang <fang2hou@outlook.com>
2026-03-25 09:54:14 +0900

    docs: update changelog

---
+++ Core/Changelog/4.17.lua
@@ -0,0 +1,51 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[417] = {
+	RELEASE_DATE = "2026/03/25",
+	IMPORTANT = {
+		["zhCN"] = {
+			"最低支持的 ElvUI 版本提升到 15.10.",
+			"适配版本 12.0.1.66562 中的 Hotfix 改动.",
+		},
+		["zhTW"] = {
+			"最新支援的 ElvUI 版本提升到 15.10.",
+			"適配版本 12.0.1.66562 中的 Hotfix 改動.",
+		},
+		["enUS"] = {
+			"The minimum supported ElvUI version has been raised to 15.10.",
+			"Adapt to the changes of the Hotfix in version 12.0.1.66562.",
+		},
+		["koKR"] = {
+			"최소 지원 ElvUI 버전이 15.10으로 상향되었습니다.",
+			"버전 12.0.1.66562의 핫픽스 변경 사항에 적응합니다.",
+		},
+		["ruRU"] = {
+			"Минимальная поддерживаемая версия ElvUI повышена до 15.10.",
+			"Адаптация к изменениям Hotfix в версии 12.0.1.66562.",
+		},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[高级] - [配置文件] 中新增了一个选项, 允许为新角色自动拷贝角色专属配置.",
+		},
+		["zhTW"] = {
+			"[高級] - [配置文件] 中新增了一個選項, 允許為新角色自動拷貝角色專用設定檔.",
+		},
+		["enUS"] = {
+			"[Advanced] - [Profiles] has a new option that allows automatically copying private profile for new characters.",
+		},
+		["koKR"] = {
+			"[고급] - [프로필]에 새 옵션이 추가되어 새 캐릭터에 대한 개인 프로필을 자동으로 복사할 수 있습니다.",
+		},
+		["ruRU"] = {
+			"[Расширенные] - [Профили] имеет новую опцию, которая позволяет автоматически копировать личный профиль для новых персонажей.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+}

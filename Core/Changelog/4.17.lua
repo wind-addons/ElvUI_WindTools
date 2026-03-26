f4be372012535de4a4bf6571601c501026fa47de
Zhou Fang <fang2hou@outlook.com>
2026-03-26 15:19:16 +0900

    docs: update changelog

--- Core/Changelog/4.17.lua
+++ Core/Changelog/4.17.lua
@@ -27,30 +27,40 @@ W.Changelog[417] = {
 	NEW = {
 		["zhCN"] = {
 			"[高级] - [配置文件] 中新增了一个选项, 允许为新角色自动拷贝 ElvUI 角色专属配置. 感谢 aquariuslt",
-			"[怪物狩猎] 新增附近陷阱数量提示. 默认开启.",
+			"[怪物狩猎] 新增附近小地图图标监控文本. 默认开启.",
 		},
 		["zhTW"] = {
 			"[高級] - [配置文件] 中新增了一個選項, 允許為新角色自動拷貝 ElvUI 角色專用設定檔. 感謝 aquariuslt",
-			"[怪物狩獵] 新增附近陷阱數量提示. 預設開啟.",
+			"[怪物狩獵] 新增附近小地圖圖示監控文本. 預設開啟.",
 		},
 		["enUS"] = {
 			"[Advanced] - [Profiles] has a new option that allows automatically copying ElvUI private profile for new characters. Thanks to aquariuslt",
-			"[Prey Hunt] Added nearby trap count display. Enabled by default.",
+			"[Prey Hunt] Added nearby minimap vignette text. Enabled by default.",
 		},
 		["koKR"] = {
 			"[고급] - [프로필]에 새로운 옵션이 추가되어 새 캐릭터에 대해 ElvUI 개인 프로필을 자동으로 복사할 수 있습니다. aquariuslt에게 감사드립니다.",
-			"[먹이 사냥] 근처 함정 수 표시가 추가되었습니다. 기본적으로 활성화되어 있습니다.",
+			"[먹이 사냥] 근처 미니맵 비네팅 텍스트가 추가되었습니다. 기본적으로 활성화되어 있습니다.",
 		},
 		["ruRU"] = {
 			"[Расширенные] - [Профили] появилась новая опция, которая позволяет автоматически копировать личный профиль ElvUI для новых персонажей. Спасибо aquariuslt",
-			"[Охота на добычу] Добавлено отображение количества ловушек поблизости. Включено по умолчанию.",
+			"[Охота на добычу] Добавлен текст в мини-карту рядом с виньеткой. Включено по умолчанию.",
 		},
 	},
 	IMPROVEMENT = {
-		["zhCN"] = {},
-		["zhTW"] = {},
-		["enUS"] = {},
-		["koKR"] = {},
-		["ruRU"] = {},
+		["zhCN"] = {
+			"[预组建队伍] 右侧面板中的词缀显示适配了新版本.",
+		},
+		["zhTW"] = {
+			"[預組隊伍] 右側面板中的詞綴顯示適配了新版本.",
+		},
+		["enUS"] = {
+			"[LFG List] The affix display in the right panel has been adapted to the new version.",
+		},
+		["koKR"] = {
+			"[파티 찾기 목록] 오른쪽 패널의 접두사 표시가 새 버전에 맞게 조정되었습니다.",
+		},
+		["ruRU"] = {
+			"[Список LFG] Отображение аффиксов в правой панели было адаптировано к новой версии.",
+		},
 	},
 }

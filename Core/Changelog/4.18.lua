6b969132cf9a1d017a1b93bc4b13e764fe969ef5
Zhou Fang <fang2hou@outlook.com>
2026-03-29 19:24:58 +0900

    docs: update changelog

---
+++ Core/Changelog/4.18.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[418] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[其他] - [静音] 新增了 黑曜夜之翼 的静音选项. 默认关闭.",
+		},
+		["zhTW"] = {
+			"[雜項] - [靜音] 中新增了 黑曜夜翼 的靜音選項. 預設關閉.",
+		},
+		["enUS"] = {
+			"[Misc] - [Mute] Added a new mute option for Obsidian Nightwing. Disabled by default.",
+		},
+		["koKR"] = {
+			"[기타] - [음소거] 흑요석 밤날개표범 음소거 옵션이 추가되었습니다. 기본적으로 비활성화되어 있습니다.",
+		},
+		["ruRU"] = {
+			"[Разное] - [Отключение звуков] Добавлена новая опция отключения звука для Обсидианового ночного крыла. Отключено по умолчанию.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+}

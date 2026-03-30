161338a712e515537da9bb72fabaf4b26f112aa9
Zhou Fang <fang2hou@outlook.com>
2026-03-30 09:49:32 +0900

    docs: update changelog

--- Core/Changelog/4.18.lua
+++ Core/Changelog/4.18.lua
@@ -3,34 +3,69 @@ local W = unpack((select(2, ...))) ---@type WindTools
 W.Changelog[418] = {
 	RELEASE_DATE = "TBD",
 	IMPORTANT = {
-		["zhCN"] = {},
-		["zhTW"] = {},
-		["enUS"] = {},
-		["koKR"] = {},
-		["ruRU"] = {},
+		["zhCN"] = {
+			"更新库 LibOpenRaid. 感谢 Valdemar",
+			"[兼容性检查] 优化和 mMediaTag 的兼容性检查. 感谢 Merathilis",
+		},
+		["zhTW"] = {
+			"更新庫 LibOpenRaid. 感謝 Valdemar",
+			"[兼容性检查] 優化和 mMediaTag 的兼容性检查. 感謝 Merathilis",
+		},
+		["enUS"] = {
+			"Updated library LibOpenRaid. Thanks to Valdemar",
+			"[Compatibility Check] Optimized compatibility check with mMediaTag. Thanks to Merathilis",
+		},
+		["koKR"] = {
+			"라이브러리 LibOpenRaid가 업데이트되었습니다. Valdemar에게 감사드립니다.",
+			"[호환성 검사] mMediaTag와의 호환성 검사가 최적화되었습니다. Merathilis에게 감사드립니다.",
+		},
+		["ruRU"] = {
+			"Обновлена библиотека LibOpenRaid. Спасибо Valdemar",
+			"[Проверка совместимости] Оптимизирована проверка совместимости с mMediaTag. Спасибо Merathilis",
+		},
 	},
 	NEW = {
 		["zhCN"] = {
 			"[其他] - [静音] 新增了 黑曜夜之翼 的静音选项. 默认关闭.",
+			"[美化外观] 新增了 [失控警报] 美化的独立选项. 默认开启.",
 		},
 		["zhTW"] = {
 			"[雜項] - [靜音] 中新增了 黑曜夜翼 的靜音選項. 預設關閉.",
+			"[美化外觀] 新增了 [失控警報] 美化的獨立選項. 預設開啟.",
 		},
 		["enUS"] = {
 			"[Misc] - [Mute] Added a new mute option for Obsidian Nightwing. Disabled by default.",
+			"[Skins] Added new independent options for [Loss of Control] skin. Enabled by default.",
 		},
 		["koKR"] = {
 			"[기타] - [음소거] 흑요석 밤날개표범 음소거 옵션이 추가되었습니다. 기본적으로 비활성화되어 있습니다.",
+			"[스킨] [제어 상실] 스킨에 대한 새로운 독립 옵션이 추가되었습니다. 기본적으로 활성화되어 있습니다.",
 		},
 		["ruRU"] = {
 			"[Разное] - [Отключение звуков] Добавлена новая опция отключения звука для Обсидианового ночного крыла. Отключено по умолчанию.",
+			"[Скины] Добавлены новые независимые опции для скина [Потеря контроля]. Включено по умолчанию.",
 		},
 	},
 	IMPROVEMENT = {
-		["zhCN"] = {},
-		["zhTW"] = {},
-		["enUS"] = {},
-		["koKR"] = {},
-		["ruRU"] = {},
+		["zhCN"] = {
+			"[额外物品条] 更新大餐还有传送门的物品数据. 感谢 Dack",
+			"[额外物品条] 更新专业物品数据. 感谢 mcc",
+		},
+		["zhTW"] = {
+			"[額外物品條] 更新大餐還有傳送門的物品數據. 感謝 Dack",
+			"[額外物品條] 更新專業物品數據. 感謝 mcc",
+		},
+		["enUS"] = {
+			"[Extra Item Bar] Updated the item data for the Feasts and Portals. Thanks to Dack",
+			"[Extra Item Bar] Updated the item data for professions. Thanks to mcc",
+		},
+		["koKR"] = {
+			"[추가 아이템 바] 잔치와 포탈에 대한 아이템 데이터를 업데이트했습니다. Dack에게 감사드립니다.",
+			"[추가 아이템 바] 전문 기술에 대한 아이템 데이터를 업데이트했습니다. mcc에게 감사드립니다.",
+		},
+		["ruRU"] = {
+			"[Дополнительная панель предметов] Обновлены данные предметов для банкетов и порталов. Спасибо Dack",
+			"[Дополнительная панель предметов] Обновлены данные предметов для профессий. Спасибо mcc",
+		},
 	},
 }

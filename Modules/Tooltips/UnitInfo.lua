08c2d83530f0e69d6157075a2522dc156a852f1f
Cullen Daugherty <dakjaniels@outlook.com>
2026-08-24 13:30:24 -0500

    refactor: enhance unit tooltip information handling by adding secret value checks for guild, race, and gender, ensuring accurate display of non-secret attributes

--- Modules/Tooltips/UnitInfo.lua
+++ Modules/Tooltips/UnitInfo.lua
@@ -30,24 +30,34 @@ function T:SetUnitText(_, tt, unit, isPlayerUnit)
 	end
 
 	local guildName = GetGuildInfo(unit)
+	if E:IsSecretValue(guildName) then
+		guildName = nil
+	end
 	local levelLine, specLine = ET:GetLevelLine(tt, (guildName and 2) or 1)
 	local level, realLevel = UnitEffectiveLevel(unit), UnitLevel(unit)
 
 	if levelLine then
 		local diffColor = GetCreatureDifficultyColor(level)
 		local race, englishRace = UnitRace(unit)
+		if E:IsSecretValue(race) or E:IsSecretValue(englishRace) then
+			race = nil
+			englishRace = nil
+		end
 		local gender = UnitSex(unit)
+		if E:IsSecretValue(gender) then
+			gender = nil
+		end
 		local _, localizedFaction = E:GetUnitBattlefieldFaction(unit)
 		if localizedFaction and (englishRace == "Pandaren" or englishRace == "Dracthyr") then
 			race = localizedFaction .. " " .. race
 		end
 		local hexColor = E:RGBToHex(diffColor.r, diffColor.g, diffColor.b)
-		local unitGender = ET.db.gender and genderTable[gender]
+		local unitGender = ET.db.gender and gender and genderTable[gender]
 
-		if etdb.raceIcon.enable then
+		if etdb.raceIcon.enable and englishRace then
 			local raceIcon = F.GetRaceAtlasString(englishRace, gender or 1, etdb.raceIcon.height, etdb.raceIcon.width)
 			if raceIcon then
-				race = raceIcon .. " " .. race
+				race = raceIcon .. " " .. (race or "")
 			end
 		end
 
@@ -68,30 +78,34 @@ function T:SetUnitText(_, tt, unit, isPlayerUnit)
 		local specText = specLine and specLine:GetText()
 		if E:NotSecretValue(specText) and specText then
 			local localeClass, class, classID = UnitClass(unit)
-			if not localeClass or not class then
-				return
-			end
+			if
+				E:NotSecretValue(localeClass)
+				and E:NotSecretValue(class)
+				and E:NotSecretValue(classID)
+				and localeClass
+				and class
+			then
+				local nameColor = E:ClassColor(class) or RAID_CLASS_COLORS_PRIEST
 
-			local nameColor = E:ClassColor(class) or RAID_CLASS_COLORS_PRIEST
+				local specIcon
 
-			local specIcon
-
-			-- Because inspect need some extra time, we can extract the sepcialization info just from the text
-			if etdb.specIcon.enable and classID and W.SpecializationInfo[classID] then
-				for _, spec in ipairs(W.SpecializationInfo[classID]) do
-					if strfind(specText, spec.name) then
-						specIcon = spec.icon
-						break
+				-- Because inspect need some extra time, we can extract the sepcialization info just from the text
+				if etdb.specIcon.enable and classID and W.SpecializationInfo[classID] then
+					for _, spec in ipairs(W.SpecializationInfo[classID]) do
+						if strfind(specText, spec.name) then
+							specIcon = spec.icon
+							break
+						end
 					end
 				end
-			end
 
-			if specIcon then
-				local iconString = F.GetIconString(specIcon, etdb.specIcon.height, etdb.specIcon.width, true)
-				specText = iconString .. " " .. specText
-			end
+				if specIcon then
+					local iconString = F.GetIconString(specIcon, etdb.specIcon.height, etdb.specIcon.width, true)
+					specText = iconString .. " " .. specText
+				end
 
-			specLine:SetFormattedText("|c%s%s|r", nameColor.colorStr, specText)
+				specLine:SetFormattedText("|c%s%s|r", nameColor.colorStr, specText)
+			end
 		end
 
 		levelLine:SetFormattedText(levelText)

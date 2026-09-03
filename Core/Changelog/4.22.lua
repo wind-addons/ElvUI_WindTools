9dcb882d6a0b3c677dc57cfa5c470e5eb95ce2c3
Cullen Daugherty <dakjaniels@outlook.com>
2026-09-03 11:11:14 -0500

    chore: update version to 4.22 and add changelog for new Midnight weekly quest lines

---
+++ Core/Changelog/4.22.lua
@@ -0,0 +1,36 @@
+local W = unpack((select(2, ...))) ---@type WindTools
+
+W.Changelog[422] = {
+	RELEASE_DATE = "TBD",
+	IMPORTANT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+	NEW = {
+		["zhCN"] = {
+			"[事件追踪] 新增至暗之夜每周任务线: 怪物狩猎、Silvermoon Court、Zul'jarra's Forces 和 Slayer's Duellum.",
+		},
+		["zhTW"] = {
+			"[事件追蹤] 新增至暗之夜每週任務線: 獵物狩獵、Silvermoon Court、Zul'jarra's Forces 和 Slayer's Duellum.",
+		},
+		["enUS"] = {
+			"[Event Tracker] Added new Midnight weekly quest lines: Prey Hunt, Silvermoon Court, Zul'jarra's Forces, and Slayer's Duellum.",
+		},
+		["koKR"] = {
+			"[이벤트 추적기] 새로운 미드나이트 주간 퀘스트 라인을 추가했습니다: 사냥감 활동, Silvermoon Court, Zul'jarra's Forces, Slayer's Duellum.",
+		},
+		["ruRU"] = {
+			"[Трекер событий] Добавлены новые еженедельные цепочки заданий Полуночи: Охота на добычу, Silvermoon Court, Zul'jarra's Forces и Slayer's Duellum.",
+		},
+	},
+	IMPROVEMENT = {
+		["zhCN"] = {},
+		["zhTW"] = {},
+		["enUS"] = {},
+		["koKR"] = {},
+		["ruRU"] = {},
+	},
+}
